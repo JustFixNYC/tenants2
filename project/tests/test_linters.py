7a02c901f07d0917665b17064c0d4bf9937782ea
Atul Varma <varmaa@gmail.com>
2018-08-09 12:59:36 -0400

    Run the GraphQL querybuilder as part of our test suite.

--- project/tests/test_linters.py
+++ project/tests/test_linters.py
@@ -9,3 +9,17 @@ def test_mypy():
 
 def test_flake8():
     subprocess.check_call(['flake8'], cwd=BASE_DIR)
+
+
+def test_graphql_schema():
+    # TODO: We should actually try compiling the front-end
+    # after this to make sure it still works, and
+    # possibly also make sure that the generated
+    # TypeScript code has't changed, i.e. that the
+    # repo contains the latest version of the generated
+    # code (at least during CI).
+    subprocess.check_call(
+        ['npm', 'run', 'querybuilder'],
+        cwd=BASE_DIR,
+        shell=True
+    )

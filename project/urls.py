00729f8807d52f789e1cfdcca71cbfdcd3da84d9
Atul Varma <varmaa@gmail.com>
2018-08-09 18:45:48 -0400

    Use path() instead of re_path().

--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from django.conf import settings
 from graphene_django.views import GraphQLView
 
@@ -24,5 +24,5 @@ from .views import index
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
-    re_path(r'^graphql', GraphQLView.as_view(graphiql=settings.DEBUG)),
+    path('graphql', GraphQLView.as_view(graphiql=settings.DEBUG)),
 ]

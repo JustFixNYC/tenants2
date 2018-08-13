4c55cb942e98aaa1c4684a142e1b87017228ce21
Atul Varma <varmaa@gmail.com>
2018-08-13 09:11:38 -0400

    Add schema.json to repo and ensure it's always up to date.

--- project/tests/test_schema.py
+++ project/tests/test_schema.py
@@ -1,4 +1,7 @@
+import json
+from pathlib import Path
 from django.contrib.auth.models import User
+from django.core.management import call_command
 
 
 def test_hello_works_when_logged_out(graphql_client):
@@ -16,3 +19,22 @@ def test_hello_works_when_logged_in(graphql_client):
 def test_there_works(graphql_client):
     result = graphql_client.execute('query { there }')
     assert result['data']['there'] == 123
+
+
+def test_schema_json_is_up_to_date():
+    repo_schema = Path('schema.json')
+    current_schema = Path('current_schema.json')
+    call_command('graphql_schema', '--indent', '2', '--out', str(current_schema))
+
+    try:
+        current_schema_json = json.loads(current_schema.read_text())
+        repo_schema_json = json.loads(repo_schema.read_text())
+        try:
+            assert current_schema_json == repo_schema_json
+        except AssertionError:
+            raise Exception(
+                f'{repo_schema} is out of date! '
+                f'Please run "python manage.py graphql_schema --indent 2" to rebuild it.'
+            )
+    finally:
+        current_schema.unlink()

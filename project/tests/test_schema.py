d99d1d779adf538bedb92430084900eae27ae955
Atul Varma <varmaa@gmail.com>
2018-08-13 11:17:24 -0400

    Simplify test_schema_json_is_up_to_date.

--- project/tests/test_schema.py
+++ project/tests/test_schema.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 from django.contrib.auth.models import User
-from django.core.management import call_command
+from graphene_django.settings import graphene_settings
 
 
 def test_hello_works_when_logged_out(graphql_client):
@@ -23,18 +23,15 @@ def test_there_works(graphql_client):
 
 def test_schema_json_is_up_to_date():
     repo_schema = Path('schema.json')
-    current_schema = Path('current_schema.json')
-    call_command('graphql_schema', '--indent', '2', '--out', str(current_schema))
-
+    current_schema_json = json.loads(json.dumps({
+        'data': graphene_settings.SCHEMA.introspect()
+    }))
+    repo_schema_json = json.loads(repo_schema.read_text())
+    assert current_schema_json == repo_schema_json
     try:
-        current_schema_json = json.loads(current_schema.read_text())
-        repo_schema_json = json.loads(repo_schema.read_text())
-        try:
-            assert current_schema_json == repo_schema_json
-        except AssertionError:
-            raise Exception(
-                f'{repo_schema} is out of date! '
-                f'Please run "python manage.py graphql_schema --indent 2" to rebuild it.'
-            )
-    finally:
-        current_schema.unlink()
+        assert current_schema_json == repo_schema_json
+    except AssertionError:
+        raise Exception(
+            f'{repo_schema} is out of date! '
+            f'Please run "python manage.py graphql_schema --indent 2" to rebuild it.'
+        )

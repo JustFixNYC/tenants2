e37bdc3d6075b6a95f8a3a785859af23f7e353d1
Atul Varma <varmaa@gmail.com>
2018-08-15 13:38:03 -0400

    Document CompressedStaticFilesStorage.

--- project/storage.py
+++ project/storage.py
@@ -3,6 +3,17 @@ from whitenoise.compress import Compressor
 
 
 class CompressedStaticFilesStorage(StaticFilesStorage):
+    '''
+    Attempts to compress the static files in a way that is
+    compatible with Whitenoise's mechanism for serving compressed
+    files.
+
+    For more details, see:
+
+        * https://github.com/evansd/whitenoise/issues/194
+        * https://code.djangoproject.com/ticket/29677
+    '''
+
     def post_process(self, paths, **options):
         compressor = Compressor()
         for path in paths:

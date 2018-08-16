4e252f734f4c76b79030e9021089379ca232d9b0
Atul Varma <varmaa@gmail.com>
2018-08-15 20:00:43 -0400

    Cache connection to mongodb.

--- legacy_tenants/mongo.py
+++ legacy_tenants/mongo.py
@@ -2,8 +2,16 @@ import pymongo
 from django.conf import settings
 
 
+_db = None
+
+
 def get_db():
-    if not settings.LEGACY_MONGODB_URL:
-        raise AssertionError('settings.LEGACY_MONGODB_URL must be defined')
-    client = pymongo.MongoClient(settings.LEGACY_MONGODB_URL)
-    return client.get_default_database()
+    global _db
+
+    if _db is None:
+        if not settings.LEGACY_MONGODB_URL:
+            raise AssertionError('settings.LEGACY_MONGODB_URL must be defined')
+        client = pymongo.MongoClient(settings.LEGACY_MONGODB_URL)
+        _db = client.get_default_database()
+
+    return _db

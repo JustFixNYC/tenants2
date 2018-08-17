cfc044e58eab2bae103ea3fea5e0b9f689105fc7
Atul Varma <varmaa@gmail.com>
2018-08-17 07:29:21 -0400

    Disable mongodb connectivity in tests by default.

--- project/settings_pytest.py
+++ project/settings_pytest.py
@@ -3,3 +3,7 @@ from . import justfix_environment  # noqa
 justfix_environment.IS_RUNNING_TESTS = True
 
 from .settings import *  # noqa
+
+# Disable legacy auth by default, tests will need
+# to override settings if they want to enable it.
+LEGACY_MONGODB_URL = ''

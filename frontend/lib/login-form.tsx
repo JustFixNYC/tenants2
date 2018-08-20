a5aa1a5293900dec08f8d6364a96638a9eced756
Atul Varma <varmaa@gmail.com>
2018-08-20 16:01:47 -0400

    Refactor LoginForm a bit.

--- frontend/lib/login-form.tsx
+++ frontend/lib/login-form.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { LoginInput } from './queries/globalTypes';
 
 import { FormErrors, ListFieldErrors } from './forms';
-
-interface LoginFormProps {
-  onSubmit: (input: LoginInput) => void;
-  loginErrors?: FormErrors<LoginInput>;
-}
+import autobind from 'autobind-decorator';
 
 /** Get just the keys of a type whose values are strings. */
 type StringPropNames<T> = NonNullable<{ [K in keyof T]: T[K] extends string ? K : never }[keyof T]>;
@@ -59,13 +55,33 @@ const LOGIN_FIELD_METADATA: FormFieldMetadataMap<LoginInput> = {
   }
 };
 
-export class LoginForm extends React.Component<LoginFormProps, FillableFormInput<LoginInput>> {
+interface LoginFormProps {
+  onSubmit: (input: LoginInput) => void;
+  loginErrors?: FormErrors<LoginInput>;
+}
+
+type LoginFormState = FillableFormInput<LoginInput>;
+
+export class LoginForm extends React.Component<LoginFormProps, LoginFormState> {
   constructor(props: LoginFormProps) {
     super(props);
     this.state = { phoneNumber: '', password: '' };
   }
 
-  renderTextualWidget(fieldName: FillableFormFields<StringProps<LoginInput>>) {
+  handleFieldChange<K extends keyof LoginFormState>(field: K, value: LoginFormState[K]) {
+    // We really shouldn't have to provide a full state here,
+    // but TypeScript complains if we attempt to provide a
+    // partial one. :(
+    this.setState(state => ({ ...state, [field]: value }));
+  }
+
+  @autobind
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+  }
+
+  renderTextualWidget(fieldName: StringPropNames<LoginFormState>) {
     const errors = this.props.loginErrors;
     const meta = LOGIN_FIELD_METADATA[fieldName];
 
@@ -77,16 +93,7 @@ export class LoginForm extends React.Component<LoginFormProps, FillableFormInput
             type={meta.type}
             placeholder={meta.label}
             value={this.state[fieldName]}
-            onChange={(e) => {
-              const value = e.target.value;
-              // We really shouldn't have to provide a full state here,
-              // but TypeScript complains if we attempt to provide a
-              // partial one. :(
-              this.setState(state => ({
-                ...state,
-                [fieldName]: value
-              }));
-            }}
+            onChange={(e) => { this.handleFieldChange(fieldName, e.target.value) }}
           />
         </p>
         <ListFieldErrors errors={errors && errors.fieldErrors[fieldName]} />
@@ -98,10 +105,7 @@ export class LoginForm extends React.Component<LoginFormProps, FillableFormInput
     const errors = this.props.loginErrors;
 
     return (
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        this.props.onSubmit(this.state);
-      }}>
+      <form onSubmit={this.handleSubmit}>
         <ListFieldErrors errors={errors && errors.nonFieldErrors} />
         {this.renderTextualWidget('phoneNumber')}
         {this.renderTextualWidget('password')}

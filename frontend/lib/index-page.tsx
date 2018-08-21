cb99674a37ceba96cd23ab373cc8e23a94ce181a
Atul Varma <varmaa@gmail.com>
2018-08-21 18:40:28 -0400

    Add a navbar.

--- frontend/lib/index-page.tsx
+++ frontend/lib/index-page.tsx
@@ -4,10 +4,10 @@ import classnames from 'classnames';
 
 import { LoginInput } from './queries/globalTypes';
 import { LoginForm } from './login-form';
-import GraphQlClient from './graphql-client';
 import { AppServerInfo } from './app-server-info';
 import { AllSessionInfo } from './queries/AllSessionInfo';
 import { FormErrors } from './forms';
+import Navbar from './navbar';
 
 export interface IndexPageProps {
   server: AppServerInfo;
@@ -26,30 +26,6 @@ export default class IndexPage extends React.Component<IndexPageProps, IndexPage
   constructor(props: IndexPageProps) {
     super(props);
     this.state = {};
-  }
-
-  renderDebugInfo(): JSX.Element|null {
-    const { server } = this.props;
-
-    if (server.debug) {
-      return (
-        <React.Fragment>
-          <p>
-            For more details on the size of our JS bundle, see the {` `}
-            <a href={`${server.staticURL}frontend/report.html`}>webpack bundle analysis report</a>.
-          </p>
-          <p>
-            You can interactively inspect GraphQL queries with <a href="/graphiql">GraphiQL</a>.
-          </p>
-          <p>
-            Or you can visit the <a href={server.adminIndexURL}>admin</a>, though
-            you will probably want to run <code>manage.py createsuperuser</code> first.
-          </p>
-        </React.Fragment>
-      );
-    }
-
-    return null;
   }
 
   renderLoginInfo(): JSX.Element {
@@ -79,13 +55,17 @@ export default class IndexPage extends React.Component<IndexPageProps, IndexPage
 
     return (
       <section className="hero is-fullheight">
-        <div className="hero-head"></div>
+        <div className="hero-head">
+          <Navbar server={server} />
+        </div>
         <div className="hero-body">
           <div className="container content box has-background-white">
             <h1 className="title">Ahoy, { server.debug ? "developer" : "human" }! </h1>
             {this.renderLoginInfo()}
-            {this.renderDebugInfo()}
-            <p>Go to <Link to="/about">another page</Link>.</p>
+            <div className="content">
+              <br/>
+              <p>Go to <Link to="/about">another page</Link>.</p>
+            </div>
           </div>
         </div>
         <div className="hero-foot"></div>
---
+++ frontend/lib/navbar.tsx
@@ -0,0 +1,97 @@
+import React, { ReactEventHandler } from 'react';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+
+import { AppServerInfo } from './app-server-info';
+
+type Dropdown = 'developer';
+
+interface NavbarProps {
+  server: AppServerInfo;
+}
+
+interface NavbarState {
+  currentDropdown: Dropdown|null;
+}
+
+export default class Navbar extends React.Component<NavbarProps, NavbarState> {
+  constructor(props: NavbarProps) {
+    super(props);
+    this.state = { currentDropdown: null };
+  }
+
+  toggleDropdown(dropdown: Dropdown) {
+    this.setState(state => ({
+      currentDropdown: state.currentDropdown === dropdown ? null : dropdown
+    }));
+  }
+
+  render() {
+    const { state } = this;
+    const { server } = this.props;
+
+    let developerMenu = null;
+
+    if (server.debug) {
+      developerMenu = (
+        <NavbarDropdown
+          name="Developer"
+          isActive={state.currentDropdown === 'developer'}
+          onToggle={() => this.toggleDropdown('developer')}
+        >
+          <a className="navbar-item" href={`${server.staticURL}frontend/report.html`}>Webpack bundle analysis</a>
+          <a className="navbar-item" href="/graphiql">GraphiQL</a>
+          <a className="navbar-item" href={server.adminIndexURL}>Admin</a>
+        </NavbarDropdown>
+      );
+    }
+
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              <img src={`${server.staticURL}frontend/img/logo.png`} alt="Home" />
+            </Link>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              {developerMenu}
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+const KEY_ENTER = 13;
+const KEY_SPACE = 32;
+
+interface NavbarDropdownProps {
+  name: string;
+  children: any;
+  isActive: boolean;
+  onToggle: () => void;
+}
+
+function NavbarDropdown(props: NavbarDropdownProps): JSX.Element {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.which === KEY_ENTER || e.which === KEY_SPACE) {
+      e.preventDefault();
+      props.onToggle();
+    }
+  };
+
+  return (
+    <div className={classnames('navbar-item', 'has-dropdown', props.isActive ? 'is-active' : null)}>
+      <a className="navbar-link" tabIndex={0} onClick={props.onToggle} onKeyDown={handleKeyDown}>
+        {props.name}
+      </a>
+
+      <div className="navbar-dropdown">
+        {props.children}
+      </div>
+    </div>
+  );
+}

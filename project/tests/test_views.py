b358e849297cecfddc7cd9dee3b545d98ce1b2ce
Atul Varma <varmaa@gmail.com>
2018-08-22 14:44:11 -0400

    Add test to ensure SSR rendering of <title> works.

--- project/tests/test_views.py
+++ project/tests/test_views.py
@@ -9,6 +9,7 @@ def test_get_initial_session_works(graphql_client):
 def test_index_works(client):
     response = client.get('/')
     assert response.status_code == 200
+    assert b'<title data-react-helmet="true">JustFix.nyc' in response.content
     assert b"JustFix.nyc" in response.content
     assert b"data-reactroot" in response.content
 

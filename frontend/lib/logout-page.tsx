ee74b886e0a0191af5ea53c1ca13b9b6149f92d6
Atul Varma <varmaa@gmail.com>
2018-08-27 08:01:30 -0400

    Improve signout page a bit.

--- frontend/lib/logout-page.tsx
+++ frontend/lib/logout-page.tsx
@@ -2,43 +2,38 @@ import React from 'react';
 
 import Page from './page';
 import { Link } from 'react-router-dom';
+import { AppContext } from './app-context';
+import { bulmaClasses } from './bulma';
 
 export interface LogoutPageProps {
   logoutLoading: boolean;
   onLogout: () => void;
 }
 
-interface LogoutPageState {
-  hasMounted: boolean;
-}
-
-export default class LogoutPage extends React.Component<LogoutPageProps, LogoutPageState> {
-  constructor(props: LogoutPageProps) {
-    super(props);
-    this.state = { hasMounted: false };
-  }
-
-  componentDidMount() {
-    this.setState({ hasMounted: true });
-    this.props.onLogout();
-  }
-
+export default class LogoutPage extends React.Component<LogoutPageProps> {
   render() {
-    let msg = <p>Signing you out...</p>;
-
-    if (this.state.hasMounted && !this.props.logoutLoading) {
-      msg = (
-        <React.Fragment>
-          <p>You are now signed out.</p>
-          <p><Link to="/login">Sign back in</Link></p>
-        </React.Fragment>
-      );
-    }
-
     return (
-      <Page title="Log out">
-        {msg}
-      </Page>
+      <AppContext.Consumer>
+        {(appContext) => {
+          if (appContext.session.phoneNumber) {
+            return (
+              <Page title="Sign out">
+                <h1 className="title">Are you sure you want to sign out?</h1>
+                <button type="submit" className={bulmaClasses('button', 'is-primary', {
+                  'is-loading': this.props.logoutLoading
+                })} onClick={this.props.onLogout}>Yes, sign out</button>
+              </Page>
+            );
+          } else {
+            return (
+              <Page title="Signed out">
+                <h1 className="title">You are now signed out.</h1>
+                <p><Link to="/login">Sign back in</Link></p>
+              </Page>
+            );
+          }
+        }}
+      </AppContext.Consumer>
     );
   }
 }

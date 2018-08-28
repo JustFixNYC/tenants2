43450e91f6d67b2f8e64c9a86df2cd6d32f6f1da
Atul Varma <varmaa@gmail.com>
2018-08-28 14:46:19 -0400

    Factor out MyFileWatcher.

--- frontend/querybuilder/watcher.ts
+++ frontend/querybuilder/watcher.ts
@@ -5,28 +5,38 @@ import chokidar from 'chokidar';
 import chalk from 'chalk';
 import { main, ToolError, MainOptions, SCHEMA_PATH, LIB_PATH_PARTS, DOT_GRAPHQL } from './querybuilder';
 import { debouncer } from './util';
+import autobind from 'autobind-decorator';
 
+
+class MyFileWatcher {
+  private contents: Buffer;
+
+  constructor(readonly myFile: string) {
+    this.contents = fs.readFileSync(myFile);
+    chokidar.watch([myFile], {
+      ignoreInitial: true,
+      awaitWriteFinish: true
+    }).on('all', this.checkAndWarnIfChanged);
+  }
+
+  @autobind
+  checkAndWarnIfChanged() {
+    if (!this.contents.equals(fs.readFileSync(this.myFile))) {
+      console.log(chalk.yellowBright(
+        `WARNING: ${this.myFile} has changed, consider pressing Ctrl-C\n` +
+        `and re-running this command.`
+      ));
+    }
+  }
+}
 
 /** Watch GraphQL queries and schema and re-build queries when they change. */
 export function watch(options: MainOptions, debounceMs = 250) {
-  const myFile = path.basename(process.argv[1]);
-  const myFileContents = fs.readFileSync(myFile);
+  const myFileWatcher = new MyFileWatcher(path.basename(process.argv[1]));
   const paths = [
     SCHEMA_PATH,
     path.posix.join(...LIB_PATH_PARTS, `*${DOT_GRAPHQL}`)
   ];
-  const checkIfMyFileHasChanged = () => {
-    if (!myFileContents.equals(fs.readFileSync(myFile))) {
-      console.log(chalk.yellowBright(
-        `WARNING: ${myFile} has changed, consider pressing Ctrl-C\n` +
-        `and re-running this command.`
-      ));
-    }
-  };
-  chokidar.watch([myFile], {
-    ignoreInitial: true,
-    awaitWriteFinish: true
-  }).on('all', checkIfMyFileHasChanged);
   chokidar.watch(paths).on('all', debouncer(() => {
     let exitCode = 1;
 
@@ -44,6 +54,6 @@ export function watch(options: MainOptions, debounceMs = 250) {
       console.log(chalk.redBright('ERROR: Rebuilding GraphQL queries failed!'));
     }
     console.log(`Waiting for changes in ${paths.join(', ')}...`);
-    checkIfMyFileHasChanged();
+    myFileWatcher.checkAndWarnIfChanged();
   }, debounceMs));
 }

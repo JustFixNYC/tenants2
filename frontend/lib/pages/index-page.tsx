fa424f9b72b0fa22d6ea9d0bfbfbab054e061074
Atul Varma <varmaa@gmail.com>
2018-08-30 09:02:18 -0400

    Add real copy to index page.

--- frontend/lib/pages/index-page.tsx
+++ frontend/lib/pages/index-page.tsx
@@ -13,13 +13,36 @@ export default class IndexPage extends React.Component<IndexPageProps> {
   renderLoggedOut() {
     return (
       <Page title="Technology for Housing Justice">
-        <h1 className="title">Level the playing field between you and your landlord.</h1>
-        <h2 className="subtitle">
-          Learn the steps to take action to fight for your right to a safe and healthy home!
-        </h2>
+        <div className="content">
+        <h1>Need repairs in your apartment? JustFix.nyc will mail a letter to your landlord!</h1>
+        <p>
+          Sending a letter of complaint to your landlord will help document all the issues that need to be fixed.
+        </p>
         <Link className="button is-medium is-fullwidth is-primary" to={Routes.onboarding.latestStep}>Get started</Link>
         <br/>
         <p>Already have an account? <Link to={Routes.login}>Sign in!</Link></p>
+
+        <h2>Why mail a Certified Letter of Complaint?</h2>
+
+        <p>
+          Under the Warranty of Habitability, your landlord is responsible for keeping your apartment and the building safe and livable at all times.
+        </p>
+
+        <p>
+          In order to have an official record of the requests you've made to your landlord, you should send to mail a Letter of Complaint to your landlord via Certified Mail. We've drafted this letter for you, and we can also mail this letter on your behalf! All you need to do are enter the issues you are experiencing.
+        </p>
+
+        <Link className="button is-medium is-fullwidth is-primary" to={Routes.onboarding.latestStep}>Get started</Link>
+
+        <h2>About JustFix.nyc</h2>
+
+        <p>
+          JustFix.nyc is a nonprofit that builds tools for tenants and organizers fighting displacement. We encourage tenants to take affirmative actions to fight for safe and healthy homes. Want to know more? <a href="https://www.justfix.nyc">Visit our website.</a>
+        </p>
+
+        <p><strong>Disclaimer:</strong> The information contained in JustFix.nyc does not constitute legal advice and must not be used as a substitute for the advice of a lawyer qualified to give advice on legal issues pertaining to housing. We can help direct you to free and/or low-cost legal services as necessary.</p>
+
+        </div>
       </Page>
     );
   }

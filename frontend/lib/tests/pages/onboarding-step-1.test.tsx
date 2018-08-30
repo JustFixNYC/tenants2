7c874e0d690b3064bc260a908160eff1dc9ba517
Atul Varma <varmaa@gmail.com>
2018-08-30 17:55:52 -0400

    Add a terrible test.

--- frontend/lib/tests/pages/onboarding-step-1.test.tsx
+++ frontend/lib/tests/pages/onboarding-step-1.test.tsx
@@ -4,6 +4,8 @@ import * as rt from 'react-testing-library'
 
 import OnboardingStep1, { areAddressesTheSame } from '../../pages/onboarding-step-1';
 import { MemoryRouter } from 'react-router';
+import { createTestGraphQlClient, FakeSessionInfo } from '../util';
+import { AllSessionInfo } from '../../queries/AllSessionInfo';
 
 
 describe('onboarding step 1 page', () => {
@@ -23,6 +25,41 @@ describe('onboarding step 1 page', () => {
     })).toBeTruthy();
     const closeBtn = thing.getByText("Got it!");
     rt.fireEvent.click(closeBtn);
+  });
+
+  it('opens confirmation modal if address returned from server is different', async () => {
+    const onSuccess = jest.fn();
+    const resolvers: Function[] = [];
+    const fetch = () => new Promise((resolve, reject) => resolvers.push(resolve));
+    const thing = rt.render(
+      <MemoryRouter>
+        <OnboardingStep1 fetch={fetch} onSuccess={onSuccess} />
+      </MemoryRouter>
+    );
+
+    const setField = (matcher: RegExp, value: string) => {
+      const input = thing.getByLabelText(matcher, { selector: 'input, select' }) as HTMLInputElement;
+      input.value = value;
+    };
+    setField(/full name/i, 'boop jones');
+    setField(/address/i, '150 court');
+    setField(/borough/i, 'BROOKLYN');
+    setField(/apartment number/i, '2');
+    rt.fireEvent.click(thing.getByText('Next'));
+    expect(resolvers).toHaveLength(1);
+    let session: AllSessionInfo = {
+      ...FakeSessionInfo,
+      onboardingStep1: {
+        name: 'boop jones',
+        address: '150 COURT STREET',
+        borough: 'BROOKLYN',
+        aptNumber: '2'
+      }
+    };
+    resolvers[0]({ onboardingStep1: { errors: [], session } });
+    await rt.waitForElement(() => thing.getByLabelText(/Is this your address/i, {
+      selector: 'div[role="dialog"]'
+    }));
   });
 });
 

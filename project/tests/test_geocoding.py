bc61630c5979e4a06b73b62e83cf120a7e82e638
Atul Varma <varmaa@gmail.com>
2018-08-30 12:10:47 -0400

    Use requests_mock fixture.

--- project/tests/test_geocoding.py
+++ project/tests/test_geocoding.py
@@ -1,7 +1,6 @@
 import json
 from pathlib import Path
 import requests.exceptions
-import requests_mock
 
 from project import geocoding
 
@@ -11,26 +10,22 @@ MY_DIR = Path(__file__).parent.resolve()
 EXAMPLE_SEARCH = json.loads((MY_DIR / 'test_geocoding_example_search.json').read_text())
 
 
-def test_search_works():
-    with requests_mock.Mocker() as m:
-        m.get(geocoding.SEARCH_URL, json=EXAMPLE_SEARCH)
-        results = geocoding.search("150 court")
-        assert results[0].properties.label == "150 COURT STREET, Brooklyn, New York, NY, USA"
+def test_search_works(requests_mock):
+    requests_mock.get(geocoding.SEARCH_URL, json=EXAMPLE_SEARCH)
+    results = geocoding.search("150 court")
+    assert results[0].properties.label == "150 COURT STREET, Brooklyn, New York, NY, USA"
 
 
-def assert_response_is_none(**kwargs):
-    with requests_mock.Mocker() as m:
-        m.get(geocoding.SEARCH_URL, **kwargs)
-        assert geocoding.search("150 court") is None
+def test_search_returns_none_on_500(requests_mock):
+    requests_mock.get(geocoding.SEARCH_URL, status_code=500)
+    assert geocoding.search("150 court") is None
 
 
-def test_search_returns_none_on_500():
-    assert_response_is_none(status_code=500)
+def test_search_returns_none_on_request_exception(requests_mock):
+    requests_mock.get(geocoding.SEARCH_URL, exc=requests.exceptions.Timeout)
+    assert geocoding.search("150 court") is None
 
 
-def test_search_returns_none_on_request_exception():
-    assert_response_is_none(exc=requests.exceptions.Timeout)
-
-
-def test_search_returns_none_on_bad_result():
-    assert_response_is_none(json={'blarg': False})
+def test_search_returns_none_on_bad_result(requests_mock):
+    requests_mock.get(geocoding.SEARCH_URL, json={'blarg': False})
+    assert geocoding.search("150 court") is None

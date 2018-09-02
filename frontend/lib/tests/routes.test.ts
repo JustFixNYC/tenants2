3595062b1caa9e9479359014b3d6193051c03107
Atul Varma <varmaa@gmail.com>
2018-09-02 15:11:34 -0400

    Add RouteMap tests.

--- frontend/lib/tests/routes.test.ts
+++ frontend/lib/tests/routes.test.ts
@@ -1,6 +1,14 @@
-import { isModalRoute } from "../routes";
+import { isModalRoute, RouteMap } from "../routes";
 
 test('isModalRoute() works', () => {
   expect(isModalRoute('/blah')).toBe(false);
   expect(isModalRoute('/blah', '/oof/flarg-modal')).toBe(true);
+});
+
+test('RouteMap works', () => {
+  const map = new RouteMap({ blah: '/blah', thing: { a: '/a', b: '/b' } });
+  expect(map.exists('/blah')).toBe(true);
+  expect(map.exists('/a')).toBe(true);
+  expect(map.exists('/b')).toBe(true);
+  expect(map.exists('/c')).toBe(false);
 });

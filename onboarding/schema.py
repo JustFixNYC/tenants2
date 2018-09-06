843ca871d7206383b14d134267fb93a0f77ef9dc
Atul Varma <varmaa@gmail.com>
2018-09-05 20:03:50 -0400

    Slightly improve DRY.

--- onboarding/schema.py
+++ onboarding/schema.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import Optional, Dict, Any
 from django.contrib.auth import login, authenticate
+from django.http import HttpRequest
 import graphene
 from graphql import ResolveInfo
 from graphene_django.forms.mutation import fields_for_form
@@ -10,11 +11,19 @@ from onboarding import forms
 from onboarding.models import OnboardingInfo
 
 
-ONBOARDING_STEP_1_SESSION_KEY = 'onboarding_step_1'
+# The onboarding steps we store in the request session.
+SESSION_STEPS = [1, 2, 3]
 
-ONBOARDING_STEP_2_SESSION_KEY = 'onboarding_step_2'
 
-ONBOARDING_STEP_3_SESSION_KEY = 'onboarding_step_3'
+def session_key_for_step(step: int) -> str:
+    '''
+    We store the results of the user's onboarding steps in
+    the session. This function returns the key we use to
+    store the data for a particular step in.
+    '''
+
+    assert step in SESSION_STEPS
+    return f'onboarding_step_{step}'
 
 
 def get_session_info():
@@ -74,21 +83,21 @@ class OnboardingStep1(StoreToSessionForm):
     class Meta:
         form_class = forms.OnboardingStep1Form
 
-    SESSION_KEY = ONBOARDING_STEP_1_SESSION_KEY
+    SESSION_KEY = session_key_for_step(1)
 
 
 class OnboardingStep2(StoreToSessionForm):
     class Meta:
         form_class = forms.OnboardingStep2Form
 
-    SESSION_KEY = ONBOARDING_STEP_2_SESSION_KEY
+    SESSION_KEY = session_key_for_step(2)
 
 
 class OnboardingStep3(StoreToSessionForm):
     class Meta:
         form_class = forms.OnboardingStep3Form
 
-    SESSION_KEY = ONBOARDING_STEP_3_SESSION_KEY
+    SESSION_KEY = session_key_for_step(3)
 
 
 def pick_model_fields(model, **kwargs):
@@ -116,25 +125,29 @@ class OnboardingStep4(DjangoFormMutation):
     session = graphene.Field('project.schema.SessionInfo')
 
     @classmethod
+    def __extract_all_step_session_data(cls, request: HttpRequest) -> Dict[str, Any]:
+        result: Dict[str, Any] = {}
+        for step in SESSION_STEPS:
+            key = session_key_for_step(step)
+            result.update(request.session[key])
+            del request.session[key]
+        return result
+
+    @classmethod
     def perform_mutate(cls, form: forms.OnboardingStep4Form, info: ResolveInfo):
         request = info.context
         phone_number = form.cleaned_data['phone_number']
         password = form.cleaned_data['password']
-        step_1 = request.session[ONBOARDING_STEP_1_SESSION_KEY]
-        step_2 = request.session[ONBOARDING_STEP_2_SESSION_KEY]
-        step_3 = request.session[ONBOARDING_STEP_3_SESSION_KEY]
-        del request.session[ONBOARDING_STEP_1_SESSION_KEY]
-        del request.session[ONBOARDING_STEP_2_SESSION_KEY]
-        del request.session[ONBOARDING_STEP_3_SESSION_KEY]
+        prev_steps = cls.__extract_all_step_session_data(request)
         user = JustfixUser.objects.create_user(
             username=phone_number,
-            full_name=step_1['name'],
+            full_name=prev_steps['name'],
             phone_number=phone_number,
             password=password,
         )
 
         oi = OnboardingInfo(user=user, **pick_model_fields(
-            OnboardingInfo, **step_1, **step_2, **step_3, **form.cleaned_data))
+            OnboardingInfo, **prev_steps, **form.cleaned_data))
         oi.full_clean()
         oi.save()
 
@@ -171,10 +184,10 @@ class OnboardingSessionInfo(object):
         return field_class(**obinfo) if obinfo else None
 
     def resolve_onboarding_step_1(self, info: ResolveInfo) -> Optional[OnboardingStep1Info]:
-        return self.__get(info, ONBOARDING_STEP_1_SESSION_KEY, OnboardingStep1Info)
+        return self.__get(info, session_key_for_step(1), OnboardingStep1Info)
 
     def resolve_onboarding_step_2(self, info: ResolveInfo) -> Optional[OnboardingStep2Info]:
-        return self.__get(info, ONBOARDING_STEP_2_SESSION_KEY, OnboardingStep2Info)
+        return self.__get(info, session_key_for_step(2), OnboardingStep2Info)
 
     def resolve_onboarding_step_3(self, info: ResolveInfo) -> Optional[OnboardingStep3Info]:
-        return self.__get(info, ONBOARDING_STEP_3_SESSION_KEY, OnboardingStep3Info)
+        return self.__get(info, session_key_for_step(3), OnboardingStep3Info)

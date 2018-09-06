726cc46bd6ca4160ae96728727eeb7010833ab43
Atul Varma <varmaa@gmail.com>
2018-09-06 09:50:07 -0400

    encourage jest to not be an asshole

--- frontend/lib/tests/setup.ts
+++ frontend/lib/tests/setup.ts
@@ -3,6 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import { defaultContext } from '../app-context';
 import { FakeAppContext } from './util';
+import chalk from 'chalk';
 
 configure({ adapter: new Adapter() });
 
@@ -18,3 +19,16 @@ if (typeof(window) !== 'undefined') {
   // it's called. So we'll just stub it out.
   window.scroll = jest.fn();
 }
+
+const originalLog = console.log;
+
+/** 
+ * Apparently jest sometimes doesn't log stuff to the console, which is
+ * AWESOME. So here we'll force some newlines and things to hopefully
+ * increase the chance that Jest will actually output something to the
+ * console in time for the user to see it.
+ */
+console.log = function() {
+  originalLog.apply(this, arguments);
+  originalLog.call(this, chalk.green('\n ^^^ SOMETHING GOT LOGGED! ^^^\n'));
+};

3e67932dd4e356bc99f40a3f84cd43ef28ce8301
Atul Varma <varmaa@gmail.com>
2018-09-09 12:20:22 -0400

    Remove unneeded AppContext consumer.

--- frontend/lib/pages/example-form-page.tsx
+++ frontend/lib/pages/example-form-page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Page from "../page";
 import { LegacyFormSubmitter } from '../forms';
-import { AppContext } from '../app-context';
 import { ExampleMutation } from '../queries/ExampleMutation';
 import { TextualFormField } from '../form-fields';
 import { bulmaClasses } from '../bulma';
@@ -10,29 +9,25 @@ import { bulmaClasses } from '../bulma';
 /* istanbul ignore next: this is tested by integration tests. */
 export default function ExampleFormPage(): JSX.Element {
   return (
-    <AppContext.Consumer>
-    {(appCtx) => (
-      <Page title="Example form page">
-        This is an example form page.
-        <LegacyFormSubmitter
-          mutation={ExampleMutation}
-          initialState={{ exampleField: '' }}
-        >
-          {(ctx) => (
-            <React.Fragment>
-              <TextualFormField label="Example field" {...ctx.fieldPropsFor('exampleField')} />
-              <div className="field">
-                <div className="control">
-                  <button type="submit" className={bulmaClasses('button', 'is-primary', {
-                    'is-loading': ctx.isLoading
-                  })}>Submit</button>
-                </div>
+    <Page title="Example form page">
+      This is an example form page.
+      <LegacyFormSubmitter
+        mutation={ExampleMutation}
+        initialState={{ exampleField: '' }}
+      >
+        {(ctx) => (
+          <React.Fragment>
+            <TextualFormField label="Example field" {...ctx.fieldPropsFor('exampleField')} />
+            <div className="field">
+              <div className="control">
+                <button type="submit" className={bulmaClasses('button', 'is-primary', {
+                  'is-loading': ctx.isLoading
+                })}>Submit</button>
               </div>
-            </React.Fragment>
-          )}
-        </LegacyFormSubmitter>
-      </Page>
-    )}
-    </AppContext.Consumer>
+            </div>
+          </React.Fragment>
+        )}
+      </LegacyFormSubmitter>
+    </Page>
   );
 }

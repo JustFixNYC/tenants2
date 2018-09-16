f0094d5ba2469823f4d5bcb8eb6bb0abb2256bc9
Atul Varma <varmaa@gmail.com>
2018-09-16 06:53:36 -0400

    Fix typo

--- frontend/lib/tests/pages/onboarding-step-4.test.tsx
+++ frontend/lib/tests/pages/onboarding-step-4.test.tsx
@@ -4,7 +4,7 @@ import OnboardingStep4 from '../../pages/onboarding-step-4';
 import { AppTesterPal } from '../app-tester-pal';
 
 
-describe('onboarding step 2 page', () => {
+describe('onboarding step 4 page', () => {
   afterEach(AppTesterPal.cleanup);
 
   it('displays welcome modal on successful signup', async () => {

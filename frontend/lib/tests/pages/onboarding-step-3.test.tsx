5676f027a0c5a7f40cd2bbc322a315e2db71fd0b
Atul Varma <varmaa@gmail.com>
2018-09-16 06:58:47 -0400

    Make onboarding step 3 use AppTesterPal.

--- frontend/lib/tests/pages/onboarding-step-3.test.tsx
+++ frontend/lib/tests/pages/onboarding-step-3.test.tsx
@@ -1,45 +1,28 @@
 import React from 'react';
 
 import OnboardingStep3, { LEASE_CHOICES, LEASE_MODALS } from '../../pages/onboarding-step-3';
-import { MemoryRouter } from 'react-router';
-import ReactTestingLibraryPal from '../rtl-pal';
 import { validateDjangoChoices } from '../../common-data';
-import { createTestGraphQlClient, FakeSessionInfo, FakeAppContext } from '../util';
-import { AllSessionInfo } from '../../queries/AllSessionInfo';
-import { AppContextType, AppContext } from '../../app-context';
+import { AppTesterPal } from '../app-tester-pal';
+import { OnboardingStep3Mutation_output } from '../../queries/OnboardingStep3Mutation';
 
-
-function createOnboarding(props: Partial<AppContextType> = {}): JSX.Element {
-  const appCtx: AppContextType = {
-    ...FakeAppContext,
-    ...props
-  };
-  return (
-    <MemoryRouter>
-      <AppContext.Provider value={appCtx}>
-        <OnboardingStep3 />
-      </AppContext.Provider>
-    </MemoryRouter>
-  );
-}
 
 describe('onboarding step 3 page', () => {
-  afterEach(ReactTestingLibraryPal.cleanup);
+  afterEach(AppTesterPal.cleanup);
 
   it('displays modal when user chooses "rent stabilized"', async () => {
-    const { client } = createTestGraphQlClient();
-    const pal = ReactTestingLibraryPal.render(createOnboarding({ fetch: client.fetch }));
+    const pal = new AppTesterPal(<OnboardingStep3 />);
 
     pal.click(/rent stabilized/i, 'label');
     pal.clickButtonOrLink('Next');
-    let session: AllSessionInfo = {
-      ...FakeSessionInfo,
-      onboardingStep3: {
-        leaseType: 'RENT_STABILIZED',
-        receivesPublicAssistance: false
+    pal.respondWithFormOutput<OnboardingStep3Mutation_output>({
+      errors: [],
+      session: {
+        onboardingStep3: {
+          leaseType: 'RENT_STABILIZED',
+          receivesPublicAssistance: false
+        }  
       }
-    };
-    client.getRequestQueue()[0].resolve({ output: { errors: [], session } });
+    });
     await pal.rt.waitForElement(() => pal.getDialogWithLabel(/Great news/i));
   });
 });

d0ad5a668d7246c33474aa638eac3c0a17d6cb98
Atul Varma <varmaa@gmail.com>
2018-09-17 19:10:32 -0400

    Add tests.

--- frontend/lib/pages/login-page.tsx
+++ frontend/lib/pages/login-page.tsx
@@ -58,7 +58,20 @@ export class LoginForm extends React.Component<LoginFormProps> {
   }
 }
 
-function getQuerystringVar(routeInfo: RouteComponentProps<any>, name: string): string|undefined {
+/**
+ * This is intentionally structured as a subset of react-router's
+ * router context, to make it easy to interoperate with.
+ */
+type LocationSearchInfo = {
+  location: {
+    search: string
+  }
+};
+
+/**
+ * Return the value of the last-defined key in the given querystring.
+ */
+export function getQuerystringVar(routeInfo: LocationSearchInfo, name: string): string|undefined {
   let val = querystring.parse(routeInfo.location.search.slice(1))[name];
 
   if (Array.isArray(val)) {
@@ -68,15 +81,31 @@ function getQuerystringVar(routeInfo: RouteComponentProps<any>, name: string): s
   return val;
 }
 
-export interface LoginPageProps {}
+/**
+ * This is intentionally structured as a subset of our app context,
+ * to make it easy to interoperate with.
+ */
+type HttpPostInfo = {
+  legacyFormSubmission?: {
+    POST: Partial<{ [key: string]: string }>;
+  }
+};
+
+/**
+ * If this is a POST, returns the last value of the given key, or undefined if
+ * not present.
+ * 
+ * Otherwise, returns the last-defined key in the given querystring.
+ */
+export function getPostOrQuerystringVar(info: LocationSearchInfo & HttpPostInfo, name: string): string|undefined {
+  if (info.legacyFormSubmission) {
+    return info.legacyFormSubmission.POST[name];
+  }
+  return getQuerystringVar(info, name);
+}
 
 const LoginPage = withAppContext((props: RouteComponentProps<any> & AppContextType): JSX.Element => {
-  let next = Routes.home;
-  if (props.legacyFormSubmission) {
-    next = props.legacyFormSubmission.POST[NEXT] || next;
-  } else {
-    next = getQuerystringVar(props, NEXT) || next;
-  }
+  let next = getPostOrQuerystringVar(props, NEXT) || Routes.home;
   // TODO: Validate the next URL.
 
   return (

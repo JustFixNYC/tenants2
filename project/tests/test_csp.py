22dee6db58457e20ada0cf0a5ab807ad12d070d3
Atul Varma <varmaa@gmail.com>
2018-09-20 06:07:03 -0400

    Add tests.

--- project/tests/test_csp.py
+++ project/tests/test_csp.py
@@ -1,6 +1,10 @@
 import pytest
 from django.http import HttpResponse
 from django.urls import path
+from csp.decorators import csp_update
+
+from project.middleware import CSPHashingMiddleware
+
 
 EXPECTED_CSP = "default-src 'self'"
 
@@ -15,9 +19,18 @@ def view_with_inline_script(request):
     return HttpResponse(f'<script>{script}</script>')
 
 
+@csp_update(SCRIPT_SRC='https://blarg')
+def view_with_everything(request):
+    script = 'console.log("hello")'
+    request.allow_inline_script(script)
+    request.csp_update(SCRIPT_SRC='https://boop')
+    return HttpResponse(f'<script>{script}</script>')
+
+
 urlpatterns = [
     path('basic', basic_view),
     path('with-inline-script', view_with_inline_script),
+    path('with-everything', view_with_everything),
 ]
 
 
@@ -42,6 +55,18 @@ def test_hash_is_added_for_inline_scripts(client):
     assert EXPECTED_CSP in csp
 
 
+@pytest.mark.urls(__name__)
+def test_all_csp_directives_are_merged(client, settings):
+    settings.CSP_SCRIPT_SRC = ['https://bam']
+    response = client.get('/with-everything')
+    assert 200 == response.status_code
+    assert (
+        "script-src https://bam 'self' https://boop https://blarg "
+        "'unsafe-inline' "
+        "'sha256-Ql3n7tC/2D6wSTlQY8RcOKXhq02zfdaSDviOhpvbYWw='"
+    ) in response['Content-Security-Policy']
+
+
 def test_csp_works_on_static_assets(client, staticfiles):
     assert (staticfiles / 'admin' / 'css' / 'base.css').exists()
     response = client.get('/static/admin/css/base.css')
@@ -50,3 +75,13 @@ def test_csp_works_on_static_assets(client, staticfiles):
     csp = response['Content-Security-Policy']
     assert 'unsafe-inline' not in csp
     assert EXPECTED_CSP in csp
+
+
+def test_merge_csp_updates_works():
+    m = CSPHashingMiddleware()
+    assert m._merge_csp_updates([
+        {'script-src': 'https://boop'},
+        {'script-src': ['https://bap', 'https://goo']},
+    ]) == {
+        'script-src': ["'self'", 'https://boop', 'https://bap', 'https://goo']
+    }

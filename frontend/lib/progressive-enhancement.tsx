7042775a9ed489cae70c76d521d04864843d53a9
Atul Varma <varmaa@gmail.com>
2018-09-22 07:14:51 -0400

    Make ProgressiveEnhancement an error boundary.

--- frontend/lib/progressive-enhancement.tsx
+++ frontend/lib/progressive-enhancement.tsx
@@ -8,20 +8,36 @@ interface ProgressiveEnhancementProps {
 
 interface ProgressiveEnhancementState {
   isMounted: boolean;
+  caughtError: boolean;
 }
 
 export class ProgressiveEnhancement extends React.Component<ProgressiveEnhancementProps, ProgressiveEnhancementState> {
   constructor(props: ProgressiveEnhancementProps) {
     super(props);
-    this.state = { isMounted: false };
+    this.state = {
+      isMounted: false,
+      caughtError: false
+    };
+  }
+
+  componentDidCatch(error: Error) {
+    if (this.isEnhanced()) {
+      this.setState({ caughtError: true });
+    } else {
+      throw error;
+    }
   }
 
   componentDidMount() {
     this.setState({ isMounted: true });
   }
 
+  isEnhanced() {
+    return this.state.isMounted && !this.props.disabled && !this.state.caughtError;
+  }
+
   render() {
-    if (this.state.isMounted && !this.props.disabled) {
+    if (this.isEnhanced()) {
       return this.props.renderEnhanced();
     } else {
       return this.props.renderBaseline();

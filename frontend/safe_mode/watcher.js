2cc3ed9176f15e5b150ed55299c5e9441d60f107
Atul Varma <varmaa@gmail.com>
2018-09-23 12:19:20 -0400

    improve safe mode watcher.

--- frontend/safe_mode/watcher.js
+++ frontend/safe_mode/watcher.js
@@ -2,17 +2,22 @@
 
 const { execSync } = require('child_process');
 const chokidar = require('chokidar');
+const chalk = require('chalk').default;
 
 const FILENAME = 'safe-mode.js';
 
 function uglify() {
   console.log(`Uglifying ${FILENAME}.`);
-  execSync('npm run safe_mode_snippet', { stdio: 'inherit' });
+  try {
+    execSync('npm run safe_mode_snippet', { stdio: 'inherit' });
+  } catch (e) {
+    console.log(chalk.redBright(`Uglification failed!`));
+  }
 }
 
 if (!module.parent) {
   console.log(`Waiting for changes to ${FILENAME}...`);
-  chokidar.watch(`${__dirname}/${FILENAME}`)
+  chokidar.watch(`${__dirname}/${FILENAME}`, { awaitWriteFinish: true })
     .on('ready', uglify)
     .on('change', uglify);
 }

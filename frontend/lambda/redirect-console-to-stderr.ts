52af2e9c6e4a0c7c76a985bd53ee14d357727c8b
Atul Varma <varmaa@gmail.com>
2018-09-24 08:50:14 -0400

    Fix production type error, oof.

--- frontend/lambda/redirect-console-to-stderr.ts
+++ frontend/lambda/redirect-console-to-stderr.ts
@@ -3,7 +3,7 @@ import { Console } from 'console';
 /* istanbul ignore next: this is tested by integration tests. */
 // We don't want to redirect the console to stderr when running
 // tests because they will mess up logging output.
-if (typeof(describe) !== 'function') {
+if (typeof((global as any).describe) !== 'function') {
   Object.defineProperty(global, 'console', {
     value: new Console(process.stderr)
   });

8099e58e89c0b4592683868a9dd86a06844d355d
Atul Varma <varmaa@gmail.com>
2018-09-27 08:40:03 -0400

    Improve issue autocomplete test.

--- frontend/lib/tests/pages/issues.test.tsx
+++ frontend/lib/tests/pages/issues.test.tsx
@@ -87,10 +87,20 @@ test('groupByTwo() works', () => {
 test('IssueAutocomplete works', () => {
   const mockChange = jest.fn();
   const pal = new ReactTestingLibraryPal(
-    <IssueAutocomplete
-      inputValue="zzzzz"
-      onInputValueChange={mockChange}
-    />
+    <IssueAutocomplete inputValue="" onInputValueChange={mockChange} />
   );
-  pal.rr.getByLabelText(/search/i);
+
+  pal.fillFormFields([[/search/i, "mice"]]);
+  expect(mockChange.mock.calls).toHaveLength(1);
+  pal.rr.rerender(
+    <IssueAutocomplete inputValue="mice" onInputValueChange={mockChange} />
+  );
+  mockChange.mockClear();
+  pal.clickListItem(/mice/i);
+  expect(mockChange.mock.calls[1][0]).toBe('Entire home and hallways - Mice');
+
+  pal.rr.rerender(
+    <IssueAutocomplete inputValue="zzzzzzz" onInputValueChange={mockChange} />
+  );
+  expect(pal.rr.container.querySelector('li')).toBeNull();
 });

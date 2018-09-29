a52244d7f7c9a01a28401b8e0c188c93cff0ef19
Atul Varma <varmaa@gmail.com>
2018-09-28 21:00:57 -0400

    Add GitInfo class.

--- project/tests/test_git.py
+++ project/tests/test_git.py
@@ -2,9 +2,18 @@ from project.justfix_environment import BASE_DIR
 from project.util import git
 
 
-def test():
+def test_is_dirty_returns_bool():
+    assert isinstance(git.is_dirty(BASE_DIR), bool)
+
+
+def test_has_extra_files_returns_bool():
+    assert isinstance(git.has_extra_files(BASE_DIR), bool)
+
+
+def test_get_git_info_works():
     assert git.have_git() is True
     assert git.is_git_repo(BASE_DIR) is True
-    assert len(git.git_revision(BASE_DIR)) == 40
-    assert isinstance(git.is_dirty(BASE_DIR), bool)
-    assert isinstance(git.has_extra_files(BASE_DIR), bool)
+
+    gitinfo = git.get_git_info(BASE_DIR)
+    assert len(gitinfo.GIT_REVISION) == 40
+    assert isinstance(gitinfo.IS_GIT_REPO_PRISTINE, bool)
--- project/util/git.py
+++ project/util/git.py
@@ -1,16 +1,19 @@
 """Utilities for verifying git integrity."""
 
+from typing import Dict
 from pathlib import Path
 import subprocess
+
+from .typed_environ import BaseEnvironment
 
 # The following code is largely copy-pasted from mypy's source code:
 #
 # https://github.com/python/mypy/blob/master/mypy/git.py
 
 
-def is_git_repo(dirpath: Path) -> bool:
+def is_git_repo(dir: Path) -> bool:
     """Is the given directory version-controlled with git?"""
-    return (dirpath / '.git').exists()
+    return (dir / '.git').exists()
 
 
 def have_git() -> bool:
@@ -24,18 +27,37 @@ def have_git() -> bool:
         return False
 
 
-def git_revision(dir: str) -> bytes:
+def git_revision(dir: Path) -> bytes:
     """Get the SHA-1 of the HEAD of a git repository."""
     return subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=dir).strip()
 
 
-def is_dirty(dir: str) -> bool:
+def is_dirty(dir: Path) -> bool:
     """Check whether a git repository has uncommitted changes."""
     output = subprocess.check_output(["git", "status", "-uno", "--porcelain"], cwd=dir)
     return output.strip() != b""
 
 
-def has_extra_files(dir: str) -> bool:
+def has_extra_files(dir: Path) -> bool:
     """Check whether a git repository has untracked files."""
     output = subprocess.check_output(["git", "clean", "--dry-run", "-d"], cwd=dir)
     return output.strip() != b""
+
+
+class GitInfo(BaseEnvironment):
+    GIT_REVISION: str
+
+    IS_GIT_REPO_PRISTINE: bool
+
+    @staticmethod
+    def create_env_dict(dir: Path) -> Dict[str, str]:
+        return dict(
+            GIT_REVISION=git_revision(dir).decode('ascii'),
+            IS_GIT_REPO_PRISTINE=str(is_dirty(dir) or has_extra_files(dir)),
+        )
+
+
+def get_git_info(dir: Path) -> GitInfo:
+    if have_git() and is_git_repo(dir):
+        return GitInfo(env=GitInfo.create_env_dict(dir))
+    return GitInfo()

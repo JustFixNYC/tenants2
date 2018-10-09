7c2ba2b72d2490fed0d78b5931b8818f2c1b5ab0
Atul Varma <varmaa@gmail.com>
2018-10-09 08:31:14 -0400

    Add a test for example loading page.

---
+++ frontend/lib/tests/pages/example-loading-page.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { AppTesterPal } from "../app-tester-pal";
+import ExampleLoadingPage from "../../pages/example-loading-page";
+
+test("example loading page works", () => {
+  const pal = new AppTesterPal(
+    <ExampleLoadingPage />
+  );
+  pal.clickRadioOrCheckbox('mount');
+});

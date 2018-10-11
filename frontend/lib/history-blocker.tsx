fcc1c7bba1dd4a38c9ea346547b22ee2d369aac9
Atul Varma <varmaa@gmail.com>
2018-10-11 08:32:29 -0400

    Add some docs for history-blocker.tsx.

--- frontend/lib/history-blocker.tsx
+++ frontend/lib/history-blocker.tsx
@@ -12,16 +12,36 @@ const DEFAULT_PROMPT = (
   "Changes you made may not be saved."
 );
 
+/**
+ * Our custom function for confirming whether the user wants
+ * to navigate away from the page. For more details, see:
+ * 
+ * https://github.com/ReactTraining/history#customizing-the-confirm-dialog
+ */
 export function getNavigationConfirmation(message: string, callback: (result: boolean) => void) {
+  // This is almost identical to the default implementation; we just
+  // want to record the user's response for analytics purposes.
   const allowTransition = window.confirm(message);
   ga('send', 'event', 'before-navigate', 'confirm', allowTransition ? 'ok' : 'cancel');
   callback(allowTransition);
 }
 
+/**
+ * A history blocker callback should return true if navigation
+ * should be "blocked" by a confirmation dialog (e.g. if the user
+ * has a partially filled-out form they haven't submitted yet),
+ * false otherwise.
+ */
 type HistoryBlockerCb = () => boolean;
 
 type HistoryBlockerContextType = {
+  /** Register a history blocker callback. */
   block(blockCb: HistoryBlockerCb): void;
+
+  /**
+   * Unregister a history blocker callback. If the callback
+   * wasn't previously registered, an exception will be thrown.
+   */
   unblock(blockCb: HistoryBlockerCb): void;
 };
 
@@ -32,10 +52,14 @@ export const HistoryBlockerContext = React.createContext<HistoryBlockerContextTy
 
 type ManagerProps = RouteComponentProps<any>;
 
-interface ManagerState {  
-}
-
-export class HistoryBlockerManagerWithoutRouter extends React.Component<ManagerProps, ManagerState> {
+/**
+ * The history blocker manager keeps track of blocker callbacks
+ * and calls them when the user attempts to navigate away from
+ * the page (either to another page, or outside our single-page
+ * application entirely). If any one of them tells us to
+ * display a confirmation dialog, we do so.
+ */
+export class HistoryBlockerManagerWithoutRouter extends React.Component<ManagerProps> {
   callbacks: HistoryBlockerCb[];
   unblockHistory: UnregisterCallback|null;
 
@@ -110,7 +134,35 @@ export class HistoryBlockerManagerWithoutRouter extends React.Component<ManagerP
 export const HistoryBlockerManager = withRouter(HistoryBlockerManagerWithoutRouter);
 
 interface HistoryBlockerProps {
+  /**
+   * Don't actually raise a confirmation dialog if the user
+   * navigates away from the current page, but *do* log an
+   * analytics event that lets us know that a user navigated
+   * away from the page (presumably without submitting their
+   * data or some other criteria).
+   */
   reportOnly?: boolean;
+  children?: never;
+}
+
+/**
+ * When this component is present in the component heirarchy,
+ * a confirmation modal will be displayed when the user
+ * tries to navigate away from the current page.
+ * 
+ * Clients can use this logic to conditionally render
+ * the component based on some criteria; e.g., a form
+ * can only render it if the user has started filling it
+ * out but hasn't submitted it yet.
+ * 
+ * This component doesn't render anything.
+ */
+export function HistoryBlocker(props: HistoryBlockerProps): JSX.Element {
+  return (
+    <HistoryBlockerContext.Consumer children={(ctx) => (
+      <HistoryBlockerWithoutContext {...ctx} {...props} />
+    )} />
+  );
 }
 
 class HistoryBlockerWithoutContext extends React.Component<HistoryBlockerProps & HistoryBlockerContextType> {
@@ -134,12 +186,4 @@ class HistoryBlockerWithoutContext extends React.Component<HistoryBlockerProps &
   render() {
     return null;
   }
-}
-
-export function HistoryBlocker(props: HistoryBlockerProps): JSX.Element {
-  return (
-    <HistoryBlockerContext.Consumer children={(ctx) => (
-      <HistoryBlockerWithoutContext {...ctx} {...props} />
-    )} />
-  );
 }

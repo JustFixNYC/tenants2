93f112bd7ce35d5b7b2305234fe3e8f52f60592b
Atul Varma <varmaa@gmail.com>
2018-10-11 14:48:08 -0400

    Add tests for form-field-equality.

--- frontend/lib/form-field-equality.ts
+++ frontend/lib/form-field-equality.ts
@@ -1,3 +1,7 @@
+/**
+ * Returns whether or not the two arrays have the same items,
+ * even if they're not in the same order.
+ */
 export function areArraysEqualIgnoringOrder(a: any[], b: any[]): boolean {
   if (a.length !== b.length) return false;
 
@@ -10,6 +14,14 @@ export function areArraysEqualIgnoringOrder(a: any[], b: any[]): boolean {
   return true;
 }
 
+/**
+ * Returns whether or not the two sets of form fields have the
+ * same values.
+ * 
+ * Note that we currently treat arrays of items as being
+ * order-indepenedent, e.g. a field with the value ["A", "B"] is,
+ * for our purposes, the same as a field with the value ["B", "A"].
+ */
 export function areFieldsEqual<FormInput>(initial: FormInput, current: FormInput): boolean {
   for (let key in initial) {
     const initialValue = initial[key];
---
+++ frontend/lib/tests/form-field-equality.test.ts
@@ -0,0 +1,32 @@
+import { areArraysEqualIgnoringOrder, areFieldsEqual } from "../form-field-equality";
+
+describe("areArraysEqualIgnoringOrder", () => {
+  it("returns false if lengths are different", () => {
+    expect(areArraysEqualIgnoringOrder([], [1])).toBe(false);
+  });
+
+  it("returns false if contents are different", () => {
+    expect(areArraysEqualIgnoringOrder([2], [1])).toBe(false);
+  });
+
+  it("returns true if contents are same but order is not", () => {
+    expect(areArraysEqualIgnoringOrder([2, 1], [1, 2])).toBe(true);
+  });
+});
+
+describe('areFieldsEqual', () => {
+  it("returns true when fields are equal", () => {
+    expect(areFieldsEqual({a: 1}, {a: 1})).toBe(true);
+    expect(areFieldsEqual({a: 'a'}, {a: 'a'})).toBe(true);
+    expect(areFieldsEqual({a: true}, {a: true})).toBe(true);
+    expect(areFieldsEqual({a: false}, {a: false})).toBe(true);
+    expect(areFieldsEqual({a: [1, 2]}, {a: [2, 1]})).toBe(true);
+  });
+
+  it("returns false when fields are not equal", () => {
+    expect(areFieldsEqual({a: 1}, {a: 2})).toBe(false);
+    expect(areFieldsEqual({a: 'a'}, {a: 'b'})).toBe(false);
+    expect(areFieldsEqual({a: true}, {a: false})).toBe(false);
+    expect(areFieldsEqual({a: [1, 2], c: 2}, {a: [2, 1], c: 1})).toBe(false);
+  });
+});

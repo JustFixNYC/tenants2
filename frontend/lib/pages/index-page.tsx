05d64844b200aa567c10bd6507aea19825563969
Dan Kass <romeboards@gmail.com>
2018-10-22 11:20:41 -0400

    Navbar tweaks and further landing page improvements

--- frontend/lib/pages/index-page.tsx
+++ frontend/lib/pages/index-page.tsx
@@ -21,16 +21,16 @@ export default class IndexPage extends React.Component<IndexPageProps> {
         <section className="hero is-light">
           <div className="hero-body">
             <div className="has-text-centered">
-
-              <div className="jf-loc-image">{locSvg}</div>
-
+              <div className="jf-loc-image">
+                {locSvg}
+              </div>
               <h1 className="title is-spaced">
                 Is your landlord not responding? Take action today!
               </h1>
               <h2 className="subtitle">
                 JustFix.nyc is a free tool that notifies your landlord of repair issues via <b>USPS<sup>&copy;</sup> Certified Mail</b>. Everything is kept confidential and secure.
               </h2>
-              <CenteredPrimaryButtonLink to={Routes.onboarding.latestStep} className="is-medium">
+              <CenteredPrimaryButtonLink to={Routes.onboarding.latestStep} className="is-large">
                 Start my letter
               </CenteredPrimaryButtonLink>
               <p className="secondary-cta">Already have an account? <Link to={Routes.login}>Sign in!</Link></p>
@@ -67,51 +67,29 @@ export default class IndexPage extends React.Component<IndexPageProps> {
                 </div>
               </div>
             </div>
-            <CenteredPrimaryButtonLink to={Routes.onboarding.latestStep} className="is-medium">
+            <CenteredPrimaryButtonLink to={Routes.onboarding.latestStep} className="is-large">
               Start my letter
             </CenteredPrimaryButtonLink>
           </div>
         </section>
 
-
         <section className="section">
-          <div className="content">
-            <h1 className="has-text-centered">What are the benefits of mailing a Certified Letter of Complaint?</h1>
-            <p>
-              Your landlord is responsible for keeping your apartment and the building safe and livable at all times. This is called the <strong>Warranty of Habitability</strong>.
-            </p>
-            <p>
-              If your landlord has been unresponsive to your requests to make repairs, a letter is a great tactic! By mailing a Letter of Complaint via Certified mail, you will have an official record of the requests you’ve made to your landlord. <strong>It is also good to have this letter as evidence for a future legal action.</strong>
-            </p>
-          </div>
+          <h1 className="title is-spaced has-text-centered">What are the benefits of mailing a Certified Letter of Complaint?</h1>
+          <h5 className="subtitle">
+            Your landlord is responsible for keeping your apartment and the building safe and livable at all times. This is called the <strong>Warranty of Habitability</strong>.
+          </h5>
+          <h5 className="subtitle">
+            If your landlord has been unresponsive to your requests to make repairs, a letter is a great tactic! By mailing a Letter of Complaint via Certified mail, you will have an official record of the requests you’ve made to your landlord. <strong>It is also good to have this letter as evidence for a future legal action.</strong>
+          </h5>
         </section>
 
         <section className="section">
-          <div className="content">
-            <h1 className="has-text-centered">About JustFix.nyc</h1>
-            <p>
-              JustFix.nyc is a nonprofit that builds tools for tenants and organizers fighting displacement. We encourage tenants to take affirmative actions to fight for safe and healthy homes. Want to know more? <OutboundLink href="https://www.justfix.nyc/our-mission">Visit our website.</OutboundLink>
-            </p>
-            <p><strong>Disclaimer:</strong> The information contained in JustFix.nyc does not constitute legal advice and must not be used as a substitute for the advice of a lawyer qualified to give advice on legal issues pertaining to housing. We can help direct you to free and/or low-cost legal services as necessary.</p>
-          </div>
+          <h1 className="title is-spaced has-text-centered">About JustFix.nyc</h1>
+          <h5 className="subtitle">
+            JustFix.nyc is a nonprofit that builds tools for tenants and organizers fighting displacement. We encourage tenants to take affirmative actions to fight for safe and healthy homes. Want to know more? <OutboundLink href="https://www.justfix.nyc/our-mission">Visit our website.</OutboundLink>
+          </h5>
+          <h5 className="subtitle"><strong>Disclaimer:</strong> The information contained in JustFix.nyc does not constitute legal advice and must not be used as a substitute for the advice of a lawyer qualified to give advice on legal issues pertaining to housing. We can help direct you to free and/or low-cost legal services as necessary.</h5>
         </section>
-
-        {/* <div className="content">
-            <WhyMailALetterOfComplaint heading="h2" />
-
-            <CenteredPrimaryButtonLink to={Routes.onboarding.latestStep} className="is-medium">
-              Get started
-            </CenteredPrimaryButtonLink>
-
-            <h2>About JustFix.nyc</h2>
-
-
-
-
-          </div> */}
-
-
-
 
       </Page>
     );

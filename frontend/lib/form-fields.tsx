224ccc8ad75d22175bc1f5f5a2aaaf2e037ddc46
Dan Kass <romeboards@gmail.com>
2018-10-22 15:23:57 -0400

    autoComplete camelcase monstrosity

--- frontend/lib/form-fields.tsx
+++ frontend/lib/form-fields.tsx
@@ -257,7 +257,7 @@ export function TextualFormField(props: TextualFormFieldProps): JSX.Element {
           aria-invalid={ariaBool(!!props.errors)}
           aria-label={ariaLabel}
           name={props.name}
-          autocomplete={props.autocomplete}
+          autoComplete={props.autocomplete}
           id={props.id}
           min={props.min}
           type={type}

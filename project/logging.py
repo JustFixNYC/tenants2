3eae28e895e76d9f8a5a735e3595aec27f820994
Atul Varma <varmaa@gmail.com>
2018-10-24 13:17:49 -0400

    Make skip_static_requests() more resilient.

--- project/logging.py
+++ project/logging.py
@@ -1,4 +1,5 @@
 import re
+import logging
 from django.conf import settings
 
 IGNORE_PATHS = [
@@ -14,7 +15,15 @@ STATIC_REGEX = re.compile(
 )
 
 
-def skip_static_requests(record) -> bool:
-    if STATIC_REGEX.match(record.args[0]):
+def skip_static_requests(record: logging.LogRecord) -> bool:
+    '''
+    A logging filter that filters out requests for static
+    assets.
+    '''
+
+    if not (record.args and isinstance(record.args, tuple)):
+        return True
+    request_line = record.args[0]
+    if isinstance(request_line, str) and STATIC_REGEX.match(request_line):
         return False
     return True
--- project/tests/test_logging.py
+++ project/tests/test_logging.py
@@ -6,7 +6,7 @@ from project.logging import skip_static_requests
 
 def mkrecord(text: str) -> logging.LogRecord:
     return logging.makeLogRecord({
-        'args': [text]
+        'args': (text,)
     })
 
 
@@ -19,3 +19,12 @@ def mkrecord(text: str) -> logging.LogRecord:
 ])
 def test_skip_static_requests(request_line, noskip):
     assert skip_static_requests(mkrecord(request_line)) is noskip
+
+
+def test_skip_static_requests_ignores_weirdly_typed_log_records():
+    assert skip_static_requests(logging.makeLogRecord({
+        'args': {'blah': 1}})) is True
+    assert skip_static_requests(logging.makeLogRecord({
+        'args': tuple()})) is True
+    assert skip_static_requests(logging.makeLogRecord({
+        'args': (1234,)})) is True

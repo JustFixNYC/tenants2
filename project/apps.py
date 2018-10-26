64e74a4dcb417e0b4bf88b137de10f072a60521d
Atul Varma <varmaa@gmail.com>
2018-10-26 10:25:05 -0400

    Only regenerate schema.json if running dev server.

--- project/apps.py
+++ project/apps.py
@@ -1,9 +1,25 @@
 import logging
+import sys
 from django.apps import AppConfig
 from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
+
+
+def is_running_dev_server(argv=sys.argv):
+    '''
+    Returns whether or not we are running the development
+    server, e.g.:
+
+        >>> is_running_dev_server(['manage.py', '--help'])
+        False
+
+        >>> is_running_dev_server(['manage.py', 'runserver'])
+        True
+    '''
+
+    return 'manage.py' in argv and 'runserver' in argv
 
 
 class DefaultConfig(AppConfig):
@@ -14,7 +30,7 @@ class DefaultConfig(AppConfig):
 
         twilio.validate_settings()
 
-        if settings.DEBUG:
+        if settings.DEBUG and is_running_dev_server():
             from project.util import schema_json
 
             if not schema_json.is_up_to_date():

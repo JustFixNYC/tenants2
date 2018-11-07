75b8b338bba9f1b07df791d1e4f69437b45263e3
Atul Varma <varmaa@gmail.com>
2018-11-07 05:40:51 -0500

    Add an 'hpclean' command.

---
+++ hpaction/management/commands/hpclean.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from hpaction.models import HPActionDocuments, UploadToken
+
+
+class Command(BaseCommand):
+    help = (
+        'Purge all HP Action-related information and documents '
+        'that have been scheduled for deletion.'
+    )
+
+    def handle(self, *args, **options):
+        self.stdout.write('Purging unneeded HP Action documents...\n')
+        HPActionDocuments.objects.purge()
+        self.stdout.write('Purging expired upload tokens...\n')
+        UploadToken.objects.remove_expired()

274a357a8a491912a38a2e8e4b2b787f78bbc1d5
Atul Varma <varmaa@gmail.com>
2018-11-13 18:13:25 -0500

    Factor out an HDComponentLibrary class.

--- hpaction/management/commands/hpparse.py
+++ hpaction/management/commands/hpparse.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 from typing import NamedTuple, List, Dict
+from pathlib import Path
 from django.core.management.base import BaseCommand
 
 from project.justfix_environment import BASE_DIR
@@ -57,111 +58,107 @@ class HDMultipleChoice(HDVariable):
         return base_desc
 
 
-class HDVars:
-    d: Dict[str, HDVariable]
-
-    def __init__(self):
-        self.d = {}
-
-    def append(self, var: HDVariable):
-        self.d[var.name] = var
-
-
 class HDRepeat(NamedTuple):
     name: str
     variables: List[HDVariable]
 
 
-def get_help_text(el: ET.Element) -> str:
-    # Absolutely no idea why el.find() doesn't work here.
-    for prompt in el.findall('hd:prompt', NS):
-        if prompt.text:
-            return prompt.text
-    return ''
+class HDComponentLibrary:
+    vars: Dict[str, HDVariable]
+    repeats: List[HDRepeat]
 
+    def __init__(self, path: Path):
+        self.vars = {}
+        self.repeats = []
 
-def get_mc_options(el: ET.Element) -> List[HDOption]:
-    results: List[HDOption] = []
-    for option in el.findall('hd:options/hd:option', NS):
-        results.append(HDOption(
-            name=option.attrib['name'],
-            label=get_help_text(option)
-        ))
-    return results
+        tree = ET.parse(str(path))
+        root = tree.getroot()
+        components = root.find('hd:components', NS)
+        if not components:
+            raise Exception('Could not find <hd:components> element')
+        self.populate_vars(components)
+        self.populate_repeats(components)
 
+    def get_help_text(self, el: ET.Element) -> str:
+        # Absolutely no idea why el.find() doesn't work here.
+        for prompt in el.findall('hd:prompt', NS):
+            if prompt.text:
+                return prompt.text
+        return ''
 
-def get_hd_repeats(components: ET.Element, hd_vars: HDVars) -> List[HDRepeat]:
-    result: List[HDRepeat] = []
-    for dialog in components.iter(f'{HD}dialog'):
-        is_sheet = len(dialog.findall('hd:style/hd:spreadsheetOnParent', NS)) > 0
-        if not is_sheet:
-            continue
-        repeat_vars: List[HDVariable] = []
-        for item in dialog.findall('hd:contents/hd:item', NS):
-            name = item.attrib['name']
-            value = hd_vars.d[name]
-            del hd_vars.d[name]
-            repeat_vars.append(value)
-        result.append(HDRepeat(
-            name=dialog.attrib['name'],
-            variables=repeat_vars
-        ))
-    return result
+    def get_mc_options(self, el: ET.Element) -> List[HDOption]:
+        results: List[HDOption] = []
+        for option in el.findall('hd:options/hd:option', NS):
+            results.append(HDOption(
+                name=option.attrib['name'],
+                label=self.get_help_text(option)
+            ))
+        return results
 
+    def populate_repeats(self, components: ET.Element) -> None:
+        for dialog in components.iter(f'{HD}dialog'):
+            is_sheet = len(dialog.findall('hd:style/hd:spreadsheetOnParent', NS)) > 0
+            if not is_sheet:
+                continue
+            repeat_vars: List[HDVariable] = []
+            for item in dialog.findall('hd:contents/hd:item', NS):
+                name = item.attrib['name']
+                value = self.vars[name]
+                del self.vars[name]
+                repeat_vars.append(value)
+            self.repeats.append(HDRepeat(
+                name=dialog.attrib['name'],
+                variables=repeat_vars
+            ))
 
-def get_hd_vars(components: ET.Element) -> HDVars:
-    hd_vars = HDVars()
-    for el in components.findall('hd:text', NS):
-        hd_vars.append(HDText(
-            name=el.attrib['name'],
-            help_text=get_help_text(el)
-        ))
-    for el in components.findall('hd:date', NS):
-        hd_vars.append(HDDate(
-            name=el.attrib['name'],
-            help_text=get_help_text(el)
-        ))
-    for el in components.findall('hd:number', NS):
-        hd_vars.append(HDNumber(
-            name=el.attrib['name'],
-            help_text=get_help_text(el)
-        ))
-    for el in components.findall('hd:trueFalse', NS):
-        hd_vars.append(HDBoolean(
-            name=el.attrib['name'],
-            help_text=get_help_text(el)
-        ))
-    for el in components.findall('hd:multipleChoice', NS):
-        sm = len(el.findall('hd:multipleSelection', NS)) > 0
-        hd_vars.append(HDMultipleChoice(
-            name=el.attrib['name'],
-            help_text=get_help_text(el),
-            options=get_mc_options(el),
-            select_multiple=sm
-        ))
-    return hd_vars
+    def add_var(self, var: HDVariable) -> None:
+        self.vars[var.name] = var
+
+    def populate_vars(self, components: ET.Element) -> None:
+        for el in components.findall('hd:text', NS):
+            self.add_var(HDText(
+                name=el.attrib['name'],
+                help_text=self.get_help_text(el)
+            ))
+        for el in components.findall('hd:date', NS):
+            self.add_var(HDDate(
+                name=el.attrib['name'],
+                help_text=self.get_help_text(el)
+            ))
+        for el in components.findall('hd:number', NS):
+            self.add_var(HDNumber(
+                name=el.attrib['name'],
+                help_text=self.get_help_text(el)
+            ))
+        for el in components.findall('hd:trueFalse', NS):
+            self.add_var(HDBoolean(
+                name=el.attrib['name'],
+                help_text=self.get_help_text(el)
+            ))
+        for el in components.findall('hd:multipleChoice', NS):
+            sm = len(el.findall('hd:multipleSelection', NS)) > 0
+            self.add_var(HDMultipleChoice(
+                name=el.attrib['name'],
+                help_text=self.get_help_text(el),
+                options=self.get_mc_options(el),
+                select_multiple=sm
+            ))
 
 
 class Command(BaseCommand):
     help = f"Parse {MASTER_CMP_PATH.name} and show results."
 
     def handle(self, *args, **options):
-        tree = ET.parse(str(MASTER_CMP_PATH))
-        root = tree.getroot()
-        components = root.find('hd:components', NS)
-        if not components:
-            raise Exception('could not find components')
-        hd_vars = get_hd_vars(components)
-        hd_repeats = get_hd_repeats(components, hd_vars)
+        lib = HDComponentLibrary(MASTER_CMP_PATH)
 
         print("## Variables\n")
 
-        for var in hd_vars.d.values():
+        for var in lib.vars.values():
             print(var.describe())
 
         print("\n## Repeats")
 
-        for repeat in hd_repeats:
+        for repeat in lib.repeats:
             print()
             print(repeat.name)
             for var in repeat.variables:

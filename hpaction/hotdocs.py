dfb9f13b074545fb507833668fab69ead2874024
Atul Varma <varmaa@gmail.com>
2018-11-15 05:57:58 -0500

    Refactor create_answer_value().

--- hpaction/hotdocs.py
+++ hpaction/hotdocs.py
@@ -64,41 +64,62 @@ class AnswerSet:
         self.answer_set.setAttribute('title', 'New Answer File')
         self.answer_set.setAttribute('version', '1.1')
 
+    def create_unanswered(self, value: Unanswered) -> Element:
+        node = self.doc.createElement(value.type.value)
+        node.setAttribute('unans', "true")
+        return node
+
+    def create_text(self, value: str) -> Element:
+        node = self.doc.createElement(AnswerType.TEXT.value)
+        node.appendChild(self.doc.createTextNode(value))
+        return node
+
+    def create_true_false(self, value: bool) -> Element:
+        node = self.doc.createElement(AnswerType.TF.value)
+        text = 'true' if value else 'false'
+        node.appendChild(self.doc.createTextNode(text))
+        return node
+
+    def create_number(self, value: Union[int, float]) -> Element:
+        node = self.doc.createElement(AnswerType.NUM.value)
+        node.appendChild(self.doc.createTextNode(str(value)))
+        return node
+
+    def create_date(self, value: date) -> Element:
+        node = self.doc.createElement(AnswerType.DATE.value)
+        date_str = f'{value.month}/{value.day}/{value.year}'
+        node.appendChild(self.doc.createTextNode(date_str))
+        return node
+
+    def create_mc(self, value: MCValue) -> Element:
+        node = self.doc.createElement(AnswerType.MC.value)
+        for item in value.items:
+            child_node = self.doc.createElement('SelValue')
+            child_node.appendChild(self.doc.createTextNode(item))
+            node.appendChild(child_node)
+        return node
+
+    def create_repeat(self, value: List[BaseAnswerValue]) -> Element:
+        node = self.doc.createElement('RptValue')
+        for child in value:
+            node.appendChild(self.create_answer_value(child))
+        return node
+
     def create_answer_value(self, value: AnswerValue) -> Element:
         if isinstance(value, Unanswered):
-            node = self.doc.createElement(value.type.value)
-            node.setAttribute('unans', "true")
-            return node
+            return self.create_unanswered(value)
         elif isinstance(value, str):
-            node = self.doc.createElement(AnswerType.TEXT.value)
-            node.appendChild(self.doc.createTextNode(value))
-            return node
+            return self.create_text(value)
         elif isinstance(value, bool):
-            node = self.doc.createElement(AnswerType.TF.value)
-            text = 'true' if value else 'false'
-            node.appendChild(self.doc.createTextNode(text))
-            return node
+            return self.create_true_false(value)
         elif isinstance(value, (int, float)):
-            node = self.doc.createElement(AnswerType.NUM.value)
-            node.appendChild(self.doc.createTextNode(str(value)))
-            return node
+            return self.create_number(value)
         elif isinstance(value, date):
-            node = self.doc.createElement(AnswerType.DATE.value)
-            date_str = f'{value.month}/{value.day}/{value.year}'
-            node.appendChild(self.doc.createTextNode(date_str))
-            return node
+            return self.create_date(value)
         elif isinstance(value, MCValue):
-            node = self.doc.createElement(AnswerType.MC.value)
-            for item in value.items:
-                child_node = self.doc.createElement('SelValue')
-                child_node.appendChild(self.doc.createTextNode(item))
-                node.appendChild(child_node)
-            return node
+            return self.create_mc(value)
         elif isinstance(value, list):
-            node = self.doc.createElement('RptValue')
-            for child in value:
-                node.appendChild(self.create_answer_value(child))
-            return node
+            return self.create_repeat(value)
         raise ValueError(f'cannot convert {type(value).__name__} to a valid answer type')
 
     def add(self, name: str, value: AnswerValue) -> None:

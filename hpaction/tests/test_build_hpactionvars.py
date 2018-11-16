1ee3682425589dec850a622dd6816fe7b5151b8e
Atul Varma <varmaa@gmail.com>
2018-11-16 17:49:52 -0500

    Add further smoke testing.

--- hpaction/tests/test_build_hpactionvars.py
+++ hpaction/tests/test_build_hpactionvars.py
@@ -11,6 +11,7 @@ def test_user_to_hpactionvars_populates_basic_info(db):
     v = user_to_hpactionvars(user)
     assert v.tenant_name_first_te == "Boop"
     assert v.tenant_name_last_te == "Jones"
+    v.to_answer_set()
 
 
 def test_user_to_hpactionvars_populates_onboarding_info(db):
@@ -19,6 +20,7 @@ def test_user_to_hpactionvars_populates_onboarding_info(db):
     assert v.tenant_address_apt_no_te == '2B'
     assert v.court_county_mc == hp.CourtCountyMC.KINGS
     assert v.court_location_mc == hp.CourtLocationMC.KINGS
+    v.to_answer_set()
 
 
 def test_user_to_hpactionvars_populates_issues(db):
@@ -44,9 +46,11 @@ def test_user_to_hpactionvars_populates_issues(db):
     assert second.area_complained_of_mc == hp.AreaComplainedOfMC.PUBLIC_AREA
     assert second.which_room_mc.value == "Public areas"  # type: ignore
     assert second.conditions_complained_of_te == "Lobby is consumed by darkness"
+    v.to_answer_set()
 
 
 def test_user_to_hpactionvars_populates_landlord_info(db):
     ld = LandlordDetailsFactory(name="Landlordo Calrissian")
     v = user_to_hpactionvars(ld.user)
     assert v.landlord_entity_name_te == "Landlordo Calrissian"
+    v.to_answer_set()

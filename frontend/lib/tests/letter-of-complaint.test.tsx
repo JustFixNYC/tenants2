19005614eabdca990ca1344cbcf46422c87fa0b7
Atul Varma <varmaa@gmail.com>
2018-11-28 19:12:31 -0500

    Factor out a ProgressRoutes component, a ProgressRoutesTester class.

--- frontend/lib/tests/letter-of-complaint.test.tsx
+++ frontend/lib/tests/letter-of-complaint.test.tsx
@@ -1,36 +1,19 @@
-import React from 'react';
-import LetterOfComplaintRoutes, { letterOfComplaintSteps, RedirectToLatestLetterOfComplaintStep } from '../letter-of-complaint';
-import { AppTesterPal } from './app-tester-pal';
-import { getLatestStep } from '../progress-redirection';
-import { FakeSessionInfo, ensureRedirect } from './util';
+import { LOCProgressRoutesProps } from '../letter-of-complaint';
 import Routes from '../routes';
+import { ProgressRoutesTester } from './progress-routes-tester';
 
-describe("letter of complaint steps", () => {
-  afterEach(AppTesterPal.cleanup);
+const tester = new ProgressRoutesTester(LOCProgressRoutesProps, 'letter of complaint');
 
-  letterOfComplaintSteps.forEach(step => {
-    it(`${step.path} renders without throwing`, () => {
-      new AppTesterPal(<LetterOfComplaintRoutes />, {
-        url: step.path
-      });
-    });
-  });
-});
+tester.defineSmokeTests();
 
 describe('latest step redirector', () => {
   it('returns welcome page by default', () => {
-    expect(getLatestStep(FakeSessionInfo, letterOfComplaintSteps))
-      .toBe(Routes.loc.home);
+    expect(tester.getLatestStep()).toBe(Routes.loc.home);
   });
 
   it('returns confirmation page if letter request has been submitted', () => {
-    expect(getLatestStep({
-      ...FakeSessionInfo,
+    expect(tester.getLatestStep({
       letterRequest: {} as any
-    }, letterOfComplaintSteps)).toBe(Routes.loc.confirmation);
+    })).toBe(Routes.loc.confirmation);
   });
-});
-
-test('RedirectToLatestLetterOfComplaintStep returns a redirect', () => {
-  ensureRedirect(<RedirectToLatestLetterOfComplaintStep />, '/loc/welcome');
 });

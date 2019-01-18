3a83df6c0d5f93b11f72d6fcfe1032f1c06abf40
Atul Varma <varmaa@gmail.com>
2019-01-18 11:28:01 -0500

    Factor out a GeoSearchRequester class.

--- frontend/lib/geo-autocomplete-base.tsx
+++ frontend/lib/geo-autocomplete-base.tsx
@@ -1,4 +1,11 @@
 /**
+ * For documentation about this endpoint, see:
+ *
+ * https://geosearch.planninglabs.nyc/docs/#autocomplete
+ */
+const GEO_AUTOCOMPLETE_URL = 'https://geosearch.planninglabs.nyc/v1/autocomplete';
+
+/**
  * The keys here were obtained experimentally, I'm not actually sure
  * if/where they are formally specified.
  */
@@ -33,4 +40,78 @@ export interface GeoSearchResults {
     geometry: unknown;
     properties: GeoSearchProperties
   }[];
+}
+
+export interface GeoSearchRequesterOptions {
+  createAbortController: () => AbortController|undefined;
+  fetch: typeof window.fetch,
+  throttleMs: number,
+  onError: (e: Error) => void;
+  onResults: (results: GeoSearchResults) => void;
+}
+
+export class GeoSearchRequester {
+  private requestId: number;
+  private abortController?: AbortController;
+  private throttleTimeout: number|null;
+
+  constructor(readonly options: GeoSearchRequesterOptions) {
+    this.requestId = 0;
+    this.abortController = options.createAbortController();
+    this.throttleTimeout = null;
+  }
+
+  private async fetchResults(value: string): Promise<GeoSearchResults|null> {
+    const originalRequestId = this.requestId;
+    const url = `${GEO_AUTOCOMPLETE_URL}?text=${encodeURIComponent(value)}`;
+    let results: GeoSearchResults;
+    try {
+      const res = await this.options.fetch(url, {
+        signal: this.abortController && this.abortController.signal
+      });
+      results = await res.json();
+    } catch (e) {
+      if (e instanceof DOMException && e.name === 'AbortError') {
+        // Don't worry about it, the user just aborted the request.
+        return null;
+      } else {
+        throw e;
+      }
+    }
+    if (this.requestId === originalRequestId) {
+      return results;
+    }
+    return null;
+  }
+
+  private resetSearchRequest() {
+    if (this.throttleTimeout !== null) {
+      window.clearTimeout(this.throttleTimeout);
+      this.throttleTimeout = null;
+    }
+    if (this.abortController) {
+      this.abortController.abort();
+      this.abortController = this.options.createAbortController();
+    }
+    this.requestId++;
+  }
+
+  changeSearchRequest(value: string): boolean {
+    this.resetSearchRequest();
+    if (value.length > 0) {
+      this.throttleTimeout = window.setTimeout(() => {
+        this.fetchResults(value).catch(this.options.onError).then(results => {
+          if (results) {
+            this.options.onResults(results);
+          }
+        });
+      }, this.options.throttleMs);
+      return true;
+    }
+    return false;
+  }
+
+  shutdown() {
+    this.resetSearchRequest();
+  }
 }

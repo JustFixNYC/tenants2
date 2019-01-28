5367fa0f39262e1322824fbabf9d2d2b235094f5
Atul Varma <varmaa@gmail.com>
2019-01-28 11:01:16 -0500

    Add CORS to the S3 static files bucket. Fixes #446.

--- project/storage.py
+++ project/storage.py
@@ -29,6 +29,15 @@ class CompressedStaticFilesStorage(StaticFilesStorage):
 
 
 class S3StaticFilesStorage(S3Boto3Storage):
+    CORS_CONFIG = {
+        'CORSRules': [
+            {
+                'AllowedMethods': ['GET'],
+                'AllowedOrigins': ['*']
+            }
+        ]
+    }
+
     def __init__(self):
         super().__init__(
             bucket_name=settings.AWS_STORAGE_STATICFILES_BUCKET_NAME,
@@ -37,3 +46,8 @@ class S3StaticFilesStorage(S3Boto3Storage):
             bucket_acl='public-read',
             querystring_auth=False
         )
+
+    def _get_or_create_bucket(self, name):
+        bucket = super()._get_or_create_bucket(name)
+        bucket.Cors().put(CORSConfiguration=self.CORS_CONFIG)
+        return bucket

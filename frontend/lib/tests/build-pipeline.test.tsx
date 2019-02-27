ece3d0d7da4f4e85789676c28c7922eb829b00f4
Atul Varma <varmaa@gmail.com>
2019-02-27 12:48:18 -0500

    Add tests for the build pipeline (#493)
    * Add tests for the build pipeline.
    
    * Improve autobind decorator test.

---
+++ frontend/lib/tests/build-pipeline.test.tsx
@@ -0,0 +1,44 @@
+import autobind from 'autobind-decorator';
+
+test("dynamic import works", () => {
+  return import('./build-pipeline-dynamic-import').then(mod => {
+    expect(mod.blah(5)).toEqual(6);
+  });
+});
+
+test('autobind decorator works', () => {
+  class Blarg {
+    constructor(readonly base: number) {}
+
+    @autobind
+    boop(x: number) {
+      return this.base + x;
+    }
+  }
+
+  const blarg = new Blarg(6);
+  const boop = blarg.boop;
+
+  expect(boop(1)).toEqual(7);
+});
+
+test('super() works', () => {
+  class Foo {
+    thingy: number;
+
+    constructor() {
+      this.thingy = 1;
+    }
+  }
+
+  class Bar extends Foo {
+    constructor() {
+      super()
+      this.thingy += 1;
+    }
+  }
+
+  const bar = new Bar();
+
+  expect(bar.thingy).toEqual(2);
+});

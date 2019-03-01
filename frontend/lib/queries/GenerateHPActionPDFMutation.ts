de06420f39fa7363e20ac877cf3544022a419276
Atul Varma <varmaa@gmail.com>
2019-03-01 16:16:57 -0500

    Re-run querybuilder, which has apparently un-regressed.

--- frontend/lib/queries/GenerateHPActionPDFMutation.ts
+++ frontend/lib/queries/GenerateHPActionPDFMutation.ts
@@ -1,6 +1,7 @@
 // This file was automatically generated and should not be edited.
 
 /* tslint:disable */
+/* eslint-disable */
 // This file was automatically generated and should not be edited.
 
 import { GeneratePDFInput, HPUploadStatus } from "./globalTypes";

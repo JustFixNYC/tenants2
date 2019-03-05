963ea540f496ea55e907939678189489f7c30c7a
Atul Varma <varmaa@gmail.com>
2019-03-05 05:53:48 -0500

    Remove MyFileWatcher from querybuilder.
    This isn't very useful as of #500, because querybuilder.js
    is no longer a bundle that consists of all its code. Implementing
    this kind of watcher now would require tracing all its imports
    and watching those, which is way too much work.

--- frontend/querybuilder/watcher.ts
+++ frontend/querybuilder/watcher.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import fs from 'fs';
 
 import chokidar from 'chokidar';
 import chalk from 'chalk';
@@ -7,34 +6,8 @@ import { main, ToolError, MainOptions, SCHEMA_PATH, LIB_PATH_PARTS, DOT_GRAPHQL 
 import { debouncer } from './util';
 
 
-/**
- * This lets us know if querybuilder itself has changed.
- */
-class MyFileWatcher {
-  private contents: Buffer;
-
-  constructor(readonly myFile: string) {
-    this.contents = fs.readFileSync(myFile);
-    chokidar.watch([myFile], {
-      ignoreInitial: true,
-      awaitWriteFinish: true
-    }).on('all', this.checkAndWarnIfChanged);
-    this.checkAndWarnIfChanged = this.checkAndWarnIfChanged.bind(this);
-  }
-
-  checkAndWarnIfChanged() {
-    if (!this.contents.equals(fs.readFileSync(this.myFile))) {
-      console.log(chalk.yellowBright(
-        `WARNING: ${this.myFile} has changed, consider pressing Ctrl-C\n` +
-        `and re-running this command.`
-      ));
-    }
-  }
-}
-
 /** Watch GraphQL queries and schema and re-build queries when they change. */
 export function watch(options: MainOptions, debounceMs = 250) {
-  const myFileWatcher = new MyFileWatcher(path.basename(process.argv[1]));
   const paths = [
     SCHEMA_PATH,
     path.posix.join(...LIB_PATH_PARTS, `*${DOT_GRAPHQL}`)
@@ -56,6 +29,5 @@ export function watch(options: MainOptions, debounceMs = 250) {
       console.log(chalk.redBright('ERROR: Rebuilding GraphQL queries failed!'));
     }
     console.log(`Waiting for changes in ${paths.join(', ')}...`);
-    myFileWatcher.checkAndWarnIfChanged();
   }, debounceMs));
 }

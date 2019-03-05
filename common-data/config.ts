b29b8e130d8073092aa6a8ffa23a3295cc5851c4
Atul Varma <varmaa@gmail.com>
2019-03-05 07:10:05 -0500

    Add a commondatabuilder tool.

---
+++ common-data/config.ts
@@ -0,0 +1,14 @@
+import { DjangoChoicesTypescriptConfig } from '../frontend/commondatabuilder/commondatabuilder';
+
+const config: DjangoChoicesTypescriptConfig = {
+  rootDir: __dirname,
+  files: [
+    {
+      jsonFilename: 'issue-choices.json',
+      enumName: 'IssueChoice',
+      exportLabels: true,
+    }
+  ]
+};
+
+export default config;

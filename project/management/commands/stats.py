7a629d26279100df06fb94a6dff96225fe8432b2
Atul Varma <varmaa@gmail.com>
2019-03-16 06:57:20 -0400

    Add a barebones 'stats' management command.

---
+++ project/management/commands/stats.py
@@ -0,0 +1,18 @@
+import csv
+from pathlib import Path
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+MY_DIR = Path(__file__).parent.resolve()
+STATS_SQLFILE = MY_DIR / 'stats.sql'
+
+
+class Command(BaseCommand):
+    help = 'Output a CSV of statistics about users.'
+
+    def handle(self, *args, **options):
+        writer = csv.writer(self.stdout)
+        with connection.cursor() as cursor:
+            cursor.execute(STATS_SQLFILE.read_text())
+            row = cursor.fetchone()
+            writer.writerow(row)
---
+++ project/management/commands/stats.sql
@@ -0,0 +1,4 @@
+SELECT
+    onb.pad_bbl
+FROM
+    onboarding_onboardinginfo as onb

ff527b3d58effe1b59c7bb56ba9947f84ec07ced
Atul Varma <varmaa@gmail.com>
2019-03-20 06:31:20 -0400

    Add tests.

--- rapidpro/management/commands/syncrapidpro.py
+++ rapidpro/management/commands/syncrapidpro.py
@@ -1,6 +1,6 @@
 import re
 from typing import List, Optional
-from datetime import timedelta
+from datetime import datetime, timedelta
 from django.conf import settings
 from django.db import transaction
 from django.core.management.base import CommandError, BaseCommand
@@ -37,29 +37,30 @@ def find_user_from_urns(urns: List[str]) -> Optional[JustfixUser]:
     return JustfixUser.objects.filter(phone_number=phone_number).first()
 
 
+def get_contact_batches(after: Optional[datetime]):
+    client = TembaClient(settings.RAPIDPRO_HOSTNAME, settings.RAPIDPRO_API_TOKEN)
+    return client.get_contacts(
+        after=after
+    ).iterfetches(retry_on_rate_exceed=True)
+
+
 class Command(BaseCommand):
     help = 'Sync with RapidPro.'
 
     @transaction.atomic
-    def handle(self, *args, **options):
-        if not settings.RAPIDPRO_API_TOKEN:
-            raise CommandError("RAPIDPRO_API_TOKEN must be configured.")
-
+    def sync(self):
         hostname = settings.RAPIDPRO_HOSTNAME
         self.stdout.write(f"Syncing with {hostname}...")
         sync_time = timezone.now() - CLOCK_SKEW
         metadata, _ = Metadata.objects.get_or_create()
-        client = TembaClient(hostname, settings.RAPIDPRO_API_TOKEN)
-        batches = client.get_contacts(
-            after=metadata.last_sync
-        ).iterfetches(retry_on_rate_exceed=True)
+        batches = get_contact_batches(after=metadata.last_sync)
         for contact_batch in batches:
             self.stdout.write(f"Processing a batch of {len(contact_batch)} contacts.\n")
             for contact in contact_batch:
                 user = find_user_from_urns(contact.urns)
                 if user is None:
                     continue
-                self.stdout.write(f"Syncing {user} ({len(contact.groups)} groups).\n")
+                self.stdout.write(f"Syncing user {user} ({len(contact.groups)} groups).\n")
                 for group in contact.groups:
                     # Get the contact group from the database, creating it if needed.
                     cg, _ = ContactGroup.objects.get_or_create(
@@ -83,3 +84,8 @@ class Command(BaseCommand):
         metadata.last_sync = sync_time
         metadata.save()
         self.stdout.write(f"Done syncing with {hostname}.\n")
+
+    def handle(self, *args, **options):
+        if not settings.RAPIDPRO_API_TOKEN:
+            raise CommandError("RAPIDPRO_API_TOKEN must be configured.")
+        self.sync()

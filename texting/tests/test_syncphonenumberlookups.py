3ee7f3a3b03f2d8fb38c3deba4529b9601ce564a
Atul Varma <varmaa@gmail.com>
2019-03-22 18:03:07 -0400

    Improve test a bit.

--- texting/tests/test_syncphonenumberlookups.py
+++ texting/tests/test_syncphonenumberlookups.py
@@ -3,7 +3,6 @@ from django.core.management import call_command, CommandError
 import pytest
 
 from .test_models import MockTwilioDbTest
-from .test_twilio import apply_twilio_settings
 from users.tests.factories import UserFactory
 from texting.models import PhoneNumberLookup
 
@@ -15,12 +14,13 @@ class TestCommand(MockTwilioDbTest):
         return out.getvalue().splitlines()
 
     def test_it_works(self, settings):
-        apply_twilio_settings(settings)
         UserFactory(phone_number='5551234567', username='boop')
         UserFactory(phone_number='5551230000', username='blarg')
         lookup = PhoneNumberLookup(
             phone_number='5551234567', is_valid=True, carrier={'type': 'mobile'})
         lookup.save()
+
+        settings.TWILIO_ACCOUNT_SID = 'blarg'
 
         with self.mock_twilio(is_valid=False):
             assert self.run_command() == [
@@ -28,12 +28,13 @@ class TestCommand(MockTwilioDbTest):
                 'Done syncing phone number lookups.'
             ]
             self.is_phone_number_valid.assert_called_once_with('5551230000')
-
-        with self.mock_twilio():
+            self.is_phone_number_valid.reset_mock()
             assert self.run_command() == [
                 'Done syncing phone number lookups.'
             ]
             self.is_phone_number_valid.assert_not_called()
+
+        assert PhoneNumberLookup.objects.get(phone_number='5551230000').is_valid is False
 
 
 def test_it_raises_error_when_twilio_is_disabled():

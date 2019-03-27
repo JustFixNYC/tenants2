fe70824e184c82fd83053c08e9d4cafffd314038
Atul Varma <varmaa@gmail.com>
2019-03-27 19:20:10 -0400

    Actually just remove all explicit version tracking stuff for now.

--- issues/models.py
+++ issues/models.py
@@ -1,11 +1,8 @@
 import json
-from typing import List, Optional
-from enum import Enum
+from typing import List
 from django.db import models, transaction
 from django.core.exceptions import ValidationError
 from django.core import serializers
-from django.contrib.postgres.fields import JSONField
-import pydantic
 
 from users.models import JustfixUser
 from project.common_data import Choices
@@ -40,71 +37,26 @@ def queryset_to_json(queryset):
     return json.loads(serializers.serialize("json", queryset))
 
 
-class Event(pydantic.BaseModel):
-    class Config:
-        use_enum_values = True
-
-
-class IssuesChangedEvent(Event):
-    added: List[str]
-    removed: List[str]
-
-
-class CustomIssueEventType(Enum):
-    added = 'added'
-    removed = 'removed'
-    changed = 'changed'
-
-
-class CustomIssueEvent(Event):
-    kind: CustomIssueEventType
-    area: str
-    description: Optional[str]
-
-
-class IssueEvent(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(JustfixUser, on_delete=models.CASCADE)
-    kind = models.CharField(max_length=100)
-    data = JSONField()
-
-    @staticmethod
-    def create(user: JustfixUser, event: Event) -> 'IssueEvent':
-        return IssueEvent(
-            user=user,
-            kind=event.__class__.__name__,
-            data=event.dict()
-        )
-
-
 class IssueManager(models.Manager):
     @transaction.atomic
     def set_area_issues_for_user(self, user: JustfixUser, area: str, issues: List[str]):
-        issues = list(set(issues))  # Remove duplicates.
-        curr_models = self.filter(user=user, area=area)
-        models_to_delete = curr_models.exclude(value__in=issues)
-        values_to_remove = [
-            model['value']
-            for model in models_to_delete.values('value')
-        ]
-        if values_to_remove:
-            models_to_delete.delete()
-        values_to_ignore = set(
-            model['value']
-            for model in curr_models.filter(value__in=issues).values('value')
+        issues_set = set(issues)  # Remove duplicates and make lookup fast.
+        curr_models = list(self.filter(user=user, area=area))
+        models_to_delete = [model for model in curr_models if model.value not in issues_set]
+        for model in models_to_delete:
+            model.delete()
+        values_that_already_exist = set(
+            model.value
+            for model in curr_models if model.value in issues_set
         )
         models_to_create = [
             Issue(user=user, area=area, value=value)
-            for value in issues
-            if value not in values_to_ignore
+            for value in issues_set
+            if value not in values_that_already_exist
         ]
-        values_to_add = [model.value for model in models_to_create]
         for model in models_to_create:
             model.full_clean()
-        self.bulk_create(models_to_create)
-        if values_to_remove or values_to_add:
-            event = IssuesChangedEvent(added=values_to_add, removed=values_to_remove)
-            IssueEvent.create(user=user, event=event).save()
+            model.save()
 
     def get_area_issues_for_user(self, user: JustfixUser, area: str) -> List[str]:
         return [
@@ -117,7 +69,7 @@ class Issue(models.Model):
         unique_together = ('user', 'value')
         ordering = ("value",)
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -147,21 +99,12 @@ class CustomIssueManager(models.Manager):
         if description:
             if issue is None:
                 issue = CustomIssue(user=user, area=area)
-                kind = CustomIssueEventType.added
-            elif issue.description != description:
-                kind = CustomIssueEventType.changed
-            else:
+            elif issue.description == description:
                 return
-            event = CustomIssueEvent(kind=kind, area=area, description=description)
-            IssueEvent.create(user=user, event=event)
             issue.description = description
             issue.full_clean()
             issue.save()
         elif issue:
-            IssueEvent.create(user=user, event=CustomIssueEvent(
-                kind=CustomIssueEventType.removed,
-                area=area
-            )).save()
             issue.delete()
 
     def get_for_user(self, user: JustfixUser, area: str) -> str:
@@ -175,7 +118,7 @@ class CustomIssue(models.Model):
     class Meta:
         unique_together = ('user', 'area')
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     updated_at = models.DateTimeField(auto_now=True)
 

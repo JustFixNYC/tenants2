2d3c8bd6afcbc9669c35b4e1958c70bcb1ce0083
Atul Varma <varmaa@gmail.com>
2019-04-29 18:14:47 -0400

    Show 'has-text-danger' when user is at 10% of limit.

--- frontend/lib/chars-remaining.tsx
+++ frontend/lib/chars-remaining.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { SimpleProgressiveEnhancement } from "./progressive-enhancement";
 
+/**
+ * Once the user has this percentage of their maximum limit left,
+ * we will be more noticeable.
+ */
+const DANGER_ALERT_PCT = 0.10;
+
 export type CharsRemainingProps = {
   max: number,
   current: number
@@ -8,8 +14,8 @@ export type CharsRemainingProps = {
 
 export function CharsRemaining({ max, current }: CharsRemainingProps): JSX.Element {
   const remaining = max - current;
-  const className = remaining < 0 ? 'has-text-danger' : '';
-  const text = `${remaining} characters remaining.`;
+  const className = remaining < (max * DANGER_ALERT_PCT) ? 'has-text-danger' : '';
+  const text = `${remaining} character${remaining === 1 ? '' : 's'} remaining.`;
 
   return (
     <SimpleProgressiveEnhancement>

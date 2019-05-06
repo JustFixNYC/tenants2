3e3584b006f6d66d6031bba857e498775eb3451c
Atul Varma <varmaa@gmail.com>
2019-05-06 18:27:41 -0400

    Add links to WoW, DOB BIS. Fixes #583.

--- onboarding/admin.py
+++ onboarding/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
 from .models import OnboardingInfo, AddressWithoutBoroughDiagnostic
+from project.util.admin_util import admin_field
 
 
 class OnboardingInline(admin.StackedInline):
     model = OnboardingInfo
     verbose_name = "Onboarding info"
     verbose_name_plural = verbose_name
+
+    readonly_fields = ['building_links']
+
+    @admin_field(short_description='Building links', allow_tags=True)
+    def building_links(self, obj: OnboardingInfo) -> str:
+        return obj.building_links_html
 
 
 @admin.register(AddressWithoutBoroughDiagnostic)
---
+++ project/util/hyperlink.py
@@ -0,0 +1,35 @@
+from typing import NamedTuple, List
+from django.utils.html import format_html
+from django.utils.text import SafeText
+
+
+class Hyperlink(NamedTuple):
+    '''
+    A hyperlink for embedding in HTML.
+    '''
+
+    # The text that will be hyperlinked.
+    name: str
+
+    # The URL to link to.
+    url: str
+
+    @property
+    def admin_button_html(self) -> str:
+        '''
+        HTML for embedding as a button in the Django admin UI.
+        '''
+
+        return format_html(
+            '<a href="{}" class="button" target="blank" rel="nofollow noopener">{}</a>',
+            self.url, self.name
+        )
+
+    @staticmethod
+    def join_admin_buttons(links: List['Hyperlink']) -> str:
+        '''
+        Joins together multiple Hyperlinks into a single HTML string consisting
+        of buttons.
+        '''
+
+        return SafeText(' '.join(map(lambda link: link.admin_button_html, links)))

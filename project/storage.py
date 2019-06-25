556231b5d1afb08f843d0754ba753f2ce489ed30
Atul Varma <varmaa@gmail.com>
2019-06-25 08:28:49 -0400

    Upgrade to whitenoise 4.1.2.

--- project/storage.py
+++ project/storage.py
@@ -1,31 +1,5 @@
 from django.conf import settings
-from django.contrib.staticfiles.storage import StaticFilesStorage
-from whitenoise.compress import Compressor
 from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class CompressedStaticFilesStorage(StaticFilesStorage):
-    '''
-    Attempts to compress the static files in a way that is
-    compatible with Whitenoise's mechanism for serving compressed
-    files.
-
-    For more details, see:
-
-        * https://github.com/evansd/whitenoise/issues/194
-        * https://code.djangoproject.com/ticket/29677
-    '''
-
-    def _compress_path(self, path, compressor):
-        if compressor.should_compress(path):
-            abspath = self.path(path)
-            for compressed_path in compressor.compress(abspath):
-                yield path, compressed_path, True
-
-    def post_process(self, paths, **options):
-        compressor = Compressor()
-        for path in paths:
-            yield from self._compress_path(path, compressor)
 
 
 class S3StaticFilesStorage(S3Boto3Storage):

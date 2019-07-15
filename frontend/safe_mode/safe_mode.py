f52c53bd6f568ccb621835ad15d3320d7e9cae42
Atul Varma <varmaa@gmail.com>
2019-07-15 11:18:56 -0400

    Exempt safe mode from CSRF checks. Fixes #746.

--- frontend/safe_mode/safe_mode.py
+++ frontend/safe_mode/safe_mode.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from django.http import HttpResponseRedirect, HttpRequest
 from django.urls import path
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 
 MY_DIR = Path(__file__).parent.resolve()
@@ -17,12 +18,22 @@ def is_enabled(request: HttpRequest) -> bool:
     return request.session.get(SESSION_KEY, False)
 
 
+# The page we're on might be dynamic and perform login/logout
+# without a page refresh, which would reset the CSRF token. Since
+# enabling/disabling safe mode doesn't meaningfully change the
+# server state in any way or impact the user experience in an
+# irrevocable way--i.e., exempting it from CSRF won't introduce
+# vulnerabilities--we're just going to avoid such issues by
+# disabling CSRF.
+
+@csrf_exempt
 @require_POST
 def enable(request: HttpRequest):
     request.session[SESSION_KEY] = True
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
 
+@csrf_exempt
 @require_POST
 def disable(request: HttpRequest):
     request.session[SESSION_KEY] = False

19185878bd2fe66de3d99e856f3211dfc323b2e7
Atul Varma <varmaa@gmail.com>
2019-07-17 07:19:57 -0400

    Sync URL querystring w/ search field value.

--- frontend/lib/pages/data-requests.tsx
+++ frontend/lib/pages/data-requests.tsx
@@ -1,26 +1,67 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { RouteComponentProps, Switch, Route, Redirect } from "react-router";
 import Page from '../page';
 import Routes from '../routes';
 import { FormSubmitter } from '../form-submitter';
 import { AppContext } from '../app-context';
 import { DataRequestMultiLandlordQuery } from '../queries/DataRequestMultiLandlordQuery';
-import { TextualFormField } from '../form-fields';
+import { TextualFormField, BaseFormFieldProps } from '../form-fields';
 import { NextButton } from '../buttons';
 import { createSimpleQuerySubmitHandler } from '../forms-graphql-simple-query';
+import { getQuerystringVar } from '../querystring';
+import { FormContext } from '../form-context';
+
+const QUERYSTRING_VAR = 'landlords';
+
+function SyncFieldWithQuerystring(props: {
+  currentQuery: string,
+  field: BaseFormFieldProps<string>,
+  ctx: FormContext<any>
+}) {
+  const [triggeredChange, setTriggeredChange] = useState(false);
+
+  // This effect detects when the current query in our URL has changed,
+  // and matches our search field to sync with it.
+  useEffect(() => {
+    if (props.field.value !== props.currentQuery) {
+      props.field.onChange(props.currentQuery);
+      setTriggeredChange(true);
+    }
+  }, [props.currentQuery]);
+
+  // This effect detects when our search field has caught up with our
+  // URL change, and immediately triggers a form submission.
+  useEffect(() => {
+    if (triggeredChange && props.field.value === props.currentQuery) {
+      props.ctx.submit();
+      setTriggeredChange(false);
+    }
+  }, [props.field.value]);
+
+  return null;
+}
+
+function maybePushHistory(router: RouteComponentProps, varName: string, newValue: string) {
+  const currentValue = getQuerystringVar(router, varName) || '';
+  if (currentValue !== newValue) {
+    router.history.push(router.location.pathname + `?${varName}=${encodeURIComponent(newValue)}`);
+  }
+}
 
 function MultiLandlordPage(props: RouteComponentProps) {
   const appCtx = useContext(AppContext);
   const [snippet, setSnippet] = useState('');
   const [lastSearch, setLastSearch] = useState('');
+  const currentQuery = getQuerystringVar(props, QUERYSTRING_VAR) || '';
   const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataRequestMultiLandlordQuery.fetch, input => {
     setLastSearch(input.landlords);
+    maybePushHistory(props, QUERYSTRING_VAR, input.landlords);
   });
   const lastSearchFragment = <>&ldquo;{lastSearch}&rdquo;</>;
 
   return <Page title="Multi-landlord data request" withHeading>
     <FormSubmitter
-      initialState={{landlords: ""}}
+      initialState={{landlords: currentQuery}}
       onSubmit={onSubmit}
       onSuccess={output => {
         const { simpleQueryOutput } = output;
@@ -28,17 +69,20 @@ function MultiLandlordPage(props: RouteComponentProps) {
       }}
     >
       {ctx => <>
-        <TextualFormField {...ctx.fieldPropsFor('landlords')} label="Landlords (comma-separated)" />
+        <SyncFieldWithQuerystring currentQuery={currentQuery} field={ctx.fieldPropsFor(QUERYSTRING_VAR)} ctx={ctx} />
+        <TextualFormField {...ctx.fieldPropsFor(QUERYSTRING_VAR)} label="Landlords (comma-separated)" />
         <NextButton label="Request data" isLoading={ctx.isLoading} />
+        {!ctx.isLoading && <>
+          <div className="content">
+            <br/>
+            {snippet ? <>
+              <h3>Query results for {lastSearchFragment}</h3>
+              <pre>{snippet}</pre>
+            </> : (lastSearch && <p>No results for {lastSearchFragment}.</p>)}
+          </div>
+        </>}
       </>}
     </FormSubmitter>
-    <br/>
-    <div className="content">
-      {snippet ? <>
-        <h3>Query results for {lastSearchFragment}</h3>
-        <pre>{snippet}</pre>
-      </> : (lastSearch && <p>No results for {lastSearchFragment}.</p>)}
-    </div>
   </Page>;
 }
 

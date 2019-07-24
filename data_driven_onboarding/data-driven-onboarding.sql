54a2e709b57489556285c127717a2403093a1c78
LC15 <clabiba97@gmail.com>
2019-07-24 11:47:08 -0400

    Update data-driven-onboarding.sql
    Adding subqueries for:
    finding the category of hpd complaints that is most common for the entered bbl.

--- data_driven_onboarding/data-driven-onboarding.sql
+++ data_driven_onboarding/data-driven-onboarding.sql
@@ -89,7 +89,31 @@ Avg_Wait_Time_For_Portfolio as(
         AVG(length_of_violation) as AverageWaitTimeForPortfolio
     from violation_lengths_for_portfolio
     group by Enteredbbl
+),
+
+Major_Complaint as (
+	select
+		majorcategory,
+		count(*) filter (where majorcategory is not null) as NumberOfComplaints
+	from public.hpd_complaint_problems as p
+		left join public.hpd_complaints h on p.complaintid =h.complaintid
+	where bbl= %(bbl)s
+	group by majorcategory
+	order by NumberOfComplaints desc
+	limit 1
+),
+
+Major_Complaint_With_BBL as (
+	select
+		majorcategory,
+		NumberOfComplaints,
+		case 
+			when majorcategory is not null then %(bbl)s
+			else %(bbl)s
+		end as bbl
+	from Major_Complaint
 )
+
 
 select
     -- zipcode for the entered bbl. 
@@ -145,7 +169,9 @@ select
 
     -- average wait time for repairs after a landlord has been notified of a violation. for the the entire associated portfolio
     -- may return null if unknown
-    P.AverageWaitTimeForPortfolio as average_wait_time_for_repairs_for_portfolio
+    P.AverageWaitTimeForPortfolio as average_wait_time_for_repairs_for_portfolio,
+    M.majorcategory as Most_Common_Category_Of_HPD_Complaint,
+	M.NumberOfComplaints as Number_Of_Complaints_Of_Most_Common_Category
 from Total_Res_Units T
     left join Count_HPD HPD on T.bbl=HPD.bbl
     left join Count_Open_HPD OpenHPD on T.bbl=OpenHPD.bbl
@@ -153,3 +179,4 @@ from Total_Res_Units T
     left join public.rentstab_summary R on T.bbl= R.ucbbl
     left join Avg_Wait_Time W on T.bbl= W.bbl
     left join Avg_Wait_Time_For_Portfolio P on T.bbl= P.bbl
+    left join Major_Complaint_With_BBL M on T.bbl= M.bbl

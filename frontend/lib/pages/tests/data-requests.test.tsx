9a037c3ea788891b7e41123f5b3570f58c6316bb
Atul Varma <varmaa@gmail.com>
2019-07-24 06:47:36 -0400

    Add some front-end tests.

--- frontend/lib/pages/tests/data-requests.test.tsx
+++ frontend/lib/pages/tests/data-requests.test.tsx
@@ -3,38 +3,7 @@ import { AppTesterPal } from "../../tests/app-tester-pal";
 import DataRequestsRoutes from "../data-requests";
 import Routes from '../../routes';
 import { DataRequestMultiLandlordQuery } from '../../queries/DataRequestMultiLandlordQuery';
-
-const SUPPRESSED_PREFIXES = [
-  "Warning: Do not await the result of calling ReactTestUtils.act(...)",
-  "Warning: An update to %s inside a test was not wrapped in act(...)",
-];
-
-function isSuppressedErrorMessage(message: string): boolean {
-  return SUPPRESSED_PREFIXES.some(sp => message.startsWith(sp));
-}
-
-/**
- * This is a workaround for the following issue:
- * 
- *   https://github.com/testing-library/react-testing-library/issues/281
- * 
- * It *should* be fixed in React 16.9, at which point we can remove this.
- */
-async function suppressSpuriousActErrors(cb: () => Promise<any>): Promise<any> {
-  const oldError = window.console.error;
-
-  window.console.error = (...args: any[]) => {
-    if (!isSuppressedErrorMessage(args[0])) {
-        oldError(...args);
-    }
-  };
-
-  try {
-    await cb();
-  } finally {
-    window.console.error = oldError;
-  }
-}
+import { suppressSpuriousActErrors } from '../../tests/react-act-workaround';
 
 describe('Data requests', () => {
   afterEach(AppTesterPal.cleanup);

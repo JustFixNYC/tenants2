c893cec74b9b8566d4c568448f334c9189426386
Atul Varma <varmaa@gmail.com>
2019-07-25 14:26:56 -0400

    Style ddo cards better (#774)

--- frontend/lib/tests/wow-link.tsx
+++ frontend/lib/tests/wow-link.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export function WhoOwnsWhatLink(props: {bbl: string, children: any}): JSX.Element {
+export function WhoOwnsWhatLink(props: {bbl: string, className?: string, children: any}): JSX.Element {
   return <a
     href={`https://whoownswhat.justfix.nyc/bbl/${props.bbl}`}
     target="_blank"
     rel="noopener noreferrer"
+    className={props.className}
   >{props.children}</a>;
 }

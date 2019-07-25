JustFixNYC/tenants2

commit 27d68208aabaf68faffb87e477c0bae8b405fe2e

Submit GET forms on mount if needed. Fixes #770.

frontend/lib/form-submitter.tsx
@@ -7,6 +7,7 @@ import autobind from 'autobind-decorator';
 import { areFieldsEqual } from './form-field-equality';
 import { ga } from './google-analytics';
 import { HistoryBlocker } from './history-blocker';
+import { isDeepEqual } from './util';
 
 export type FormSubmitterProps<FormInput, FormOutput extends WithServerFormFieldErrors> = {
   /**
@@ -64,6 +65,20 @@ export type FormSubmitterProps<FormInput, FormOutput extends WithServerFormField
    * at least one element; it should *never* be an empty array.
    */
   initialErrors?: FormErrors<FormInput>;
+
+  /**
+   * Whether to automatically submit the form as soon as the
+   * component is mounted. This can be useful for HTTP GET-based
+   * forms that don't have cached/pre-fetched results available.
+   */
+  submitOnMount?: boolean;
+
+  /**
+   * A representation of empty form input. If the form is
+   * ever submitted when in this state, we won't bother actually
+   * submitting the form, since we know nothing will happen.
+   */
+  emptyState?: FormInput;
 } & Pick<FormProps<FormInput>, 'idPrefix'|'initialState'|'children'|'extraFields'|'extraFormAttributes'>;
 
 /**
@@ -140,6 +155,9 @@ export class FormSubmitterWithoutRouter<FormInput, FormOutput extends WithServer
 
   @autobind
   handleSubmit(input: FormInput) {
+    if (this.props.emptyState && isDeepEqual(input, this.props.emptyState)) {
+      return;
+    }
     const submissionId = this.state.currentSubmissionId + 1;
     this.setState({
       isLoading: true,
@@ -192,6 +210,12 @@ export class FormSubmitterWithoutRouter<FormInput, FormOutput extends WithServer
     }).catch(e => {
       this.setState({ isLoading: false });
     });
+  }
+
+  componentDidMount() {
+    if (this.props.submitOnMount) {
+      this.handleSubmit(this.props.initialState);
+    }
   }
 
   componentWillUnmount() {

frontend/lib/pages/data-driven-onboarding.tsx
@@ -78,12 +78,12 @@ const ACTION_CARDS: ActionCardPropsCreator[] = [
     return {
       title: fullAddress,
       indicators: [
-        associatedBuildingCount && portfolioUnitCount && <p className="subtitle">
+        associatedBuildingCount && portfolioUnitCount && <>
           Your landlord owns <Indicator value={associatedBuildingCount} unit="building"/> and <Indicator value={portfolioUnitCount} unit="unit"/>.
-        </p>,
-        unitCount && <p className="subtitle">
+        </>,
+        unitCount && <>
           There <Indicator verb="is/are" value={unitCount} unit="unit" /> in your building.
-        </p>,  
+        </>,  
       ],
       cta: <WhoOwnsWhatLink bbl={bbl}>Learn more at Who Owns What</WhoOwnsWhatLink>
     };
@@ -172,8 +172,8 @@ function Results(props: {
 
 function DataDrivenOnboardingPage(props: RouteComponentProps) {
   const appCtx = useContext(AppContext);
-  const defaultState = {address: '', borough: ''};
-  const initialState = getInitialQueryInputFromQs(props, defaultState);
+  const emptyState = {address: '', borough: ''};
+  const initialState = getInitialQueryInputFromQs(props, emptyState);
   const [latestOutput, setLatestOutput] = useLatestQueryOutput(props, DataDrivenOnboardingSuggestions, initialState);
   const [autoSubmit, setAutoSubmit] = useState(false);
   const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataDrivenOnboardingSuggestions.fetch, input => {
@@ -183,6 +183,8 @@ function DataDrivenOnboardingPage(props: RouteComponentProps) {
 
   return <Page title="Data-driven onboarding prototype">
     <FormSubmitter
+      submitOnMount={latestOutput === undefined}
+      emptyState={emptyState}
       initialState={initialState}
       onSubmit={onSubmit}
       onSuccess={output => {
@@ -203,7 +205,7 @@ function DataDrivenOnboardingPage(props: RouteComponentProps) {
           ctx.fieldPropsFor('borough'),
         ]} ctx={ctx} />
         <NextButton label="Gimme some info" isLoading={ctx.isLoading} />
-        {!ctx.isLoading && <Results address={ctx.fieldPropsFor('address').value} output={latestOutput} />}
+        {!ctx.isLoading && latestOutput !== undefined && <Results address={ctx.fieldPropsFor('address').value} output={latestOutput} />}
       </>}
     </FormSubmitter>
   </Page>;

frontend/lib/pages/data-requests.tsx
@@ -74,8 +74,8 @@ function SearchResults({ output, query }: SearchResultsProps) {
 
 function MultiLandlordPage(props: RouteComponentProps) {
   const appCtx = useContext(AppContext);
-  const defaultState = { landlords: '' };
-  const initialState = getInitialQueryInputFromQs(props, defaultState);
+  const emptyState = { landlords: '' };
+  const initialState = getInitialQueryInputFromQs(props, emptyState);
   const [latestResults, setLatestResults] = useLatestQueryOutput(props, DataRequestMultiLandlordQuery, initialState);
   const [latestQuery, setLatestQuery] = useState(initialState.landlords);
   const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataRequestMultiLandlordQuery.fetch, input => {
@@ -86,6 +86,8 @@ function MultiLandlordPage(props: RouteComponentProps) {
 
   return <Page title="Multi-landlord data request" withHeading>
     <FormSubmitter
+      submitOnMount={latestResults === undefined}
+      emptyState={emptyState}
       initialState={initialState}
       onSubmit={onSubmit}
       onSuccess={output => setLatestResults(output && output.simpleQueryOutput) }
@@ -96,7 +98,7 @@ function MultiLandlordPage(props: RouteComponentProps) {
         ]} ctx={ctx} />
         <TextualFormField {...ctx.fieldPropsFor('landlords')} label="Landlords (comma-separated)" />
         <NextButton label="Request data" isLoading={ctx.isLoading} />
-        {!ctx.isLoading && <SearchResults output={latestResults} query={latestQuery} />}
+        {!ctx.isLoading && latestResults !== undefined && <SearchResults output={latestResults} query={latestQuery} />}
       </>}
     </FormSubmitter>
   </Page>;

frontend/lib/http-get-query-util.tsx
@@ -187,7 +187,7 @@ export function useLatestQueryOutput<Input, Output>(
 ) {
   const appCtx = useContext(AppContext);
   const qlp = new QueryLoaderPrefetcher(router, appCtx, query, initialState);
-  let initialResults: Output|null = null;
+  let initialResults: Output|undefined = undefined;
 
   qlp.maybeQueueForPrefetching();
 

commit 623b20f1a070de95676481dc3be4b700961d5a00

Improve blank form submission via caching (#773)
This fixes a regression introduced in #771 whereby navigating from a filled-out form to an empty form wouldn't clear the stale form output.

frontend/lib/form-submitter.tsx
@@ -7,7 +7,6 @@ import autobind from 'autobind-decorator';
 import { areFieldsEqual } from './form-field-equality';
 import { ga } from './google-analytics';
 import { HistoryBlocker } from './history-blocker';
-import { isDeepEqual } from './util';
 
 export type FormSubmitterProps<FormInput, FormOutput extends WithServerFormFieldErrors> = {
   /**
@@ -72,13 +71,6 @@ export type FormSubmitterProps<FormInput, FormOutput extends WithServerFormField
    * forms that don't have cached/pre-fetched results available.
    */
   submitOnMount?: boolean;
-
-  /**
-   * A representation of empty form input. If the form is
-   * ever submitted when in this state, we won't bother actually
-   * submitting the form, since we know nothing will happen.
-   */
-  emptyState?: FormInput;
 } & Pick<FormProps<FormInput>, 'idPrefix'|'initialState'|'children'|'extraFields'|'extraFormAttributes'>;
 
 /**
@@ -155,9 +147,6 @@ export class FormSubmitterWithoutRouter<FormInput, FormOutput extends WithServer
 
   @autobind
   handleSubmit(input: FormInput) {
-    if (this.props.emptyState && isDeepEqual(input, this.props.emptyState)) {
-      return;
-    }
     const submissionId = this.state.currentSubmissionId + 1;
     this.setState({
       isLoading: true,

frontend/lib/forms-graphql-simple-query.tsx
@@ -1,5 +1,6 @@
 import { GraphQLFetch } from "./graphql-client";
 import { WithServerFormFieldErrors } from "./form-errors";
+import { isDeepEqual } from "./util";
 
 export interface FetchSimpleQuery<FormInput, FormOutput> {
   (fetch: GraphQLFetch, args: FormInput): Promise<{ output: FormOutput }>;
@@ -8,6 +9,14 @@ export interface FetchSimpleQuery<FormInput, FormOutput> {
 export interface FetchSimpleQueryInfo<FormInput, FormOutput> {
   graphQL: string;
   fetch: FetchSimpleQuery<FormInput, FormOutput>;
+};
+
+type SimpleQuerySubmitHandlerOptions<FormInput, FormOutput> = {
+  /** A cache to consult that allows us to bypass the network. */
+  cache: [FormInput, FormOutput][],
+
+  /** A callback that's triggered whenever our form is submitted. */
+  onSubmit: (input: FormInput) => void
 };
 
 /**
@@ -22,10 +31,22 @@ export interface FetchSimpleQueryInfo<FormInput, FormOutput> {
 export function createSimpleQuerySubmitHandler<FormInput, FormOutput>(
   fetchImpl: GraphQLFetch,
   fetchQuery: FetchSimpleQuery<FormInput, FormOutput>,
-  onSubmit?: (input: FormInput) => void
+  options?: Partial<SimpleQuerySubmitHandlerOptions<FormInput, FormOutput>>,
 ): (input: FormInput) => Promise<{ simpleQueryOutput: FormOutput } & WithServerFormFieldErrors> {
+  const defaultOptions: SimpleQuerySubmitHandlerOptions<FormInput, FormOutput> = {
+    cache: [],
+    onSubmit: () => {}
+  };
+  const {onSubmit, cache} = Object.assign(defaultOptions, options || {});
   return async (input: FormInput) => {
-    if (onSubmit) onSubmit(input);
+    onSubmit(input);
+
+    for (let [cachedInput, cachedOutput] of cache) {
+      if (isDeepEqual(input, cachedInput)) {
+        return { simpleQueryOutput: cachedOutput, errors: [] };
+      }
+    }
+
     const result = await fetchQuery(fetchImpl, input);
 
     return { simpleQueryOutput: result.output, errors: [] };

frontend/lib/pages/data-driven-onboarding.tsx
@@ -176,15 +176,17 @@ function DataDrivenOnboardingPage(props: RouteComponentProps) {
   const initialState = getInitialQueryInputFromQs(props, emptyState);
   const [latestOutput, setLatestOutput] = useLatestQueryOutput(props, DataDrivenOnboardingSuggestions, initialState);
   const [autoSubmit, setAutoSubmit] = useState(false);
-  const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataDrivenOnboardingSuggestions.fetch, input => {
-    setAutoSubmit(false);
-    maybePushQueryInputToHistory(props, input);
+  const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataDrivenOnboardingSuggestions.fetch, {
+    cache: [[emptyState, null]],
+    onSubmit(input) {
+      setAutoSubmit(false);
+      maybePushQueryInputToHistory(props, input);
+    }
   });
 
   return <Page title="Data-driven onboarding prototype">
     <FormSubmitter
       submitOnMount={latestOutput === undefined}
-      emptyState={emptyState}
       initialState={initialState}
       onSubmit={onSubmit}
       onSuccess={output => {

frontend/lib/pages/data-requests.tsx
@@ -78,16 +78,18 @@ function MultiLandlordPage(props: RouteComponentProps) {
   const initialState = getInitialQueryInputFromQs(props, emptyState);
   const [latestResults, setLatestResults] = useLatestQueryOutput(props, DataRequestMultiLandlordQuery, initialState);
   const [latestQuery, setLatestQuery] = useState(initialState.landlords);
-  const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataRequestMultiLandlordQuery.fetch, input => {
-    setLatestResults(null);
-    setLatestQuery(input.landlords);
-    maybePushQueryInputToHistory(props, input);
+  const onSubmit = createSimpleQuerySubmitHandler(appCtx.fetch, DataRequestMultiLandlordQuery.fetch, {
+    cache: [[emptyState, null]],
+    onSubmit(input) {
+      setLatestResults(null);
+      setLatestQuery(input.landlords);
+      maybePushQueryInputToHistory(props, input);
+    }
   });
 
   return <Page title="Multi-landlord data request" withHeading>
     <FormSubmitter
       submitOnMount={latestResults === undefined}
-      emptyState={emptyState}
       initialState={initialState}
       onSubmit={onSubmit}
       onSuccess={output => setLatestResults(output && output.simpleQueryOutput) }

frontend/lib/pages/tests/data-driven-onboarding.test.tsx
@@ -17,13 +17,14 @@ async function simulateResponse(response: Partial<DataDrivenOnboardingSuggestion
   const pal = new AppTesterPal(<DataDrivenOnboardingRoutes/>, {
     url: Routes.locale.dataDrivenOnboarding
   });
-  fetch.mockReturnJson(FakeGeoResults);
-  pal.fillFormFields([[/address/i, '150 cou']]);
-  await fetch.resolvePromisesAndTimers();
-  pal.clickListItem(/150 COURT STREET/);
-  pal.clickButtonOrLink(/gimme some info/i);
-  pal.expectGraphQL(/ddoSuggestions/);
   await suppressSpuriousActErrors(async () => {
+    await pal.nextTick();
+    fetch.mockReturnJson(FakeGeoResults);
+    pal.fillFormFields([[/address/i, '150 cou']]);
+    await fetch.resolvePromisesAndTimers();
+    pal.clickListItem(/150 COURT STREET/);
+    pal.clickButtonOrLink(/gimme some info/i);
+    pal.expectGraphQL(/ddoSuggestions/);
     pal.getFirstRequest().resolve({output});
     await pal.nextTick();
   });

frontend/lib/pages/tests/data-requests.test.tsx
@@ -12,6 +12,7 @@ describe('Data requests', () => {
     const pal = new AppTesterPal(<DataRequestsRoutes/>, {
       url: Routes.locale.dataRequests.multiLandlord
     });
+    await pal.nextTick();
     pal.fillFormFields([[/landlords/i, "Boop Jones"]]);
     pal.clickButtonOrLink(/request data/i);
 
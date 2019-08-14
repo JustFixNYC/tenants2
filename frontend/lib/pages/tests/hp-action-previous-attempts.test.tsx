b569bf5a48121065df61778f256decaa882282d8
Atul Varma <varmaa@gmail.com>
2019-08-14 10:55:35 -0400

    Pause for more modal focusing.

--- frontend/lib/pages/tests/hp-action-previous-attempts.test.tsx
+++ frontend/lib/pages/tests/hp-action-previous-attempts.test.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { AppTesterPal } from "../../tests/app-tester-pal";
 import HPActionRoutes from '../../hp-action';
+import { pauseForModalFocus } from '../../tests/util';
 
 describe('HP Action flow', () => {
   afterEach(AppTesterPal.cleanup);
 
-  it('should show 311 modal', () => {
+  it('should show 311 modal', async () => {
     const pal = new AppTesterPal(<HPActionRoutes />, {
       url: '/hp/previous-attempts/311-modal',
     });
     pal.rr.getByText('311 is an important tool');
+    await pauseForModalFocus();
   });
 });
--- frontend/lib/pages/tests/onboarding-step-2.test.tsx
+++ frontend/lib/pages/tests/onboarding-step-2.test.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import OnboardingStep2 from '../onboarding-step-2';
 import { AppTesterPal } from '../../tests/app-tester-pal';
 import Routes from '../../routes';
+import { pauseForModalFocus } from '../../tests/util';
 
 
 describe('onboarding step 2 page', () => {
   afterEach(AppTesterPal.cleanup);
 
-  it('opens eviction modal', () => {
+  it('opens eviction modal', async () => {
     const pal = new AppTesterPal(<OnboardingStep2 routes={Routes.locale.onboarding} />);
     const getDialog = () => pal.getDialogWithLabel(/You need legal help/i);
 
     // When we enable the checkbox, the dialog should show.
     pal.clickRadioOrCheckbox(/I received an eviction notice/i);
     getDialog();
+    await pauseForModalFocus();
     pal.clickButtonOrLink("Continue with letter");
   });
 });

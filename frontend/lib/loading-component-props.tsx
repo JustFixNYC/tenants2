f63056ad67c3ea3480a69eda7d808f4351c0606d
Atul Varma <varmaa@gmail.com>
2019-08-14 07:49:26 -0400

    Rename MinimalLoadingComponentProps -> RetryableLoadingComponentProps.

--- frontend/lib/loading-component-props.tsx
+++ frontend/lib/loading-component-props.tsx
@@ -1,10 +1,13 @@
 /**
- * These props are a subset of the ones react-loadable takes.
- * As such, components that use them can be passed as a 'loading'
- * option to react-loadable's Loadable HOC factory function, but
- * they can also be used with other code.
+ * These props can be used for any component that presents
+ * both a loading interstital and, optionally, a "retry"
+ * button to retry the loading if any network errors occur.
+ * 
+ * Historical note: these props were originally a subset of the
+ * ones react-loadable took, but we've since migrated away from
+ * react-loadable because it was unmaintained.
  */
-export type MinimalLoadingComponentProps = {
+export type RetryableLoadingComponentProps = {
   /** The exception that occurred while loading, if any. */
   error: any;
 

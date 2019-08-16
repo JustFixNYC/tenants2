88bfef3fb8cc5ed57112b880dfdd10361e7624b5
Atul Varma <varmaa@gmail.com>
2019-08-16 18:48:54 -0400

    Remove boop task.

--- project/tasks.py
+++ project/tasks.py
@@ -3,13 +3,4 @@ from celery import shared_task
 from project import slack
 
 
-print("HELLO FROM TASK-LAND")
-
-
-@shared_task
-def boop(x):
-    print(f"BOOP {x}")
-    return x + 1
-
-
 shared_task(ignore_result=True)(slack.sendmsg)

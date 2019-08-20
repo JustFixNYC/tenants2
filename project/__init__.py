2c1c208aea0020d739a1516f59e50ec246cc8503
Atul Varma <varmaa@gmail.com>
2019-08-20 09:10:10 -0400

    Argh make project importable w/o dependencies.

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,19 @@
-from .celery import app as celery_app
+# Ugh, we've always wanted this package to be importable without requiring
+# any dependencies, but Celery really wants us to import our app here, so
+# we're going to compromise by first checking to see if Celery is even
+# available. If it is, we'll go ahead and import our app, otherwise we
+# won't.
+#
+# Sigh.
 
-__all__ = ('celery_app',)
+_celery_available = True
+
+try:
+    import celery  # NOQA
+except ModuleNotFoundError:
+    _celery_available = False
+
+if _celery_available:
+    from .celery import app as celery_app
+
+    __all__ = ('celery_app',)

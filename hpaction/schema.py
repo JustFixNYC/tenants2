4e434424ca8ac5b125f319023c0a8c992fa7818e
Atul Varma <varmaa@gmail.com>
2019-08-23 18:59:49 -0400

    Use Celery for HP action PDF processing (#826)

--- hpaction/schema.py
+++ hpaction/schema.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from threading import Thread
 import graphene
 from graphene_django.types import DjangoObjectType
 from graphql import ResolveInfo
@@ -8,8 +7,7 @@ from django.forms import inlineformset_factory
 
 from users.models import JustfixUser
 from project.util.session_mutation import SessionFormMutation
-from project.util.site_util import absolute_reverse
-from project import slack, schema_registry
+from project import schema_registry
 from project.util.model_form_util import (
     ManyToOneUserModelFormMutation,
     OneToOneUserModelFormMutation,
@@ -17,47 +15,7 @@ from project.util.model_form_util import (
     create_models_for_user_resolver
 )
 from .models import HPUploadStatus, COMMON_DATA
-from . import models, forms
-from .build_hpactionvars import user_to_hpactionvars
-from .hpactionvars import HPActionVariables
-from . import lhiapi
-
-
-class GetAnswersAndDocumentsThread(Thread):
-    '''
-    A separate thread that attempts to generate the user's HP Action packet.
-    Ideally this should be done in a worker queue but we don't have
-    time to set one up right now, and this should take about a minute
-    anyways, so it's unlikely that Heroku (or whatever else is running us)
-    will kill us before we're done.
-    '''
-
-    def __init__(self, token_id: str, hdinfo: HPActionVariables) -> None:
-        super().__init__()
-        self.token_id = token_id
-        self.hdinfo = hdinfo
-
-    def run(self) -> None:
-        token = models.UploadToken.objects.find_unexpired(self.token_id)
-        assert token is not None
-        user = token.user
-        docs = lhiapi.get_answers_and_documents(token, self.hdinfo)
-        if docs is not None:
-            user.send_sms(
-                f"JustFix.nyc here! Follow this link to your completed "
-                f"HP Action legal forms. You will need to print these "
-                f"papers before bringing them to court! "
-                f"{absolute_reverse('hpaction:latest_pdf')}",
-                fail_silently=True
-            )
-            slack.sendmsg_async(
-                f"{slack.hyperlink(text=user.first_name, href=user.admin_url)} "
-                f"has generated HP Action legal forms!",
-                is_safe=True
-            )
-
-
-GET_ANSWERS_AND_DOCUMENTS_ASYNC = True
+from . import models, forms, lhiapi
 
 
 @schema_registry.register_mutation
@@ -70,13 +28,8 @@ class GenerateHpActionPdf(SessionFormMutation):
     @classmethod
     def perform_mutate(cls, form: forms.GeneratePDFForm, info: ResolveInfo):
         user = info.context.user
-        hdinfo = user_to_hpactionvars(user)
         token = models.UploadToken.objects.create_for_user(user)
-        thread = GetAnswersAndDocumentsThread(token.id, hdinfo)
-        if GET_ANSWERS_AND_DOCUMENTS_ASYNC:
-            thread.start()
-        else:
-            thread.run()
+        lhiapi.async_get_answers_and_documents_and_notify(token.id)
         return cls.mutation_success()
 
 

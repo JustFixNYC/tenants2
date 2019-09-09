8940e02b7082e636cbef498c297a400978b3593f
Sam Raby <sraby@users.noreply.github.com>
2019-09-09 12:57:17 -0400

    Added dimensions to all staticImage components (#845)
    Using Bulma Image Classes, this fixes an issue where static images on Firefox would create jolts in the formatting before/after loading.

--- frontend/lib/bulma.ts
+++ frontend/lib/bulma.ts
@@ -6,6 +6,32 @@ import classnames from 'classnames';
  * If this gets really annoying to maintain, we can just change this
  * type to be a string.
  */
+
+export type BulmaImageClass = 
+  'is-128x128' | 
+  'is-16by9' | 
+  'is-16x16' | 
+  'is-1by1' | 
+  'is-1by2' | 
+  'is-1by3' | 
+  'is-24x24' | 
+  'is-2by1' | 
+  'is-2by3' | 
+  'is-32x32' | 
+  'is-3by1' | 
+  'is-3by2' | 
+  'is-3by4' | 
+  'is-3by5' | 
+  'is-48x48' | 
+  'is-4by3' | 
+  'is-4by5' | 
+  'is-5by3' | 
+  'is-5by4' | 
+  'is-64x64' | 
+  'is-96x96' | 
+  'is-9by16' | 
+  'is-square';
+ 
 export type BulmaClassName =
   'is-active' |
   'is-primary' |
@@ -25,7 +51,7 @@ export type BulmaClassName =
   'textarea' |
   'navbar-burger' |
   'navbar-menu' |
-  'navbar-item';
+  'navbar-item' | BulmaImageClass;
 
 type BulmaClassNameMap = {
   [K in BulmaClassName]?: boolean;
--- frontend/lib/static-image.tsx
+++ frontend/lib/static-image.tsx
@@ -1,18 +1,23 @@
 import React, { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
 import { AppContext } from './app-context';
+import { BulmaImageClass } from './bulma';
 
 type ImgProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
 export type StaticImageProps = ImgProps & {
   src: string;
   alt: string;
+  ratio: BulmaImageClass;
 };
 
 export function StaticImage(props: StaticImageProps): JSX.Element {
+  const {ratio, ...imgProps} = props;
   return (
     <AppContext.Consumer>
       {(appCtx) => (
-        <img {...props} src={`${appCtx.server.staticURL}${props.src}`} />
+        <figure className={"image " + props.ratio}>
+          <img {...imgProps} src={`${appCtx.server.staticURL}${props.src}`} />
+        </figure>
       )}
     </AppContext.Consumer>
   );

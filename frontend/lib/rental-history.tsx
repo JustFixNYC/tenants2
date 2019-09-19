f5571a018a8710eb4cc08ed2fab8871616d5fe3f
Sam Raby <Sam@Sams-MBP-4.fios-router.home>
2019-09-19 16:08:08 -0400

    added scaffolding for confirmation page

--- frontend/lib/rental-history.tsx
+++ frontend/lib/rental-history.tsx
@@ -81,7 +81,8 @@ function RentalHistoryForm(): JSX.Element {
 function RentalHistoryPreview(): JSX.Element {
   return (
     <Page title="Preview your email to the DHCR:">
-      <h1 className="title is-4">Preview your email to the DHCR.</h1>
+      <h1 className="title is-4">Review your email to the DHCR</h1>
+      <p>Here is a preview of your email to request your rental history. It includes your address and apartment number so that the DHCR can mail you.</p>
       <article className="message">
         <div className="message-header">
           <p className="has-text-weight-normal">To: New York DHCR &#60;rentinfo@nyshcr.org&#62;</p>
@@ -91,7 +92,7 @@ function RentalHistoryPreview(): JSX.Element {
             <div className="is-divider jf-divider-narrow" />
           <p>DHCR administrator,</p>
             <br />
-          <p>I, YOUR NAME HERE, am currently renting YOUR ADDRESS, APARTMENT NUMBER, BOROUGH, ZIPCODE and would like to request the complete rent history for this apartment back to the year 1984.</p>
+          <p>I, @contact.name, am currently living at @extra.features.0.properties.label in apartment @flow.contact_apt.text and would like to request the complete rent history for this apartment back to the year 1984.</p>
             <br />
           <p>Thank you,</p>
             <br />
@@ -104,10 +105,14 @@ function RentalHistoryPreview(): JSX.Element {
 
 function RentalHistoryConfirmation(): JSX.Element {
   return (
-    <Page title="Sue your landlord for Repairs and/or Harassment through an HP Action proceeding" withHeading="big" className="content">
-      <p>Hello BOOP!</p>
-      <p>An <strong>HP Action</strong> is a legal case you can bring against your landlord for failing to make repairs, not providing essential services, or harassing you.</p>
-      <p><em>This service is free, secure, and confidential.</em></p>
+    <Page title="Your rental history has been requested!" withHeading="big" className="content">
+      <h1 className="title is-4">What happens next?</h1>
+      <p>You should receive your rental history in the mail in about a week. If you have more questions, please email us at <a href="mailto:support@justfix.nyc" target="_blank" rel="noopener noreferrer">support@justfix.nyc</a>.</p>
+      <h1 className="title is-4">Want to read more about your rights?</h1>
+      <ul>
+        <li><a href="http://metcouncilonhousing.org/help_and_answers" target="_blank">MetCouncil on Housing</a></li>
+        <li><a href="http://housingcourtanswers.org/glossary/" target="_blank">Housing Court Answers</a></li>
+      </ul>
     </Page>
   );
 }

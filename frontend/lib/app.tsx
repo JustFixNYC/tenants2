a16b4e3e3ace38e97272c524ba686f733e6be09e
Atul Varma <varmaa@gmail.com>
2019-10-18 10:17:24 -0400

    Remove unused import.

--- frontend/lib/app.tsx
+++ frontend/lib/app.tsx
@@ -1,7 +1,7 @@
 import React, { RefObject } from 'react';
 import ReactDOM from 'react-dom';
 import autobind from 'autobind-decorator';
-import { BrowserRouter, Switch, Route, RouteComponentProps, withRouter, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, RouteComponentProps, withRouter } from 'react-router-dom';
 import loadable, { loadableReady } from '@loadable/component';
 
 import GraphQlClient from './graphql-client';

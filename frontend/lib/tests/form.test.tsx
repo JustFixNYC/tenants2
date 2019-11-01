96d613cf8abcdeeda71e070d60acc59690ccd06b
Atul Varma <varmaa@gmail.com>
2019-11-01 19:23:11 -0400

    Convert form.test.tsx to RTL.

--- frontend/lib/tests/form.test.tsx
+++ frontend/lib/tests/form.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { MyFormInput, myInitialState, renderMyFormFields } from "./my-form";
 import { BaseFormProps, Form } from "../form";
-import { mount } from "enzyme";
 import { FormErrors } from "../form-errors";
 import { simpleFormErrors } from "./util";
+import ReactTestingLibraryPal from './rtl-pal';
 
 describe('Form', () => {
   type MyFormProps = {
@@ -18,12 +18,17 @@ describe('Form', () => {
     );
   }
 
+  afterEach(ReactTestingLibraryPal.cleanup);
+
   it('submits field values', () => {
     const mockSubmit = jest.fn();
-    const form = mount(<MyForm onSubmit={mockSubmit} isLoading={false} />);
-    form.find('input[name="phoneNumber"]').simulate('change', { target: { value: '5551234567' } });
-    form.find('input[name="password"]').simulate('change', { target: { value: 'test123' } });
-    form.simulate('submit');
+    const pal = new ReactTestingLibraryPal(<MyForm onSubmit={mockSubmit} isLoading={false} />);
+    pal.fillFormFields([
+      ['Phone number', '5551234567'],
+      ['Password', 'test123']
+    ]);
+    const wasDefaultPrevented = !pal.rt.fireEvent.submit(pal.getElement('form'));
+    expect(wasDefaultPrevented).toBe(true);
     expect(mockSubmit.mock.calls.length).toBe(1);
     expect(mockSubmit.mock.calls[0][0]).toEqual({
       phoneNumber: '5551234567',
@@ -38,16 +43,17 @@ describe('Form', () => {
         phoneNumber: simpleFormErrors('bar')
       }
     };
-    const form = mount(<MyForm onSubmit={jest.fn()} errors={errors} isLoading={false} />);
-    const html = form.html();
+    const pal = new ReactTestingLibraryPal(<MyForm onSubmit={jest.fn()} errors={errors} isLoading={false} />);
+    const html = pal.rr.container.innerHTML;
     expect(html).toContain('foo');
     expect(html).toContain('bar');
   });
 
   it('does not trigger submissions when already loading', () => {
     const mockSubmit = jest.fn();
-    const form = mount(<MyForm onSubmit={mockSubmit} isLoading={true} />);
-    form.simulate('submit');
+    const pal = new ReactTestingLibraryPal(<MyForm onSubmit={mockSubmit} isLoading={true} />);
+    const wasDefaultPrevented = !pal.rt.fireEvent.submit(pal.getElement('form'));
+    expect(wasDefaultPrevented).toBe(true);
     expect(mockSubmit.mock.calls.length).toBe(0);
   });
 });

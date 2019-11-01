33dad0731947dba5b5744db0443ae218aa10f324
Atul Varma <varmaa@gmail.com>
2019-11-01 19:13:29 -0400

    Convert navbar.test.tsx to use RTL.

--- frontend/lib/tests/navbar.test.tsx
+++ frontend/lib/tests/navbar.test.tsx
@@ -1,74 +1,65 @@
 import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
 import Navbar from '../navbar';
 import { FakeDebugAppContext } from './util';
 import { MemoryRouter } from 'react-router';
 import { AppContext } from '../app-context';
-
+import ReactTestingLibraryPal from './rtl-pal';
+import { assertNotNull } from '../util';
 
 describe('Navbar', () => {
-  let navbar: ReactWrapper;
-  let burger: ReactWrapper;
-  let devDropdown: ReactWrapper;
+  afterEach(ReactTestingLibraryPal.cleanup);
 
-  beforeEach(() => {
-    navbar = mount(
+  const createNavbar = () => {
+    const pal = new ReactTestingLibraryPal(
       <MemoryRouter>
         <AppContext.Provider value={FakeDebugAppContext}>
           <Navbar/>
         </AppContext.Provider>
       </MemoryRouter>
     );
-    setLocals();
-  });
-
-  function updateNavBar() {
-    navbar.update();
-
-    // We need to rebind our locals, because the component
-    // tree has changed.
-    setLocals();
-  }
-
-  function click(target: ReactWrapper) {
-    target.simulate('click');
-
-    // Because our event handlers asynchronously update the state,
-    // we need to force an update.
-    updateNavBar();
-  }
-
-  function setLocals() {
-    burger = navbar.find('a.navbar-burger');
-    devDropdown = navbar.find('.navbar-item.has-dropdown').last();
-  }
+    return {
+      pal,
+      click: pal.rt.fireEvent.click,
+      get burger() {
+        return pal.getElement('a', '.navbar-burger');
+      },
+      get devDropdown() {
+        return assertNotNull(pal.rr.container.querySelector('.navbar-item.has-dropdown'));
+      },
+      get devDropdownLink() {
+        return assertNotNull(this.devDropdown.querySelector('a.navbar-link'));
+      }
+    };
+  };
 
   it('toggles burger when clicked', () => {
-    expect(burger.hasClass('is-active')).toBe(false);
+    const {burger, click} = createNavbar();
+    expect(burger.classList.contains('is-active')).toBe(false);
     click(burger);
-    expect(burger.hasClass('is-active')).toBe(true);
+    expect(burger.classList.contains('is-active')).toBe(true);
     click(burger);
-    expect(burger.hasClass('is-active')).toBe(false);
+    expect(burger.classList.contains('is-active')).toBe(false);
   });
 
   it('toggles dev dropdown when clicked', () => {
-    expect(devDropdown.hasClass('is-active')).toBe(false);
-    click(devDropdown.find('a.navbar-link'));
-    expect(devDropdown.hasClass('is-active')).toBe(true);
-    click(devDropdown.find('a.navbar-link'));
-    expect(devDropdown.hasClass('is-active')).toBe(false);
+    const {devDropdown, devDropdownLink, click} = createNavbar();
+    expect(devDropdown.classList.contains('is-active')).toBe(false);
+    click(devDropdownLink);
+    expect(devDropdown.classList.contains('is-active')).toBe(true);
+    click(devDropdownLink);
+    expect(devDropdown.classList.contains('is-active')).toBe(false);
   });
 
   it('collapses when focus moves outside the navbar', () => {
-    click(burger);
-    expect(burger.hasClass('is-active')).toBe(true);
+    const navbar = createNavbar();
+    navbar.click(navbar.burger);
+    expect(navbar.burger.classList.contains('is-active')).toBe(true);
 
     const btn = document.createElement('button');
     document.body.appendChild(btn);
     try {
       btn.focus();
-      updateNavBar();
-      expect(burger.hasClass('is-active')).toBe(false);
+      expect(navbar.burger.classList.contains('is-active')).toBe(false);
     } finally {
       document.body.removeChild(btn);
     }

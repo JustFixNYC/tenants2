e81a35f7905ca6d57648b2c9bfa87fc1462fdeea
Atul Varma <varmaa@gmail.com>
2019-11-01 20:04:49 -0400

    Partially migrate form-submitter.test.tsx.

--- frontend/lib/tests/form-submitter.test.tsx
+++ frontend/lib/tests/form-submitter.test.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
 import { MyFormInput, MyFormOutput, myInitialState, renderMyFormFields } from './my-form';
 import { createTestGraphQlClient, simpleFormErrors } from './util';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { FormSubmitterWithoutRouter, FormSubmitter } from '../form-submitter';
 import { MemoryRouter, Switch, Route } from 'react-router';
+import ReactTestingLibraryPal from './rtl-pal';
+
+describe('FormSubmitter (RTL)', () => {
+  afterEach(ReactTestingLibraryPal.cleanup);
+
+  it('optionally uses performRedirect() for redirection', async () => {
+    const promise = Promise.resolve({ errors: [] });
+    const performRedirect = jest.fn();
+    const pal = new ReactTestingLibraryPal(
+      <MemoryRouter>
+        <Switch>
+          <Route>
+            <FormSubmitter
+              onSubmit={() => promise}
+              onSuccess={() => {}}
+              onSuccessRedirect="/blah"
+              performRedirect={performRedirect}
+              initialState={myInitialState}
+              children={renderMyFormFields} />
+          </Route>
+        </Switch>
+      </MemoryRouter>
+    );
+    pal.rt.fireEvent.submit(pal.getElement('form'));
+    await promise;
+    expect(performRedirect.mock.calls).toHaveLength(1);
+    expect(performRedirect.mock.calls[0][0]).toBe('/blah');
+  });
+
+  it('optionally calls onSuccess(), then redirects when successful', async () => {
+    const promise = Promise.resolve({ errors: [] });
+    const glob = { word: "foo" };
+    const BlahPage = () => <p>This is {glob.word}.</p>;
+    const pal = new ReactTestingLibraryPal(
+      <MemoryRouter>
+        <Switch>
+          <Route path="/blah" exact component={BlahPage}/>
+          <Route>
+            <FormSubmitter
+              onSubmit={() => promise}
+              onSuccess={() => { glob.word = "blah"; }}
+              onSuccessRedirect="/blah"
+              initialState={myInitialState}
+              children={renderMyFormFields} />
+          </Route>
+        </Switch>
+      </MemoryRouter>
+    );
+    pal.rt.fireEvent.submit(pal.getElement('form'));
+    await promise;
+    expect(pal.rr.container.innerHTML).toBe('<p>This is blah.</p>');
+  });
+});
 
 describe('FormSubmitter', () => {
   const payload: MyFormInput = { phoneNumber: '1', password: '2' };
@@ -27,54 +80,6 @@ describe('FormSubmitter', () => {
     const form = wrapper.instance() as FormSubmitterWithoutRouter<MyFormInput, MyFormOutput>;
     return { form, client, onSuccess };
   };
-
-  it('optionally uses performRedirect() for redirection', async () => {
-    const promise = Promise.resolve({ errors: [] });
-    const performRedirect = jest.fn();
-    const wrapper = mount(
-      <MemoryRouter>
-        <Switch>
-          <Route>
-            <FormSubmitter
-              onSubmit={() => promise}
-              onSuccess={() => {}}
-              onSuccessRedirect="/blah"
-              performRedirect={performRedirect}
-              initialState={myInitialState}
-              children={renderMyFormFields} />
-          </Route>
-        </Switch>
-      </MemoryRouter>
-    );
-    wrapper.find('form').simulate('submit');
-    await promise;
-    expect(performRedirect.mock.calls).toHaveLength(1);
-    expect(performRedirect.mock.calls[0][0]).toBe('/blah');
-  });
-
-  it('optionally calls onSuccess(), then redirects when successful', async () => {
-    const promise = Promise.resolve({ errors: [] });
-    const glob = { word: "foo" };
-    const BlahPage = () => <p>This is {glob.word}.</p>;
-    const wrapper = mount(
-      <MemoryRouter>
-        <Switch>
-          <Route path="/blah" exact component={BlahPage}/>
-          <Route>
-            <FormSubmitter
-              onSubmit={() => promise}
-              onSuccess={() => { glob.word = "blah"; }}
-              onSuccessRedirect="/blah"
-              initialState={myInitialState}
-              children={renderMyFormFields} />
-          </Route>
-        </Switch>
-      </MemoryRouter>
-    );
-    wrapper.find('form').simulate('submit');
-    await promise;
-    expect(wrapper.html()).toBe('<p>This is blah.</p>');
-  });
 
   it('sets state when successful', async () => {
     const { form, client, onSuccess } = buildForm();

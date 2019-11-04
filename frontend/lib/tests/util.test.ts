4f36543e035f2c3612493b7f63bd66b33a67262c
Atul Varma <varmaa@gmail.com>
2019-11-04 09:56:43 -0500

    Use getHTMLElement() from @justfixnyc/util. Fixes #915.

--- frontend/lib/tests/util.test.ts
+++ frontend/lib/tests/util.test.ts
@@ -1,23 +1,4 @@
-import { getElement, assertNotNull, dateAsISO, addDays, friendlyDate, callOnceWithinMs, getFunctionProperty, exactSubsetOrDefault, assertNotUndefined, twoTuple, isDeepEqual, properNoun, numberWithCommas } from '../util';
-
-describe('getElement()', () => {
-  it('throws error when element not found', () => {
-    expect(() => getElement('div', '#blarg'))
-    .toThrow('Couldn\'t find any elements matching "div#blarg"');
-  });
-
-  it('returns element when found', () => {
-    const div = document.createElement('div');
-    div.id = 'blarg';
-    document.body.appendChild(div);
-
-    try {
-      expect(getElement('div', '#blarg')).toBe(div);
-    } finally {
-      document.body.removeChild(div);
-    }
-  });
-});
+import { assertNotNull, dateAsISO, addDays, friendlyDate, callOnceWithinMs, getFunctionProperty, exactSubsetOrDefault, assertNotUndefined, twoTuple, isDeepEqual, properNoun, numberWithCommas } from '../util';
 
 describe('properNoun()', () => {
   it('works', () => {

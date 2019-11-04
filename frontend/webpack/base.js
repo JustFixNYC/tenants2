9ab48f0f07a58317254088497df87ea13bc1bfc8
Atul Varma <varmaa@gmail.com>
2019-11-04 07:04:35 -0500

    Enable nullish coalescing and use it.

--- frontend/webpack/base.js
+++ frontend/webpack/base.js
@@ -72,6 +72,7 @@ const baseBabelOptions = {
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
     "@loadable/babel-plugin",
   ]
 };

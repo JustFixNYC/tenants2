ed507fdcaf21f909487e6b410ca6596dd3533cfb
Atul Varma <varmaa@gmail.com>
2019-12-12 06:51:06 -0500

    Show single row per custom issue.

--- frontend/lib/pages/issue-pages.tsx
+++ frontend/lib/pages/issue-pages.tsx
@@ -44,12 +44,12 @@ export class IssuesArea extends React.Component<IssuesAreaPropsWithCtx> {
         <p>Don't see your issues listed? You can add up to {MAX_CUSTOM_ISSUES_PER_AREA} additional issues below.</p>
         <br/>
         <Formset {...ctx.formsetPropsFor('customIssues')}
-                 maxNum={5}
-                 extra={5}
+                 maxNum={MAX_CUSTOM_ISSUES_PER_AREA}
+                 extra={MAX_CUSTOM_ISSUES_PER_AREA}
                  emptyForm={BlankCustomIssuesCustomIssueFormFormSetInput}>
           {(ciCtx, i) =>
-            <FormsetItem {...formsetItemProps(ciCtx)}>
-              <TextualFormField {...ciCtx.fieldPropsFor('description')} label={`Custom issue #${i + 1} (optional, ${CUSTOM_ISSUE_MAX_LENGTH} characters max)`} />
+            <FormsetItem {...formsetItemProps(ciCtx)} singleRow>
+              <TextualFormField {...ciCtx.fieldPropsFor('description')} fieldProps={{style: {maxWidth: `${CUSTOM_ISSUE_MAX_LENGTH}em`}}} label={`Custom issue #${i + 1} (optional, ${CUSTOM_ISSUE_MAX_LENGTH} characters max)`} />
             </FormsetItem>
           }
         </Formset>

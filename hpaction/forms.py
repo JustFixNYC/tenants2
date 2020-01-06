acb7f0b816f9583c37a84636d18cfe898f161b38
Atul Varma <varmaa@gmail.com>
2020-01-06 08:46:18 -0500

    Make families per apt conditional on 2 apts or less.

--- hpaction/forms.py
+++ hpaction/forms.py
@@ -159,16 +159,31 @@ class PreviousAttemptsForm(DynamicallyRequiredBoolMixin, forms.ModelForm):
         return cleaned_data
 
 
-class HarassmentApartmentForm(forms.ModelForm):
+TWO_OR_LESS_APARTMENTS_IN_BUILDING = 'two_or_less_apartments_in_building'
+MORE_THAN_ONE_FAMILY_PER_APARTMENT = 'more_than_one_family_per_apartment'
+
+
+class HarassmentApartmentForm(DynamicallyRequiredBoolMixin, forms.ModelForm):
     class Meta:
         model = models.HarassmentDetails
         fields = [
-            'two_or_less_apartments_in_building',
-            'more_than_one_family_per_apartment',
+            TWO_OR_LESS_APARTMENTS_IN_BUILDING,
+            MORE_THAN_ONE_FAMILY_PER_APARTMENT,
         ]
 
     two_or_less_apartments_in_building = YesNoRadiosField()
-    more_than_one_family_per_apartment = YesNoRadiosField()
+    more_than_one_family_per_apartment = YesNoRadiosField(required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        two_or_less = YesNoRadiosField.coerce(cleaned_data.get(TWO_OR_LESS_APARTMENTS_IN_BUILDING))
+
+        if two_or_less is True:
+            self.require_bool_field(MORE_THAN_ONE_FAMILY_PER_APARTMENT, cleaned_data)
+        elif two_or_less is False:
+            cleaned_data[MORE_THAN_ONE_FAMILY_PER_APARTMENT] = ''
+
+        return cleaned_data
 
 
 class HarassmentAllegations1Form(forms.ModelForm):

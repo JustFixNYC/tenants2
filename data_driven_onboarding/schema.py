97cbbea05ec48e52d1464e738bc4ea22e1bc2dee
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-01-29 11:18:40 -0500

    fixed flake8 error

--- data_driven_onboarding/schema.py
+++ data_driven_onboarding/schema.py
@@ -77,7 +77,8 @@ class DDOSuggestionsResult(graphene.ObjectType):
     )
 
     building_class = graphene.String(
-        description="The 2-character building class of the BBL, as defined by the Dept. of City Planning."
+        description="The 2-character building class of the BBL, as defined by the "
+                    "Dept. of City Planning."
     )
 
     unit_count = graphene.Int(

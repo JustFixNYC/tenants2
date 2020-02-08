960f12fd2d995b4f9e2a70a9b3dfd58f5996a82f
Atul Varma <varmaa@gmail.com>
2020-02-07 19:07:44 -0500

    Get rid of unnecessary .jf-centered-button class.

--- frontend/lib/centered-buttons.tsx
+++ frontend/lib/centered-buttons.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const CenteredButtons: React.FC<{children: JSX.Element[]}> = ({children}) => {
   return <div>
-    {React.Children.map(children, (child, i) => <div key={i} className="jf-centered-button">{child}</div>)}
+    {React.Children.map(children, (child, i) => <div key={i} className="has-text-centered">{child}</div>)}
   </div>;
 };

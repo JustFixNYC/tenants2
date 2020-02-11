ac461ed901257a256bf2a6aacdb6f44ba11d0f0b
Atul Varma <varmaa@gmail.com>
2020-02-11 18:40:43 -0500

    Factor out NodeDesc.

--- rapidpro/management/commands/rapidpro_analytics.py
+++ rapidpro/management/commands/rapidpro_analytics.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any, Iterator
+from typing import List, Dict, Any, Iterator, NamedTuple
 import re
 from django.core.management.base import BaseCommand
 from temba_client.v2 import TembaClient, Run
@@ -12,6 +12,11 @@ from .syncrapidpro import (
 RH_FOLLOWUP_1_UUID = "be922331-eb0b-4823-86d2-647dc5a014e3"
 
 RH_FOLLOWUP_2_UUID = "52c3d0fc-d198-45d1-86be-c6fed577ad3a"
+
+
+class NodeDesc(NamedTuple):
+    regex: str
+    expected: int = 1
 
 
 class Flow:
@@ -36,8 +41,8 @@ class Flow:
             result.append(flows_by_uuid[uuid])
         return result
 
-    def find_node_uuids(self, regex: str, expected: int) -> List[str]:
-        pattern = re.compile(regex)
+    def find_node_uuids(self, desc: NodeDesc) -> List[str]:
+        pattern = re.compile(desc.regex)
         uuids: List[str] = []
         for action_set in self._f['action_sets']:
             uuid = action_set['uuid']
@@ -48,12 +53,16 @@ class Flow:
                 if pattern.match(msg['base']):
                     uuids.append(uuid)
                     break
-        if len(uuids) != expected:
+        if len(uuids) != desc.expected:
             raise ValueError(
-                f'Expected to find {expected} node(s) matching pattern {pattern} '
-                f'in flow https://textit.in/flow/editor/{self.uuid}/, but found {len(uuids)}!'
+                f'Expected to find {desc.expected} node(s) matching pattern "{desc.regex}" '
+                f'in flow {self.url}, but found {len(uuids)}!'
             )
         return uuids
+
+    @property
+    def url(self) -> str:
+        return f"https://textit.in/flow/editor/{self.uuid}/"
 
     def iter_runs(self, client: TembaClient) -> Iterator[Run]:
         run_batches = client.get_runs(flow=self.uuid).iterfetches(retry_on_rate_exceed=True)
@@ -62,18 +71,14 @@ class Flow:
                 yield run
 
 
-def process_rh_followups(
-    client: TembaClient,
-    flow: Flow,
-    yes_uuids=List[str],
-    no_uuids=List[str]
-):
-    name = flow.name
+def process_rh_followups(client: TembaClient, flow: Flow, yes_nodes=NodeDesc, no_nodes=NodeDesc):
+    yes_uuids = flow.find_node_uuids(yes_nodes)
+    no_uuids = flow.find_node_uuids(no_nodes)
     for run in flow.iter_runs(client):
         exited = False
         for step in run.path:
             date = step.time.date()
-            preamble = f"{date} {name}:"
+            preamble = f"{date} {flow.name}:"
             if step.node in yes_uuids:
                 print(f"{preamble} RH RECEIVED")
                 exited = True
@@ -81,7 +86,7 @@ def process_rh_followups(
                 print(f"{preamble} RH NOT RECEIVED")
                 exited = True
         if not exited:
-            print(f"{date} {name}: {run.exit_type}")
+            print(f"{date} {flow.name}: {run.exit_type}")
 
 
 class Command(BaseCommand):
@@ -98,13 +103,13 @@ class Command(BaseCommand):
         process_rh_followups(
             client,
             rhf1,
-            yes_uuids=rhf1.find_node_uuids(r"^That’s great", 1),
-            no_uuids=rhf1.find_node_uuids(r"^No worries", 1),
+            yes_nodes=NodeDesc(r"^That’s great"),
+            no_nodes=NodeDesc(r"^No worries"),
         )
 
         process_rh_followups(
             client,
             rhf2,
-            yes_uuids=rhf2.find_node_uuids(r"^That’s great", 1),
-            no_uuids=rhf2.find_node_uuids(r"^We're sorry to hear", 1),
+            yes_nodes=NodeDesc(r"^That’s great", 1),
+            no_nodes=NodeDesc(r"^We're sorry to hear", 1),
         )

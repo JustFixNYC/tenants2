428b992969ad322bec549f1cd499db8529553a52
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-02-12 16:42:20 -0500

    fixed codeclimate error

--- frontend/lib/pages/data-driven-onboarding.tsx
+++ frontend/lib/pages/data-driven-onboarding.tsx
@@ -172,12 +172,10 @@ export function isBuildingClassBorC(buildingClass: string|null): boolean {
 
 const buildingIntroCard: ActionCardPropsCreator = (data): ActionCardProps => {
   const hasHpdRegistration = data.associatedBuildingCount && data.associatedBuildingCount > 0;
-
   return ({
     title: data.fullAddress,
     titleProps: {
-      className: 'title is-spaced is-size-3',
-      ...useQueryFormResultFocusProps()
+      className: 'title is-spaced is-size-3', ...useQueryFormResultFocusProps()
     },
     cardClass: 'has-background-light',
     indicators: [
@@ -192,10 +190,9 @@ const buildingIntroCard: ActionCardPropsCreator = (data): ActionCardProps => {
       data.unitCount && data.yearBuilt && hasHpdRegistration && <>
         Your building was built in {data.yearBuilt} or earlier.
       </>,
-      !hasHpdRegistration && (isBuildingClassBorC(data.buildingClass) ? <>
-          <span className="has-text-danger has-text-weight-semibold">No registration found.</span> Your landlord may be breaking the law! 
-        </> : 
-        <span className="has-text-danger has-text-weight-semibold">No registration found.</span>),
+      !hasHpdRegistration && (isBuildingClassBorC(data.buildingClass) 
+        ? <><span className="has-text-danger has-text-weight-semibold">No registration found.</span> Your landlord may be breaking the law! </> 
+        : <span className="has-text-danger has-text-weight-semibold">No registration found.</span>),
       !hasHpdRegistration && (isBuildingClassBorC(data.buildingClass)
         ? <>It looks like this building may require registration with HPD. Landlords who don't properly register their properties incur fines and also cannot bring tenants to court for nonpayment of rent. You can find more information on <OutboundLink href="https://www1.nyc.gov/site/hpd/services-and-information/register-your-property.page" target="_blank">HPD's Property Management page</OutboundLink>.</>
         : <>It doesn't seem like this property is required to register with HPD. You can learn about the City's registration requirements on <OutboundLink href="https://www1.nyc.gov/site/hpd/services-and-information/register-your-property.page" target="_blank">HPD's Property Management page</OutboundLink>.</>)

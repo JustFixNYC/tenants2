fbb9637a46e15aceb0d1340a233fc70c4b824e66
Atul Varma <varmaa@gmail.com>
2020-02-11 20:04:16 -0500

    Analyze RH requests.

--- rapidpro/management/commands/rapidpro_analytics.py
+++ rapidpro/management/commands/rapidpro_analytics.py
@@ -1,5 +1,4 @@
 from typing import List, Dict, Any, Iterator, NamedTuple, Optional
-import datetime
 import re
 from django.core.management.base import BaseCommand
 from temba_client.v2 import TembaClient, Run
@@ -44,6 +43,12 @@ class Flow:
             result.append(flows_by_uuid[uuid])
         return result
 
+    def find_all_node_uuids(self, descs: List[NodeDesc]) -> List[str]:
+        uuids: List[str] = []
+        for desc in descs:
+            uuids.extend(self.find_node_uuids(desc))
+        return uuids
+
     def find_node_uuids(self, desc: NodeDesc) -> List[str]:
         pattern = re.compile(desc.regex)
         uuids: List[str] = []
@@ -78,33 +83,36 @@ class AnalyticsLogger:
     def __init__(self, client: TembaClient):
         self.client = client
 
-    def log(
-        self,
-        flow: Flow,
-        run: Run,
-        timestamp: datetime.datetime,
-        event_name: str,
-        **extra
-    ):
+    def log_run(self, flow: Flow, run: Run, **extra):
         user_id = run.contact.uuid
-        print(f"{timestamp.date()} user={user_id} {flow.name}: {event_name} {extra}")
+        timestamp = run.created_on
+        num_steps = len(run.path)
+        exit_type = run.exit_type
+        print(f"{timestamp.date()} user={user_id[:8]} steps={num_steps} exit={exit_type} "
+              f" {flow.name} {extra}")
+
+    def process_rh_requests(self, flow: Flow, error_nodes=List[NodeDesc]):
+        error_uuids = flow.find_all_node_uuids(error_nodes)
+        for run in flow.iter_runs(self.client):
+            errors = 0
+            for step in run.path:
+                if step.node in error_uuids:
+                    errors += 1
+            self.log_run(flow, run, num_errors=errors)
 
     def process_rh_followups(self, flow: Flow, yes_nodes=NodeDesc, no_nodes=NodeDesc):
         yes_uuids = flow.find_node_uuids(yes_nodes)
         no_uuids = flow.find_node_uuids(no_nodes)
         for run in flow.iter_runs(self.client):
-            exit_type: Optional[str] = None
+            rh_received: Optional[bool] = None
             for step in run.path:
-                timestamp = step.time
                 if step.node in yes_uuids:
-                    exit_type = "rh_received"
-                    break
+                    assert rh_received is None or rh_received is True
+                    rh_received = True
                 elif step.node in no_uuids:
-                    exit_type = "rh_not_received"
-                    break
-            if exit_type is None:
-                exit_type = "rh_followup_failed"
-            self.log(flow, run, timestamp, "rh_followup", exit_type=exit_type)
+                    assert rh_received is None or rh_received is False
+                    rh_received = False
+            self.log_run(flow, run, rh_received=rh_received)
 
 
 class Command(BaseCommand):
@@ -120,6 +128,14 @@ class Command(BaseCommand):
             RH_FOLLOWUP_1_UUID,
             RH_FOLLOWUP_2_UUID
         ])
+
+        analytics.process_rh_requests(
+            rh,
+            error_nodes=[
+                NodeDesc(r"^Sorry", expected=2),
+                NodeDesc(r"^Oops", expected=1),
+            ]
+        )
 
         analytics.process_rh_followups(
             rhf1,

f2726359e21ff40fca094d79589f63b803294d1c
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-02-19 14:09:56 -0500

    specified format of origin url globals

--- frontend/webpack/webpack-defined-globals.d.ts
+++ frontend/webpack/webpack-defined-globals.d.ts
@@ -33,11 +33,11 @@ declare const DISABLE_DEV_SOURCE_MAPS: boolean;
 declare const ENABLE_WEBPACK_CONTENT_HASH: boolean;
 
 /**
- * The base url for outbound links to Who Owns What. 
+ * The base url for outbound links to Who Owns What (without trailing slash).
  */
 declare const WOW_ORIGIN: string;
 
 /**
- * The base url for outbound links to Eviction Free NYC. 
+ * The base url for outbound links to Eviction Free NYC (without trailing slash).
  */
 declare const EFNYC_ORIGIN: string;

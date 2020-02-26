0fb49985c893b1701754f4c0a3d845b0d8fbcf62
Atul Varma <varmaa@gmail.com>
2020-02-26 08:30:16 -0500

    Factor out useQuery().

--- frontend/lib/admin-conversations.tsx
+++ frontend/lib/admin-conversations.tsx
@@ -2,30 +2,34 @@ import React, { useEffect, useContext, useState } from 'react';
 import { Switch, Route } from "react-router-dom";
 import Routes from "./routes";
 import { AppContext } from './app-context';
-import { AdminConversations, AdminConversations_output } from './queries/AdminConversations';
+import { AdminConversations } from './queries/AdminConversations';
+import { QueryLoaderQuery } from './query-loader-prefetcher';
 
-const AdminConversationsPage: React.FC<{}> = (props) => {
+function useQuery<Input, Output>(query: QueryLoaderQuery<Input, Output>, input: Input): Output|null {
+  const [value, setValue] = useState<Output|null>(null);
   const appCtx = useContext(AppContext);
-  const [conversations, setConversations] = useState<AdminConversations_output[]|null>(null);
   const { fetch } = appCtx;
 
   useEffect(() => {
     let isMounted = true;
-    const result = AdminConversations.fetch(appCtx.fetch, {
-      query: '',
-      page: 1,
-    });
-    result.then(convos => isMounted && setConversations(convos.output));
+    const result = query.fetch(fetch, input);
+    result.then(v => isMounted && setValue(v));
     return () => { isMounted = false; };
   }, [fetch]);
 
+  return value;
+}
+
+const AdminConversationsPage: React.FC<{}> = (props) => {
+  const conversations = useQuery(AdminConversations, { query: '', page: 1 });
+
   return <div>
-    {conversations ? conversations.map(conv => {
+    {conversations?.output?.map(conv => {
       return <div key={conv.userPhoneNumber}>
         <div>{conv.userPhoneNumber}</div>
         <div>{conv.body}</div>
       </div>
-    }) : <p>Loading...</p>}
+    }) || <p>Loading...</p>}
   </div>;
 };
 

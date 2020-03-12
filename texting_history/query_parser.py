80aa61f7190beca3c65a67f19c757912909a8714
Atul Varma <varmaa@gmail.com>
2020-03-11 21:56:48 -0400

    Allow conversations to be searched by users w/ HP action packets, message body (#1027)
    This makes it possible for the conversations admin view to be searched by two new filtering mechanisms:
    
    * Using `has:hpa` will show only users who have generated HP Action packets.
    * Surrounding any query in double quotes, e.g. `"sorry"`, will show only conversations that include the text (sans quotes) in the message body.
    
    Currently these mechanisms are mutually exclusive--you can't use both in the same query, so `"boop has:hpa"` will _not_ search for users who generated HP packets and have had "boop" mentioned in their conversation.  This may change in the future, though!

---
+++ texting_history/query_parser.py
@@ -0,0 +1,29 @@
+import re
+from dataclasses import dataclass
+
+from project.util.phone_number import ALL_DIGITS_RE
+
+IN_QUOTES_RE = re.compile(r'"(?P<text>.+)"')
+
+
+@dataclass
+class Query:
+    full_name: str = ''
+    phone_number: str = ''
+    has_hpa_packet: bool = False
+    message_body: str = ''
+
+    @staticmethod
+    def parse(query: str) -> 'Query':
+        result = Query()
+        quoted_match = IN_QUOTES_RE.fullmatch(query)
+
+        if quoted_match:
+            result.message_body = quoted_match.group('text')
+        elif ALL_DIGITS_RE.fullmatch(query):
+            result.phone_number = query
+        elif query.lower() == 'has:hpa':
+            result.has_hpa_packet = True
+        else:
+            result.full_name = query
+        return result
--- texting_history/tests/test_schema.py
+++ texting_history/tests/test_schema.py
@@ -140,6 +140,8 @@ def test_conversations_query_works(auth_graphql_client):
     ["boop", 1],
     ["5551234567", 1],
     ["blarg", 0],
+    ["has:hpa", 0],
+    ['\\"some text in the message body\\"', 0],
 ])
 def test_conversations_queries_produce_expected_results(auth_graphql_client, query, num_results):
     MessageFactory.create()

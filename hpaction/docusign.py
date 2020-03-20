1eff3af2dd59aeca4bb00a3a27d76b591a3a48dc
Atul Varma <varmaa@gmail.com>
2020-03-20 17:00:48 -0400

    Factor out hpaction.docusign module.

---
+++ hpaction/docusign.py
@@ -0,0 +1,148 @@
+from typing import Tuple
+import base64
+import docusign_esign as docusign
+from django.conf import settings
+
+from users.models import JustfixUser
+from .models import HPActionDocuments
+
+# The recipient ID for the tenant in the signing flow. This appears to be a
+# number local to a specific signing, rather than a globally unique identifier.
+TENANT_RECIPIENT_ID = '1'
+
+# The document ID for the HP Action packet in the signing flow. This appears
+# to be a number local to a specific signing, rather than a globally unique
+# identifier.
+HPA_DOCUMENT_ID = '1'
+
+
+def docusign_client_user_id(user: JustfixUser) -> str:
+    '''
+    Given a user, return the DocuSign client user ID representing it.
+
+    According to the DocuSign documentation, the client user ID is:
+
+    > An alphanumeric value that serves as an ID for the signer throughout
+    > the signing process. The combination of this value, the signer's name,
+    > and the signer's email is used to uniquely identify them.
+
+    For more details, see:
+
+    https://developers.docusign.com/esign-rest-api/code-examples/quickstart-request-signature-embedded
+    '''
+
+    return str(user.pk)
+
+
+def create_envelope_definition_for_hpa(docs: HPActionDocuments) -> docusign.EnvelopeDefinition:
+    '''
+    Create a DocuSign envelope definition for the given HP Action documents.
+    '''
+
+    user = docs.user
+    pdf_bytes = docs.pdf_file.open().read()
+    base64_pdf = base64.b64encode(pdf_bytes).decode('ascii')
+
+    document = docusign.Document(
+        document_base64=base64_pdf,
+        name=f"HP Action forms for {user.full_name}",
+        file_extension="pdf",
+        document_id=HPA_DOCUMENT_ID,
+    )
+
+    signer = docusign.Signer(
+        email=user.email,
+        name=user.full_name,
+        recipient_id=TENANT_RECIPIENT_ID,
+        routing_order="1",
+        client_user_id=docusign_client_user_id(user),
+    )
+
+    sign_here_petition = docusign.SignHere(
+        document_id=HPA_DOCUMENT_ID,
+        page_number='4',
+        recipient_id=TENANT_RECIPIENT_ID,
+        tab_label='SignHereTab',
+        x_position='419',
+        y_position='556',
+    )
+
+    sign_here_verification = docusign.SignHere(
+        document_id=HPA_DOCUMENT_ID,
+        page_number='4',
+        recipient_id=TENANT_RECIPIENT_ID,
+        tab_label='SignHereTab',
+        x_position='419',
+        y_position='667',
+    )
+
+    sign_here_hpd_inspection = docusign.SignHere(
+        document_id=HPA_DOCUMENT_ID,
+        page_number='5',
+        recipient_id=TENANT_RECIPIENT_ID,
+        tab_label='SignHereTab',
+        x_position='446',
+        y_position='625',
+    )
+
+    signer.tabs = docusign.Tabs(
+        sign_here_tabs=[
+            sign_here_petition,
+            sign_here_verification,
+            sign_here_hpd_inspection
+        ],
+    )
+
+    envelope_definition = docusign.EnvelopeDefinition(
+        email_subject=f"{user.full_name}, please sign these HP Action forms",
+        documents=[document],
+        recipients=docusign.Recipients(signers=[signer]),
+        status="sent",
+    )
+
+    assert isinstance(envelope_definition, docusign.EnvelopeDefinition)
+
+    return envelope_definition
+
+
+def create_envelope_and_recipient_view_for_hpa(
+    user: JustfixUser,
+    envelope_definition: docusign.EnvelopeDefinition,
+    access_token: str,
+    api_base_path: str,
+    return_url: str,
+) -> Tuple[docusign.EnvelopeSummary, str]:
+    '''
+    Create a DocuSign envelope and recipient view request for
+    HP Action documents represented by a given envelope definition.
+    '''
+
+    api_client = docusign.ApiClient()
+    api_client.host = api_base_path
+    api_client.set_default_header('Authorization', f'Bearer {access_token}')
+
+    envelope_api = docusign.EnvelopesApi(api_client)
+    envelope = envelope_api.create_envelope(
+        settings.DOCUSIGN_ACCOUNT_ID,
+        envelope_definition=envelope_definition
+    )
+
+    assert isinstance(envelope, docusign.EnvelopeSummary)
+
+    envelope_id = envelope.envelope_id
+    recipient_view_request = docusign.RecipientViewRequest(
+        authentication_method='None',  # TODO: This should probably change?
+        client_user_id=docusign_client_user_id(user),
+        recipient_id=TENANT_RECIPIENT_ID,
+        return_url=return_url,
+        user_name=user.full_name,
+        email=user.email,
+    )
+
+    results = envelope_api.create_recipient_view(
+        settings.DOCUSIGN_ACCOUNT_ID,
+        envelope_id,
+        recipient_view_request=recipient_view_request,
+    )
+
+    return (envelope, results.url)
--- hpaction/management/commands/hpsign.py
+++ hpaction/management/commands/hpsign.py
@@ -1,9 +1,8 @@
 import os
-import base64
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-import docusign_esign as docusign
 
+from hpaction import docusign
 from users.models import JustfixUser
 from hpaction.models import HPActionDocuments
 from project.util.site_util import absolutify_url
@@ -62,93 +61,16 @@ class Command(BaseCommand):
         if not docs:
             raise CommandError(f"user {username} has no HP Action documents to sign!")
 
-        pdf_bytes = docs.pdf_file.open().read()
-        base64_pdf = base64.b64encode(pdf_bytes).decode('ascii')
-        client_user_id = str(user.pk)
         return_url = absolutify_url('/')
 
-        document = docusign.Document(
-            document_base64=base64_pdf,
-            name=f"HP Action forms for {user.full_name}",
-            file_extension="pdf",
-            document_id=1,
-        )
-
-        signer = docusign.Signer(
-            email=user.email,
-            name=user.full_name,
-            recipient_id="1",
-            routing_order="1",
-            client_user_id=client_user_id,
-        )
-
-        sign_here_petition = docusign.SignHere(
-            document_id='1',
-            page_number='4',
-            recipient_id='1',
-            tab_label='SignHereTab',
-            x_position='419',
-            y_position='556',
-        )
-
-        sign_here_verification = docusign.SignHere(
-            document_id='1',
-            page_number='4',
-            recipient_id='1',
-            tab_label='SignHereTab',
-            x_position='419',
-            y_position='667',
-        )
-
-        sign_here_hpd_inspection = docusign.SignHere(
-            document_id='1',
-            page_number='5',
-            recipient_id='1',
-            tab_label='SignHereTab',
-            x_position='446',
-            y_position='625',
-        )
-
-        signer.tabs = docusign.Tabs(
-            sign_here_tabs=[
-                sign_here_petition,
-                sign_here_verification,
-                sign_here_hpd_inspection
-            ],
-        )
-
-        envelope_definition = docusign.EnvelopeDefinition(
-            email_subject=f"{user.full_name}, please sign these HP Action forms",
-            documents=[document],
-            recipients=docusign.Recipients(signers=[signer]),
-            status="sent",
-        )
-
-        api_client = docusign.ApiClient()
-        api_client.host = API_BASE_PATH
-        api_client.set_default_header('Authorization', f'Bearer {access_token}')
-
-        envelope_api = docusign.EnvelopesApi(api_client)
-        results = envelope_api.create_envelope(
-            settings.DOCUSIGN_ACCOUNT_ID,
-            envelope_definition=envelope_definition
-        )
-
-        envelope_id = results.envelope_id
-        recipient_view_request = docusign.RecipientViewRequest(
-            authentication_method='None',  # TODO: This should probably change?
-            client_user_id=client_user_id,
-            recipient_id='1',
+        envelope_definition = docusign.create_envelope_definition_for_hpa(docs)
+        _, url = docusign.create_envelope_and_recipient_view_for_hpa(
+            user=user,
+            envelope_definition=envelope_definition,
+            access_token=access_token,
+            api_base_path=API_BASE_PATH,
             return_url=return_url,
-            user_name=user.full_name,
-            email=user.email,
         )
 
-        results = envelope_api.create_recipient_view(
-            settings.DOCUSIGN_ACCOUNT_ID,
-            envelope_id,
-            recipient_view_request=recipient_view_request,
-        )
-
-        print(f"To sign, visit this URL within 5 minutes: {results.url}")
+        print(f"To sign, visit this URL within 5 minutes: {url}")
         print(f"When done, you will be redirected to {return_url}.")

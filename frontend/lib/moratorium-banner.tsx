471083b8af20941dc533388759c4de09ca86c040
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-03-24 17:05:25 -0400

    styling and formatting updates

--- frontend/lib/moratorium-banner.tsx
+++ frontend/lib/moratorium-banner.tsx
@@ -21,18 +21,18 @@ class MoratoriumBanner extends Component<Props,State> {
   
     render() {
       return (
-        <section className={"hero is-warning is-small " + (this.state.isHidden && "is-hidden")}>
+        <section className={"jf-moratorium-banner hero is-warning is-small " + (this.state.isHidden && "is-hidden")}>
           <div className="hero-body">
-          <div className="close-button is-absolute is-size-5 is-pulled-right" onClick = {this.closeBanner}>✕</div>
             <div className="container">
-              <p>
-                  <span className="has-text-weight-bold">COVID-19 Update: </span>
-                  JustFix.nyc is still in operation, and we are adapting our products to match new rules put in place during the Covid-19 public health crisis. 
-                  Thanks to organizing from tenant leaders, renters now have stronger protections during this time, including a full halt on eviction cases. 
-                  {' '}<a href="https://www.righttocounselnyc.org/moratorium_faq" rel="noopener noreferrer">
-                    <span className="has-text-weight-bold">Learn more</span>
-                  </a>
-              </p>
+                <div className="jf-close-button is-size-5 is-pulled-right" onClick = {this.closeBanner}>✕</div>
+                <p>
+                    <span className="has-text-weight-bold">COVID-19 Update: </span>
+                    JustFix.nyc is still in operation, and we are adapting our products to match new rules put in place during the Covid-19 public health crisis. 
+                    Thanks to organizing from tenant leaders, renters now have stronger protections during this time, including a full halt on eviction cases. 
+                    {' '}<a href="https://www.righttocounselnyc.org/moratorium_faq" rel="noopener noreferrer">
+                        <span className="has-text-weight-bold">Learn more</span>
+                    </a>
+                </p>
             </div>
           </div>
         </section>

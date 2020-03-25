db38a78eff1743c10f90bb2080d65a52513f3e06
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-03-24 20:56:03 -0400

    added missing target="_blank"

--- frontend/lib/moratorium-banner.tsx
+++ frontend/lib/moratorium-banner.tsx
@@ -17,7 +17,7 @@ const MoratoriumBanner = () => {
                 <span className="has-text-weight-bold">COVID-19 Update: </span>
                 JustFix.nyc is still in operation, and we are adapting our products to match new rules put in place during the Covid-19 public health crisis. 
                 Thanks to organizing from tenant leaders, renters now have stronger protections during this time, including a full halt on eviction cases. 
-                {' '}<a href="https://www.righttocounselnyc.org/moratorium_faq" rel="noopener noreferrer">
+                {' '}<a href="https://www.righttocounselnyc.org/moratorium_faq" target="_blank" rel="noopener noreferrer">
                     <span className="has-text-weight-bold">Learn more</span>
                 </a>
             </p>

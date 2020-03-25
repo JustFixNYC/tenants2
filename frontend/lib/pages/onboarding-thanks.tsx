34056eecace847f7856194b57e5db3fed0b31ab2
Atul Varma <varmaa@gmail.com>
2020-03-25 16:58:33 -0400

    Give continue button more room to breathe.

--- frontend/lib/pages/onboarding-thanks.tsx
+++ frontend/lib/pages/onboarding-thanks.tsx
@@ -7,6 +7,7 @@ export const OnboardingThanks: React.FC<{next: string}> = ({next}) => {
     <h1>Thanks for signing up!</h1>
     <p className="subtitle is-4 is-marginless">An email to verify your account is on its way.</p>
     <p>Don't see one? Check your spam folder.</p>
+    <br/>
     <p><Link to={next} className="button is-primary is-medium">Continue</Link></p>
   </Page>;
 };

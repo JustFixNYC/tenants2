e41c408f477a4398292dec48101f7a2a4631fe45
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-03-27 09:57:20 -0400

    added Noun Project icons and updated styling to sanitation guide

--- frontend/lib/pages/loc-confirmation.tsx
+++ frontend/lib/pages/loc-confirmation.tsx
@@ -34,29 +34,29 @@ const SanitationGuidelines = () => {
       <div className={classnames("content", !isExpanded && "is-hidden")}>
         <div className="columns">
           <div className="column is-one-quarter">
-            <SquareImage size={128} src='frontend/img/96x96.png' alt="" />
+            <SquareImage size={128} src='frontend/img/sanitation-guide/chat.svg' alt="" />
           </div>
           <div className="column">
-            <h6 className="is-uppercase has-text-weight-bold">Before the repair-worker arrives</h6>
+            <h3 className="is-size-6 has-text-grey-darker is-uppercase has-text-weight-bold">Before the repair-worker arrives</h3>
             <p>Talk to anyone that you live with and let them know that a repair-worker is coming to perform the repairs that you requested.</p>
           </div>
         </div>
         <div className="columns">
           <div className="column is-one-quarter">
-            <SquareImage size={128} src='frontend/img/96x96.png' alt="" />
+            <SquareImage size={128} src='frontend/img/sanitation-guide/hands.svg' alt="" />
           </div>
           <div className="column">
-            <h6 className="is-uppercase has-text-weight-bold">While the repair-worker is inside your home</h6>
+            <h3 className="is-size-6 is-uppercase has-text-weight-bold">While the repair-worker is inside your home</h3>
             <p>Have the repair-worker wash their hands with soap for at least 20 seconds as soon as they come into your house.</p>
             <p>If possible, stay in a different room from where the work is being done. If a separate room is not available, maintain at least a six-foot distance from the repair-worker until the repair is completed.</p>
           </div>
         </div>
         <div className="columns">
           <div className="column is-one-quarter">
-            <SquareImage size={128} src='frontend/img/96x96.png' alt="" />
+            <SquareImage size={128} src='frontend/img/sanitation-guide/wipe.svg' alt="" />
           </div>
           <div className="column">
-            <h6 className="is-uppercase has-text-weight-bold">After the repair-worker leaves</h6>
+            <h3 className="is-size-6 is-uppercase has-text-weight-bold">After the repair-worker leaves</h3>
             <p>Immediately sanitize all surfaces in your home, especially doorknobs, the sink where the repair-worker washed their hands, and any surfaces you know they have likely been in contact with.</p>
             <p className="is-size-7">For guidance on how to thoroughly sanitize your home and a list of recommended effective cleaning products visit 
               {' '}<OutboundLink href="https://www.cdc.gov/coronavirus/2019-ncov/prepare/cleaning-disinfection.html" target="_blank">Center for Disease Control (CDC) Guide on How to Clean and Disinfect</OutboundLink>

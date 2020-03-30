9d7d84ba5899452ad017839f7c3ad649992fcb31
Atul Varma <varmaa@gmail.com>
2020-03-30 07:57:38 -0400

    Remove unused imports.

--- frontend/lib/pages/issue-pages.tsx
+++ frontend/lib/pages/issue-pages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import { allCapsToSlug, slugToAllCaps, toDjangoChoices } from "../common-data";
 import Page from '../page';

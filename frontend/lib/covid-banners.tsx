e528f4e3bacdb1b8410c04eb27cd33c5122bd2f9
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-03-30 17:33:20 -0400

    fixed issue with <u> tag and sentence cased some stuff

--- frontend/lib/covid-banners.tsx
+++ frontend/lib/covid-banners.tsx
@@ -67,6 +67,6 @@ export const CovidRiskBanner = () => (
 export const MoratoriumWarning = () => (
     <div className="content has-text-centered is-size-7">
         <Icon type="notice" />{' '}Have you been given an eviction notice? <strong>This is illegal.</strong> An Eviction Moratorium is currently in place across New York State. 
-        {' '}<OutboundLink href="https://www.righttocounselnyc.org/moratorium_faq" target="_blank"><u className="has-text-primary">Learn more</u></OutboundLink>
+        {' '}<OutboundLink href="https://www.righttocounselnyc.org/moratorium_faq" target="_blank"><span className="has-text-primary jf-has-text-underline">Learn more</span></OutboundLink>
     </div>
 )

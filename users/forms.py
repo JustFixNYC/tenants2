b2c7a853fa14ff25b0b03581b0298cc30708b67c
Atul Varma <varmaa@gmail.com>
2020-03-30 20:34:50 -0400

    Add a sendVerificationEmail GraphQL mutation.

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 from .models import JustfixUser
 
 
@@ -12,3 +14,11 @@ class JustfixUserChangeForm(UserChangeForm):
     class Meta:
         model = JustfixUser
         fields = ('username', 'phone_number')
+
+
+class SendVerificationEmailForm(forms.Form):
+    email = forms.EmailField(help_text=(
+        "The email address of the user. If this is different from their "
+        "current email address, their email address will be changed and "
+        "marked as unverified."
+    ))
---
+++ users/schema.py
@@ -0,0 +1,37 @@
+from graphql import ResolveInfo
+
+from project import schema_registry
+from project.util.session_mutation import SessionFormMutation
+from .forms import SendVerificationEmailForm
+from .email_verify import send_verification_email_async
+
+
+@schema_registry.register_mutation
+class SendVerificationEmail(SessionFormMutation):
+    '''
+    Sends the currently logged-in user an email with a link
+    to follow; when they follow the link, their account will be marked
+    as having a verified email address.
+
+    Note that this endpoint requires the user's email address. If
+    the one provided is different from the one they currently have
+    set, it will be changed, and their account will be marked as
+    having an unverified email address (until they click on the
+    link that has been sent to them, of course).
+    '''
+
+    class Meta:
+        form_class = SendVerificationEmailForm
+
+    login_required = True
+
+    @classmethod
+    def perform_mutate(cls, form, info: ResolveInfo):
+        user = info.context.user
+        email = form.cleaned_data['email']
+        if user.email != email:
+            user.email = email
+            user.is_email_verified = False
+            user.save()
+        send_verification_email_async(user.pk)
+        return cls.mutation_success()

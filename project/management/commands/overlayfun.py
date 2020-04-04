113de2798c4d863e02593fdc933cf75c2eb56807
Atul Varma <varmaa@gmail.com>
2020-04-04 13:57:47 -0400

    Add overlayfun command.

---
+++ project/management/commands/overlayfun.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from io import BytesIO
+from django.core.management import BaseCommand
+import weasyprint
+import PyPDF2
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options) -> None:
+        css = weasyprint.CSS(
+            string="""
+            .county { position: absolute; top: 65px; left: 140px; }
+            @page { margin: 0; size: letter; }
+            """
+        )
+        html = weasyprint.HTML(
+            string="""<!DOCTYPE html>
+            <meta charset="utf-8">
+            <title>boop</title>
+            <div class="county">queens</div>
+            """
+        )
+        overlay_file = BytesIO(html.write_pdf(
+            stylesheets=[css]
+        ))
+        overlay_pdf = PyPDF2.PdfFileReader(overlay_file)
+
+        print("Generated overlay pdf.")
+
+        pdf_writer = PyPDF2.PdfFileWriter()
+
+        blank_path = Path("blank-hp-forms.pdf")
+        with blank_path.open('rb') as blank_file:
+            blank_pdf = PyPDF2.PdfFileReader(blank_file)
+            for i in range(blank_pdf.numPages):
+                page = blank_pdf.getPage(i)
+                if i < overlay_pdf.numPages:
+                    print(f"Overlaying page {i}.")
+                    overlay_page = overlay_pdf.getPage(i)
+                    # https://stackoverflow.com/a/23633769
+                    page.mergeRotatedTranslatedPage(
+                        overlay_page,
+                        270,
+                        overlay_page.mediaBox.getWidth() / 2,
+                        overlay_page.mediaBox.getWidth() / 2
+                    )
+                else:
+                    print(f"Copying page {i}.")
+                pdf_writer.addPage(page)
+
+            out_path = Path("overlay-fun.pdf")
+
+            with out_path.open('wb') as out_pdf:
+                pdf_writer.write(out_pdf)
+
+            print(f'Wrote {out_path}.')

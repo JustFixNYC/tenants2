aa388fbbb20bbb31d1410c3f3fe2bd9b3c83be75
Atul Varma <varmaa@gmail.com>
2020-04-07 09:27:01 -0400

    Add more tests.

--- frontend/lib/pages/tests/hp-action-your-landlord.test.tsx
+++ frontend/lib/pages/tests/hp-action-your-landlord.test.tsx
@@ -4,9 +4,16 @@ import { HPActionYourLandlord } from "../hp-action-your-landlord";
 import { Route } from "react-router-dom";
 import { BlankOnboardingInfo } from '../../queries/OnboardingInfo';
 import { OnboardingInfoLeaseType } from '../../queries/globalTypes';
+import { BlankLandlordDetailsType, LandlordDetailsType } from '../../queries/LandlordDetailsType';
 
 describe('HPActionYourLandlord', () => {
   afterEach(AppTesterPal.cleanup);
+
+  const landlordInfo: LandlordDetailsType = {
+    ...BlankLandlordDetailsType,
+    name: 'Landlordo Calrissian',
+    address: '1 Cloud City',
+  };
 
   const makeRoute = () => <Route render={
     props => <HPActionYourLandlord nextStep="/next" prevStep="/prev" {...props} />
@@ -24,5 +31,18 @@ describe('HPActionYourLandlord', () => {
     pal.rr.getByText("NYC Housing Authority");
   });
 
+  it('shows manually-entered address in form fields', () => {
+    const pal = new AppTesterPal(makeRoute(), {
+      session: {landlordDetails: {...landlordInfo, isLookedUp: false}}
+    });
+    const input = pal.rr.getByLabelText("Landlord name") as HTMLInputElement;
+    expect(input.value).toBe('Landlordo Calrissian');
+  });
 
+  it('shows automatically looked-up address as read-only', () => {
+    const pal = new AppTesterPal(makeRoute(), {
+      session: {landlordDetails: {...landlordInfo, isLookedUp: true}}
+    });
+    pal.rr.getByText("Landlordo Calrissian");
+  });
 });

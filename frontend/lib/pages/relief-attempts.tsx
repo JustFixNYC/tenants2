fc7a743b22e828e70cb606e0229a7c9b7404cb7e
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-08 15:46:00 -0400

    Attempt to add form submitter to new loc page

--- frontend/lib/pages/relief-attempts.tsx
+++ frontend/lib/pages/relief-attempts.tsx
@@ -1,37 +1,44 @@
 import React from 'react';
 
 import Page from "../page";
-// import { SessionUpdatingFormSubmitter } from '../session-updating-form-submitter';
-// import Routes from '../routes';
-// import { OnboardingStep2Mutation, BlankOnboardingStep2Input } from '../queries/OnboardingStep2Mutation';
-// import { OnboardingStep2Input } from '../queries/globalTypes';
-// import { FormContext } from '../form-context';
-// import { YesNoRadiosFormField } from '../yes-no-radios-form-field';
+import { SessionUpdatingFormSubmitter } from '../session-updating-form-submitter';
+import Routes from '../routes';
+import { FormContext } from '../form-context';
+import { ReliefAttemptsMutation } from '../queries/ReliefAttemptsMutation';
+import { ReliefAttemptsInput } from "../queries/globalTypes"
+import { YesNoRadiosFormField } from '../yes-no-radios-form-field';
+import { ProgressButtons } from '../buttons';
+import { FormInputConverter } from '../form-input-converter';
 
-// function renderForm(ctx: FormContext<OnboardingStep2Input>): JSX.Element {
-//   console.log({...ctx.fieldPropsFor('hasCalled311')})
-//   return (
-//     <YesNoRadiosFormField
-//         {...ctx.fieldPropsFor('hasCalled311')}
-//         label="Have you previously called 311 with no results?"
-//       />
-//   );
-// }
+
+function renderForm(ctx: FormContext<ReliefAttemptsInput>): JSX.Element {
+  var hasCalled311 = ctx.fieldPropsFor('hasCalled311');
+  var conv = new FormInputConverter(hasCalled311);
+  console.log(conv);
+  return (
+    <React.Fragment>
+      <YesNoRadiosFormField {...hasCalled311} label="Have you previously called 311 with no results?" />
+      <ProgressButtons back={Routes.locale.loc.accessDates} isLoading={ctx.isLoading} />
+    </React.Fragment>
+  );
+}
 
 export default function ReliefAttemptsPage(): JSX.Element {
   return (
-    <Page title="Called 311">
+    <Page title="Previous attempts to get help">
       <div>
         <h1 className="title is-4 is-spaced">Previous attempts to get official help</h1>
-        <p className="subtitle is-6">It is encouraged that you <strong>call 311 to report housing complaints</strong> directly with the City. 
+        <p className="subtitle is-6">It is encouraged that you call 311 to report housing complaints directly with the City. 
         By calling, you will trigger a formal inspection process that may lead to official violations being issued.</p>
-        {/* <SessionUpdatingFormSubmitter
-          mutation={OnboardingStep2Mutation}
-          initialState={(session) => session.onboardingStep2 || BlankOnboardingStep2Input}
+        <SessionUpdatingFormSubmitter
+          mutation={ReliefAttemptsMutation}
+          initialState={(session) => ({
+              hasCalled311: session.onboardingInfo?.hasCalled311 
+            })}
           onSuccessRedirect={Routes.locale.loc.yourLandlord}
-        >
-          {renderForm}
-        </SessionUpdatingFormSubmitter> */}
+          >
+            {renderForm}
+        </SessionUpdatingFormSubmitter>
       </div>
     </Page>
   );

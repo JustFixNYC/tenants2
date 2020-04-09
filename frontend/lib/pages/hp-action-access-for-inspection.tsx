a5b3d30055e8da6315f3c9c21b90a32db63f665b
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-09 13:16:26 -0400

    created alternate Access for Inspection page for ehp

--- frontend/lib/pages/hp-action-access-for-inspection.tsx
+++ frontend/lib/pages/hp-action-access-for-inspection.tsx
@@ -5,14 +5,19 @@ import { TextualFormField } from '../form-fields';
 
 const onboardingStepBuilder = new SessionStepBuilder(sess => sess.onboardingInfo);
 
-export const AccessForInspection = onboardingStepBuilder.createStep({
+const AccessForInspectionGenerator = (type?: string) => onboardingStepBuilder.createStep({
   title: "Access for Your HPD Inspection",
   mutation: AccessForInspectionMutation,
   toFormInput: onb => onb.finish(),
   renderIntro: () => <>
-    <p>On the day of your HPD Inspection, the Inspector will need access to your apartment during a window of time that you will choose with the HP Clerk when you submit your paperwork in Court.</p>
+    <p>On the day of your HPD Inspection, the Inspector will need access to your apartment during a window of time that you will choose with 
+      {' '}{type === 'ehp' ? "your lawyer" : "the HP Clerk when you submit your paperwork in Court"}.
+    </p>
   </>,
   renderForm: ctx => <>
     <TextualFormField {...ctx.fieldPropsFor('floorNumber')} type="number" min="0" label="What floor do you live on?" />
   </>,
 });
+
+export const AccessForInspection = AccessForInspectionGenerator();
+export const EhpAccessForInspection = AccessForInspectionGenerator('ehp');

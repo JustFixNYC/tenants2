f2800d68e21ce602b933d32d4bdd3173565d28c2
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-09 10:40:06 -0400

    refactor LOC steps to make sure step skipping for nycha works

--- frontend/lib/pages/access-dates.tsx
+++ frontend/lib/pages/access-dates.tsx
@@ -6,11 +6,10 @@ import { TextualFormField } from '../form-fields';
 import { AccessDatesMutation, BlankAccessDatesInput } from '../queries/AccessDatesMutation';
 import { AccessDatesInput } from '../queries/globalTypes';
 import { ProgressButtons } from "../buttons";
-import Routes from '../routes';
 import { dateAsISO, addDays } from '../util';
 
 import validation from '../../../common-data/access-dates-validation.json';
-import { FormContext } from '../form-context';
+import { MiddleProgressStep } from '../progress-step-route';
 
 /**
  * The minimum number of days from today that the first access date
@@ -35,33 +34,28 @@ export function getInitialState(accessDates: string[], now: Date = new Date()): 
   return result;
 }
 
-function renderForm(ctx: FormContext<AccessDatesInput>): JSX.Element {
+const AccessDatesPage = MiddleProgressStep(props => {
   const minDate = dateAsISO(addDays(new Date(), MIN_DAYS));
-
-  return (
-    <React.Fragment>
-      <TextualFormField label={`First access date (at least ${MIN_DAYS_TEXT} from today)`} type="date" min={minDate} required {...ctx.fieldPropsFor('date1')} />
-      <TextualFormField label="Second access date (optional)" type="date" min={minDate} {...ctx.fieldPropsFor('date2')} />
-      <TextualFormField label="Third access date (optional)" type="date" min={minDate} {...ctx.fieldPropsFor('date3')} />
-      <ProgressButtons back={Routes.locale.loc.issues.home} isLoading={ctx.isLoading} />
-    </React.Fragment>
-  );
-}
-
-export default function AccessDatesPage(): JSX.Element {
-  return (
-    <Page title="Access dates">
+  return (<Page title="Access dates">
       <div>
         <h1 className="title is-4 is-spaced">Landlord/super access dates</h1>
         <p className="subtitle is-6">Access dates are times you know when you will be home for the landlord to schedule repairs. Please provide <strong>1 - 3</strong> access dates when you can be available (allowing at least {MIN_DAYS_TEXT} for the letter to be received).</p>
         <SessionUpdatingFormSubmitter
           mutation={AccessDatesMutation}
           initialState={(session) => getInitialState(session.accessDates)}
-          onSuccessRedirect={Routes.locale.loc.reliefAttempts}
+          onSuccessRedirect={props.nextStep}
         >
-          {renderForm}
+          {ctx => <>
+            <TextualFormField label={`First access date (at least ${MIN_DAYS_TEXT} from today)`} type="date" min={minDate} required {...ctx.fieldPropsFor('date1')} />
+            <TextualFormField label="Second access date (optional)" type="date" min={minDate} {...ctx.fieldPropsFor('date2')} />
+            <TextualFormField label="Third access date (optional)" type="date" min={minDate} {...ctx.fieldPropsFor('date3')} />
+            <ProgressButtons back={props.prevStep} isLoading={ctx.isLoading} />
+          </>}
         </SessionUpdatingFormSubmitter>
       </div>
     </Page>
-  );
-}
+    );
+  }
+);
+
+export default AccessDatesPage;

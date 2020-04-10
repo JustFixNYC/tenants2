9b78ffdd6eab7ab1c78a5c51d0a3be3720e6855d
Sam Raby <sraby@users.noreply.github.com>
2020-04-09 21:18:05 -0400

    Added new page to the LOC flow asking about previous 311 requests (#1156)
    This update adds a new "Relief Attempts" page to the LOC flow to replaces our ask of previous 311 question that used to exist on Onboarding's Step 2. It adds a new mutation that allows access to the hasCalled311 prop which is now present within the OnboardingInfo class. The hope is to implement a new Radio Buttons Yes/No form that allows users to specify a boolean value for this field in the server.

--- frontend/lib/pages/landlord-details.tsx
+++ frontend/lib/pages/landlord-details.tsx
@@ -1,37 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Page from "../page";
 import { SessionUpdatingFormSubmitter } from '../session-updating-form-submitter';
 import { TextualFormField } from '../form-fields';
 
 import { BackButton, ProgressButtons } from "../buttons";
-import Routes from '../routes';
-import { LandlordDetailsV2Input } from '../queries/globalTypes';
-import { AppContextType, withAppContext } from '../app-context';
+import { AppContext } from '../app-context';
 import { exactSubsetOrDefault } from '../util';
 import { Link } from 'react-router-dom';
 import { AllSessionInfo_landlordDetails } from '../queries/AllSessionInfo';
-import { FormContext } from '../form-context';
 import { LandlordDetailsV2Mutation, BlankLandlordDetailsV2Input } from '../queries/LandlordDetailsV2Mutation';
 import { USStateFormField } from '../mailing-address-fields';
-
-
-const PREV_STEP = () => Routes.locale.loc.accessDates;
-
-const NEXT_STEP = () => Routes.locale.loc.preview;
-
-function renderForm(ctx: FormContext<LandlordDetailsV2Input>): JSX.Element {
-  return (
-    <React.Fragment>
-      <TextualFormField label="Landlord's name" type="text" {...ctx.fieldPropsFor('name')} />
-      <TextualFormField {...ctx.fieldPropsFor('primaryLine')} label="Street address" />
-      <TextualFormField {...ctx.fieldPropsFor('city')} label="City" />
-      <USStateFormField {...ctx.fieldPropsFor('state')} />
-      <TextualFormField {...ctx.fieldPropsFor('zipCode')} label="Zip code" />
-      <ProgressButtons back={PREV_STEP()} isLoading={ctx.isLoading} nextLabel="Preview letter" />
-    </React.Fragment>
-  );
-}
+import { MiddleProgressStep } from '../progress-step-route';
 
 function getIntroText(isLookedUp: boolean|null): JSX.Element {
   return isLookedUp
@@ -52,45 +32,57 @@ function splitLines(text: string): JSX.Element[] {
   return text.split('\n').map((line, i) => <div key={i}>{line}</div>);
 }
 
-function ReadOnlyLandlordDetails(props: {details: AllSessionInfo_landlordDetails}): JSX.Element {
-  const { details } = props;
-
-  return (
-    <div className="content">
-      <dl>
-        <dt><strong>Landlord name</strong></dt>
-        <dd>{details.name}</dd>
-        <br/>
-        <dt><strong>Landlord address</strong></dt>
-        <dd>{splitLines(details.address)}</dd>
-      </dl>
-      <ProgressButtons>
-        <BackButton to={PREV_STEP()} />
-        <Link to={NEXT_STEP()} className="button is-primary is-medium">Preview letter</Link>
-      </ProgressButtons>
-    </div>
-  );
+function ReadOnlyLandlordDetails(props: {
+  details: AllSessionInfo_landlordDetails, 
+  nextStep: string, 
+  prevStep: string 
+}): JSX.Element {
+    const { details, nextStep, prevStep } = props;
+    return (
+      <div className="content">
+        <dl>
+          <dt><strong>Landlord name</strong></dt>
+          <dd>{details.name}</dd>
+          <br/>
+          <dt><strong>Landlord address</strong></dt>
+          <dd>{splitLines(details.address)}</dd>
+        </dl>
+        <ProgressButtons>
+          <BackButton to={prevStep} />
+          <Link to={nextStep} className="button is-primary is-medium">Preview letter</Link>
+        </ProgressButtons>
+      </div>
+    );
 }
 
-export default withAppContext(function LandlordDetailsPage(props: AppContextType): JSX.Element {
-  const { landlordDetails } = props.session;
-
+const LandlordDetailsPage = MiddleProgressStep(props => {
+  const { session } = useContext(AppContext);
+  const { landlordDetails } = session;
   return (
     <Page title="Landlord information">
       <div>
         <h1 className="title is-4 is-spaced">Landlord information</h1>
         {getIntroText(landlordDetails && landlordDetails.isLookedUp)}
         {landlordDetails && landlordDetails.isLookedUp
-          ? <ReadOnlyLandlordDetails details={landlordDetails} />
+          ? <ReadOnlyLandlordDetails details={landlordDetails} nextStep={props.nextStep} prevStep={props.prevStep} />
           : <SessionUpdatingFormSubmitter
               mutation={LandlordDetailsV2Mutation}
               initialState={(session) => exactSubsetOrDefault(session.landlordDetails, BlankLandlordDetailsV2Input)}
-              onSuccessRedirect={NEXT_STEP}
+              onSuccessRedirect={props.nextStep}
             >
-              {renderForm}
+              {ctx => <>
+                <TextualFormField label="Landlord's name" type="text" {...ctx.fieldPropsFor('name')} />
+                <TextualFormField {...ctx.fieldPropsFor('primaryLine')} label="Street address" />
+                <TextualFormField {...ctx.fieldPropsFor('city')} label="City" />
+                <USStateFormField {...ctx.fieldPropsFor('state')} />
+                <TextualFormField {...ctx.fieldPropsFor('zipCode')} label="Zip code" />
+                <ProgressButtons back={props.prevStep} isLoading={ctx.isLoading} nextLabel="Preview letter" />
+              </>}
             </SessionUpdatingFormSubmitter>
         }
       </div>
     </Page>
   );
 });
+
+export default LandlordDetailsPage;

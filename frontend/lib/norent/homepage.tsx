649473a8e1d1f27b481167c5bc728e63e1b1644e
Atul Varma <varmaa@gmail.com>
2020-04-12 09:54:00 -0400

    Add norent fonts.

--- frontend/lib/norent/homepage.tsx
+++ frontend/lib/norent/homepage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Page from "../ui/page";
 
 export const NorentHomepage: React.FC<{}> = () => (
-  <Page title="NoRent.org">
+  <Page title="NoRent.org" withHeading>
     <p>Hello, this is the no rent site.</p>
   </Page>
 );

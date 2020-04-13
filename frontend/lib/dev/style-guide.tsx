e6d11847288f38a042f1db6081861c51fff19011
Atul Varma <varmaa@gmail.com>
2020-04-13 08:52:29 -0400

    Add a very janky style guide.

---
+++ frontend/lib/dev/style-guide.tsx
@@ -0,0 +1,59 @@
+import React, { useContext } from "react";
+import Page from "../ui/page";
+import { Link } from "react-router-dom";
+import { AppContext } from "../app-context";
+import {
+  NextButton,
+  BackButton,
+  CenteredPrimaryButtonLink,
+} from "../ui/buttons";
+import { BigList } from "../ui/big-list";
+
+const PathLink: React.FC<{ to: string }> = ({ to }) => (
+  <Link to={to}>{to}</Link>
+);
+
+export const StyleGuide: React.FC<{}> = () => {
+  const { dev } = useContext(AppContext).siteRoutes;
+
+  return (
+    <Page title="Style guide" withHeading="big" className="content">
+      <p>Here is a rough style guide for the site.</p>
+      <h2>Buttons</h2>
+      <p>
+        The centered primary button link can be used for major calls to action.
+      </p>
+      <CenteredPrimaryButtonLink to={dev.home}>
+        Centered primary button link
+      </CenteredPrimaryButtonLink>
+      <p>
+        The back button can be used to go back to the previous step of a
+        process.
+      </p>
+      <p>
+        <BackButton to={dev.home} />
+      </p>
+      <p>
+        The next button can be used to submit the current form. It also has a
+        loading state.
+      </p>
+      <p>
+        <NextButton isLoading={false} /> <NextButton isLoading={true} />
+      </p>
+      <h2>Forms</h2>
+      <p>
+        For examples of how forms look, see <PathLink to={dev.examples.form} />{" "}
+        and <PathLink to={dev.examples.radio} />.
+      </p>
+      <h2>Modals</h2>
+      <p>
+        See <PathLink to={dev.examples.modal} />.
+      </p>
+      <h2>Big list</h2>
+      <BigList>
+        <li>Here is item one.</li>
+        <li>Here is item two.</li>
+      </BigList>
+    </Page>
+  );
+};

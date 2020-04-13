725f96f7ebc8f9e19f4407f6fd17dd78a3b0ad14
Atul Varma <varmaa@gmail.com>
2020-04-13 08:57:45 -0400

    Link to style guide instead of /dev/.

--- frontend/lib/norent/homepage.tsx
+++ frontend/lib/norent/homepage.tsx
@@ -7,8 +7,8 @@ export const NorentHomepage: React.FC<{}> = () => (
   <Page title="NoRent.org" withHeading="big" className="content">
     <p>Hello, this is the no rent site.</p>
     <p>
-      Not much is here right now, but you can visit{" "}
-      <Link to={NorentRoutes.dev.home}>{NorentRoutes.dev.home}</Link>.
+      Not much is here right now, but you can visit the{" "}
+      <Link to={NorentRoutes.dev.styleGuide}>style guide</Link>.
     </p>
   </Page>
 );

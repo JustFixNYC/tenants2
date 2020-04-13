e2857d247ae714c5e4cefc461a927a2d97b8db21
Atul Varma <varmaa@gmail.com>
2020-04-13 08:21:14 -0400

    Add a link to /dev/ from homepage.

--- frontend/lib/norent/homepage.tsx
+++ frontend/lib/norent/homepage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Page from "../ui/page";
+import { Link } from "react-router-dom";
+import { NorentRoutes } from "./routes";
 
 export const NorentHomepage: React.FC<{}> = () => (
-  <Page title="NoRent.org" withHeading>
+  <Page title="NoRent.org" withHeading="big" className="content">
     <p>Hello, this is the no rent site.</p>
+    <p>Not much is here right now, but you can visit <Link to={NorentRoutes.dev.home}>{NorentRoutes.dev.home}</Link>.</p>
   </Page>
 );

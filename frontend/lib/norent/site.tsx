7aa7854010e1083065fb7d5389b97544176c7676
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-14 11:22:56 -0400

    added header links and updated styling

--- frontend/lib/norent/site.tsx
+++ frontend/lib/norent/site.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppSiteProps } from "../app";
 import { NorentRoutes as Routes } from "./routes";
-import { RouteComponentProps, Switch, Route } from "react-router-dom";
+import { RouteComponentProps, Switch, Route, Link } from "react-router-dom";
 import { NotFound } from "../pages/not-found";
 import { NorentHomepage } from "./homepage";
 import {
@@ -12,6 +12,7 @@ import {
 import loadable from "@loadable/component";
 import Navbar from "../ui/navbar";
 import { NorentLetterRoutes } from "./letter-builder";
+import { AppContext } from "../app-context";
 
 const LoadableDevRoutes = loadable(() => friendlyLoad(import("../dev/dev")), {
   fallback: <LoadingPage />,
@@ -35,11 +36,39 @@ const NorentRoute: React.FC<RouteComponentProps> = (props) => {
   );
 };
 
+const NorentMenuItems: React.FC<{}> = () => {
+  const { session } = useContext(AppContext);
+  // These are placeholders just to show styling.
+  // Will replace when we have site scaffolding ready.
+  return (
+    <>
+      <Link className="navbar-item" to={Routes.locale.letter.latestStep}>
+        Build my letter
+      </Link>
+      <Link className="navbar-item" to={Routes.locale.home}>
+        Faqs
+      </Link>
+      <Link className="navbar-item" to={Routes.locale.home}>
+        About
+      </Link>
+      {session.phoneNumber ? (
+        <Link className="navbar-item" to={Routes.locale.home}>
+          Sign out
+        </Link>
+      ) : (
+        <Link className="navbar-item" to={Routes.locale.home}>
+          Sign in
+        </Link>
+      )}
+    </>
+  );
+};
+
 const NorentSite = React.forwardRef<HTMLDivElement, AppSiteProps>(
   (props, ref) => {
     return (
       <>
-        <Navbar />
+        <Navbar menuItemsComponent={NorentMenuItems} />
         <section className="section">
           <div
             className="container"

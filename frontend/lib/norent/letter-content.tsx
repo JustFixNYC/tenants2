9cf84b80631e2f0757467490ec1047a4782d1976
Atul Varma <varmaa@gmail.com>
2020-04-16 13:24:32 -0400

    Improve NoRent letter copy (#1201)
    This is at least a bit better than the placeholder text.

--- frontend/lib/norent/letter-content.tsx
+++ frontend/lib/norent/letter-content.tsx
@@ -6,12 +6,75 @@ import { AppContext } from "../app-context";
 import { NotFound } from "../pages/not-found";
 import { Route } from "react-router-dom";
 import { AllSessionInfo_norentScaffolding } from "../queries/AllSessionInfo";
+import { friendlyDate } from "../util/util";
+import { formatPhoneNumber } from "../forms/phone-number-form-field";
 
-const LetterContent: React.FC<AllSessionInfo_norentScaffolding> = (props) => {
+// TODO: This is temporary, it should be passed in as a prop.
+const NONPAY_START_DATE = "2020-05-01T15:41:37.114Z";
+
+export type NorentLetterContentProps = AllSessionInfo_norentScaffolding;
+
+export const NorentLetterContent: React.FC<NorentLetterContentProps> = (
+  props
+) => {
+  const landlordName = props.landlordName.toUpperCase();
+  const nonpayStartDate = friendlyDate(new Date(NONPAY_START_DATE));
+  const fullName = `${props.firstName} ${props.lastName}`;
+
   return (
     <>
+      {/*
+       * We originally had a <br> in this <h1>, but React self-closes the
+       * tag as <br/>, which WeasyPrint doesn't seem to like, so we'll
+       * include an actual newline and set the style to preserve whitespace.
+       */}
+      <h1 className="has-text-right" style={{ whiteSpace: "pre" }}>
+        <span className="is-uppercase">Notice of non-payment of rent</span>
+        {"\n"}
+        at {props.street}, {props.city}, {props.state} {props.zipCode}
+      </h1>
+
+      <p className="has-text-right">{friendlyDate(new Date())}</p>
+
+      <dl className="jf-letter-heading">
+        <dt>To</dt>
+        <dd>
+          {landlordName}
+          <br />
+          {props.landlordPrimaryLine}
+          <br />
+          {props.landlordCity}, {props.landlordState} {props.landlordZipCode}
+        </dd>
+        <dt>From</dt>
+        <dd>
+          {fullName}
+          <br />
+          {props.street}
+          <br />
+          {props.city}, {props.state} {props.zipCode}
+          <br />
+          {formatPhoneNumber(props.phoneNumber)}
+          <br />
+          {props.email}
+        </dd>
+      </dl>
+
+      <p>Dear {landlordName},</p>
+
       <p>
-        TODO: Create letter content for {props.firstName} {props.lastName}
+        This letter is to notify you that I will be unable to pay rent starting
+        on {nonpayStartDate} and until further notice due to loss of income,
+        increased expenses, and/or other financial circumstances related to
+        COVID-19.
+      </p>
+
+      <p>Thank you for your understanding and cooperation.</p>
+
+      <p className="jf-signature">
+        Regards,
+        <br />
+        <br />
+        {fullName}
       </p>
     </>
   );
@@ -36,7 +99,7 @@ export const NorentLetterStaticPage: React.FC<{ isPdf?: boolean }> = ({
             isPdf={isPdf}
             css={output.letterStyles}
           >
-            <LetterContent {...norentScaffolding} />
+            <NorentLetterContent {...norentScaffolding} />
           </LetterStaticPage>
         );
       }}

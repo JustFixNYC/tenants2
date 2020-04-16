9a04b023c878e9e102cf511db6c7f10393a71dba
Atul Varma <varmaa@gmail.com>
2020-04-16 09:18:34 -0400

    Run prettier.

--- frontend/lib/dev/example-static-page-pdf.tsx
+++ frontend/lib/dev/example-static-page-pdf.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StaticPage } from "../static-page";
 
 export const ExampleStaticPagePDF: React.FC<{}> = () => (
-  <StaticPage httpHeaders={{'Content-Type': 'application/pdf'}}>
+  <StaticPage httpHeaders={{ "Content-Type": "application/pdf" }}>
     <html>
       {/* Yes, this is valid HTML5. */}
       <meta charSet="utf-8" />

584bf67f3d6650044231761b42982041d942449d
Atul Varma <varmaa@gmail.com>
2020-04-18 07:40:09 -0400

    Add another test.

--- norent/tests/test_models.py
+++ norent/tests/test_models.py
@@ -1,3 +1,6 @@
+from django.core.exceptions import ValidationError
+import pytest
+
 from .factories import NationalOnboardingInfoFactory
 
 
@@ -5,3 +8,8 @@ class TestNationalOnboardingInfo:
     def test_it_works(self, db):
         onb = NationalOnboardingInfoFactory()
         onb.full_clean()
+
+    def test_it_raises_validation_error_on_invalid_state(self, db):
+        onb = NationalOnboardingInfoFactory(state='ZZ')
+        with pytest.raises(ValidationError):
+            onb.full_clean()

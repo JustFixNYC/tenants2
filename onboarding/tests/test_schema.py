858abeedf32a8d254f9c629efecc482c88660386
Atul Varma <varmaa@gmail.com>
2020-04-18 16:31:52 -0400

    Add yet another assertion.

--- onboarding/tests/test_schema.py
+++ onboarding/tests/test_schema.py
@@ -113,6 +113,7 @@ def test_onboarding_works(graphql_client, smsoutbox, mailoutbox):
     assert oi.state == "NY"
     assert oi.needs_repairs is None
     assert oi.lease_type == 'MARKET_RATE'
+    assert oi.receives_public_assistance is False
     assert len(smsoutbox) == 1
     assert smsoutbox[0].to == "+15551234567"
     assert "Welcome to JustFix.nyc, boop" in smsoutbox[0].body

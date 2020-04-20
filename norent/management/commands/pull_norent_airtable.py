c083a4a6e4db7cab0098e8258c4eaaf2221d3e51
Atul Varma <varmaa@gmail.com>
2020-04-20 15:29:35 -0400

    Add tests, generated JSON.

--- norent/management/commands/pull_norent_airtable.py
+++ norent/management/commands/pull_norent_airtable.py
@@ -1,12 +1,13 @@
 import urllib.parse
 import json
+from typing import Any, List, Dict, Iterator
 from enum import Enum
 from pathlib import Path
 from django.core.management import BaseCommand, CommandError
 from django.conf import settings
 
 from project.common_data import COMMON_DATA_DIR
-from airtable.api import Airtable
+from airtable.api import Airtable, RawRow
 
 
 MY_DIR = Path(__file__).parent.resolve()
@@ -17,12 +18,40 @@ BASE_URL = "https://api.airtable.com/v0"
 # and this is likely a temporary solution anyways (famous last words).
 NORENT_VARIABLES_BASE_ID = "appcHw4ksitvRgc4y"
 
+StateDict = Dict[str, Any]
+
 
 class Table(Enum):
     STATE_LAW_FOR_BUILDER = "State Legislation for Letter Builder Screens"
     STATE_LAW_FOR_LETTER = "State Legislation for letter itself"
     STATE_PARTNERS_FOR_BUILDER = "State Partner Organizations for Letter Builder Screens"
     DOCUMENTATION_REQUIREMENTS = "Documentation Requirements"
+
+
+def convert_rows_to_state_dict(table: Table, rows: Iterator[RawRow]) -> StateDict:
+    '''
+    Convert raw Airtable rows into a table that maps state codes
+    to metadata about the states.
+    '''
+
+    states: StateDict = {}
+    for row in rows:
+        fields: Dict[str, Any] = row['fields']
+
+        # Only some of our tables have an 'ID' column, for some reason, which we
+        # don't actually need, so remove it.
+        if 'ID' in fields:
+            fields.pop('ID')
+
+        if not fields:
+            # It's an empty row.
+            continue
+
+        state = fields.pop('State')
+        assert state not in states, f"{state} should only have one row"
+        if fields:
+            states[state] = fields
+    return states
 
 
 class Command(BaseCommand):
@@ -33,11 +62,17 @@ class Command(BaseCommand):
             url=url,
             api_key=settings.AIRTABLE_API_KEY,
         )
-        rows = list(api.list_raw())
+        rows = convert_rows_to_state_dict(table, api.list_raw())
         basename = table.name.lower().replace('_', '-')
         output_path = COMMON_DATA_DIR / f"norent-{basename}.json"
         print(f"Writing {output_path}.")
-        output_path.write_text(json.dumps(rows, indent="  "))
+
+        json_blob = json.dumps(rows, indent='  ', sort_keys=True)
+
+        # Prettier wants newlines at the end of JSON files, so we'll add it.
+        json_blob = f"{json_blob}\n"
+
+        output_path.write_text(json_blob)
 
     def handle(self, *args, **options):
         if not settings.AIRTABLE_API_KEY:

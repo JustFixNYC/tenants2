19c997239b646d79f56b87eb34328616ca3b1713
Atul Varma <varmaa@gmail.com>
2020-04-22 19:13:54 -0400

    Add docs for react component.

--- frontend/lib/ui/demo-deployment-note.tsx
+++ frontend/lib/ui/demo-deployment-note.tsx
@@ -5,6 +5,13 @@ export type DemoDeploymentNoteProps = {
   children: JSX.Element | JSX.Element[];
 };
 
+/**
+ * If the current deployment happens to be a demo intended for
+ * instructional and training purposes, this component displays a note
+ * that explains how this demo differs from "the real thing".
+ *
+ * If the current deployment is *not* a demo, nothing will be shown.
+ */
 export const DemoDeploymentNote: React.FC<DemoDeploymentNoteProps> = ({
   children,
 }) => {

5e475e087502dfd38632ff1d244336c196aa9999
Atul Varma <varmaa@gmail.com>
2020-04-26 19:53:38 -0400

    Add address-finding mapbox code (#1302)
    This adds non-NYC address validation to the NoRent flow, done via Mapbox.
    
    As with the Lob verification of the landlord address, we don't actually _prevent_ the user from entering an invalid address, as we don't fully trust Mapbox's results (or our own additional validation logic) at this stage.  Instead, we're adding an additional `isValid` field to the `NorentNationalAddressMutation` that indicates whether the address is valid or not.

--- project/mapbox.py
+++ project/mapbox.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, List
+from typing import Optional, Dict, List, NamedTuple
 import re
 import urllib.parse
 import pydantic
@@ -13,18 +13,29 @@ MAPBOX_PLACES_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 
 MAPBOX_STATE_SHORT_CODE_RE = r"^US-([A-Z][A-Z])$"
 
+MAPBOX_CITY_ID_RE = r"^(place|locality)\..*"
+
 
 class MapboxFeatureContext(pydantic.BaseModel):
+    id: str
+    text: str
     short_code: Optional[str]
 
 
 class MapboxFeature(pydantic.BaseModel):
     context: List[MapboxFeatureContext]
     text: str
+    address: Optional[str]
+    place_type: List[str]
 
 
 class MapboxResults(pydantic.BaseModel):
     features: List[MapboxFeature]
+
+
+class StreetAddress(NamedTuple):
+    address: str
+    zip_code: str
 
 
 def mapbox_places_request(query: str, args: Dict[str, str]) -> Optional[MapboxResults]:
@@ -43,6 +54,7 @@ def mapbox_places_request(query: str, args: Dict[str, str]) -> Optional[MapboxRe
             {
                 'access_token': settings.MAPBOX_ACCESS_TOKEN,
                 'country': 'US',
+                'autocomplete': 'false',
                 **args,
             },
             timeout=settings.MAPBOX_TIMEOUT
@@ -56,7 +68,7 @@ def mapbox_places_request(query: str, args: Dict[str, str]) -> Optional[MapboxRe
 
 def find_city(city: str, state: str) -> Optional[List[str]]:
     '''
-    Attempts to find the match for the closest city name in the given
+    Attempts to find matches for the closest city name in the given
     state using the Mapbox Places API.  The return value is a list of
     cities in the given state that match the query.
 
@@ -78,6 +90,52 @@ def find_city(city: str, state: str) -> Optional[List[str]]:
     return cities
 
 
+def find_address(
+    address: str,
+    city: str,
+    state: str,
+    zip_code: str
+) -> Optional[List[StreetAddress]]:
+    '''
+    Attempts to find matches for the closest street address in the given
+    city and state using the given zip code.
+
+    If Mapbox isn't configured or a network error occurs, returns None.
+    '''
+
+    city = city.strip()
+    results = mapbox_places_request(f"{address}, {city}, {state} {zip_code}", {
+        'types': 'address',
+    })
+    if not results:
+        return None
+    addrs: List[StreetAddress] = []
+    for result in results.features:
+        state_matches = get_mapbox_state(result) == state
+        result_zip_code = get_mapbox_zip_code(result)
+        if state_matches and result_zip_code and does_city_match(city, result):
+            addrs.append(StreetAddress(
+                address=get_mapbox_street_addr(result),
+                zip_code=result_zip_code
+            ))
+    return addrs
+
+
+def get_mapbox_street_addr(feature: MapboxFeature) -> str:
+    '''
+    Given a Mapbox Feature that represents an address, returns
+    the street address, e.g. "150 Court Street".
+    '''
+
+    assert 'address' in feature.place_type
+
+    # Not really sure if any real-world addresses don't have the address
+    # property, but the Mapbox docs do say it's optional...
+    if feature.address:
+        return f"{feature.address} {feature.text}"
+    return feature.text
+
+
 def get_mapbox_state(feature: MapboxFeature) -> Optional[str]:
     '''
     Returns the two-letter state code for the given Mapbox Feature, if
@@ -89,3 +147,26 @@ def get_mapbox_state(feature: MapboxFeature) -> Optional[str]:
         if match:
             return match[1]
     return None
+
+
+def get_mapbox_zip_code(feature: MapboxFeature) -> Optional[str]:
+    '''
+    Returns the U.S. Zip Code for the given Mapbox Feature, if one
+    exists.
+    '''
+
+    for context in feature.context:
+        if context.id.startswith("postcode."):
+            return context.text
+    return None
+
+
+def does_city_match(city: str, feature: MapboxFeature) -> bool:
+    '''
+    Returns whether the given Mapbox Feature is inside the given city.
+    '''
+
+    for context in feature.context:
+        if re.match(MAPBOX_CITY_ID_RE, context.id) and context.text.lower() == city.lower():
+            return True
+    return False
--- project/tests/test_mapbox.py
+++ project/tests/test_mapbox.py
@@ -8,6 +8,11 @@ from project.mapbox import (
     mapbox_places_request,
     find_city,
     get_mapbox_state,
+    get_mapbox_zip_code,
+    get_mapbox_street_addr,
+    does_city_match,
+    find_address,
+    StreetAddress,
     MapboxFeature,
     MAPBOX_PLACES_URL,
 )
@@ -16,9 +21,22 @@ JSON_DIR = BASE_DIR / 'frontend' / 'lib' / 'forms' / 'mapbox' / 'tests'
 
 BROOKLYN_FEATURE_JSON = json.loads((JSON_DIR / 'brooklyn.json').read_text())
 BROOKLYN_FEATURE = MapboxFeature(**BROOKLYN_FEATURE_JSON)
+BRL_FEATURE_JSON = json.loads((JSON_DIR / 'brl.json').read_text())
+BRL_FEATURE = MapboxFeature(**BRL_FEATURE_JSON)
+BRL_RESULTS_JSON = {
+    'features': [BRL_FEATURE_JSON],
+}
 BROOKLYN_RESULTS_JSON = {
     'features': [BROOKLYN_FEATURE_JSON],
 }
+
+
+def mkfeature(base=BROOKLYN_FEATURE_JSON, **kwargs):
+    final_kwargs = {
+        **base,
+        **kwargs,
+    }
+    return MapboxFeature(**final_kwargs)
 
 
 @pytest.fixture(autouse=True)
@@ -26,17 +44,54 @@ def setup_fixture(settings):
     settings.MAPBOX_ACCESS_TOKEN = 'boop'
 
 
-def mock_brooklyn_results(query: str, requests_mock):
+def mock_places_request(query: str, json_data, requests_mock):
     url = f"{MAPBOX_PLACES_URL}/{urllib.parse.quote(query)}.json"
-    requests_mock.get(url, json=BROOKLYN_RESULTS_JSON)
+    requests_mock.get(url, json=json_data)
+
+
+def mock_brooklyn_results(query: str, requests_mock):
+    mock_places_request(query, BROOKLYN_RESULTS_JSON, requests_mock)
+
+
+def mock_brl_results(query: str, requests_mock):
+    mock_places_request(query, BRL_RESULTS_JSON, requests_mock)
+
+
+def mock_no_results(query: str, requests_mock):
+    mock_places_request(query, {'features': []}, requests_mock)
 
 
 class TestGetMapboxState:
     def test_it_returns_none_on_no_match(self):
-        assert get_mapbox_state(MapboxFeature(context=[], text="blah")) is None
+        assert get_mapbox_state(mkfeature(context=[])) is None
 
     def test_it_returns_state_on_match(self):
         assert get_mapbox_state(BROOKLYN_FEATURE) == "NY"
+
+
+class TestGetMapboxZipCode:
+    def test_it_returns_none_on_no_match(self):
+        assert get_mapbox_zip_code(mkfeature(context=[])) is None
+
+    def test_it_returns_zipcode_on_match(self):
+        assert get_mapbox_zip_code(BRL_FEATURE) == "11201"
+
+
+class TestDoesCityMatch:
+    def test_it_returns_false_on_no_match(self):
+        assert does_city_match('columbus', BRL_FEATURE) is False
+
+    def test_it_returns_true_on_match(self):
+        assert does_city_match('BROOKLYN', BRL_FEATURE) is True
+        assert does_city_match('Brooklyn', BRL_FEATURE) is True
+
+
+@pytest.mark.parametrize('feature,expected', [
+    (BRL_FEATURE, '150 Court Street'),
+    (mkfeature(BRL_FEATURE_JSON, address=None), 'Court Street'),
+])
+def test_get_mapbox_street_addr(feature, expected):
+    assert get_mapbox_street_addr(feature) == expected
 
 
 class TestMapboxPlacesRequest:
@@ -68,6 +123,27 @@ class TestFindCity:
         assert find_city('brook', 'NY') == ['Brooklyn']
 
 
+class TestFindAddress:
+    def test_it_returns_none_on_mapbox_failure(self, settings):
+        settings.MAPBOX_ACCESS_TOKEN = ''
+        assert find_address('zzz', 'blarg', 'OH', '12345') is None
+
+    def test_it_returns_empty_list_when_no_addresses_match(self, requests_mock):
+        mock_brl_results('1 boop st, bespin, OH 12345', requests_mock)
+        assert find_address('1 boop st', 'bespin', 'OH', '12345') == []
+
+    def test_it_returns_nonempty_list_when_addresses_match(self, requests_mock):
+        mock_brl_results('150 court st, brooklyn, NY 12345', requests_mock)
+        assert find_address('150 court st', 'brooklyn', 'NY', '12345') == [
+            StreetAddress('150 Court Street', '11201'),
+        ]
+
+
 def test_findmapboxcity_command_does_not_explode(settings):
     settings.MAPBOX_ACCESS_TOKEN = ''
     call_command('findmapboxcity', 'brooklyn', 'NY')
+
+
+def test_findmapboxaddr_command_does_not_explode(settings):
+    settings.MAPBOX_ACCESS_TOKEN = ''
+    call_command('findmapboxaddr', '150 court st', 'brooklyn', 'NY', '11201')

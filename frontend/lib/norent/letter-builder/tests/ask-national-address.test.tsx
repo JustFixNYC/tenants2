f42eaa1dd3dbfdae5346b35999cb3e76d0626cd0
Atul Varma <varmaa@gmail.com>
2020-04-27 07:07:00 -0400

    Factor out override() helper.

--- frontend/lib/norent/letter-builder/tests/ask-national-address.test.tsx
+++ frontend/lib/norent/letter-builder/tests/ask-national-address.test.tsx
@@ -8,7 +8,7 @@ import {
 } from "../../../queries/NorentNationalAddressMutation";
 import { BlankNorentScaffolding } from "../../../queries/NorentScaffolding";
 import { BlankAllSessionInfo } from "../../../queries/AllSessionInfo";
-import { NorentNationalAddressInput } from "../../../queries/globalTypes";
+import { override } from "../../../tests/util";
 
 const {
   getSuccessRedirect,
@@ -46,51 +46,48 @@ describe("getSuccessRedirect()", () => {
     },
   };
 
-  const VALID_OUTPUT = {
-    ...OUTPUT,
+  const VALID_OUTPUT = override(OUTPUT, {
     isValid: true,
-  };
+  });
 
-  const INPUT: NorentNationalAddressInput = {
-    ...BlankNorentNationalAddressInput,
+  const INVALID_OUTPUT = override(OUTPUT, {
+    isValid: false,
+  });
+
+  const SAME_INPUT = override(BlankNorentNationalAddressInput, {
     street: "150 Court Street",
     zipCode: "11201",
-  };
+  });
+
+  const DIFF_STREET_INPUT = override(SAME_INPUT, {
+    street: "150 court st",
+  });
+
+  const DIFF_ZIP_INPUT = override(SAME_INPUT, {
+    zipCode: "12345",
+  });
 
   it("returns next step when no validation occurred on server", () => {
-    expect(getSuccessRedirect("/next", OUTPUT, INPUT)).toBe("/next");
+    expect(getSuccessRedirect("/next", OUTPUT, SAME_INPUT)).toBe("/next");
   });
 
   it("returns next step when validation matches input", () => {
-    expect(getSuccessRedirect("/next", VALID_OUTPUT, INPUT)).toBe("/next");
+    expect(getSuccessRedirect("/next", VALID_OUTPUT, SAME_INPUT)).toBe("/next");
   });
 
   it("returns confirmation modal when validation does not match input", () => {
     expect(
-      getSuccessRedirect("/next", VALID_OUTPUT, {
-        ...INPUT,
-        street: "150 court st",
-      })
+      getSuccessRedirect("/next", VALID_OUTPUT, DIFF_STREET_INPUT)
     ).toMatch(/\/confirm-modal$/);
 
-    expect(
-      getSuccessRedirect("/next", VALID_OUTPUT, {
-        ...INPUT,
-        zipCode: "12345",
-      })
-    ).toMatch(/\/confirm-modal$/);
+    expect(getSuccessRedirect("/next", VALID_OUTPUT, DIFF_ZIP_INPUT)).toMatch(
+      /\/confirm-modal$/
+    );
   });
 
   it("returns invalid address confirmation modal when validation fails", () => {
-    expect(
-      getSuccessRedirect(
-        "/next",
-        {
-          ...VALID_OUTPUT,
-          isValid: false,
-        },
-        INPUT
-      )
-    ).toMatch(/\/confirm-invalid-modal$/);
+    expect(getSuccessRedirect("/next", INVALID_OUTPUT, SAME_INPUT)).toMatch(
+      /\/confirm-invalid-modal$/
+    );
   });
 });

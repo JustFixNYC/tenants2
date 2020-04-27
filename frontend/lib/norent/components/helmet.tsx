b6b07be3aa6bc0f4df41167df429f158470486a2
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-27 19:05:06 -0400

    added norent helmet with all needed metadata

--- frontend/lib/norent/components/helmet.tsx
+++ frontend/lib/norent/components/helmet.tsx
@@ -1,50 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { getImageSrc } from "../homepage";
+import { useSiteName } from "../../ui/page";
+import { AppContext } from "../../app-context";
 
 const favicon16 = getImageSrc("favicon-16x16", "png");
 const favicon32 = getImageSrc("favicon-32x32", "png");
 const favicon96 = getImageSrc("favicon-96x96", "png");
 
-const SITE_MAIN_URL = "https://www.norent.org";
-const TWITTER_HANDLE = "@JustFixNYC";
 // The public-facing FB App ID associated with the JustFixNYC Facebook organization
 const FB_APP_ID = "247990609143668";
+const TWITTER_HANDLE = "@JustFixNYC";
 
-const title = "NoRent.org";
 const description =
   "Millions of Americans won’t be able to pay rent because of COVID‑19. Use our free tool to take action by writing a letter to your landlord. You're not alone.";
 const keywords = "pay rent, rent, can't pay rent, may rent, may 1";
 const shareImageSrc = getImageSrc("social-share", "png");
 
-export const NorentHelmet = () => (
-  <Helmet
-    link={[
-      { rel: "icon", type: "image/png", sizes: "16x16", href: `${favicon16}` },
-      { rel: "icon", type: "image/png", sizes: "32x32", href: `${favicon32}` },
-      { rel: "shortcut icon", type: "image/png", href: `${favicon96}` },
-      { rel: "canonical", href: `${SITE_MAIN_URL}` },
-    ]}
-  >
-    <meta name="description" content={description} />
-    <meta name="keywords" content={keywords} />
-    <meta name="author" content="JustFix.nyc" />
+export const NorentHelmet = () => {
+  const { server } = useContext(AppContext);
+  const siteName = useSiteName();
+  return (
+    <Helmet
+      link={[
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: `${server.staticURL}${favicon16}`,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: `${server.staticURL}${favicon32}`,
+        },
+        {
+          rel: "shortcut icon",
+          type: "image/png",
+          href: `${server.staticURL}${favicon96}`,
+        },
+        { rel: "canonical", href: `${server.originURL}` },
+      ]}
+    >
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content="JustFix.nyc" />
 
-    <meta property="fb:app_id" content={FB_APP_ID} />
-    <meta property="og:site_name" content={title} />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:url" content={SITE_MAIN_URL} />
-    <meta property="og:image" content={encodeURI(shareImageSrc)} />
-    <meta property="og:type" content="website" />
+      <meta property="fb:app_id" content={FB_APP_ID} />
+      <meta property="og:site_name" content={siteName} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={server.originURL} />
+      <meta property="og:image" content={shareImageSrc} />
+      <meta property="og:type" content="website" />
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content={TWITTER_HANDLE} />
-    <meta name="twitter:creator" content={TWITTER_HANDLE} />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    <meta name="twitter:url" content={SITE_MAIN_URL} />
-    <meta name="twitter:image" content={encodeURI(shareImageSrc)} />
-    <meta name="twitter:image:alt" content={title} />
-  </Helmet>
-);
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={TWITTER_HANDLE} />
+      <meta name="twitter:creator" content={TWITTER_HANDLE} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:url" content={server.originURL} />
+      <meta name="twitter:image" content={shareImageSrc} />
+      <meta name="twitter:image:alt" content="Can't pay rent?" />
+    </Helmet>
+  );
+};

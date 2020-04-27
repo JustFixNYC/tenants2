c9aff93053dcdcb7938bc718b5fae6844eed9e73
Atul Varma <varmaa@gmail.com>
2020-04-27 17:33:01 -0400

    Add special KYR content if user is in state w/o protections.

--- frontend/lib/norent/letter-builder/know-your-rights.tsx
+++ frontend/lib/norent/letter-builder/know-your-rights.tsx
@@ -4,49 +4,89 @@ import Page from "../../ui/page";
 import { Link } from "react-router-dom";
 import { AppContext } from "../../app-context";
 import { BackButton } from "../../ui/buttons";
+import { getUSStateChoiceLabels } from "../../../../common-data/us-state-choices";
 import {
-  getUSStateChoiceLabels,
-  USStateChoice,
-} from "../../../../common-data/us-state-choices";
-import { getNorentMetadataForUSState } from "./national-metadata";
+  StatePartnerForBuilderEntry,
+  getNorentMetadataForUSState,
+  assertIsUSState,
+  NorentMetadataForUSState,
+} from "./national-metadata";
 import { OutboundLink } from "../../analytics/google-analytics";
+
+const StateWithoutProtectionsContent: React.FC<NorentMetadataForUSState> = (
+  props
+) => {
+  return (
+    <>
+      <p>
+        Unfortunately, we do not currently recommend sending a notice of
+        non-payment to your landlord. Sending a notice could put you at risk.
+      </p>
+
+      <div className="content">
+        <p>
+          We’ve partnered with <PartnerLink {...props.partner} /> to provide
+          additional support.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export const PartnerLink: React.FC<StatePartnerForBuilderEntry> = (props) => (
+  <OutboundLink
+    href={props.organizationWebsiteLink}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {props.organizationName}
+  </OutboundLink>
+);
+
+export const StateWithProtectionsContent: React.FC<NorentMetadataForUSState> = (
+  props
+) => (
+  <>
+    <p>{props.lawForBuilder.textOfLegislation}</p>
+
+    <div className="content">
+      <p>
+        We’ve partnered with <PartnerLink {...props.partner} /> to provide
+        additional support once you’ve sent your letter.
+      </p>
+    </div>
+  </>
+);
 
 export const NorentLbKnowYourRights = MiddleProgressStep((props) => {
   const { session } = useContext(AppContext);
-  const state = session.norentScaffolding?.state as USStateChoice;
+  const scf = session.norentScaffolding;
 
-  // Content from national metadata:
-  const legislation = getNorentMetadataForUSState(state)?.lawForBuilder
-    ?.textOfLegislation;
+  if (!scf?.state) {
+    return (
+      <p>
+        Please <Link to={props.prevStep}>go back and choose a state</Link>.
+      </p>
+    );
+  }
 
-  const partner = getNorentMetadataForUSState(state)?.partner;
+  const state = assertIsUSState(scf.state);
+  const stateName = getUSStateChoiceLabels()[state];
+  const metadata = getNorentMetadataForUSState(state);
+  const hasNoProtections = metadata.lawForBuilder.stateWithoutProtections;
 
   return (
     <Page title="Know your rights">
       <h2 className="title">
-        You're in{" "}
-        <span className="has-text-info">
-          {state && getUSStateChoiceLabels()[state]}
-        </span>
+        You're in <span className="has-text-info">{stateName}</span>
       </h2>
 
-      {legislation && <p>{legislation}</p>}
-
-      {partner && (
-        <div className="content">
-          <p>
-            We’ve partnered with{" "}
-            <OutboundLink
-              href={partner.organizationWebsiteLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {partner.organizationName}
-            </OutboundLink>{" "}
-            to provide additional support once you’ve sent your letter.
-          </p>
-        </div>
+      {hasNoProtections ? (
+        <StateWithoutProtectionsContent {...metadata} />
+      ) : (
+        <StateWithProtectionsContent {...metadata} />
       )}
+
       <br />
       <div className="buttons jf-two-buttons">
         <BackButton to={props.prevStep} />

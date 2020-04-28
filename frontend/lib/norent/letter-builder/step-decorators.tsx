fc398d5d2c0e1f54517bf4878a96cbd7e64b500f
Atul Varma <varmaa@gmail.com>
2020-04-28 12:40:16 -0400

    Add some docs.

--- frontend/lib/norent/letter-builder/step-decorators.tsx
+++ frontend/lib/norent/letter-builder/step-decorators.tsx
@@ -16,6 +16,10 @@ import { AllSessionInfo } from "../../queries/AllSessionInfo";
 type MiddleStepComponent = React.ComponentType<MiddleProgressStepProps>;
 type StepComponent = React.ComponentType<ProgressStepProps>;
 
+/**
+ * Returns whether the current user has sent a no rent letter
+ * for the current rent period.
+ */
 export function hasNorentLetterBeenSentForThisRentPeriod(
   s: AllSessionInfo
 ): boolean {
@@ -25,6 +29,9 @@ export function hasNorentLetterBeenSentForThisRentPeriod(
   return letter.paymentDate === rentPeriod.paymentDate;
 }
 
+/**
+ * A step that requires the user to be logged-in to view.
+ */
 export const NorentRequireLoginStep = (c: StepComponent) =>
   withSessionErrorHandling(isUserLoggedOut, NorentNotLoggedInErrorPage, c);
 
@@ -40,8 +47,16 @@ const requireNotSentLetter = (c: StepComponent) =>
     )
   );
 
+/**
+ * A middle step before the user has created an account.
+ */
 export const NorentOnboardingStep = (c: MiddleStepComponent) =>
   requireLogout(MiddleProgressStep(c));
 
+/**
+ * A middle step after the user has created an account, but
+ * before they have sent a no rent letter for the current
+ * rent period.
+ */
 export const NorentNotSentLetterStep = (c: MiddleStepComponent) =>
   requireNotSentLetter(MiddleProgressStep(c));

d1c5596af2ae7757e9d5c9c9fc9a556387e15109
Atul Varma <varmaa@gmail.com>
2020-04-28 21:06:41 -0400

    Add tests and a SessionBuilder.

--- frontend/lib/norent/letter-builder/tests/know-your-rights.test.tsx
+++ frontend/lib/norent/letter-builder/tests/know-your-rights.test.tsx
@@ -1,8 +1,12 @@
 import { AppTesterPal } from "../../../tests/app-tester-pal";
-import { NorentLbKnowYourRights } from "../know-your-rights";
+import {
+  NorentLbKnowYourRights,
+  hasUserSeenRttcCheckboxYet,
+} from "../know-your-rights";
 import { override } from "../../../tests/util";
 import { BlankNorentScaffolding } from "../../../queries/NorentScaffolding";
 import { createProgressStepJSX } from "../../../progress/tests/progress-step-test-util";
+import { newSb } from "../../../tests/session-builder";
 
 describe("<NorentLbKnowYourRights>", () => {
   afterEach(AppTesterPal.cleanup);
@@ -25,5 +29,40 @@ describe("<NorentLbKnowYourRights>", () => {
   it("dissuades user for states w/o protections", () => {
     const pal = createPal("GA");
     pal.rr.getByText(/unfortunately.+we do not currently recommend/i);
+  });
+
+  it("asks users to go to previous step if they have no state set", () => {
+    const pal = createPal("");
+    pal.rr.getByText(/go back and choose a state/i);
+  });
+
+  it("defaults RTTC checkbox to checked", () => {
+    const pal = createPal("NY");
+    const checkbox = pal.rr.getByLabelText(
+      /contact me to provide/
+    ) as HTMLInputElement;
+    expect(checkbox.checked).toBe(true);
+  });
+});
+
+describe("hasUserSeenRttcCheckboxYet", () => {
+  it("returns false", () => {
+    expect(hasUserSeenRttcCheckboxYet(newSb().value)).toBe(false);
+  });
+
+  it("returns true", () => {
+    for (let canReceiveRttcComms of [true, false]) {
+      expect(
+        hasUserSeenRttcCheckboxYet(
+          newSb().withNorentScaffolding({ canReceiveRttcComms }).value
+        )
+      ).toBe(true);
+
+      expect(
+        hasUserSeenRttcCheckboxYet(
+          newSb().withOnboardingInfo({ canReceiveRttcComms }).value
+        )
+      ).toBe(true);
+    }
   });
 });

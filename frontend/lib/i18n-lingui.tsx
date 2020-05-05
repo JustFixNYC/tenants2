a99b9fe6169c5155411883b7f7030d34ba2e106e
Atul Varma <varmaa@gmail.com>
2020-05-05 18:17:36 -0400

    Add docs, fallback component.

--- frontend/lib/i18n-lingui.tsx
+++ frontend/lib/i18n-lingui.tsx
@@ -5,6 +5,14 @@ import { I18nProvider } from "@lingui/react";
 import i18n from "./i18n";
 import { setupI18n as linguiSetupI18n } from "@lingui/core";
 
+/**
+ * We use code splitting to make sure that we only load the message
+ * catalog needed for our currently selected locale.
+ *
+ * This defines the type of component whose children prop is a
+ * callable that receives a Lingui message catalog as its only
+ * argument.
+ */
 export type LoadableCatalog = LoadableLibrary<Catalog>;
 
 const EnCatalog: LoadableCatalog = loadable.lib(
@@ -15,6 +23,10 @@ const EsCatalog: LoadableCatalog = loadable.lib(
   () => import("../../locales/es/messages") as any
 );
 
+/**
+ * Returns a component that loads the Lingui message catalog for
+ * the given string.
+ */
 function getLinguiCatalogForLanguage(locale: string): LoadableCatalog {
   switch (locale) {
     case "en":
@@ -25,10 +37,6 @@ function getLinguiCatalogForLanguage(locale: string): LoadableCatalog {
   throw new Error(`Unsupported locale "${locale}"`);
 }
 
-type LinguiI18nProps = {
-  children: React.ReactNode;
-};
-
 const SetupI18n: React.FC<
   LinguiI18nProps & {
     locale: string;
@@ -36,6 +44,8 @@ const SetupI18n: React.FC<
   }
 > = (props) => {
   const { locale, catalog } = props;
+
+  // This useMemo() call might be overkill. -AV
   const ourLinguiI18n = useMemo(() => {
     li18n.load({
       [locale]: catalog,
@@ -51,16 +61,41 @@ const SetupI18n: React.FC<
   );
 };
 
+export type LinguiI18nProps = {
+  /** Children to render once localization data is loaded. */
+  children: React.ReactNode;
+};
+
+/**
+ * Loads the Lingui message catalog for the currently selected
+ * locale, as dictated by our global i18n module. Children
+ * will then be rendered with the catalog loaded and ready
+ * to translate.
+ *
+ * While a loading message will appear while the catalog is being loaded,
+ * because we do server-side rendering and pre-load JS bundles in the
+ * server-rendered HTML output, the user won't see the message most
+ * (possibly all) of the time.
+ *
+ * Note that this component is currently a singleton; more than one
+ * instance of it should never exist in a component tree at once.
+ */
 export const LinguiI18n: React.FC<LinguiI18nProps> = (props) => {
   const locale = i18n.locale;
 
   const Catalog = getLinguiCatalogForLanguage(locale);
 
   return (
-    <Catalog>
+    <Catalog fallback={<p>Loading locale data...</p>}>
       {(catalog) => <SetupI18n {...props} locale={locale} catalog={catalog} />}
     </Catalog>
   );
 };
 
+/**
+ * A global instance of Lingui's I18n object, which can be used to perform
+ * localization outside of React JSX. Note that this object is populated
+ * by the <LinguiI18n> component, however, so it should only really be
+ * used by components that exist below it in the hierarchy.
+ */
 export const li18n = linguiSetupI18n();

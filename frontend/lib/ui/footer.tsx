4b0ce8457f78dd2276255edab8da6301072a828b
Sam Raby <sraby@users.noreply.github.com>
2020-05-07 09:05:50 -0400

    Dynamically add a credit to LHI in the footer (#1399)
    This PR credits LHI for helping with the implementation of EHP. It was requested that we credit them via a bit of text in the footer on all EHP pages.

--- frontend/lib/ui/footer.tsx
+++ frontend/lib/ui/footer.tsx
@@ -1,42 +1,61 @@
 import React from "react";
 import { PrivacyPolicyLink, TermsOfUseLink } from "./privacy-info-modal";
+import Routes from "../routes";
+import { ROUTE_PREFIX } from "../util/route-util";
 
-export const Footer: React.FC<{}> = () => (
-  <footer>
-    <div className="container">
-      <div className="columns">
-        <div className="column is-8">
-          <div className="content">
+const CreditForLHI = (props: { pathname?: string }) =>
+  /* Include credit for LHI only on ehp routes */
+  props.pathname &&
+  props.pathname.startsWith(Routes.locale.ehp[ROUTE_PREFIX]) ? (
+    <p>
+      Developed with{" "}
+      <a href="https://lawhelpinteractive.org/">Law Help Interactive</a>
+    </p>
+  ) : (
+    <></>
+  );
+
+export const Footer = (props: { pathname?: string }) => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-8">
+            <div className="content">
+              <p>
+                Disclaimer: The information in JustFix.nyc does not constitute
+                legal advice and must not be used as a substitute for the advice
+                of a lawyer qualified to give advice on legal issues pertaining
+                to housing. We can help direct you to free legal services if
+                necessary.
+              </p>
+              <p>
+                JustFix.nyc is a registered 501(c)(3) nonprofit organization.
+              </p>
+            </div>
+          </div>
+          <div className="column is-4 has-text-right content">
             <p>
-              Disclaimer: The information in JustFix.nyc does not constitute
-              legal advice and must not be used as a substitute for the advice
-              of a lawyer qualified to give advice on legal issues pertaining to
-              housing. We can help direct you to free legal services if
-              necessary.
+              Made with NYC ♥ by the team at{" "}
+              <a href="https://justfix.nyc">JustFix.nyc</a>
             </p>
-            <p>JustFix.nyc is a registered 501(c)(3) nonprofit organization.</p>
+            <CreditForLHI pathname={props.pathname} />
           </div>
         </div>
-        <div className="column is-4 has-text-right content">
-          <p>
-            Made with NYC ♥ by the team at{" "}
-            <a href="https://justfix.nyc">JustFix.nyc</a>
-          </p>
+        <div className="columns">
+          <div className="column is-8">
+            <hr />
+            <ul>
+              <li>
+                <PrivacyPolicyLink />
+              </li>
+              <li>
+                <TermsOfUseLink />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="columns">
-        <div className="column is-8">
-          <hr />
-          <ul>
-            <li>
-              <PrivacyPolicyLink />
-            </li>
-            <li>
-              <TermsOfUseLink />
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};

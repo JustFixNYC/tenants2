468e691fe244392c2e84152403f6e9f3b6d3fd88
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-05-11 13:40:30 -0400

    added mention of no-reply@justfix.nyc to email thanks page

--- frontend/lib/onboarding/onboarding-thanks.tsx
+++ frontend/lib/onboarding/onboarding-thanks.tsx
@@ -7,7 +7,8 @@ export const OnboardingThanks: React.FC<{ next: string }> = ({ next }) => {
     <Page title="Thanks for signing up!" className="content has-text-centered">
       <h1>Thanks for signing up!</h1>
       <p className="subtitle is-4 is-marginless">
-        An email to verify your account is on its way.
+        An email to verify your account is on its way, from{" "}
+        <strong>no-reply@justfix.nyc</strong>.
       </p>
       <p>Don't see one? Check your spam folder.</p>
       <br />

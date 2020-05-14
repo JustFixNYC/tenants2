02649e059eccbfb2ac9ec0ada5ff24565f7225b3
Atul Varma <varmaa@gmail.com>
2020-05-14 12:05:12 -0400

    Add more tests.

--- frontend/lib/progress/tests/progress-routes-tester.tsx
+++ frontend/lib/progress/tests/progress-routes-tester.tsx
@@ -8,7 +8,7 @@ import {
 } from "../progress-routes";
 import { AllSessionInfo } from "../../queries/AllSessionInfo";
 import { FakeSessionInfo } from "../../tests/util";
-import { AppTesterPal } from "../../tests/app-tester-pal";
+import { AppTesterPal, AppTesterPalOptions } from "../../tests/app-tester-pal";
 import { ProgressStepRoute, getBestNextStep } from "../progress-step-route";
 import { SessionBuilder } from "../../tests/session-builder";
 
@@ -19,7 +19,11 @@ export class ProgressRoutesTester {
   /** A concatenation of all the steps in the flow. */
   readonly allSteps: ProgressStepRoute[];
 
-  constructor(readonly props: ProgressRoutesProps, readonly name: string) {
+  constructor(
+    readonly props: ProgressRoutesProps,
+    readonly name: string,
+    readonly appTesterPalOptions: Partial<AppTesterPalOptions> = {}
+  ) {
     this.allSteps = getAllSteps(props);
   }
 
@@ -39,6 +43,33 @@ export class ProgressRoutesTester {
    */
   getNextSteps(count: number, sb: SessionBuilder, path = this.firstStep) {
     return Array.from(this.nextStepIterator(sb, path)).slice(0, count);
+  }
+
+  /**
+   * Helper to define a test that runs through a series of steps
+   * given by an initial session and initial step, making assertions
+   * at each step along the way.
+   */
+  defineTest(options: DefineTestOptions) {
+    it(options.it, () => {
+      const { usingSession: sb, expectSteps } = options;
+      const path = options.startingAtStep || this.firstStep;
+      const steps = this.getNextSteps(expectSteps.length, sb, path);
+      expect(steps.length).toBe(expectSteps.length);
+      const el = this.render();
+      const pal = new AppTesterPal(el, {
+        ...this.appTesterPalOptions,
+        session: sb.value,
+        url: path,
+      });
+      for (let i = 0; i < steps.length; i++) {
+        const step = steps[i];
+        const sa = normalizeStepAssertion(expectSteps[i]);
+        expect(step).toBe(sa.url);
+        pal.history.push(step);
+        sa.test(pal);
+      }
+    });
   }
 
   /**
@@ -70,9 +101,46 @@ export class ProgressRoutesTester {
     describe(`${this.name} steps`, () => {
       this.allSteps.forEach((step) => {
         it(`${step.path} renders without throwing`, () => {
-          new AppTesterPal(this.render(), { url: step.path });
+          new AppTesterPal(this.render(), {
+            ...this.appTesterPalOptions,
+            url: step.path,
+          });
         });
       });
     });
   }
 }
+
+type FullStepAssertion = { url: string; test: AssertionFn };
+
+type StepAssertion = string | FullStepAssertion;
+
+type AssertionFn = (pal: AppTesterPal) => void;
+
+const noopAsserter: AssertionFn = () => {};
+
+function normalizeStepAssertion(sa: StepAssertion): FullStepAssertion {
+  return typeof sa === "string" ? { url: sa, test: noopAsserter } : sa;
+}
+
+/** Options for `ProgressRoutesTester.defineTest()`. */
+type DefineTestOptions = {
+  /** The name of the test, used as the first parameter for Jest's `it()` function. */
+  it: string;
+
+  /** The session used by the test. */
+  usingSession: SessionBuilder;
+
+  /** The step to start at; defaults to the first step. */
+  startingAtStep?: string;
+
+  /**
+   * What steps the progress routes are expected to go through, not including
+   * the initial step.
+   *
+   * Each entry in the array can be either a string identifying the URL we
+   * expect to be at, or an object identifying the URL and a test function
+   * that can make further assertions about the content of the step.
+   */
+  expectSteps: StepAssertion[];
+};

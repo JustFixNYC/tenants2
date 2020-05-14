520a90d0cac9185e09989e23174721a48c4248e1
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-05-14 09:23:07 -0400

    added basic tests for letter preview

--- frontend/lib/norent/letter-builder/tests/letter-preview.test.tsx
+++ frontend/lib/norent/letter-builder/tests/letter-preview.test.tsx
@@ -1,54 +1,46 @@
 import { AppTesterPal } from "../../../tests/app-tester-pal";
-import { NorentConfirmation, CANCEL_RENT_PETITION_URL } from "../confirmation";
 import { createProgressStepJSX } from "../../../progress/tests/progress-step-test-util";
 import { override } from "../../../tests/util";
-import { initNationalMetadataForTesting } from "./national-metadata-test-util";
-import { BlankOnboardingInfo } from "../../../queries/OnboardingInfo";
+import { BlankLandlordDetailsType } from "../../../queries/LandlordDetailsType";
+import { NorentLetterPreviewPage } from "../letter-preview";
 
-beforeAll(initNationalMetadataForTesting);
-
-describe("NoRent confirmation page", () => {
-  const createPal = (state: string, trackingNumber?: string) => {
-    return new AppTesterPal(createProgressStepJSX(NorentConfirmation), {
+describe("NoRent letter preview page", () => {
+  const createPal = (email?: string, address?: string) => {
+    return new AppTesterPal(createProgressStepJSX(NorentLetterPreviewPage), {
       session: {
         phoneNumber: "1234567890",
-        onboardingInfo: override(BlankOnboardingInfo, {
-          state,
+        landlordDetails: override(BlankLandlordDetailsType, {
+          email,
+          address,
         }),
-        norentLatestLetter: trackingNumber
-          ? {
-              trackingNumber: trackingNumber,
-              letterSentAt: null,
-              paymentDate: "Boop 1st, 2099",
-            }
-          : null,
       },
     });
   };
 
   it("should work", () => {
-    const pal = createPal("NY", "1234Boopy");
-    pal.rr.getByText("You've sent your letter");
-    expect(pal.rr.getByText("Sign the petition").getAttribute("href")).toBe(
-      CANCEL_RENT_PETITION_URL
-    );
+    const pal = createPal("landlordo@gmail.com", "123 Boop Lane");
+    pal.rr.getByText(/let's review what will be sent/i);
+    pal.rr.getByText(/will be mailing this letter/i);
+    pal.rr.getByText(/Here’s a preview of the email/i);
   });
 
-  it("should work for users sending physical letters", () => {
-    const pal = createPal("NY", "1234Boopy");
-    pal.rr.getByText(/mailed to your landlord via USPS/i);
-    pal.rr.getByText(/USPS Tracking #:/i);
+  it("renders specific text for user emailing letter", () => {
+    const pal = createPal("landlordo@gmail.com");
+    pal.rr.getByText(/Here’s a preview of the email/i);
+    pal.rr.getByText(/will be attached in an email to your landlord/i);
+    expect(pal.rr.queryByText(/will be mailing this letter/i)).toBe(null);
   });
 
-  it("should work for users who only emailed letter", () => {
-    const pal = createPal("NY");
-    pal.rr.getByText(/sent to your landlord via email/i);
+  it("renders specific text for user mailing letter", () => {
+    const pal = createPal(undefined, "123 Boop Lane");
+    pal.rr.getByText(/will be mailing this letter/i);
+    expect(pal.rr.queryByText(/Here’s a preview of the email/i)).toBe(null);
   });
 
-  it("renders full state documentation requirements", () => {
-    const pal = createPal("CA");
-    pal.rr.getByText(/California has specific documentation/i);
-    pal.rr.getByText("Find out more");
-    pal.rr.getByText("7 days");
+  it("omits email-specific message if user is also mailing letter", () => {
+    const pal = createPal("landlordo@gmail.com", "123 Boop Lane");
+    expect(
+      pal.rr.queryByText(/will be attached in an email to your landlord/i)
+    ).toBe(null);
   });
 });

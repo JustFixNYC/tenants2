350cea9d4f06f3029e3db34b07a895a1a9fac4a1
Atul Varma <varmaa@gmail.com>
2020-05-15 17:49:28 -0400

    Ensure localization catalogs aren't out of date.

--- frontend/localebuilder/cli.ts
+++ frontend/localebuilder/cli.ts
@@ -10,10 +10,25 @@ import {
   MessageCatalogPaths,
   getAllMessageCatalogPaths,
 } from "./message-catalog-paths";
+import { argvHasOption } from "../querybuilder/util";
+import { checkExtractedMessagesSync } from "./check-extracted-messages";
+import { assertNotUndefined } from "../lib/util/util";
 
 const MY_DIR = __dirname;
 
 const LOCALES_DIR = path.resolve(path.join(MY_DIR, "..", "..", "locales"));
+
+/**
+ * Our default locale, which defines the canonical content of our
+ * messages to be translated.
+ */
+const DEFAULT_LOCALE = "en";
+
+/**
+ * The command to run to extract messages from our source code and
+ * regenerate PO files.
+ */
+const EXTRACT_CMD = "yarn lingui:extract clean";
 
 /**
  * The maximum preferred length of a message id.
@@ -71,7 +86,27 @@ function processLocale(paths: MessageCatalogPaths, validate: boolean) {
  * Main function to run the localebuilder CLI.
  */
 export function run() {
+  if (argvHasOption("-h", "--help")) {
+    console.log(`usage: ${process.argv[1]} [OPTIONS]`);
+    console.log(`options:\n`);
+    console.log("  --check         Ensure PO files are up to date");
+    console.log("  -h / --help     Show this help");
+    console.log("  -v / --version  Show the version number");
+    process.exit(0);
+  }
+
   const allPaths = getAllMessageCatalogPaths(LOCALES_DIR);
+  const defaultPath = allPaths.filter(
+    (path) => path.locale === DEFAULT_LOCALE
+  )[0];
+
+  assertNotUndefined(defaultPath);
+
+  if (argvHasOption("--check")) {
+    checkExtractedMessagesSync(defaultPath.po, EXTRACT_CMD);
+    process.exit(0);
+  }
+
   let validate = true;
   for (let paths of allPaths) {
     processLocale(paths, validate);

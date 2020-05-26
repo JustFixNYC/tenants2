b724afe718c6a12c76ce32ca13f8f510f48fc2dc
Atul Varma <varmaa@gmail.com>
2020-05-26 15:08:15 -0400

    Exclude users who already sent a letter.

--- norent/management/commands/find_norent_users.py
+++ norent/management/commands/find_norent_users.py
@@ -1,7 +1,9 @@
 from typing import List
+from datetime import date
 from django.core.management import BaseCommand
 
 from users.models import JustfixUser
+from norent.models import RentPeriod
 from project import common_data
 
 
@@ -17,9 +19,19 @@ def get_states_with_protections() -> List[str]:
 class Command(BaseCommand):
     def handle(self, *args, **options):
         states = get_states_with_protections()
-        users = list(JustfixUser.objects.filter(
+
+        # The payment date we want to tell people to send a letter for.
+        # We'll be sure *not* to include users who have already sent
+        # a letter for this date.
+        PAYMENT_DATE = date(2020, 6, 1)
+
+        rp = RentPeriod.objects.get(payment_date=PAYMENT_DATE)
+        users = JustfixUser.objects.filter(
             onboarding_info__state__in=states
-        ).prefetch_related('onboarding_info'))
+        ).exclude(
+            norent_letters__rent_period__pk=rp.pk
+        ).prefetch_related('onboarding_info')
+        users = list(users)
         count = len(users)
         for user in users:
             onb = user.onboarding_info

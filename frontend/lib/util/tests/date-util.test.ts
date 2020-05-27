aed5dd739a75ff5f25f32cacbee9097db9672eab
Atul Varma <varmaa@gmail.com>
2020-05-27 15:12:19 -0400

    Fix typo

--- frontend/lib/util/tests/date-util.test.ts
+++ frontend/lib/util/tests/date-util.test.ts
@@ -13,37 +13,32 @@ test("addDays() works", () => {
 
 describe("friendlyDate()", () => {
   const dateStr = "2018-01-02T04:00:00.000Z";
+  const tz = "America/New_York";
 
   beforeEach(() => i18n.initialize("en"));
 
   it("translates to time zone on platforms that support it", () => {
-    expect(friendlyDate(new Date(dateStr), "America/New_York")).toBe(
-      "Monday, January 1, 2018"
-    );
+    expect(friendlyDate(new Date(dateStr), tz)).toBe("Monday, January 1, 2018");
   });
 
   it("falls back to local timezone if platform sucks", () => {
     jest.spyOn(Intl, "DateTimeFormat").mockImplementationOnce(() => {
       throw new Error("i am ie11 and idk what a time zone is");
     });
-    expect(friendlyDate(new Date(dateStr), "America/New_York")).toMatch(
-      /January/
-    );
+    expect(friendlyDate(new Date(dateStr), tz)).toMatch(/January/);
   });
 
   it("falls back to janky string if platform really sucks", () => {
     jest.spyOn(Intl, "DateTimeFormat").mockImplementation(() => {
       throw new Error("i am a really old browser and idk what Intl is");
     });
-    expect(friendlyDate(new Date(dateStr), "America/New_York")).toMatch(
-      /Jan 0/
-    );
+    expect(friendlyDate(new Date(dateStr), tz)).toMatch(/Jan 0/);
   });
 
   it("uses currently active locale", () => {
     i18n.initialize("es");
 
-    expect(friendlyDate(new Date(dateStr), "America/New York")).toBe(
+    expect(friendlyDate(new Date(dateStr), tz)).toBe(
       "lunes, 1 de enero de 2018"
     );
   });

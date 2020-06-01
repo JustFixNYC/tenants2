4cee35c7011afb407643f217281f13b0e2de2625
Atul Varma <varmaa@gmail.com>
2020-06-01 16:53:05 -0400

    Improve stuff

--- project/management/commands/translate.py
+++ project/management/commands/translate.py
@@ -2,7 +2,7 @@ import os
 import requests
 import uuid
 import json
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
@@ -13,21 +13,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options) -> None:
         subscription_key = os.environ['TRANSLATOR_TEXT_SUBSCRIPTION_KEY']
-        endpoint = os.environ['TRANSLATOR_TEXT_ENDPOINT']
-        region = os.environ['TRANSLATOR_TEXT_REGION']
+        endpoint = os.environ.get(
+            'TRANSLATOR_TEXT_ENDPOINT',
+            'https://api.cognitive.microsofttranslator.com'
+        )
+        region = os.environ.get('TRANSLATOR_TEXT_REGION')
         path = '/translate?api-version=3.0'
         params = '&from=es&to=en'
         constructed_url = endpoint + path + params
         headers = {
             'Ocp-Apim-Subscription-Key': subscription_key,
-            'Ocp-Apim-Subscription-Region': region,
             'Content-type': 'application/json',
             'X-ClientTraceId': str(uuid.uuid4())
         }
+        if region:
+            headers['Ocp-Apim-Subscription-Region'] = region
         body = [{
             'text': options['text'],
         }]
         request = requests.post(constructed_url, headers=headers, json=body)
         response = request.json()
-        print(json.dumps(response, sort_keys=True, indent=4,
-                         ensure_ascii=False, separators=(',', ': ')))
+        if request.status_code != 200:
+            print(json.dumps(response, sort_keys=True, indent=4,
+                             ensure_ascii=False, separators=(',', ': ')))
+            raise CommandError('Accessing the Translator API failed!')
+        print(response[0]['translations'][0]['text'])

c73fcddc70a5553f8d30a26e80b5532c6f7aacd3
Atul Varma <varmaa@gmail.com>
2020-06-18 08:08:43 -0400

    Take email as arg, print member info.

--- project/management/commands/mailchimpfun.py
+++ project/management/commands/mailchimpfun.py
@@ -1,23 +1,33 @@
 import os
+import hashlib
+import pprint
 from django.core.management import BaseCommand
 from mailchimp3 import MailChimp
+import requests
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('email')
+
     def handle(self, *args, **options):
+        email: str = options['email']
         MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
         MAILCHIMP_LISTID = os.environ['MAILCHIMP_LISTID']
-        client = MailChimp(mc_api=MAILCHIMP_API_KEY)
+        headers = requests.utils.default_headers()
+        headers['User-Agent'] = "JustFix.nyc Tenant Platform"
+        client = MailChimp(mc_api=MAILCHIMP_API_KEY, request_headers=headers)
 
-        # https://api.mailchimp.com/schema/3.0/Lists/Members/Instance.json
-        client.lists.members.create(MAILCHIMP_LISTID, {
-            'email_address': 'foo@example.com',
-            'status': 'subscribed',
-            'tags': ['Who Owns What'],
+        md5hash = hashlib.md5(email.lower().encode('ascii')).hexdigest()
+        client.lists.members.create_or_update(MAILCHIMP_LISTID, md5hash, {
+            # https://api.mailchimp.com/schema/3.0/Lists/Members/Instance.json
+            'email_address': email,
+            'status_if_new': 'subscribed',
+            'tags': ['blarf'],
             'language': 'es',
         })
 
-#        oof = client.lists.members.all(MAILCHIMP_LISTID)
-#        import pprint
-#        pprint.pprint(oof['members'])
+        member = client.lists.members.get(MAILCHIMP_LISTID, md5hash)
+        pprint.pprint(member)
+
         print("Done.")

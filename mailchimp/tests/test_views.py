8e2ced588947dbf0995c18c7fb275c800d06beed
Atul Varma <varmaa@gmail.com>
2020-06-18 18:13:45 -0400

    Add more tests.

--- mailchimp/tests/test_views.py
+++ mailchimp/tests/test_views.py
@@ -1,21 +1,37 @@
 import pytest
 
+from mailchimp.mailchimp import get_email_hash
+
 
 SUBSCRIBE_PATH = '/mailchimp/subscribe'
+
+VALID_SUBSCRIBE_ARGS = {
+    'language': 'en',
+    'source': 'orgsite',
+    'email': 'boop@jones.com',
+}
 
 
 @pytest.fixture
 def mailchimp(settings):
-    settings.MAILCHIMP_API_KEY = 'boop'
+    settings.MAILCHIMP_API_KEY = '0123456789abcdefffffffffffffffff-us10'
     settings.MAILCHIMP_CORS_ORIGINS = ['https://www.justfix.nyc']
     settings.MAILCHIMP_LIST_ID = '1234'
 
 
 class TestSubscribe:
+    def post(self, client, origin="https://www.justfix.nyc", **kwargs):
+        return client.post(
+            SUBSCRIBE_PATH,
+            data={**VALID_SUBSCRIBE_ARGS, **kwargs},
+            HTTP_ORIGIN=origin,
+        )
+
     def test_it_returns_404_when_mailchimp_is_disabled(
         self, client, disable_locale_middleware
     ):
         assert client.post(SUBSCRIBE_PATH).status_code == 404
+        assert client.get(SUBSCRIBE_PATH).status_code == 404
 
     def test_it_returns_docs_on_get(self, client, mailchimp):
         res = client.get(SUBSCRIBE_PATH)
@@ -23,11 +39,41 @@ class TestSubscribe:
         assert b'Mailchimp subscribe API documentation' in res.content
 
     def test_post_returns_403_on_invalid_origin(self, client, mailchimp):
-        res = client.post(SUBSCRIBE_PATH, data={}, headers={
-            'Origin': 'https://mysite.com'
-        })
+        res = self.post(client, origin="https://mysite.com")
         assert res.status_code == 403
         assert res.json() == {
             'status': 403,
             'errorCode': 'INVALID_ORIGIN',
+        }
+
+    @pytest.mark.parametrize('args,err', {
+        ((('language', 'boop'),), 'INVALID_LANGUAGE'),
+        ((('source', 'boop'),), 'INVALID_SOURCE'),
+        ((('email', 'zzz'),), 'INVALID_EMAIL'),
+    })
+    def test_post_returns_400_on_invalid_args(self, client, mailchimp, args, err):
+        res = self.post(client, **dict(args))
+        assert res.status_code == 400
+        assert res.json() == {
+            'status': 400,
+            'errorCode': err,
+        }
+
+    def test_post_works(self, client, mailchimp, requests_mock):
+        md5hash = get_email_hash('boop@jones.com')
+        base_url = f'https://us10.api.mailchimp.com/3.0/lists/1234/members/{md5hash}'
+        requests_mock.put(
+            base_url,
+            status_code=200,
+            json={}
+        )
+        requests_mock.post(
+            f'{base_url}/tags',
+            status_code=200,
+            json={}
+        )
+        res = self.post(client)
+        assert res.status_code == 200
+        assert res.json() == {
+            'status': 200,
         }

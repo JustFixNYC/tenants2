ec7bce682b964a19860224f0f96dd1fc5a8c2564
Atul Varma <varmaa@gmail.com>
2020-06-18 12:47:39 -0400

    Rename mailchimpfun to mailchimp_subscribe.

--- mailchimp/management/commands/mailchimp_subscribe.py
+++ mailchimp/management/commands/mailchimp_subscribe.py
@@ -6,16 +6,28 @@ from mailchimp import mailchimp
 
 
 class Command(BaseCommand):
+    help = 'Subscribe an email address to MailChimp.'
+
     def add_arguments(self, parser):
         parser.add_argument('email')
+        parser.add_argument(
+            'language',
+            choices=[v.value for v in mailchimp.Language],
+        )
+        parser.add_argument(
+            'source',
+            choices=[v.value for v in mailchimp.SubscribeSource],
+        )
 
     def handle(self, *args, **options):
         email: str = options['email']
+        language = mailchimp.Language(options['language'])
+        source = mailchimp.SubscribeSource(options['source'])
 
         mailchimp.subscribe(
             email=email,
-            language=mailchimp.Language.Spanish,
-            source=mailchimp.SubscribeSource.OrgSite,
+            language=language,
+            source=source,
         )
 
         client = mailchimp.get_client()

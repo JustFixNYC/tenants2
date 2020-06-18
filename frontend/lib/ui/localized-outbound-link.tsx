c3223855f432c2e6a6658fed3aaef408ce747910
Sydney Jones <47163893+Sjones21@users.noreply.github.com>
2020-06-18 14:22:17 -0700

    fixed an accidental modification from another branch

--- frontend/lib/ui/localized-outbound-link.tsx
+++ frontend/lib/ui/localized-outbound-link.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import i18n, { SupportedLocaleMap } from "../i18n";
 import { OutboundLink } from "../analytics/google-analytics";
-import { Trans } from "@lingui/react";
+import { Trans } from "@lingui/macro";
 
 /**
  * A mapping from supported locales to URLs. At minimum,
@@ -11,6 +11,7 @@ import { Trans } from "@lingui/react";
 type PartiallyLocalizedHrefs = {
   en: string;
 } & Partial<SupportedLocaleMap<string>>;
+
 export type LocalizedOutboundLinkProps = {
   /**
    * The human-readable text of the link. It should already be

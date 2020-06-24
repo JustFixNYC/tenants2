74f5a818e4b98d5d50c348b4332e828448496e91
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-06-24 12:36:23 -0400

    added preliminary scaffolding for new email signup form

--- frontend/lib/norent/components/footer.tsx
+++ frontend/lib/norent/components/footer.tsx
@@ -3,52 +3,10 @@ import { FooterLanguageToggle } from "./language-toggle";
 import { NorentRoutes as Routes } from "../routes";
 import { Link } from "react-router-dom";
 import { NorentLogo } from "./logo";
-import { StaticImage } from "../../ui/static-image";
-import { getImageSrc } from "../homepage";
 import { PrivacyPolicyLink, TermsOfUseLink } from "../../ui/privacy-info-modal";
 import { SocialIcons } from "./social-icons";
-import { Trans, t } from "@lingui/macro";
-import { li18n } from "../../i18n-lingui";
-
-const MAILCHIMP_URL =
-  "https://nyc.us13.list-manage.com/subscribe?u=d4f5d1addd4357eb77c3f8a99&id=588f6c6ef4";
-
-const EmailSignupForm = () => (
-  <form
-    action={MAILCHIMP_URL}
-    className="email-form is-horizontal-center"
-    method="post"
-    target="_blank"
-  >
-    <div className="mc-field-group">
-      <div className="control is-expanded">
-        <label htmlFor="mce-EMAIL" className="jf-sr-only">
-          <Trans>Email</Trans>
-        </label>
-        <input
-          type="email"
-          name="EMAIL"
-          className="required email input"
-          id="mce-EMAIL"
-          placeholder={li18n._(t`ENTER YOUR EMAIL`)}
-        />
-      </div>
-      <div className="control has-text-centered-touch">
-        <button
-          className="button"
-          type="submit"
-          aria-label={li18n._(t`Submit email`)}
-        >
-          <StaticImage
-            ratio="is-16x16"
-            src={getImageSrc("submitarrow")}
-            alt={li18n._(t`Submit email`)}
-          />
-        </button>
-      </div>
-    </div>
-  </form>
-);
+import { Trans } from "@lingui/macro";
+import Subscribe from "./subscribe";
 
 export const NorentFooter: React.FC<{}> = () => (
   <footer>
@@ -61,7 +19,7 @@ export const NorentFooter: React.FC<{}> = () => (
               mailing list
             </Trans>
           </h6>
-          <EmailSignupForm />
+          <Subscribe />
           <SocialIcons color="white" />
         </div>
         <div className="column is-4 has-text-right is-uppercase content">
---
+++ frontend/lib/norent/components/subscribe.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import { Trans, t } from "@lingui/macro";
+import { li18n } from "../../i18n-lingui";
+import { StaticImage } from "../../ui/static-image";
+import { getImageSrc } from "../homepage";
+
+type SubscribeProps = {};
+
+type SubscribeState = {
+  email: string;
+  success: boolean;
+  response: string;
+};
+
+class Subscribe extends React.Component<SubscribeProps, SubscribeState> {
+  constructor(props: SubscribeProps) {
+    super(props);
+    this.state = {
+      email: "",
+      success: false,
+      response: "",
+    };
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const email = this.state.email || null;
+    const locale = li18n.language;
+
+    // check if email is missing, return undefined
+    if (!email) {
+      this.setState({
+        response: li18n._(t`Please enter an email address!`),
+      });
+      return;
+    }
+
+    const tenantPlatformOrigin =
+      process.env.GATSBY_TENANT_PLATFORM_SITE_ORIGIN ||
+      "https://demo.justfix.nyc";
+
+    fetch(`${tenantPlatformOrigin}/mailchimp/subscribe`, {
+      method: "POST",
+      mode: "cors",
+      body: `email=${encodeURIComponent(
+        email
+      )}&language=${locale}&source=norent`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((result) => result.json())
+      .then((result) => {
+        if (result.status === 200) {
+          this.setState({
+            success: true,
+            response: li18n._(t`All set! Thanks for subscribing!`),
+          });
+        } else if (result.errorCode === "INVALID_EMAIL") {
+          this.setState({
+            response: li18n._(t`Oops! That email is invalid.`),
+          });
+        } else {
+          window &&
+            window.Rollbar &&
+            window.Rollbar.error(
+              `Mailchimp email signup responded with error code ${result.errorCode}.`
+            );
+          this.setState({
+            response: li18n._(
+              t`Oops! A network error occurred. Try again later.`
+            ),
+          });
+        }
+      })
+      .catch((err) => {
+        this.setState({
+          response: li18n._(
+            t`Oops! A network error occurred. Try again later.`
+          ),
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form
+          className="email-form is-horizontal-center"
+          onSubmit={this.handleSubmit}
+        >
+          <div className="mc-field-group">
+            <div className="control is-expanded">
+              <label htmlFor="mce-EMAIL" className="jf-sr-only">
+                <Trans>Email</Trans>
+              </label>
+              <input
+                type="email"
+                name="EMAIL"
+                className="input"
+                id="mce-EMAIL"
+                onChange={this.handleChange}
+                placeholder={li18n._(t`ENTER YOUR EMAIL`)}
+              />
+            </div>
+            <div className="control has-text-centered-touch">
+              <button
+                className="button"
+                type="submit"
+                aria-label={li18n._(t`Submit email`)}
+              >
+                <StaticImage
+                  ratio="is-16x16"
+                  src={getImageSrc("submitarrow")}
+                  alt={li18n._(t`Submit email`)}
+                />
+              </button>
+            </div>
+          </div>
+        </form>
+        {this.state.response && (
+          <>
+            <p
+              className={
+                this.state.success ? "has-text-white" : "has-text-danger"
+              }
+            >
+              {this.state.response}
+            </p>
+            <br />
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Subscribe;

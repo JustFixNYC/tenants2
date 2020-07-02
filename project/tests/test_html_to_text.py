1d810f6c2005bfa1057d14c17028b122f106ff78
Atul Varma <varmaa@gmail.com>
2020-07-02 15:17:55 -0400

    Add more tests.

--- project/tests/test_html_to_text.py
+++ project/tests/test_html_to_text.py
@@ -5,6 +5,10 @@ def test_it_ignores_title_tags():
     assert html_to_text('<title>boop</title>') == ''
 
 
+def test_it_ignores_style_tags():
+    assert html_to_text('<style>html { color: pink; }</style>') == ''
+
+
 def test_it_works():
     assert html_to_text('<p>paragraph one</p><p>paragraph two</p>') == (
         'paragraph one\n\n'
@@ -19,4 +23,22 @@ def test_it_ignores_class_name():
 def test_it_supports_br():
     assert html_to_text('<p>paragraph<br/>one</p>') == (
         'paragraph\none'
+    )
+
+
+def test_it_ignores_empty_blocks():
+    assert html_to_text('<p>bop</p><p></p><p>bop</p>') == (
+        'bop\n\nbop'
+    )
+
+
+def test_it_adds_anchor_hrefs():
+    assert html_to_text('<p><a href="https://boop">visit it</a></p>') == (
+        'visit it: https://boop'
+    )
+
+
+def test_it_ignores_anchors_without_hrefs():
+    assert html_to_text('<p><a>visit it</a></p>') == (
+        'visit it'
     )

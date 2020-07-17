JustFixNYC/tenants2

commit f45d02f1cde5d6857046121667ecf4f5a8338924

Reduce lines in harassment details if needed. Fixes #1570. (#1591)
This fixes #1570 by attempting to predict how many lines the user's harassment details will take up; if it's more than we think will fit in the provided space, we replace all the newlines with ` / ` (similar to how song/poem lyrics are somehow compressed into a single line).

hpaction/build_hpactionvars.py
@@ -14,6 +14,13 @@ from .forms import EMERGENCY_HPA_ISSUE_LIST
 from . import hpactionvars as hp
 
 
+# How many lines the harassment details section of the HP action form has.
+MAX_HARASSMENT_DETAILS_LINES = 11
+
+# How many characters, on average, fit into a line in the harassment
+# details section. (The font use is not monospaced.)
+HARASSMENT_DETAILS_LINE_LENGTH = 60
+
 NYCHA_ADDRESS = common_data.load_json("nycha-address.json")
 
 
@@ -272,11 +279,38 @@ def flip_null_bool(value: Optional[bool]) -> Optional[bool]:
     return not value
 
 
+def reduce_number_of_lines(value: str, max_lines: int, line_length: int) -> str:
+    '''
+    If the given value, when text-wrapped across the given line length, is
+    greater than the given number of lines, reduce the number of lines by
+    replacing all newlines with ' / '.
+    '''
+
+    import textwrap
+
+    lines = value.split('\n')
+    wrapped_lines: List[str] = []
+    for line in lines:
+        if line.strip():
+            wrapped_lines.extend(textwrap.wrap(line, width=line_length))
+        else:
+            wrapped_lines.append('')
+
+    if len(lines) > max_lines:
+        value = ' / '.join(filter(None, lines))
+
+    return value
+
+
 def fill_harassment_details(v: hp.HPActionVariables, h: HarassmentDetails) -> None:
     fill_harassment_allegations(v, h)
     v.more_than_2_apartments_in_building_tf = flip_null_bool(h.two_or_less_apartments_in_building)
     v.more_than_one_family_per_apartment_tf = h.more_than_one_family_per_apartment
-    v.harassment_details_te = h.harassment_details
+    v.harassment_details_te = reduce_number_of_lines(
+        h.harassment_details,
+        max_lines=MAX_HARASSMENT_DETAILS_LINES,
+        line_length=HARASSMENT_DETAILS_LINE_LENGTH,
+    )
 
 
 def fill_fee_waiver_details(v: hp.HPActionVariables, fwd: FeeWaiverDetails) -> None:

hpaction/tests/test_build_hpactionvars.py
@@ -11,7 +11,7 @@ from hpaction.build_hpactionvars import (
     fill_tenant_children, get_tenant_repairs_allegations_mc,
     fill_hp_action_details, fill_harassment_details, get_hpactionvars_attr_for_harassment_alleg,
     fill_prior_cases, fill_prior_repairs_and_harassment_mcs,
-    fill_landlord_info)
+    fill_landlord_info, reduce_number_of_lines)
 from .factories import (
     TenantChildFactory, HPActionDetailsFactory, HarassmentDetailsFactory, PriorCaseFactory)
 import hpaction.hpactionvars as hp
@@ -19,6 +19,14 @@ import hpaction.hpactionvars as hp
 
 NORMAL = HP_ACTION_CHOICES.NORMAL
 EMERGENCY = HP_ACTION_CHOICES.EMERGENCY
+
+
+class TestReduceNumberOfLines:
+    def test_it_does_nothing_if_lines_are_not_greater_than_limit(self):
+        assert reduce_number_of_lines('a\nb\nc', 3, 10) == 'a\nb\nc'
+
+    def test_it_removes_lines_if_lines_are_greater_than_limit(self):
+        assert reduce_number_of_lines('a\n\nb\n\nc', 3, 10) == 'a / b / c'
 
 
 def test_justfix_issue_to_hp_room_works():

commit b2ef97c11d314792fd78c58ecd072db6f98c1fc1

Fix bug in reduce_number_of_lines(). (#1613)
Oof, a tenant filed an EHPA with harassment details that should have been compressed into one long line but weren't.  I realized that it's due to a bug in `reduce_number_of_lines()` introduced in #1591, which this fixes.

hpaction/build_hpactionvars.py
@@ -296,7 +296,7 @@ def reduce_number_of_lines(value: str, max_lines: int, line_length: int) -> str:
         else:
             wrapped_lines.append('')
 
-    if len(lines) > max_lines:
+    if len(wrapped_lines) > max_lines:
         value = ' / '.join(filter(None, lines))
 
     return value

hpaction/tests/test_build_hpactionvars.py
@@ -28,6 +28,9 @@ class TestReduceNumberOfLines:
     def test_it_removes_lines_if_lines_are_greater_than_limit(self):
         assert reduce_number_of_lines('a\n\nb\n\nc', 3, 10) == 'a / b / c'
 
+    def test_it_removes_lines_if_wrapped_lines_are_greater_than_limit(self):
+        assert reduce_number_of_lines('a beep bop boop\nb\nc', 3, 10) == 'a beep bop boop / b / c'
+
 
 def test_justfix_issue_to_hp_room_works():
     assert justfix_issue_area_to_hp_room('HOME') is hp.WhichRoomMC.ALL_ROOMS
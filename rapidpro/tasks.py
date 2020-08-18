cf41c069086f3f3c717df8e2dda7110156985ec5
Atul Varma <varmaa@gmail.com>
2020-08-18 16:50:10 -0400

    Retry TextIt requests if needed.

--- rapidpro/tasks.py
+++ rapidpro/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from temba_client.exceptions import TembaHttpError
 
 from .rapidpro_util import get_client_from_settings
 from .followup_campaigns import DjangoSettingsFollowupCampaigns
@@ -8,7 +9,11 @@ from .followup_campaigns import DjangoSettingsFollowupCampaigns
 # it's really a required argument, but we're allowing it to be optional
 # for a bit, because we might still need to process tasks added by old
 # versions of the codebase which never supplied this argument.
-@shared_task
+@shared_task(
+    autoretry_for=(TembaHttpError,),
+    retry_backoff=True,
+    default_retry_delay=30 * 60
+)
 def trigger_followup_campaign(full_name: str, phone_number: str, campaign_name: str,
                               locale: str = 'en'):
     client = get_client_from_settings()

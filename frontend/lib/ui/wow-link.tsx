551373b76bb4a614ec130042400706904c42524d
Atul Varma <varmaa@gmail.com>
2020-08-20 07:46:50 -0400

    Internationalize links to WoW, moratorium FAQ. (#1645)
    This ensures that whatever language the user is using the Tenant Platform as will be maintained when the user navigates to other WoW and the eviction moratorium FAQ.
    
    Note that even though we're now sending users to a locale-prefixed WoW link, WoW won't necessarily pay attention to it because of JustFixNYC/who-owns-what#403. But as soon as WoW fixes that bug, everything will work as expected.

--- frontend/lib/ui/wow-link.tsx
+++ frontend/lib/ui/wow-link.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { getGlobalAppServerInfo } from "../app-context";
+import i18n from "../i18n";
 
 export function whoOwnsWhatURL(bbl: string): string {
-  return `${getGlobalAppServerInfo().wowOrigin}/bbl/${bbl}`;
+  return `${getGlobalAppServerInfo().wowOrigin}/${i18n.locale}/bbl/${bbl}`;
 }
 
 export function WhoOwnsWhatLink(props: {

efa816999ad1e6b7196be59c31fe99e25287e571
Atul Varma <varmaa@gmail.com>
2020-08-20 07:40:20 -0400

    Internationalize moratorium FAQ link.

--- frontend/lib/data-driven-onboarding/data-driven-onboarding.tsx
+++ frontend/lib/data-driven-onboarding/data-driven-onboarding.tsx
@@ -22,6 +22,7 @@ import { OutboundLink, ga } from "../analytics/google-analytics";
 import { UpdateBrowserStorage } from "../browser-storage";
 import { getEmergencyHPAIssueLabels } from "../hpaction/emergency-hp-action-issues";
 import { MORATORIUM_FAQ_URL } from "../ui/covid-banners";
+import i18n from "../i18n";
 
 const CTA_CLASS_NAME = "button is-primary jf-text-wrap";
 
@@ -535,7 +536,7 @@ const ACTION_CARDS: ActionCardPropsCreator[] = [
       fallbackMessage: covidMessage,
       imageStaticURL: "frontend/img/ddo/judge.svg",
       cta: {
-        to: MORATORIUM_FAQ_URL.en,
+        to: MORATORIUM_FAQ_URL[i18n.locale],
         gaLabel: "efnyc",
         text: covidCtaText,
       },

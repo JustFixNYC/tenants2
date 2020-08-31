1ae5a15b9a8141eb11a8fbf13e28f1c4207e9a86
Sam R <sam@MacBook-Pro.local>
2020-08-31 16:18:11 -0400

    confirmation

--- frontend/lib/loc/loc-confirmation.tsx
+++ frontend/lib/loc/loc-confirmation.tsx
@@ -15,8 +15,11 @@ import { USPS_TRACKING_URL_PREFIX } from "../../../common-data/loc.json";
 import { SquareImage } from "../data-driven-onboarding/data-driven-onboarding";
 import { ariaBool } from "../ui/aria";
 import { renderSuccessHeading } from "../ui/success-heading";
+import { Trans, t } from "@lingui/macro";
+import { li18n } from "../i18n-lingui";
+import { LocalizedOutboundLink } from "../ui/localized-outbound-link";
 
-const LetterViaEmailInstructions = `If you want to send your Letter of Complaint to your landlord and/or management company via email, download the PDF and include it as an attachment to your regular email.`;
+const getLetterViaEmailInstructions = () => li18n._(t`If you want to send your Letter of Complaint to your landlord and/or management company via email, download the PDF and include it as an attachment to your regular email.`);
 
 const SanitationGuidelines = () => {
   const [isExpanded, toggleExpansion] = useState(false);
@@ -28,10 +31,10 @@ const SanitationGuidelines = () => {
         return (
           <div className="jf-sanitation-guidelines notification is-warning">
             <div>
-              Please be aware that letting a repair-worker into your home to
+              <Trans>Please be aware that letting a repair-worker into your home to
               make repairs may expose you to the Covid-19 virus. In order to
               follow social distancing guidelines and to limit your exposure,
-              please follow these steps to stay as safe as possible.
+              please follow these steps to stay as safe as possible.</Trans>
               {!inSafeMode && (
                 <>
                   {" "}
@@ -45,10 +48,10 @@ const SanitationGuidelines = () => {
                     )}
                     role="button"
                     onClick={() => toggleExpansion(true)}
-                    aria-label="Show me sanitation guidelines"
+                    aria-label={li18n._(t`Show me sanitation guidelines`)}
                     aria-expanded={ariaBool(isExpanded)}
                   >
-                    Show more
+                    <Trans>Show more</Trans>
                   </button>
                 </>
               )}
@@ -69,12 +72,12 @@ const SanitationGuidelines = () => {
                 </div>
                 <div className="column">
                   <h3 className="is-size-6 is-uppercase has-text-weight-bold has-text-grey-dark">
-                    Before the repair-worker arrives
+                    <Trans>Before the repair-worker arrives</Trans>
                   </h3>
                   <p>
-                    Talk to anyone that you live with and let them know that a
+                    <Trans>Talk to anyone that you live with and let them know that a
                     repair-worker is coming to perform the repairs that you
-                    requested.
+                    requested.</Trans>
                   </p>
                 </div>
               </div>
@@ -88,18 +91,18 @@ const SanitationGuidelines = () => {
                 </div>
                 <div className="column">
                   <h3 className="is-size-6 is-uppercase has-text-weight-bold has-text-grey-dark">
-                    While the repair-worker is inside your home
+                    <Trans>While the repair-worker is inside your home</Trans>
                   </h3>
-                  <p>
+                  <Trans><p>
                     Have the repair-worker wash their hands with soap for at
                     least 20 seconds as soon as they come into your house.
                   </p>
-                  <p>
-                    If possible, stay in a different room from where the work is
-                    being done. If a separate room is not available, maintain at
-                    least a six-foot distance from the repair-worker until the
-                    repair is completed.
-                  </p>
+                    <p>
+                      If possible, stay in a different room from where the work is
+                      being done. If a separate room is not available, maintain at
+                      least a six-foot distance from the repair-worker until the
+                      repair is completed.
+                  </p></Trans>
                 </div>
               </div>
               <div className="columns">
@@ -112,21 +115,24 @@ const SanitationGuidelines = () => {
                 </div>
                 <div className="column">
                   <h3 className="is-size-6 is-uppercase has-text-weight-bold has-text-grey-dark">
-                    After the repair-worker leaves
+                    <Trans>After the repair-worker leaves</Trans>
                   </h3>
                   <p>
-                    Immediately sanitize all surfaces in your home, especially
+                    <Trans>Immediately sanitize all surfaces in your home, especially
                     doorknobs, the sink where the repair-worker washed their
                     hands, and any surfaces you know they have likely been in
-                    contact with.
+                    contact with.</Trans>
                   </p>
                   <p className="is-size-7">
-                    For guidance on how to thoroughly sanitize your home and a
+                    <Trans>For guidance on how to thoroughly sanitize your home and a
                     list of recommended effective cleaning products visit{" "}
-                    <OutboundLink href="https://www.cdc.gov/coronavirus/2019-ncov/prepare/cleaning-disinfection.html">
-                      Center for Disease Control (CDC) Guide on How to Clean and
-                      Disinfect
-                    </OutboundLink>
+                      <LocalizedOutboundLink hrefs={{
+                        en: "https://www.cdc.gov/coronavirus/2019-ncov/prepare/cleaning-disinfection.html",
+                        es: "https://espanol.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/cleaning-disinfection.html"
+                      }} >
+                        Center for Disease Control (CDC) Guide on How to Clean and
+                        Disinfect
+                    </LocalizedOutboundLink></Trans>
                   </p>
                 </div>
               </div>
@@ -142,10 +148,10 @@ const SanitationGuidelines = () => {
                       )}
                       role="button"
                       onClick={() => toggleExpansion(false)}
-                      aria-label="Hide sanitation guidelines"
+                      aria-label={li18n._(t`Hide sanitation guidelines`)}
                       aria-expanded={ariaBool(isExpanded)}
                     >
-                      Show less
+                      <Trans>Show less</Trans>
                     </button>
                   </div>
                 </div>
@@ -154,7 +160,7 @@ const SanitationGuidelines = () => {
           </div>
         );
       }}
-    </AppContext.Consumer>
+    </AppContext.Consumer >
   );
 };
 
@@ -165,23 +171,23 @@ const DownloadLetterLink = (props: { locPdfURL: string }) => (
 const getCommonMailNextSteps = () => [
   <li>
     <p>
-      Once received, your landlord should contact you to schedule time to make
-      repairs for the access dates you provided.
+      <Trans>Once received, your landlord should contact you to schedule time to make
+      repairs for the access dates you provided.</Trans>
     </p>
     <SanitationGuidelines />
   </li>,
   <li>
-    While you wait, you should <strong>document your issues with photos</strong>{" "}
-    and <strong>call 311 to request an HPD inspection.</strong>
+    <Trans>While you wait, you should <strong>document your issues with photos</strong>{" "}
+    and <strong>call 311 to request an HPD inspection.</strong></Trans>
   </li>,
 ];
 
 const getCommonWeMailNextSteps = () => [
   ...getCommonMailNextSteps(),
   <li>
-    We will continue to follow up with you via text message. If your landlord
+    <Trans>We will continue to follow up with you via text message. If your landlord
     does not follow through, you now have better legal standing to sue your
-    landlord. <strong>This is called an HP Action proceeding.</strong>
+    landlord. <strong>This is called an HP Action proceeding.</strong></Trans>
   </li>,
 ];
 
@@ -195,28 +201,28 @@ function WeMailedLetterStatus(props: {
   return (
     <>
       <p>
-        We sent your letter of complaint
+        <Trans>We sent your letter of complaint
         {letterSentAt && (
-          <>
-            {" "}
+            <>
+              {" "}
             on <strong>{friendlyDate(new Date(letterSentAt))}</strong>
-          </>
-        )}
-        !
+            </>
+          )}
+        !</Trans>
       </p>
       <p>
-        Your{" "}
-        <b>
-          USPS Certified Mail<sup>&reg;</sup>
-        </b>{" "}
-        tracking number is <a href={url}>{trackingNumber}</a>.
+        <Trans>Your{" "}
+          <b>
+            USPS Certified Mail<sup>&reg;</sup>
+          </b>{" "}
+        tracking number is <a href={url}>{trackingNumber}</a>.</Trans>
       </p>
       <DownloadLetterLink {...props} />
-      <h2>What happens next?</h2>
+      <h2><Trans>What happens next?</Trans></h2>
       <BigList
         children={[
           <li>
-            <p>{LetterViaEmailInstructions}</p>
+            <p>{getLetterViaEmailInstructions()}</p>
           </li>,
           ...getCommonWeMailNextSteps(),
         ]}
@@ -234,26 +240,26 @@ function WeWillMailLetterStatus(props: {
   return (
     <>
       <p>
-        We've received your request to mail a letter of complaint on{" "}
-        <strong>{dateStr}</strong>. We'll text you a link to your{" "}
-        <b>
-          USPS Certified Mail<sup>&reg;</sup>
-        </b>{" "}
-        tracking number once we have it!
+        <Trans>We've received your request to mail a letter of complaint on{" "}
+          <strong>{dateStr}</strong>. We'll text you a link to your{" "}
+          <b>
+            USPS Certified Mail<sup>&reg;</sup>
+          </b>{" "}
+        tracking number once we have it!</Trans>
       </p>
       <DownloadLetterLink {...props} />
-      <h2>What happens next?</h2>
+      <h2><Trans>What happens next?</Trans></h2>
       <BigList
         children={[
           <li>
             <p>
-              We’ll mail your letter via{" "}
-              <b>
-                USPS Certified Mail<sup>&reg;</sup>
-              </b>{" "}
-              and provide a tracking number via text message.
+              <Trans>We’ll mail your letter via{" "}
+                <b>
+                  USPS Certified Mail<sup>&reg;</sup>
+                </b>{" "}
+              and provide a tracking number via text message.</Trans>
             </p>
-            <p>{LetterViaEmailInstructions}</p>
+            <p>{getLetterViaEmailInstructions()}</p>
           </li>,
           ...getCommonWeMailNextSteps(),
         ]}
@@ -265,18 +271,18 @@ function WeWillMailLetterStatus(props: {
 function UserWillMailLetterStatus(props: { locPdfURL: string }): JSX.Element {
   return (
     <>
-      <p>Here is a link to a PDF of your saved letter:</p>
+      <p><Trans>Here is a link to a PDF of your saved letter:</Trans></p>
       <DownloadLetterLink {...props} />
-      <h2>What happens next?</h2>
+      <h2><Trans>What happens next?</Trans></h2>
       <BigList
         children={[
           <li>
             <p>
-              Print out your letter and{" "}
-              <strong>mail it via Certified Mail</strong> - this allows you to
-              prove that it was sent to your landlord.
+              <Trans>Print out your letter and{" "}
+                <strong>mail it via Certified Mail</strong> - this allows you to
+              prove that it was sent to your landlord.</Trans>
             </p>
-            <p>{LetterViaEmailInstructions}</p>
+            <p>{getLetterViaEmailInstructions()}</p>
           </li>,
           ...getCommonMailNextSteps(),
         ]}
@@ -288,14 +294,12 @@ function UserWillMailLetterStatus(props: { locPdfURL: string }): JSX.Element {
 const knowYourRightsList = (
   <ul>
     <li>
-      <OutboundLink href="https://www.metcouncilonhousing.org/help-answers/getting-repairs/">
+      <LocalizedOutboundLink hrefs={{
+        en: "https://www.metcouncilonhousing.org/help-answers/getting-repairs/",
+        es: "https://www.metcouncilonhousing.org/help-answers/how-to-get-repairs-spanish/"
+      }}>
         Met Council on Housing
-      </OutboundLink>{" "}
-      (
-      <OutboundLink href="https://www.metcouncilonhousing.org/help-answers/how-to-get-repairs-spanish/">
-        en español
-      </OutboundLink>
-      )
+      </LocalizedOutboundLink>
     </li>
     <li>
       <OutboundLink href="http://housingcourtanswers.org/glossary/">
@@ -304,7 +308,7 @@ const knowYourRightsList = (
     </li>
     <li>
       <OutboundLink href="https://www.justfix.nyc/learn?utm_source=tenantplatform&utm_medium=loc">
-        JustFix.nyc's Learning Center
+        <Trans>JustFix.nyc's Learning Center</Trans>
       </OutboundLink>
     </li>
   </ul>
@@ -317,7 +321,7 @@ const LetterConfirmation = withAppContext(
     let letterConfirmationPageTitle, letterStatus;
 
     if (letterRequest && letterRequest.trackingNumber) {
-      letterConfirmationPageTitle = "Your Letter of Complaint has been sent!";
+      letterConfirmationPageTitle = li18n._(t`Your Letter of Complaint has been sent!`);
       letterStatus = (
         <WeMailedLetterStatus
           letterRequest={letterRequest}
@@ -328,7 +332,7 @@ const LetterConfirmation = withAppContext(
       letterRequest &&
       letterRequest.mailChoice === LetterRequestMailChoice.WE_WILL_MAIL
     ) {
-      letterConfirmationPageTitle = "Your Letter of Complaint is being sent!";
+    letterConfirmationPageTitle = li18n._(t`Your Letter of Complaint is being sent!`);
       letterStatus = (
         <WeWillMailLetterStatus
           letterRequest={letterRequest}
@@ -337,7 +341,7 @@ const LetterConfirmation = withAppContext(
       );
     } else {
       letterConfirmationPageTitle =
-        "Your Letter of Complaint has been created!";
+        li18n._(t`Your Letter of Complaint has been created!`);
       letterStatus = <UserWillMailLetterStatus {...letterStatusProps} />;
     }
 
@@ -351,11 +355,11 @@ const LetterConfirmation = withAppContext(
         {/* <ProgressiveLoadableConfetti regenerateForSecs={1} /> */}
         {letterStatus}
         <h2>
-          Email a copy of your letter to yourself, someone you trust, or your
-          landlord.
+          <Trans>Email a copy of your letter to yourself, someone you trust, or your
+          landlord.</Trans>
         </h2>
         <EmailAttachmentForm mutation={EmailLetterMutation} noun="letter" />
-        <h2>Want to read more about your rights?</h2>
+        <h2><Trans>Want to read more about your rights?</Trans></h2>
         {knowYourRightsList}
       </Page>
     );

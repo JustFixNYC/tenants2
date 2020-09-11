8d59949575f52203764177b579275da9b62a24fd
Atul Varma <varmaa@gmail.com>
2020-09-11 08:44:11 -0400

    Change links to point at our PR.

--- frontend/fake-opencollective/index.js
+++ frontend/fake-opencollective/index.js
@@ -1,4 +1,4 @@
 throw new Error(
   "This file should never be imported because lingui never uses it. " +
-    "For more details, see https://github.com/lingui/js-lingui/issues/480."
+    "For more details, see https://github.com/JustFixNYC/tenants2/pull/1661."
 );

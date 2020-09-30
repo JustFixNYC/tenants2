012c079cee25b20365402b1ccf7405b2d209646f
Atul Varma <varmaa@gmail.com>
2020-09-30 18:36:51 -0400

    Add more test assertions.

--- dwh/tests/test_etl.py
+++ dwh/tests/test_etl.py
@@ -10,7 +10,7 @@ from dwh.models import (
     LetterOfComplaintRequest,
     EmergencyHPASigning,
 )
-from dwh.management.commands.etl import Flow
+from dwh.management.commands.etl import Flow, NodeDesc
 
 MY_DIR = Path(__file__).parent.resolve()
 
@@ -39,6 +39,12 @@ class TestFlow:
         flow = self.get_flow("dhcr_followup_1_old_schema.json")
         assert flow.name == "DHCR Rental History Follow Up #1"
         assert flow.uuid == "be922331-eb0b-4823-86d2-647dc5a014e3"
+        assert flow.find_node_uuids(NodeDesc(r"^That’s great")) == [
+            "fa540b3a-626e-4546-82bd-bc937023ebe2"
+        ]
+        assert flow.find_node_uuids(NodeDesc(r"^No worries")) == [
+            "b7a52eb8-0f84-478a-8933-7461578861f5"
+        ]
 
     def test_it_works_with_new_schema(self):
         flow = self.get_flow("dhcr_flow_new_schema.json")

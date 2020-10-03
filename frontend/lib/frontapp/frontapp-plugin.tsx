d2a9395f5b06eccfb796c604c361dc7a2a95150a
Atul Varma <varmaa@gmail.com>
2020-10-03 16:54:42 -0400

    Show 'waiting for front...' message.

--- frontend/lib/frontapp/frontapp-plugin.tsx
+++ frontend/lib/frontapp/frontapp-plugin.tsx
@@ -5,9 +5,11 @@ import Front, { SingleConversationContext } from "@frontapp/plugin-sdk";
 
 const FrontappPlugin: React.FC<{}> = () => {
   const [recipient, setRecipient] = useState<string | undefined>(undefined);
+  const [hasConnected, setHasConnected] = useState(false);
 
   useEffect(() => {
     const sub = Front.contextUpdates.subscribe((context) => {
+      setHasConnected(true);
       if (context.type === "singleConversation") {
         const conv = (context as SingleConversationContext).conversation;
         setRecipient(conv.recipient?.handle);
@@ -16,7 +18,9 @@ const FrontappPlugin: React.FC<{}> = () => {
     return () => sub.unsubscribe();
   }, []);
 
-  if (recipient) {
+  if (!hasConnected) {
+    return <p>Waiting for Front...</p>;
+  } else if (recipient) {
     return <p>Your recipient is {recipient}.</p>;
   }
   return <p>No conversation selected.</p>;

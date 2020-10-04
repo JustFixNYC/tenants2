9e84708ec0c0a561302f7834965a520a24be9b05
Atul Varma <varmaa@gmail.com>
2020-10-04 06:39:20 -0400

    Make network request to userDetails.

--- frontend/lib/admin/frontapp-plugin.tsx
+++ frontend/lib/admin/frontapp-plugin.tsx
@@ -1,11 +1,46 @@
 import { Route, RouteComponentProps, Switch } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import JustfixRoutes from "../justfix-routes";
 import Front, {
   ApplicationContext,
   SingleConversationContext,
 } from "@frontapp/plugin-sdk";
 import { staffOnlyView } from "./staff-only-view";
+import { useAdminFetch } from "./admin-hooks";
+import { FrontappUserDetails } from "../queries/FrontappUserDetails";
+
+const UserInfo: React.FC<{ email: string }> = ({ email }) => {
+  const input = useMemo(
+    () => ({
+      email,
+    }),
+    [email]
+  );
+  const response = useAdminFetch(
+    FrontappUserDetails,
+    input,
+    "fake refresh token"
+  );
+
+  if (response.type === "errored") {
+    return <p>Alas, an error occurred.</p>;
+  } else if (response.type === "loaded") {
+    const { userDetails } = response.output;
+    if (!userDetails) {
+      return (
+        <p>
+          Alas, no information related to {email} is in the tenant platform.
+        </p>
+      );
+    }
+    return (
+      <p>
+        The user's username is {userDetails.username}, their email is {email}.
+      </p>
+    );
+  }
+  return <p>Loading...</p>;
+};
 
 const FrontappPlugin: React.FC<RouteComponentProps<any>> = staffOnlyView(
   (props) => {
@@ -28,7 +63,7 @@ const FrontappPlugin: React.FC<RouteComponentProps<any>> = staffOnlyView(
     if (!frontContext) {
       return <p>Waiting for Front...</p>;
     } else if (recipient) {
-      return <p>Your recipient is {recipient}.</p>;
+      return <UserInfo email={recipient} />;
     }
     return <p>No conversation selected.</p>;
   }

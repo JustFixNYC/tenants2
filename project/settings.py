JustFixNYC/tenants2

commit 30dd90d36559c77edf9686592591195cbcbd5f02

Add a Front plugin (#1681)
This adds a [Front plugin][1].  Currently it just displays the same information and links that's displayed in the text messaging user info panel.

In order to develop with the plugin, you need to set the following environment variables:

```
SESSION_COOKIE_SECURE=yup
CSRF_COOKIE_SECURE=yup
```

You also need to set up a https tunnel via a service like ngrok.

[1]: https://dev.frontapp.com/reference/plugin-sdk-introduction

project/settings.py
@@ -14,6 +14,7 @@ from typing import List, Dict, Optional, Any
 import dj_database_url
 import dj_email_url
 
+from . import monkeypatch_django  # noqa
 from . import justfix_environment, locales
 from .justfix_environment import BASE_DIR
 from .util.settings_util import (
@@ -47,6 +48,13 @@ SECURE_SSL_REDIRECT = env.SECURE_SSL_REDIRECT
 SECURE_HSTS_SECONDS = env.SECURE_HSTS_SECONDS
 
 SECURE_HSTS_PRELOAD = True
+
+# We need to set SameSite=None to allow for embedding within
+# Front.  For more information, see:
+#
+# https://medium.com/trabe/cookies-and-iframes-f7cca58b3b9e
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 SESSION_COOKIE_SECURE = env.SESSION_COOKIE_SECURE
 

frontend/lib/admin/admin-auth-expired.tsx
@@ -1,0 +1,15 @@
+import React from "react";
+
+export const AdminAuthExpired: React.FC<{}> = () => (
+  <div className="content">
+    <p>
+      Your administrative authentication has expired! Please reload the page.
+    </p>
+    <button
+      className="button is-primary"
+      onClick={() => window.location.reload()}
+    >
+      Reload
+    </button>
+  </div>
+);

frontend/lib/admin/admin-conversations.tsx
@@ -21,7 +21,6 @@ import {
 } from "../queries/AdminConversation";
 import { getQuerystringVar } from "../util/querystring";
 import { Helmet } from "react-helmet-async";
-import { whoOwnsWhatURL } from "../ui/wow-link";
 import classnames from "classnames";
 import { UpdateTextingHistoryMutation } from "../queries/UpdateTextingHistoryMutation";
 import { niceAdminTimestamp } from "./admin-util";
@@ -30,6 +29,8 @@ import { staffOnlyView } from "./staff-only-view";
 import { useDebouncedValue } from "../util/use-debounced-value";
 import { friendlyPhoneNumber } from "../util/util";
 import { friendlyDate } from "../util/date-util";
+import { AdminUserInfo } from "./admin-user-info";
+import { AdminAuthExpired } from "./admin-auth-expired";
 
 const PHONE_QS_VAR = "phone";
 
@@ -312,77 +313,6 @@ const ConversationsSidebar: React.FC<{
   );
 };
 
-/**
- * RapidPro group a user is added to if they say they have been
- * assigned an attorney.
- */
-const EHPA_ATTORNEY_ASSIGNED_GROUP = "EHPA Attorney Assignment Successful";
-
-/**
- * RapidPro group a user is added to if they say they have not yet been
- * assigned an attorney.
- */
-const EHPA_ATTORNEY_NOT_ASSIGNED_GROUP = "EHPA Attorney Assignment Pending";
-
-const EhpaAttorneyAssigned: React.FC<{ rapidproGroups: string[] }> = (
-  props
-) => {
-  const g = props.rapidproGroups;
-  const title = "EHPA attorney assigned";
-  const wasAssigned = g.includes(EHPA_ATTORNEY_ASSIGNED_GROUP)
-    ? true
-    : g.includes(EHPA_ATTORNEY_NOT_ASSIGNED_GROUP)
-    ? false
-    : null;
-
-  switch (wasAssigned) {
-    case true:
-      return <p>{title}: Yes</p>;
-    case false:
-      return <p>{title}: No</p>;
-    case null:
-      return null;
-  }
-};
-
-const UserInfo: React.FC<{
-  user: AdminConversation_userDetails;
-  showPhoneNumber: boolean;
-}> = ({ user, showPhoneNumber }) => {
-  return (
-    <>
-      {showPhoneNumber && (
-        <p>
-          This user's phone number is {friendlyPhoneNumber(user.phoneNumber)}.
-        </p>
-      )}
-      <EhpaAttorneyAssigned rapidproGroups={user.rapidproGroups} />
-      {user.onboardingInfo && (
-        <p>The user's signup intent is {user.onboardingInfo.signupIntent}.</p>
-      )}
-      {user.letterRequest && (
-        <p>
-          The user completed a letter of complaint on{" "}
-          {niceAdminTimestamp(user.letterRequest.updatedAt)}.
-        </p>
-      )}
-      <a href={user.adminUrl} className="button is-small" target="_blank">
-        Edit user
-      </a>
-      {user.onboardingInfo?.padBbl && (
-        <a
-          href={whoOwnsWhatURL(user.onboardingInfo.padBbl)}
-          className="button is-small"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View user's building in WoW
-        </a>
-      )}
-    </>
-  );
-};
-
 const ConversationMessages: React.FC<{
   messages: AdminConversation_output_messages[];
 }> = ({ messages }) => {
@@ -470,7 +400,7 @@ const ConversationPanel: React.FC<{
                   {userFullName || friendlyPhoneNumber(selectedPhoneNumber)}
                 </h1>
                 {user ? (
-                  <UserInfo showPhoneNumber={!!userFullName} user={user} />
+                  <AdminUserInfo showPhoneNumber={!!userFullName} user={user} />
                 ) : (
                   <p>
                     This phone number does not seem to have an account with us.
@@ -507,26 +437,12 @@ const ConversationPanel: React.FC<{
   );
 };
 
-const AdminAuthExpiredPage: React.FC<{}> = () => (
-  <div className="content">
-    <p>
-      Your administrative authentication has expired! Please reload the page.
-    </p>
-    <button
-      className="button is-primary"
-      onClick={() => window.location.reload()}
-    >
-      Reload
-    </button>
-  </div>
-);
-
 const AdminConversationsPageWrapper: React.FC<RouteComponentProps> = staffOnlyView(
   (props) => {
     const latestMsgTimestamp = useLatestMessageTimestamp();
 
     if (latestMsgTimestamp === AUTH_ERROR) {
-      return <AdminAuthExpiredPage />;
+      return <AdminAuthExpired />;
     }
 
     return (

frontend/lib/admin/admin-hooks.ts
@@ -60,6 +60,10 @@ const FETCH_STATE_IDLE: AdminFetchState<any> = { type: "idle" };
  * Requests are re-initiated whenever the input or refresh token changes. If
  * the input is null or the refresh token is falsy, no request is performed.
  *
+ * Note that if the input is an object, `useMemo` should be used to ensure
+ * that it's the same object that's passed in over multiple renders, or else
+ * requests will be re-initiated a lot more often than you expect!
+ *
  * This is intented to be used by admin code only; it doesn't support progressive
  * enhancement or server-side rendering in any way.
  */

frontend/lib/admin/admin-user-info.tsx
@@ -1,0 +1,76 @@
+import React from "react";
+import { niceAdminTimestamp } from "./admin-util";
+import { friendlyPhoneNumber } from "../util/util";
+import { JustfixUserType } from "../queries/JustfixUserType";
+import { whoOwnsWhatURL } from "../ui/wow-link";
+
+/**
+ * RapidPro group a user is added to if they say they have been
+ * assigned an attorney.
+ */
+const EHPA_ATTORNEY_ASSIGNED_GROUP = "EHPA Attorney Assignment Successful";
+
+/**
+ * RapidPro group a user is added to if they say they have not yet been
+ * assigned an attorney.
+ */
+const EHPA_ATTORNEY_NOT_ASSIGNED_GROUP = "EHPA Attorney Assignment Pending";
+
+const EhpaAttorneyAssigned: React.FC<{ rapidproGroups: string[] }> = (
+  props
+) => {
+  const g = props.rapidproGroups;
+  const title = "EHPA attorney assigned";
+  const wasAssigned = g.includes(EHPA_ATTORNEY_ASSIGNED_GROUP)
+    ? true
+    : g.includes(EHPA_ATTORNEY_NOT_ASSIGNED_GROUP)
+    ? false
+    : null;
+
+  switch (wasAssigned) {
+    case true:
+      return <p>{title}: Yes</p>;
+    case false:
+      return <p>{title}: No</p>;
+    case null:
+      return null;
+  }
+};
+
+export const AdminUserInfo: React.FC<{
+  user: JustfixUserType;
+  showPhoneNumber: boolean;
+}> = ({ user, showPhoneNumber }) => {
+  return (
+    <>
+      {showPhoneNumber && (
+        <p>
+          This user's phone number is {friendlyPhoneNumber(user.phoneNumber)}.
+        </p>
+      )}
+      <EhpaAttorneyAssigned rapidproGroups={user.rapidproGroups} />
+      {user.onboardingInfo && (
+        <p>The user's signup intent is {user.onboardingInfo.signupIntent}.</p>
+      )}
+      {user.letterRequest && (
+        <p>
+          The user completed a letter of complaint on{" "}
+          {niceAdminTimestamp(user.letterRequest.updatedAt)}.
+        </p>
+      )}
+      <a href={user.adminUrl} className="button is-small" target="_blank">
+        Edit user
+      </a>
+      {user.onboardingInfo?.padBbl && (
+        <a
+          href={whoOwnsWhatURL(user.onboardingInfo.padBbl)}
+          className="button is-small"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View user's building in WoW
+        </a>
+      )}
+    </>
+  );
+};

frontend/lib/admin/frontapp-plugin.tsx
@@ -1,0 +1,99 @@
+import { Route, RouteComponentProps, Switch } from "react-router-dom";
+import React, { useEffect, useMemo, useState } from "react";
+import JustfixRoutes from "../justfix-routes";
+import Front, {
+  ApplicationContext,
+  SingleConversationContext,
+} from "@frontapp/plugin-sdk";
+import { staffOnlyView } from "./staff-only-view";
+import { useAdminFetch } from "./admin-hooks";
+import {
+  FrontappUserDetails,
+  FrontappUserDetailsVariables,
+} from "../queries/FrontappUserDetails";
+import { AdminUserInfo } from "./admin-user-info";
+import Page from "../ui/page";
+import { AdminAuthExpired } from "./admin-auth-expired";
+
+const LoadedUserInfo: React.FC<FrontappUserDetails> = ({
+  isVerifiedStaffUser,
+  userDetails,
+}) => {
+  if (!isVerifiedStaffUser) {
+    return <AdminAuthExpired />;
+  }
+  if (!userDetails) {
+    return (
+      <p>
+        The selected conversation's recipient does not seem to have an account
+        with us.
+      </p>
+    );
+  }
+  return <AdminUserInfo user={userDetails} showPhoneNumber={true} />;
+};
+
+const UserInfo: React.FC<{ email: string }> = ({ email }) => {
+  const input: FrontappUserDetailsVariables = useMemo(
+    () => ({
+      email,
+    }),
+    [email]
+  );
+  const response = useAdminFetch(FrontappUserDetails, input, true);
+
+  return response.type === "errored" ? (
+    <p>Alas, a network error occurred.</p>
+  ) : response.type === "loaded" ? (
+    <LoadedUserInfo {...response.output} />
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+const FrontappPlugin: React.FC<RouteComponentProps<any>> = staffOnlyView(
+  (props) => {
+    const [recipient, setRecipient] = useState<string>();
+    const [frontContext, setFrontContext] = useState<
+      Pick<ApplicationContext, "openUrl">
+    >();
+
+    useEffect(() => {
+      const sub = Front.contextUpdates.subscribe((context) => {
+        setFrontContext(context);
+        if (context.type === "singleConversation") {
+          // Not sure why we need to typecast here, but we do.
+          const conv = (context as SingleConversationContext).conversation;
+          setRecipient(conv.recipient?.handle);
+        }
+      });
+      return () => sub.unsubscribe();
+    }, []);
+
+    return (
+      <Page title="Front app plugin" className="content">
+        {!frontContext ? (
+          <p>Waiting for Front...</p>
+        ) : recipient ? (
+          <UserInfo email={recipient} />
+        ) : (
+          <p>No conversation selected.</p>
+        )}
+      </Page>
+    );
+  }
+);
+
+const FrontappPluginRoutes: React.FC<{}> = () => {
+  return (
+    <Switch>
+      <Route
+        component={FrontappPlugin}
+        path={JustfixRoutes.adminFrontappPlugin}
+        exact
+      />
+    </Switch>
+  );
+};
+
+export default FrontappPluginRoutes;

frontend/lib/admin/staff-only-view.tsx
@@ -16,7 +16,7 @@ export function staffOnlyView<P extends RouteComponentProps>(
     const appCtx = useContext(AppContext);
     if (!appCtx.session.isStaff) {
       return (
-        <Redirect to={JustfixRoutes.locale.createLoginLink(props.location)} />
+        <Redirect to={JustfixRoutes.createAdminLoginLink(props.location)} />
       );
     }
     return <Component {...props} />;

frontend/lib/admin/tests/admin-conversations.test.tsx
@@ -75,7 +75,7 @@ describe("<AdminConversationsPage>", () => {
     const pal = new AppTesterPal(<AdminConversationsRoutes />, {
       url: "/admin/conversations/",
     });
-    expect(pal.history.location.pathname).toBe("/en/login");
+    expect(pal.history.location.pathname).toBe("/admin/login/");
   });
 
   it("works if user is staff", async () => {

commit 76334818224dc933a5e359db6dec9f4483fd7768

Only set SameSite=None on secure cookies. (#1684)
This fixes a regression caused by #1681, whereby session and CSRF cookies are broken in local development because they contain `SameSite=None` but not `Secure`, which causes them to be rejected entirely:

https://web.dev/samesite-cookies-explained/#samesitenone-must-be-secure

project/settings.py
@@ -49,16 +49,23 @@ SECURE_HSTS_SECONDS = env.SECURE_HSTS_SECONDS
 
 SECURE_HSTS_PRELOAD = True
 
+SESSION_COOKIE_SECURE = env.SESSION_COOKIE_SECURE
+
+CSRF_COOKIE_SECURE = env.CSRF_COOKIE_SECURE
+
 # We need to set SameSite=None to allow for embedding within
 # Front.  For more information, see:
 #
 # https://medium.com/trabe/cookies-and-iframes-f7cca58b3b9e
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
-
-SESSION_COOKIE_SECURE = env.SESSION_COOKIE_SECURE
-
-CSRF_COOKIE_SECURE = env.CSRF_COOKIE_SECURE
+#
+# Note that SameSite=None is only valid with secure cookies,
+# though--in fact, insecure cookies with SameSite=None will
+# be rejected entirely, thereby breaking the whole site, so
+# we need to be careful here.
+if SESSION_COOKIE_SECURE:
+    SESSION_COOKIE_SAMESITE = 'None'
+if CSRF_COOKIE_SECURE:
+    CSRF_COOKIE_SAMESITE = 'None'
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
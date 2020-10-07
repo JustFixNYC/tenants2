fa6f77e82e155c7323e1022da066a2d0b65b973f
Atul Varma <varmaa@gmail.com>
2020-10-07 14:47:26 -0400

    Temporarily 'deprecate' new session fields.

--- norent/schema.py
+++ norent/schema.py
@@ -126,6 +126,7 @@ class NorentSessionInfo(object):
             "A list of the available rent periods the current user can "
             "create a no rent letter for."
         ),
+        deprecation_reason="We have yet to use this in the front-end; will un-deprecate once we start using it, to avoid needless schema migrations in case of change.",  # noqa
     )
 
     norent_latest_letter = graphene.Field(
@@ -145,7 +146,8 @@ class NorentSessionInfo(object):
     )
 
     norent_upcoming_letter_rent_periods = graphene.List(
-        graphene.NonNull(graphene.types.Date), required=True
+        graphene.NonNull(graphene.types.Date), required=True,
+        deprecation_reason="We have yet to use this in the front-end; will un-deprecate once we start using it, to avoid needless schema migrations in case of change.",  # noqa
     )
 
     def resolve_norent_latest_rent_period(self, info: ResolveInfo):

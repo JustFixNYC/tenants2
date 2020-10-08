15f00edd486bb536eb6686b4492a982a22f80443
Sam Rabiyah <sraby@users.noreply.github.com>
2020-10-08 08:54:08 -0400

    Add NoRent letter USPS tracking info in email to user (#1694)
    This PR adds a new line to our emails to NoRent.org users that includes the USPS tracking link for their letter.

--- frontend/lib/norent/letter-email-to-user.tsx
+++ frontend/lib/norent/letter-email-to-user.tsx
@@ -9,24 +9,36 @@ import {
 import { NorentRoutes } from "./routes";
 import { li18n } from "../i18n-lingui";
 import { t, Trans } from "@lingui/macro";
+import { USPS_TRACKING_URL_PREFIX } from "../../../common-data/loc.json";
 
 export const NorentLetterEmailToUser: React.FC<{}> = () => {
   const { session, server } = useContext(AppContext);
+  const letter = session.norentLatestLetter;
   const faqURL = `${server.originURL}${NorentRoutes.locale.faqs}`;
 
   return (
     <>
       <EmailSubject value={li18n._(t`Here's a copy of your NoRent letter`)} />
-      <Trans id="norent.letterEmailToUserBody">
+      <Trans>
         <p>Hello {session.firstName},</p>
         <p>
           You've sent your NoRent letter. Attached to this email is a PDF copy
           for your records.
         </p>
-        <p>
-          To learn more about what to do next, check out our FAQ page: {faqURL}
-        </p>
       </Trans>
+      {letter?.trackingNumber && (
+        <p>
+          <Trans>
+            You can also track the delivery of your letter using USPS Tracking:
+          </Trans>{" "}
+          {USPS_TRACKING_URL_PREFIX + letter.trackingNumber}
+        </p>
+      )}
+      <p>
+        <Trans>
+          To learn more about what to do next, check out our FAQ page: {faqURL}
+        </Trans>
+      </p>
     </>
   );
 };

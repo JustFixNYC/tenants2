JustFixNYC/tenants2

commit d4ea249617e8ace8c0bd0765f9173105c620bcd5

Add send_norent_reminders command. (#1727)
This adds a management command to send NoRent California reminders for a given month/year.  Users not in CA, and users who have already sent a letter that covers the given month/year, won't be notified.

texting/models.py
@@ -1,21 +1,20 @@
-import datetime
 from typing import Optional
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.db.models.functions import Coalesce
-from django.utils import timezone
 
 from users.models import JustfixUser
-from onboarding.models import SIGNUP_INTENT_CHOICES
 from project.common_data import Choices
 from project.util import phone_number as pn
-from project.util.site_util import absolutify_url, get_site_name
 
 # https://support.twilio.com/hc/en-us/articles/223134387-What-is-a-Message-SID-
 TWILIO_SID_LENGTH = 34
 
 REMINDERS = Choices([
     ('LOC', 'Letter of complaint reminder'),
+    ('NORENT_CA_2020_11', 'NoRent California reminder - November 2020'),
+    ('NORENT_CA_2020_12', 'NoRent California reminder - December 2020'),
+    ('NORENT_CA_2021_01', 'NoRent California reminder - January 2020'),
 ])
 
 
@@ -192,48 +191,12 @@ class Reminder(models.Model):
     )
 
 
-# After these many days have passed since the user
-# signed up, we will send them a reminder, unless they
-# have completed the LoC process.
-DAYS_UNTIL_LOC_REMINDER = 3
-
-
 def exclude_users_with_invalid_phone_numbers(user_queryset):
     lookup = PhoneNumberLookup.objects.filter(
         phone_number=models.OuterRef('phone_number'))
     return user_queryset.annotate(
         is_phone_number_valid=Coalesce(models.Subquery(lookup.values('is_valid')), True)
     ).exclude(is_phone_number_valid=False)
-
-
-def get_users_to_remind_about_loc():
-    days_ago = timezone.now() - datetime.timedelta(days=DAYS_UNTIL_LOC_REMINDER)
-    users = JustfixUser.objects.filter(
-        date_joined__lte=days_ago,
-        onboarding_info__can_we_sms=True,
-        onboarding_info__signup_intent=SIGNUP_INTENT_CHOICES.LOC,
-        letter_request__isnull=True,
-    ).exclude(
-        reminders__kind=REMINDERS.LOC
-    )
-    return exclude_users_with_invalid_phone_numbers(users)
-
-
-def remind_user_about_loc(user):
-    url = absolutify_url('/')
-    sid = user.send_sms(
-        f'Hey {user.first_name}! '
-        f'Don\'t forget that you can use {get_site_name()} to address '
-        f'repair issues in your apartment. '
-        f'Follow this link to continue: {url}'
-    )
-    if sid:
-        Reminder(
-            kind=REMINDERS.LOC,
-            sent_at=timezone.now(),
-            user=user,
-            sid=sid
-        ).save()
 
 
 def get_lookup_description_for_phone_number(phone_number: str) -> str:

texting/sms_reminder.py
@@ -1,0 +1,51 @@
+import abc
+from django.utils import translation
+from django.utils import timezone
+
+from users.models import JustfixUser
+from .models import Reminder, REMINDERS
+
+
+class SmsReminder(abc.ABC):
+    reminder_kind: str = ''
+
+    def __init__(self, dry_run: bool = False):
+        self.dry_run = dry_run
+
+    @abc.abstractmethod
+    def filter_user_queryset(self, queryset):
+        pass
+
+    @abc.abstractmethod
+    def get_sms_text(self, user: JustfixUser) -> str:
+        pass
+
+    def remind_users(self):
+        SmsReminder.validate(self)
+
+        users = JustfixUser.objects.filter(onboarding_info__can_we_sms=True)
+        users = self.filter_user_queryset(users)\
+            .exclude(reminders__kind=self.reminder_kind)
+
+        for user in users:
+            with translation.override(user.locale):
+                text = self.get_sms_text(user)
+                assert text
+                print(f'Sending a {self.reminder_kind} reminder to {user.username}.')
+                sid = '' if self.dry_run else user.send_sms(text)
+                if sid:
+                    Reminder(
+                        kind=self.reminder_kind,
+                        sent_at=timezone.now(),
+                        user=user,
+                        sid=sid,
+                    ).save()
+
+    @staticmethod
+    def validate(instance: 'SmsReminder'):
+        '''
+        Validate that the SmsReminder is configured properly.
+        '''
+
+        assert instance.reminder_kind, "reminder_kind must be set"
+        REMINDERS.validate_choices(instance.reminder_kind)

texting/tests/test_sms_reminder.py
@@ -1,0 +1,54 @@
+import pytest
+from django.utils.translation import gettext as _
+
+from onboarding.tests.factories import OnboardingInfoFactory
+from users.tests.factories import SecondUserFactory
+from texting.sms_reminder import SmsReminder
+
+
+class RemindBoops(SmsReminder):
+    '''Remind users named Boop.'''
+
+    reminder_kind = 'LOC'
+
+    def filter_user_queryset(self, queryset):
+        return queryset.filter(first_name='Boop')
+
+    def get_sms_text(self, user):
+        return _("Hello world") + " " + user.full_name
+
+
+class TestSmsReminder:
+    @pytest.fixture(autouse=True)
+    def setup_fixture(self, db, smsoutbox):
+        self.smsoutbox = smsoutbox
+
+    def test_it_filters_out_users(self):
+        OnboardingInfoFactory(user=SecondUserFactory())
+        RemindBoops().remind_users()
+        assert len(self.smsoutbox) == 0
+
+    def test_it_does_not_send_message_twice(self):
+        OnboardingInfoFactory()
+        RemindBoops().remind_users()
+        assert len(self.smsoutbox) == 1
+        self.smsoutbox[:] = []
+        RemindBoops().remind_users()
+        assert len(self.smsoutbox) == 0
+
+    def test_it_sends_message(self):
+        OnboardingInfoFactory()
+        RemindBoops().remind_users()
+        assert len(self.smsoutbox) == 1
+        assert self.smsoutbox[0].body == "Hello world Boop Jones"
+
+    def test_it_sets_user_locale(self):
+        OnboardingInfoFactory(user__locale="es")
+        RemindBoops().remind_users()
+        assert len(self.smsoutbox) == 1
+        assert self.smsoutbox[0].body == "Hola mundo Boop Jones"
+
+    def test_it_ignores_users_we_cannot_sms(self):
+        OnboardingInfoFactory(can_we_sms=False)
+        RemindBoops().remind_users()
+        assert len(self.smsoutbox) == 0

loc/management/commands/sendreminders.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from texting import models
+from loc.sms_reminders import LocReminder
 
 
 class Command(BaseCommand):
@@ -11,9 +11,7 @@ class Command(BaseCommand):
                             action='store_true')
 
     def handle(self, *args, **options) -> None:
+        dry_run: bool = options['dry_run']
         print("Sending reminders to users who haven't yet finished their letter of complaint.")
-        for user in models.get_users_to_remind_about_loc():
-            print(f'Sending a reminder to {user.username}.')
-            if not options['dry_run']:
-                models.remind_user_about_loc(user)
+        LocReminder(dry_run=dry_run).remind_users()
         print("Done.")

loc/sms_reminders.py
@@ -1,0 +1,37 @@
+import datetime
+from django.utils import timezone
+
+from project.util.site_util import absolutify_url, get_site_name
+from texting.models import REMINDERS
+from texting.sms_reminder import SmsReminder
+from onboarding.models import SIGNUP_INTENT_CHOICES
+
+
+# After these many days have passed since the user
+# signed up, we will send them a reminder, unless they
+# have completed the LoC process.
+DAYS_UNTIL_LOC_REMINDER = 3
+
+
+class LocReminder(SmsReminder):
+    reminder_kind = REMINDERS.LOC
+
+    def filter_user_queryset(self, qs):
+        days_ago = timezone.now() - datetime.timedelta(days=DAYS_UNTIL_LOC_REMINDER)
+        return qs.filter(
+            date_joined__lte=days_ago,
+            onboarding_info__signup_intent=SIGNUP_INTENT_CHOICES.LOC,
+            letter_request__isnull=True,
+        )
+
+    def get_sms_text(self, user):
+        url = absolutify_url('/')
+        return (
+            f'Hey {user.first_name}! '
+            f'Don\'t forget that you can use {get_site_name()} to address '
+            f'repair issues in your apartment. '
+            f'Follow this link to continue: {url}'
+        )
+
+
+SmsReminder.validate(LocReminder())

locales/en/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2020-10-16 11:06+0000\n"
+"POT-Creation-Date: 2020-10-21 14:58+0000\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -43,9 +43,14 @@ msgstr ""
 msgid "%(city)s, %(state_name)s doesn't seem to exist!"
 msgstr ""
 
-#: norent/letter_sending.py:119
+#: norent/letter_sending.py:122
 #, python-format
 msgid "%(name)s you've sent your letter of non-payment of rent. You can track the delivery of your letter using USPS Tracking: %(url)s."
+msgstr ""
+
+#: norent/management/commands/send_norent_reminders.py:14
+#, python-format
+msgid "%(first_name)s, you've previously created an account on NoRent.org. If you are unable to pay rent next month AND you have a COVID-19 related reason for not paying, we recommend that on or before your rent due date or within 7 days of your rent due date, you send a new AB3088 declaration to your landlord through NoRent.org: %(url)s"
 msgstr ""
 
 #: norent/schema.py:497

locales/es/LC_MESSAGES/django.po
@@ -2,7 +2,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: tenants2\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2020-10-16 11:06+0000\n"
+"POT-Creation-Date: 2020-10-21 14:58+0000\n"
 "PO-Revision-Date: 2020-10-16 19:30\n"
 "Last-Translator: \n"
 "Language-Team: Spanish\n"
@@ -42,10 +42,15 @@ msgstr "cerrar"
 msgid "%(city)s, %(state_name)s doesn't seem to exist!"
 msgstr "¡%(city)s, %(state_name)s no existe!"
 
-#: norent/letter_sending.py:119
+#: norent/letter_sending.py:122
 #, python-format
 msgid "%(name)s you've sent your letter of non-payment of rent. You can track the delivery of your letter using USPS Tracking: %(url)s."
 msgstr "%(name)s has enviado tu carta de no pago de renta. Puedes seguir la entrega de tu carta usando USPS Tracking: %(url)s."
+
+#: norent/management/commands/send_norent_reminders.py:14
+#, python-format
+msgid "%(first_name)s, you've previously created an account on NoRent.org. If you are unable to pay rent next month AND you have a COVID-19 related reason for not paying, we recommend that on or before your rent due date or within 7 days of your rent due date, you send a new AB3088 declaration to your landlord through NoRent.org: %(url)s"
+msgstr ""
 
 #: norent/schema.py:497
 #, python-format
@@ -115,4 +120,3 @@ msgstr "%(areacode)s no es un prefijo telefónico válido."
 #: project/util/phone_number.py:68
 msgid "This does not look like a U.S. phone number. Please include the area code, e.g. (555) 123-4567."
 msgstr "Ese no parece un número de teléfono de los Estados Unidos de América. Por favor, incluye el prefijo telefónico, por ejemplo, (555) 123-4567."
-

commit cf0ee820710df095192eef045e85bf5070406570

Fix SMS bugs with invalid phone numbers. (#1737)
This fixes a regression in #1727 whereby we no longer skipped phone numbers that were invalid when sending reminders.

It also fixes a bug whereby we could not mark a valid phone number as being invalid (I guess this can happen if e.g. a phone number becomes disconnected).

texting/models.py
@@ -53,6 +53,21 @@ class PhoneNumberLookupManager(models.Manager):
         lookup.save()
         return lookup
 
+    def invalidate(self, phone_number: str) -> 'PhoneNumberLookup':
+        '''
+        Marks the given phone number as being invalid. Deals with the
+        case where a phone number that was once valid is now invalid.
+        '''
+
+        lookup, created = self.get_or_create(phone_number=phone_number, defaults={
+            'is_valid': False,
+        })
+        if not created:
+            lookup.is_valid = False
+            lookup.carrier = None
+            lookup.save()
+        return lookup
+
 
 class PhoneNumberLookup(models.Model):
     '''

texting/sms_reminder.py
@@ -3,7 +3,7 @@ from django.utils import translation
 from django.utils import timezone
 
 from users.models import JustfixUser
-from .models import Reminder, REMINDERS
+from .models import Reminder, REMINDERS, exclude_users_with_invalid_phone_numbers
 
 
 class SmsReminder(abc.ABC):
@@ -20,12 +20,16 @@ class SmsReminder(abc.ABC):
     def get_sms_text(self, user: JustfixUser) -> str:
         pass
 
-    def remind_users(self):
-        SmsReminder.validate(self)
-
+    def get_queryset(self):
         users = JustfixUser.objects.filter(onboarding_info__can_we_sms=True)
+        users = exclude_users_with_invalid_phone_numbers(users)
         users = self.filter_user_queryset(users)\
             .exclude(reminders__kind=self.reminder_kind)
+        return users
+
+    def remind_users(self):
+        SmsReminder.validate(self)
+        users = self.get_queryset()
 
         for user in users:
             with translation.override(user.locale):

texting/twilio.py
@@ -99,8 +99,7 @@ def send_sms(
             is_invalid_number = isinstance(e, TwilioRestException) and e.code == 21211
             if is_invalid_number:
                 logger.info(f'Phone number {phone_number} is invalid.')
-                lookup = PhoneNumberLookup(phone_number=phone_number, is_valid=False)
-                lookup.save()
+                PhoneNumberLookup.objects.invalidate(phone_number=phone_number)
                 if ignore_invalid_phone_number:
                     return ''
             if fail_silently:

texting/tests/test_models.py
@@ -60,6 +60,22 @@ class MockTwilioDbTest:
                 yield
 
 
+class TestInvalidate:
+    def test_it_works_when_no_record_existed(self, db):
+        lookup = PhoneNumberLookup.objects.invalidate('5551234567')
+        assert lookup.pk
+        assert lookup.is_valid is False
+        assert lookup.carrier is None
+
+    def test_it_modifies_existing_records(self, db):
+        orig = PhoneNumberLookup(phone_number='5551234567', is_valid=True, carrier={'hi': 1})
+        orig.save()
+        lookup = PhoneNumberLookup.objects.invalidate('5551234567')
+        assert lookup.pk == orig.pk
+        assert lookup.is_valid is False
+        assert lookup.carrier is None
+
+
 class TestGetOrLookup(MockTwilioDbTest):
     def test_it_returns_new_saved_lookup_with_carrier_info_for_valid_numbers(self):
         with self.mock_twilio(is_valid=True, carrier={'type': 'mobile'}):

texting/tests/test_sms_reminder.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext as _
 
 from onboarding.tests.factories import OnboardingInfoFactory
 from users.tests.factories import SecondUserFactory
+from texting.models import PhoneNumberLookup
 from texting.sms_reminder import SmsReminder
 
 
@@ -22,6 +23,15 @@ class TestSmsReminder:
     @pytest.fixture(autouse=True)
     def setup_fixture(self, db, smsoutbox):
         self.smsoutbox = smsoutbox
+
+    def test_it_ignores_users_with_invalid_numbers(self):
+        user = OnboardingInfoFactory().user
+        r = RemindBoops()
+        assert r.get_queryset().count() == 1
+
+        pl = PhoneNumberLookup(phone_number=user.phone_number, is_valid=False)
+        pl.save()
+        assert r.get_queryset().count() == 0
 
     def test_it_filters_out_users(self):
         OnboardingInfoFactory(user=SecondUserFactory())

texting/tests/test_twilio.py
@@ -152,6 +152,19 @@ def test_send_sms_ignores_invalid_numbers(db, settings, requests_mock):
     assert lookup.is_valid is False
 
 
+def test_send_sms_ignores_invalid_numbers_we_thought_were_valid(
+    db,
+    settings,
+    requests_mock
+):
+    apply_twilio_settings(settings)
+    mock_invalid_number(settings, requests_mock)
+    PhoneNumberLookup(phone_number='5551234567', is_valid=True, carrier={}).save()
+    assert send_sms('5551234567', 'boop', ignore_invalid_phone_number=True) == ''
+    lookup = PhoneNumberLookup.objects.get(phone_number='5551234567')
+    assert lookup.is_valid is False
+
+
 def test_send_sms_remembers_invalid_numbers_even_when_not_ignoring_them(
     db,
     settings,
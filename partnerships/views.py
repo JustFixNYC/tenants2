cb21782550d862fcab527891f45974fad5c88887
Atul Varma <varmaa@gmail.com>
2020-10-27 08:36:01 -0400

    Factor out project.util.success_url.

--- partnerships/views.py
+++ partnerships/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect, HttpRequest, Http404
 
+from project.util.success_url import get_success_url
 from .models import PartnerOrg
 from . import referral
 
@@ -8,8 +9,6 @@ def activate_referral(request: HttpRequest, partner_slug: str):
     partner = PartnerOrg.objects.filter(slug=partner_slug).first()
     if partner is None:
         raise Http404()
-
-    from twofactor.views import get_success_url
 
     referral.set_partner(request, partner)
     return HttpResponseRedirect(get_success_url(request))

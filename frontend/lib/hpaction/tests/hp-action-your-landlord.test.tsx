ba3c88dd2b5c18cda954ed83d3d9c3908a27e6e1
Atul Varma <varmaa@gmail.com>
2020-11-06 17:52:45 -0500

    Add front-end for changing LL/mgmt co info. (#1763)
    This uses the GraphQL endpoint added in #1762 to add a UI to the HP Action flow which allows users to override the recommended LL / management company, or to go back to our recommendation after overriding.
    
    If the user is overriding the default behavior, a querystring variable called `force` will be in their URL.  This is now logged as part of our Amplitude form submission event, so that we can get an idea of how many users are overriding.

--- frontend/lib/hpaction/tests/hp-action-your-landlord.test.tsx
+++ frontend/lib/hpaction/tests/hp-action-your-landlord.test.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AppTesterPal } from "../../tests/app-tester-pal";
-import { HPActionYourLandlord } from "../hp-action-your-landlord";
+import {
+  HPActionYourLandlord,
+  shouldUseRecommendedLandlordInfo,
+} from "../hp-action-your-landlord";
 import { Route } from "react-router-dom";
 import { BlankOnboardingInfo } from "../../queries/OnboardingInfo";
 import { LeaseType } from "../../queries/globalTypes";
@@ -11,13 +14,103 @@ import {
 import { RecommendedHpLandlord } from "../../queries/RecommendedHpLandlord";
 import { waitFor } from "@testing-library/dom";
 
-describe("HPActionYourLandlord", () => {
-  const landlordInfo: LandlordDetailsType = {
-    ...BlankLandlordDetailsType,
-    name: "Landlordo Calrissian",
-    address: "1 Cloud City",
-  };
+const landlordInfo: LandlordDetailsType = {
+  ...BlankLandlordDetailsType,
+  name: "Landlordo Calrissian",
+  address: "1 Cloud City",
+};
 
+const manualLandlordInfo: LandlordDetailsType = {
+  ...BlankLandlordDetailsType,
+  name: "My Manual Landlord",
+  address: "My Manual Address",
+};
+
+async function mockRecommendation(
+  pal: AppTesterPal,
+  options: { landlord?: boolean; mgmtCo?: boolean } = {}
+) {
+  pal.withQuery(RecommendedHpLandlord).respondWith({
+    recommendedHpLandlord: options.landlord
+      ? {
+          name: landlordInfo.name,
+          primaryLine: landlordInfo.address,
+          city: "Bespin",
+          state: "OH",
+          zipCode: "43220",
+        }
+      : null,
+    recommendedHpManagementCompany: options.mgmtCo
+      ? {
+          name: "Cloud City Management",
+          primaryLine: "1 Managerial Way",
+          city: "Bespin",
+          state: "OH",
+          zipCode: "43221",
+        }
+      : null,
+  });
+  await waitFor(() => pal.rr.getByText("Next"));
+}
+
+describe("shouldUseRecommendedLandlordInfo()", () => {
+  it("Returns true when LL is currently manually specified but user wants to force recommended", () => {
+    expect(
+      shouldUseRecommendedLandlordInfo({
+        hasRecommendedLandlord: true,
+        isLandlordAlreadyManuallySpecified: true,
+        forceManual: false,
+        forceRecommended: true,
+      })
+    ).toBe(true);
+  });
+
+  it("Returns false when LL is currently manually specified and user has no opinion", () => {
+    expect(
+      shouldUseRecommendedLandlordInfo({
+        hasRecommendedLandlord: true,
+        isLandlordAlreadyManuallySpecified: true,
+        forceManual: false,
+        forceRecommended: false,
+      })
+    ).toBe(false);
+  });
+
+  it("Returns true when recommendation exists and user has no opinion", () => {
+    expect(
+      shouldUseRecommendedLandlordInfo({
+        hasRecommendedLandlord: true,
+        isLandlordAlreadyManuallySpecified: false,
+        forceManual: false,
+        forceRecommended: false,
+      })
+    ).toBe(true);
+  });
+
+  it("Returns false when recommendation exists and user wants to force manual", () => {
+    expect(
+      shouldUseRecommendedLandlordInfo({
+        hasRecommendedLandlord: true,
+        isLandlordAlreadyManuallySpecified: false,
+        forceManual: true,
+        forceRecommended: false,
+      })
+    ).toBe(false);
+  });
+
+  it("Returns false when no recommendation exists", () => {
+    expect(
+      shouldUseRecommendedLandlordInfo({
+        hasRecommendedLandlord: false,
+        isLandlordAlreadyManuallySpecified: false,
+        forceManual: false,
+        forceRecommended: false,
+      })
+    ).toBe(false);
+  });
+});
+
+describe("HPActionYourLandlord", () => {
   const makeRoute = () => (
     <Route
       render={(props) => (
@@ -26,7 +119,7 @@ describe("HPActionYourLandlord", () => {
     />
   );
 
-  it("shows dynamic address when user is NYCHA", () => {
+  it("shows dynamic address when user is NYCHA", async () => {
     const pal = new AppTesterPal(makeRoute(), {
       session: {
         onboardingInfo: {
@@ -35,37 +128,52 @@ describe("HPActionYourLandlord", () => {
         },
       },
     });
-    pal.getElement("div", ".jf-loader");
+    await mockRecommendation(pal, { landlord: true });
+    pal.rr.getByText(/you are in NYCHA housing/i);
   });
 
-  it("shows manually-entered address in form fields", () => {
+  it("shows manually-entered address in form fields", async () => {
     const pal = new AppTesterPal(makeRoute(), {
-      session: { landlordDetails: { ...landlordInfo, isLookedUp: false } },
+      session: {
+        landlordDetails: { ...manualLandlordInfo, isLookedUp: false },
+        managementCompanyDetails: {
+          name: "My manual management company",
+          primaryLine: "blarg",
+          city: "Beanville",
+          state: "CA",
+          zipCode: "91234",
+        },
+      },
     });
+    await mockRecommendation(pal, { landlord: true });
+    const llInput = pal.rr.getByLabelText("Landlord name") as HTMLInputElement;
+    expect(llInput.value).toBe("My Manual Landlord");
+    const mcInput = pal.rr.getByLabelText(
+      "Management company name"
+    ) as HTMLInputElement;
+    expect(mcInput.value).toBe("My manual management company");
+  });
+
+  it("allows user to override recommendation w/ manually-entered address", async () => {
+    const pal = new AppTesterPal(makeRoute(), { url: "/?force=manual" });
+    await mockRecommendation(pal, { landlord: true });
+    pal.rr.getByText(/You have chosen to ignore the landlord recommended/i);
     const input = pal.rr.getByLabelText("Landlord name") as HTMLInputElement;
-    expect(input.value).toBe("Landlordo Calrissian");
+    expect(input.value).toBe("");
   });
 
-  it("shows automatically looked-up address as read-only", async () => {
+  it("shows newest recommended landlord instead of stale one", async () => {
     const pal = new AppTesterPal(makeRoute(), {
-      session: { landlordDetails: { ...landlordInfo, isLookedUp: true } },
-    });
-    pal.withQuery(RecommendedHpLandlord).respondWith({
-      recommendedHpLandlord: {
-        name: landlordInfo.name,
-        primaryLine: landlordInfo.address,
-        city: "Bespin",
-        state: "OH",
-        zipCode: "43220",
-      },
-      recommendedHpManagementCompany: {
-        name: "Cloud City Management",
-        primaryLine: "1 Managerial Way",
-        city: "Bespin",
-        state: "OH",
-        zipCode: "43221",
+      session: {
+        landlordDetails: {
+          ...landlordInfo,
+          name: "STALE LANDLORD BEFORE LANDLORDO",
+          isLookedUp: true,
+        },
       },
     });
-    await waitFor(() => pal.rr.getByText("Cloud City Management"));
+    await mockRecommendation(pal, { landlord: true, mgmtCo: true });
+    pal.rr.getByText("Landlordo Calrissian");
+    pal.rr.getByText("Cloud City Management");
   });
 });

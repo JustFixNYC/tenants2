1e261e77d5005d2bf99e3a556980fbfd9027eb6a
Atul Varma <varmaa@gmail.com>
2020-11-18 09:24:33 -0500

    Ensure first page of EHPA has correct LL info.

--- hpaction/tests/test_ehpa_affadavit.py
+++ hpaction/tests/test_ehpa_affadavit.py
@@ -10,7 +10,7 @@ from onboarding.tests.factories import OnboardingInfoFactory
 
 
 class TestEHPAAffadavitVarsFromUser:
-    def test_it_works_with_empty_user(self):
+    def test_it_works_with_empty_user(self, db):
         assert EHPAAffadavitVars.from_user(JustfixUser()).dict() == {
             'tenant_name': 'N/A',
             'tenant_email': 'N/A',
@@ -38,6 +38,29 @@ class TestEHPAAffadavitVarsFromUser:
             'landlord_email': 'landlordo@calrissian.net',
             'landlord_phone': '(555) 203-4032',
             'landlord_address': '123 Cloud City Drive, Bespin, NY 12345'
+        }
+
+    def test_it_works_with_looked_up_landlord(self, db, nycdb):
+        hpd_reg = nycdb.load_hpd_registration('medium-landlord.json')
+        onb = OnboardingInfoFactory(
+            user__email="boop@jones.net",
+            pad_bbl=hpd_reg.pad_bbl,
+        )
+        LandlordDetailsV2Factory(
+            user=onb.user,
+            email='landlordo@calrissian.net',
+            phone_number='5552034032',
+            is_looked_up=True,
+        )
+        assert EHPAAffadavitVars.from_user(onb.user).dict() == {
+            'tenant_name': 'Boop Jones',
+            'tenant_email': 'boop@jones.net',
+            'tenant_phone': '(555) 123-4567',
+            'tenant_address': '150 court street, Apartment 2, Brooklyn, NY',
+            'landlord_name': 'ULTRA DEVELOPERS, LLC',
+            'landlord_email': 'landlordo@calrissian.net',
+            'landlord_phone': '(555) 203-4032',
+            'landlord_address': '3 ULTRA STREET, BROOKLYN, NY 11999'
         }
 
 

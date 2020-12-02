6c94adac241675f2966648a5d7c15e0b3bd6bbfb
sraby <samrabiyah@gmail.com>
2020-12-02 13:38:13 -0500

    updated comments

--- rh/tests/test_utils.py
+++ rh/tests/test_utils.py
@@ -1,18 +1,17 @@
 from rh.schema import process_rent_stab_data
 
-# 654 Park Place, had rent stab units in 2019
+# Data for 654 Park Place, had rent stab units in 2019
 EXAMPLE_RENT_STAB_DATA = {'uc2007': 11, 'uc2008': 11, 'uc2009': 11, 'uc2010': 11, 'uc2011': 11,
                           'uc2012': 12, 'uc2013': 12, 'uc2014': 12, 'uc2015': 12, 'uc2016': 12,
                           'uc2017': 12, 'uc2018': 12, 'uc2019': 12}
 
-# 69 Montrose Avenue, had rent stab units in 2008 but not after
+# Data for 69 Montrose Avenue, had rent stab units in 2008 but not after
 EXAMPLE_OLD_RENT_STAB_DATA = {'uc2007': 6, 'uc2008': 6, 'uc2009': None, 'uc2010': None,
                               'uc2011': None, 'uc2012': None, 'uc2013': None, 'uc2014': None,
                               'uc2015': None, 'uc2016': None, 'uc2017': None, 'uc2018': None,
                               'uc2019': None}
 
-# 93 Knickerbocker Ave, has record in rent stab data
-# but lists 0 units each year
+# Data for 93 Knickerbocker Ave, has record in rent stab data but lists 0 units each year
 EXAMPLE_ZERO_RENT_STAB_DATA = {'uc2007': None, 'uc2008': None, 'uc2009': None, 'uc2010': None,
                                'uc2011': None, 'uc2012': None, 'uc2013': None, 'uc2014': None,
                                'uc2015': None, 'uc2016': None, 'uc2017': None, 'uc2018': None,

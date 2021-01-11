e8d99d7a84b6b6a54afaf20bbbdc52523e858057
Atul Varma <varmaa@gmail.com>
2021-01-11 08:41:00 -0500

    Add evictionfree message catalog chunk.

--- frontend/lib/evictionfree/site.tsx
+++ frontend/lib/evictionfree/site.tsx
@@ -1,9 +1,27 @@
+import { Trans } from "@lingui/macro";
+import loadable from "@loadable/component";
 import React from "react";
 import type { AppSiteProps } from "../app";
+import { createLinguiCatalogLoader } from "../i18n-lingui";
+
+export const EvictionFreeLinguiI18n = createLinguiCatalogLoader({
+  en: loadable.lib(
+    () => import("../../../locales/en/evictionfree.chunk") as any
+  ),
+  es: loadable.lib(
+    () => import("../../../locales/es/evictionfree.chunk") as any
+  ),
+});
 
 const EvictionFreeSite = React.forwardRef<HTMLDivElement, AppSiteProps>(
   (props, ref) => {
-    return <p>TODO IMPLEMENT THIS</p>;
+    return (
+      <EvictionFreeLinguiI18n>
+        <p>
+          <Trans>This is a test localization message for EvictionFree.</Trans>
+        </p>
+      </EvictionFreeLinguiI18n>
+    );
   }
 );
 

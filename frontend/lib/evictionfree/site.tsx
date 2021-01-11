722ac85443d137d2237c524ef9f1f6ce9d254372
Atul Varma <varmaa@gmail.com>
2021-01-11 07:56:51 -0500

    Add new, empty eviction free site.

---
+++ frontend/lib/evictionfree/site.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import type { AppSiteProps } from "../app";
+
+const EvictionFreeSite = React.forwardRef<HTMLDivElement, AppSiteProps>(
+  (props, ref) => {
+    return <p>TODO IMPLEMENT THIS</p>;
+  }
+);
+
+export default EvictionFreeSite;

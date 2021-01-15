a93a970ee354afe19d6d039be4d108d6f3795413
Atul Varma <varmaa@gmail.com>
2021-01-15 08:43:36 -0500

    Add name step to evictionfree.

--- frontend/lib/evictionfree/declaration-builder/routes.tsx
+++ frontend/lib/evictionfree/declaration-builder/routes.tsx
@@ -1,11 +1,17 @@
+import { AskNameStep } from "../../common-steps/ask-name";
 import {
   buildProgressRoutesComponent,
   ProgressRoutesProps,
 } from "../../progress/progress-routes";
+import { skipStepsIf } from "../../progress/skip-steps-if";
 import { createStartAccountOrLoginSteps } from "../../start-account-or-login/routes";
+import { isUserLoggedIn } from "../../util/session-predicates";
 import { EvictionFreeRoutes } from "../route-info";
 import { EvictionFreeDbConfirmation } from "./confirmation";
+import { EvictionFreeOnboardingStep } from "./step-decorators";
 import { EvictionFreeDbWelcome } from "./welcome";
+
+const EfAskName = EvictionFreeOnboardingStep(AskNameStep);
 
 export const getEvictionFreeDeclarationBuilderProgressRoutesProps = (): ProgressRoutesProps => {
   const routes = EvictionFreeRoutes.locale.declaration;
@@ -20,7 +26,16 @@ export const getEvictionFreeDeclarationBuilderProgressRoutesProps = (): Progress
       },
       ...createStartAccountOrLoginSteps(routes),
     ],
-    stepsToFillOut: [],
+    stepsToFillOut: [
+      // TODO: Add cross-site "agree to terms" step.
+      ...skipStepsIf(isUserLoggedIn, [
+        {
+          path: routes.name,
+          exact: true,
+          component: EfAskName,
+        },
+      ]),
+    ],
     confirmationSteps: [
       {
         path: routes.confirmation,

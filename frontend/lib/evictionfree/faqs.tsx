20114c609dccc923de0dccb64683ef589cdad805
sraby <samrabiyah@gmail.com>
2021-01-19 17:19:56 -0500

    Added basic faqs page

--- frontend/lib/evictionfree/faqs.tsx
+++ frontend/lib/evictionfree/faqs.tsx
@@ -1,22 +1,95 @@
 import { Trans } from "@lingui/macro";
 import React from "react";
+import { Link } from "react-router-dom";
+import { JumpArrow } from "../norent/homepage";
+import { Accordion } from "../ui/accordion";
 import Page from "../ui/page";
+import {
+  EvictionFreeFaq,
+  getEvictionFreeFaqsContent,
+  getEvictionFreeFaqsWithPreviewContent,
+} from "./data/faqs-content";
+import { EvictionFreeRoutes } from "./route-info";
 
-export const EvictionFreeFaqsPage: React.FC<{}> = () => (
-  <Page title="">
-    <section className="hero is-fullheight-with-navbar">
+function generateFaqsListFromData(
+  data: EvictionFreeFaq[],
+  isPreview?: boolean
+) {
+  return data.map((faq, i) => (
+    <Accordion
+      key={i}
+      question={faq.question}
+      questionClassName="title jf-alt-title-font is-size-5"
+    >
+      {isPreview ? faq.previewOptions?.answerPreview : faq.answerFull}
+    </Accordion>
+  ));
+}
+
+export const EvictionFreeFaqsPreview = () => {
+  return (
+    <section className="hero jf-faqs-preview">
       <div className="hero-body">
-        <div className="container jf-has-text-centered-tablet">
-          <p>This website is under construction.</p>
-          <p>
-            <small>
-              <Trans>
-                This is a test localization message for EvictionFree.
-              </Trans>
-            </small>
-          </p>
+        <div className="container jf-tight-container jf-has-text-centered-tablet jf-space-below-2rem">
+          <h3 className="is-spaced has-text-weight-normal is-size-5">
+            Sending a letter to your landlord is a big step. Here are a few{" "}
+            <Link to={EvictionFreeRoutes.locale.faqs}>
+              frequently asked questions
+            </Link>{" "}
+            from people who have used our tool:
+          </h3>
+          <br />
+          <div className="jf-space-below-2rem">
+            {generateFaqsListFromData(
+              getEvictionFreeFaqsWithPreviewContent(),
+              true
+            )}
+          </div>
+          <Link
+            to={EvictionFreeRoutes.locale.faqs}
+            className="is-size-5 has-text-weight-normal"
+          >
+            <Trans>See more FAQs</Trans>
+          </Link>
         </div>
       </div>
     </section>
-  </Page>
-);
+  );
+};
+
+export const EvictionFreeFaqsPage: React.FC<{}> = () => {
+  const allFaqs = getEvictionFreeFaqsContent();
+
+  return (
+    <Page title="FAQs" className="content">
+      <section className="hero is-medium">
+        <div className="hero-body">
+          <div className="container jf-has-text-centered-tablet">
+            <h2 className="title is-spaced has-text-info">
+              Frequently Asked Questions
+            </h2>
+            <br />
+            <p className="subtitle">
+              Navigating these laws is confusing. Check out our frequently asked
+              questions from people who have used our tool:
+            </p>
+          </div>
+        </div>
+        <br />
+        <div className="container jf-has-centered-images jf-space-below-2rem">
+          <JumpArrow to="#more-info" altText="Browse the FAQs" />
+          <br />
+        </div>
+      </section>
+
+      <section className="hero jf-faqs" id="more-info">
+        <div className="hero-body">
+          <div className="container jf-tight-container">
+            <br />
+            {generateFaqsListFromData(allFaqs)}
+          </div>
+        </div>
+      </section>
+    </Page>
+  );
+};

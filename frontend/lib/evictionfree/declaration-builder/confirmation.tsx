c615d959d35390a9bd04913fdc6148c513a2f779
sraby <samrabiyah@gmail.com>
2021-01-20 16:27:11 -0500

    added conditional for nyc

--- frontend/lib/evictionfree/declaration-builder/confirmation.tsx
+++ frontend/lib/evictionfree/declaration-builder/confirmation.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { OutboundLink } from "../../analytics/google-analytics";
 import { ProgressStepProps } from "../../progress/progress-step-route";
 import { USPS_TRACKING_URL_PREFIX } from "../../../../common-data/loc.json";
 import Page from "../../ui/page";
+import { AppContext } from "../../app-context";
 
 // TO DO: Replace this tracking number with the user's actual one
 const SAMPLE_USPS_TRACKING_NUMBER = "129837127326123";
@@ -104,6 +105,8 @@ const OrganizingGroupsBlurb = () => (
 export const EvictionFreeDbConfirmation: React.FC<ProgressStepProps> = (
   props
 ) => {
+  const { session } = useContext(AppContext);
+  const isInNyc = session.norentScaffolding?.isCityInNyc;
   return (
     <Page title="You've sent your hardship declaration" className="content">
       <TitleWithSymbol />
@@ -135,11 +138,12 @@ export const EvictionFreeDbConfirmation: React.FC<ProgressStepProps> = (
         </OutboundLink>
       </p>
 
-      {/* TO DO: Only show the following two sections if user is in NYC */}
-      <>
-        <RetaliationBlurb />
-        <HcaHotlineBlurb />
-      </>
+      {isInNyc && (
+        <>
+          <RetaliationBlurb />
+          <HcaHotlineBlurb />
+        </>
+      )}
 
       <OrganizingGroupsBlurb />
     </Page>

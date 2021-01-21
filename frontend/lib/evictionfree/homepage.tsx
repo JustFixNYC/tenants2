ec17adeeadb2e274809e7b232653ceb26e7f2c5b
Sam Rabiyah <sraby@users.noreply.github.com>
2021-01-21 15:10:39 -0500

    Add scaffolding of Eviction Free landing page (#1829)
    This PR is a continuation of the work on #1828: it implements the rest of the structure of the landing page on the Eviction Free site. Content is still placeholder as the actual content for these primary pages is yet to be finalized. Included in here is also some quick fixes to the header that were made clearer through the implementation of the page.

--- frontend/lib/evictionfree/homepage.tsx
+++ frontend/lib/evictionfree/homepage.tsx
@@ -11,6 +11,38 @@ export function getEFImageSrc(name: string, type?: EvictionFreeImageType) {
   return `frontend/img/evictionfree/${name}.${type || "svg"}`;
 }
 
+const checklistItems = () => [
+  `Build a letter using our free letter builder`,
+  `Send your letter by email`,
+  `Send your letter by certified mail for free`,
+  `Cite up-to-date legal ordinances in your letter`,
+];
+
+const LandingPageChecklist = () => (
+  <div className="hero">
+    <div className="hero-body">
+      <h2 className="is-size-3 is-spaced has-text-weight-bold">
+        With our free tool, you can
+      </h2>
+      <br />
+      <div className="jf-space-below-2rem">
+        {checklistItems().map((checklistItem, i) => (
+          <article className="media" key={i}>
+            <div className="media-left">
+              <StaticImage
+                ratio="is-32x32"
+                src={getEFImageSrc("checkmark")}
+                alt="You can"
+              />
+            </div>
+            <div className="media-content">{checklistItem}</div>
+          </article>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
 export const EvictionFreeHomePage: React.FC<{}> = () => (
   <Page
     title="Protect yourself from eviction in New York State"
@@ -30,7 +62,7 @@ export const EvictionFreeHomePage: React.FC<{}> = () => (
             </p>
             <br />
             <Link
-              className="button is-primary jf-build-my-declaration-btn"
+              className="button is-primary jf-build-my-declaration-btn jf-is-extra-wide"
               to={Routes.locale.declaration.latestStep}
             >
               Build my declaration
@@ -39,13 +71,14 @@ export const EvictionFreeHomePage: React.FC<{}> = () => (
           <div className="column">
             <StaticImage
               ratio="is-square"
-              src={getEFImageSrc("forms")}
+              src={getEFImageSrc("forms", "png")}
               alt=""
             />
           </div>
         </div>
       </div>
     </section>
+
     <section className="hero is-info">
       <div className="hero-body">
         <div className="columns is-centered">
@@ -58,6 +91,81 @@ export const EvictionFreeHomePage: React.FC<{}> = () => (
         </div>
       </div>
     </section>
+
+    <div className="columns">
+      <div className="column is-half">
+        <LandingPageChecklist />
+      </div>
+      <div className="column is-half"></div>
+    </div>
+
+    <div className="columns">
+      <div className="column is-half"></div>
+      <div className="column is-half">
+        <div className="hero">
+          <div className="hero-body">
+            <h2 className="is-size-3 is-spaced has-text-weight-bold">
+              For New York state tenants
+            </h2>
+            <p>This tool is for you if:</p>
+            <ul>
+              <li>
+                you are a New York State tenant who has been impacted by
+                COVID-19
+              </li>
+            </ul>
+            <p>And:</p>
+            <ul>
+              <li>
+                you have been served an eviction notice by your landlord in the
+                last X months and/or
+              </li>
+              <li>believe you are at risk of being evicted </li>
+            </ul>
+            <p className="has-text-weight-bold">
+              The protections outlined by NY state law apply to you regardless
+              of immigration status.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="columns">
+      <div className="column is-half">
+        <div className="hero">
+          <div className="hero-body">
+            <h2 className="is-size-3 is-spaced has-text-weight-bold">
+              For tenants by tenants
+            </h2>
+            <p>
+              Our free tool was built by Right to Counsel, Housing Justice for
+              All, and JustFix.nyc as part of the larger tenant movement across
+              the state.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="column is-half"></div>
+    </div>
+
+    <section className="hero is-info">
+      <div className="hero-body">
+        <div className="columns is-centered">
+          <div className="column is-three-quarters is-size-4 has-text-centered">
+            <h2 className="is-size-3 has-text-white	is-spaced has-text-weight-bold">
+              Fight to #CancelRent
+            </h2>
+            <p>
+              After sending your declaration, connect with local groups to
+              organize for greater demands with other tenants including
+              cancelling rent.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <EvictionFreeFaqsPreview />
   </Page>
 );

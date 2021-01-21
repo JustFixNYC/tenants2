15738950e00d90ed51b9ad3ccde3baca5bc3c69e
Sam Rabiyah <sraby@users.noreply.github.com>
2021-01-21 15:51:32 -0500

    Added some sample FAQs content (#1830)
    This PR adds 6 FAQs questions to both the homepage preview and the FAQs page on the Eviction Free site.

--- frontend/lib/evictionfree/data/faqs-content.tsx
+++ frontend/lib/evictionfree/data/faqs-content.tsx
@@ -21,16 +21,137 @@ export type EvictionFreeFaq = {
  */
 export const getEvictionFreeFaqsContent: () => EvictionFreeFaq[] = () => [
   {
-    question: `I'm scared. What happens if my landlord retaliates?`,
+    question: `Is this free?`,
     previewOptions: {
       priorityInPreview: 1,
-      answerPreview: <p>Blah!</p>,
+      answerPreview: (
+        <p>
+          Yes, this is a free website created by 501(c)3 non-profit
+          organizations.
+        </p>
+      ),
     },
-    answerFull: <p>Blah Blah!</p>,
+    answerFull: (
+      <p>
+        Yes, this is a free website created by 501(c)3 non-profit organizations.
+      </p>
+    ),
   },
   {
-    question: `Is this free?`,
-    answerFull: <p>Blah Blah!</p>,
+    question: `Do I have to go to the post office to mail my declaration?`,
+    previewOptions: {
+      priorityInPreview: 2,
+      answerPreview: (
+        <p>
+          No, you can use this website to send a letter to your landlord via
+          email or USPS mail. You do not have to pay for the letter to be
+          mailed. If you choose not to use this tool, you will be responsible
+          for mailing your declaration.
+        </p>
+      ),
+    },
+    answerFull: (
+      <p>
+        No, you can use this website to send a letter to your landlord via email
+        or USPS mail. You do not have to pay for the letter to be mailed. If you
+        choose not to use this tool, you will be responsible for mailing your
+        declaration.
+      </p>
+    ),
+  },
+  {
+    question: `I’m undocumented. Can I use this tool?`,
+    previewOptions: {
+      priorityInPreview: 3,
+      answerPreview: (
+        <p>
+          Yes, the protections outlined by New York State law apply to you
+          regardless of immigration status.
+        </p>
+      ),
+    },
+    answerFull: (
+      <p>
+        Yes, the protections outlined by New York State law apply to you
+        regardless of immigration status.
+      </p>
+    ),
+  },
+  {
+    question: `I live in another state that isn’t New York. Is this tool for me?`,
+    previewOptions: {
+      priorityInPreview: 4,
+      answerPreview: (
+        <p>
+          No. Unfortunately, these protections only apply to residents of New
+          York State.
+        </p>
+      ),
+    },
+    answerFull: (
+      <p>
+        No. Unfortunately, these protections only apply to residents of New York
+        State.
+      </p>
+    ),
+  },
+  {
+    question: `Can I see what forms I’m sending before I fill them out?`,
+    answerFull: (
+      <p>
+        When you use our tool, you will be able to preview your filled out form
+        before sending it. You can also view a blank copy of the Hardship
+        Declaration form.
+      </p>
+    ),
+  },
+  {
+    question: `Is the online tool the only way to submit this form?`,
+    answerFull: (
+      <>
+        <p>
+          No! You can print out the Hardship Declaration form yourself, fill it
+          out by hand, and mail/email it to your landlord and local housing
+          court.
+        </p>
+
+        <p>
+          New York City residents can send their declarations to the court in
+          their borough:
+        </p>
+        <ul>
+          <li>
+            <p>Manhattan Housing Court: </p>
+            <ul>
+              <li>Address: 111 Centre Street, New York, NY 10013</li>
+              <li>Email: NewYorkHardshipDeclaration@nycourts.gov</li>
+            </ul>
+            <p>Bronx Housing Court </p>
+            <ul>
+              <li>Address: 1118 Grand Concourse, Bronx, NY 10456</li>
+              <li>Email: BronxHardshipDeclaration@nycourts.gov </li>
+            </ul>
+            <p>Brooklyn Housing Court </p>
+            <ul>
+              <li>Address: 141 Livingston St, Brooklyn, NY 11201</li>
+              <li>Email: KingsHardshipDeclaration@nycourts.gov</li>
+            </ul>
+            <p>Queens Housing Court </p>
+            <ul>
+              <li>Address: 89-17 Sutphin Boulevard, Jamaica, New York 11435</li>
+              <li>Email: QueensHardshipDeclaration@nycourts.gov</li>
+            </ul>
+            <p>Staten Island Housing Court </p>
+            <ul>
+              <li>
+                Address: 927 Castleton Avenue, Staten Island, New York 10310
+              </li>
+              <li>Email: RichmondHardshipDeclaration@nycourts.gov </li>
+            </ul>
+          </li>
+        </ul>
+      </>
+    ),
   },
 ];
 

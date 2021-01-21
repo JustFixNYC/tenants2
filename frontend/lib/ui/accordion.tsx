1a07f58264d807224c7c4a6164aa3535afc1bdda
Atul Varma <varmaa@gmail.com>
2021-01-20 19:30:13 -0500

    Add explanatory accordions on covid impact step. (#1823)
    This adds accordion explainers that outline the full meaning of COVID hardship declaration checkboxes.

--- frontend/lib/ui/accordion.tsx
+++ frontend/lib/ui/accordion.tsx
@@ -9,27 +9,32 @@ export const Accordion = (props: {
   question: string;
   children: React.ReactNode;
   questionClassName?: string;
-}) => (
-  <div className="jf-accordion-item jf-space-below-2rem">
-    <details className="has-text-left jf-space-below-2rem">
-      <summary>
-        <div className="media">
-          <div className="media-content">
-            <span
-              className={
-                props.questionClassName ??
-                "is-size-6 has-text-primary jf-has-text-underline"
-              }
-            >
-              {props.question}
-            </span>
+  extraClassName?: string;
+}) => {
+  const extraClassName = props.extraClassName ?? "jf-space-below-2rem";
+
+  return (
+    <div className={`jf-accordion-item ${extraClassName}`}>
+      <details className={`has-text-left ${extraClassName}`}>
+        <summary>
+          <div className="media">
+            <div className="media-content">
+              <span
+                className={
+                  props.questionClassName ??
+                  "is-size-6 has-text-primary jf-has-text-underline"
+                }
+              >
+                {props.question}
+              </span>
+            </div>
+            <div className="media-right">
+              <ChevronIcon />
+            </div>
           </div>
-          <div className="media-right">
-            <ChevronIcon />
-          </div>
-        </div>
-      </summary>
-      {props.children}
-    </details>
-  </div>
-);
+        </summary>
+        {props.children}
+      </details>
+    </div>
+  );
+};

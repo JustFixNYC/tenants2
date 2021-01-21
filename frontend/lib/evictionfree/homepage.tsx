fb9a84038e7ced148defc7dac1134d64467031a5
Sam Rabiyah <sraby@users.noreply.github.com>
2021-01-21 08:55:57 -0500

    Add landing page and a few sections below header on homepage (#1828)
    This PR adds the full-height hero to our homepage and adjusts the header to match spacing. It also adds in a few more sections to the homepage for demonstrative purposes. The full homepage will be implemented later today, Inshallah :)

--- frontend/lib/evictionfree/homepage.tsx
+++ frontend/lib/evictionfree/homepage.tsx
@@ -1,22 +1,63 @@
-import { Trans } from "@lingui/macro";
 import React from "react";
+import { Link } from "react-router-dom";
 import Page from "../ui/page";
+import { StaticImage } from "../ui/static-image";
+import { EvictionFreeRoutes as Routes } from "./route-info";
+import { EvictionFreeFaqsPreview } from "./faqs";
+
+type EvictionFreeImageType = "png" | "svg";
+
+export function getEFImageSrc(name: string, type?: EvictionFreeImageType) {
+  return `frontend/img/evictionfree/${name}.${type || "svg"}`;
+}
 
 export const EvictionFreeHomePage: React.FC<{}> = () => (
-  <Page title="">
+  <Page
+    title="Protect yourself from eviction in New York State"
+    className="content"
+  >
     <section className="hero is-fullheight-with-navbar">
       <div className="hero-body">
-        <div className="container jf-has-text-centered-tablet">
-          <p>This website is under construction.</p>
-          <p>
-            <small>
-              <Trans>
-                This is a test localization message for EvictionFree.
-              </Trans>
-            </small>
-          </p>
+        <div className="columns">
+          <div className="column is-three-fifths">
+            <h1 className="title is-spaced">
+              Protect yourself from eviction in New York State
+            </h1>
+            <p className="subtitle">
+              Use our free tool to send a hardship declaration form to your
+              landlord and your local courts to put your eviction case on hold
+              until May 1st, 2021.
+            </p>
+            <br />
+            <Link
+              className="button is-primary jf-build-my-declaration-btn"
+              to={Routes.locale.declaration.latestStep}
+            >
+              Build my declaration
+            </Link>
+          </div>
+          <div className="column">
+            <StaticImage
+              ratio="is-square"
+              src={getEFImageSrc("forms")}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </section>
+    <section className="hero is-info">
+      <div className="hero-body">
+        <div className="columns is-centered">
+          <div className="column is-three-quarters is-size-4 has-text-centered">
+            On December 28, 2020, New York State signed into law an eviction
+            moratorium that protects tenants from eviction provided that they
+            fill out a hardship declaration form and send it to their landlord
+            and/or the courts.
+          </div>
+        </div>
+      </div>
+    </section>
+    <EvictionFreeFaqsPreview />
   </Page>
 );

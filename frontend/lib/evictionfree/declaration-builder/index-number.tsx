a392d9a8ebe7d5bb3ea13de4c2a9d4bbc82f03c3
Atul Varma <varmaa@gmail.com>
2021-01-25 07:43:15 -0500

    Add back-end logic for index number form.

--- frontend/lib/evictionfree/declaration-builder/index-number.tsx
+++ frontend/lib/evictionfree/declaration-builder/index-number.tsx
@@ -4,46 +4,67 @@ import {
   hideByDefault,
 } from "../../forms/conditional-form-fields";
 import { TextualFormField } from "../../forms/form-fields";
-import { usePrototypingFormFieldProps } from "../../forms/prototyping";
+import { SessionUpdatingFormSubmitter } from "../../forms/session-updating-form-submitter";
 import {
   YesNoRadiosFormField,
+  YES_NO_RADIOS_FALSE,
   YES_NO_RADIOS_TRUE,
 } from "../../forms/yes-no-radios-form-field";
 import { MiddleProgressStep } from "../../progress/progress-step-route";
-import { ProgressButtonsAsLinks } from "../../ui/buttons";
+import { EvictionFreeIndexNumberMutation } from "../../queries/EvictionFreeIndexNumberMutation";
+import { ProgressButtons, ProgressButtonsAsLinks } from "../../ui/buttons";
 import Page from "../../ui/page";
 
 export const EvictionFreeIndexNumber = MiddleProgressStep((props) => {
-  const yesNoProps = usePrototypingFormFieldProps("yesNo", "");
-  const indexNumberProps = hideByDefault(
-    usePrototypingFormFieldProps("indexNumber", "")
-  );
-
-  if (yesNoProps.value === YES_NO_RADIOS_TRUE) {
-    indexNumberProps.hidden = false;
-  }
-
   return (
     <Page
       title="Do you have a current eviction court case?"
       withHeading="big"
       className="content"
     >
-      <form>
-        <YesNoRadiosFormField {...yesNoProps} label="" />
-        <ConditionalFormField {...indexNumberProps}>
-          <>
-            <p>
-              We'll need to add your case's index number to your declaration.
-            </p>
-            <TextualFormField
-              {...indexNumberProps}
-              label="Your case's index number"
-            />
-          </>
-        </ConditionalFormField>
-        <ProgressButtonsAsLinks back={props.prevStep} next={props.nextStep} />
-      </form>
+      <SessionUpdatingFormSubmitter
+        mutation={EvictionFreeIndexNumberMutation}
+        initialState={(s) => ({
+          hasCurrentCase: s.hardshipDeclarationDetails?.indexNumber
+            ? YES_NO_RADIOS_TRUE
+            : YES_NO_RADIOS_FALSE,
+          indexNumber: s.hardshipDeclarationDetails?.indexNumber || "",
+        })}
+        onSuccessRedirect={props.nextStep}
+      >
+        {(ctx) => {
+          const yesNoProps = ctx.fieldPropsFor("hasCurrentCase");
+          const indexNumberProps = hideByDefault(
+            ctx.fieldPropsFor("indexNumber")
+          );
+
+          if (yesNoProps.value === YES_NO_RADIOS_TRUE) {
+            indexNumberProps.hidden = false;
+          }
+
+          return (
+            <>
+              <YesNoRadiosFormField {...yesNoProps} label="" />
+              <ConditionalFormField {...indexNumberProps}>
+                <>
+                  <p>
+                    We'll need to add your case's index number to your
+                    declaration.
+                  </p>
+                  <TextualFormField
+                    {...indexNumberProps}
+                    label="Your case's index number"
+                  />
+                </>
+              </ConditionalFormField>
+              <ProgressButtons
+                isLoading={ctx.isLoading}
+                back={props.prevStep}
+              />
+            </>
+          );
+        }}
+      </SessionUpdatingFormSubmitter>
     </Page>
   );
 });

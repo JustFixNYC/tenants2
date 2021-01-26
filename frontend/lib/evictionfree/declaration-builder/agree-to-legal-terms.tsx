dbc7f208364d241ee21ff7d5e6ef5b6a966893ed
Atul Varma <varmaa@gmail.com>
2021-01-26 18:35:31 -0500

    Add backend logic for evictionfree checkbox forms.

--- frontend/lib/evictionfree/declaration-builder/agree-to-legal-terms.tsx
+++ frontend/lib/evictionfree/declaration-builder/agree-to-legal-terms.tsx
@@ -1,10 +1,15 @@
 import { t, Trans } from "@lingui/macro";
 import React from "react";
-import { CheckboxView } from "../../forms/form-fields";
+import { CheckboxFormField } from "../../forms/form-fields";
+import { LegacyFormSubmitter } from "../../forms/legacy-form-submitter";
 import { li18n } from "../../i18n-lingui";
 
 import { MiddleProgressStep } from "../../progress/progress-step-route";
-import { ProgressButtonsAsLinks } from "../../ui/buttons";
+import {
+  BlankEvictionFreeAgreeToLegalTermsInput,
+  EvictionFreeAgreeToLegalTermsMutation,
+} from "../../queries/EvictionFreeAgreeToLegalTermsMutation";
+import { ProgressButtons } from "../../ui/buttons";
 import Page from "../../ui/page";
 
 export const EvictionFreeAgreeToLegalTerms = MiddleProgressStep((props) => (
@@ -20,27 +25,45 @@ export const EvictionFreeAgreeToLegalTerms = MiddleProgressStep((props) => (
         questions truthfully:
       </Trans>
     </p>
-    <CheckboxView id="1">
-      <Trans>
-        I understand that I must comply with all other lawful terms under my
-        tenancy, lease agreement or similar contract.
-      </Trans>
-    </CheckboxView>
-    <CheckboxView id="2">
-      <Trans id="evictionfree.legalAgreementCheckboxOnFees">
-        I further understand that lawful fees, penalties or interest for not
-        having paid rent in full or met other financial obligations as required
-        by my tenancy, lease agreement or similar contract may still be charged
-        or collected and may result in a monetary judgment against me.
-      </Trans>
-    </CheckboxView>
-    <CheckboxView id="3">
-      <Trans id="evictionfree.legalAgreementCheckboxOnNewProtections">
-        I further understand that my landlord may be able to seek eviction after
-        May 1, 2021, and that the law may provide certain protections at that
-        time that are separate from those available through this declaration.
-      </Trans>
-    </CheckboxView>
-    <ProgressButtonsAsLinks back={props.prevStep} next={props.nextStep} />
+    <LegacyFormSubmitter
+      mutation={EvictionFreeAgreeToLegalTermsMutation}
+      initialState={BlankEvictionFreeAgreeToLegalTermsInput}
+      onSuccessRedirect={props.nextStep}
+    >
+      {(ctx) => (
+        <>
+          <CheckboxFormField
+            {...ctx.fieldPropsFor("compliesWithOtherLawfulTerms")}
+          >
+            <Trans>
+              I understand that I must comply with all other lawful terms under
+              my tenancy, lease agreement or similar contract.
+            </Trans>
+          </CheckboxFormField>
+          <CheckboxFormField
+            {...ctx.fieldPropsFor("understandsFinancialObligations")}
+          >
+            <Trans id="evictionfree.legalAgreementCheckboxOnFees">
+              I further understand that lawful fees, penalties or interest for
+              not having paid rent in full or met other financial obligations as
+              required by my tenancy, lease agreement or similar contract may
+              still be charged or collected and may result in a monetary
+              judgment against me.
+            </Trans>
+          </CheckboxFormField>
+          <CheckboxFormField
+            {...ctx.fieldPropsFor("understandsProtectionIsTemporary")}
+          >
+            <Trans id="evictionfree.legalAgreementCheckboxOnNewProtections">
+              I further understand that my landlord may be able to seek eviction
+              after May 1, 2021, and that the law may provide certain
+              protections at that time that are separate from those available
+              through this declaration.
+            </Trans>
+          </CheckboxFormField>
+          <ProgressButtons back={props.prevStep} isLoading={ctx.isLoading} />
+        </>
+      )}
+    </LegacyFormSubmitter>
   </Page>
 ));

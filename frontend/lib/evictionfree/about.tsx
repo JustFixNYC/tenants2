87de2aff4c113cb8309edcc1fc623f277544bce4
Sam Rabiyah <sraby@users.noreply.github.com>
2021-01-27 14:28:43 -0500

    Add more outbound links to primary page content (#1863)
    Like #1862, this PR adds more missing outbound links to the homepage and about page of Eviction Free NY. Again, Localized Outbound Links were used when available, with fallback to regular Outbound Link for readability.

--- frontend/lib/evictionfree/about.tsx
+++ frontend/lib/evictionfree/about.tsx
@@ -1,6 +1,8 @@
 import { t, Trans } from "@lingui/macro";
 import React from "react";
+import { OutboundLink } from "../analytics/google-analytics";
 import { li18n } from "../i18n-lingui";
+import { LocalizedOutboundLink } from "../ui/localized-outbound-link";
 import Page from "../ui/page";
 
 export const EvictionFreeAboutPage: React.FC<{}> = () => (
@@ -46,33 +48,48 @@ export const EvictionFreeAboutPage: React.FC<{}> = () => (
           <br />
           <p className="subtitle is-size-5">
             <Trans id="evictionfree.rtcBlurb">
-              The Right to Counsel NYC Coalition is a tenant-led, broad-based
-              coalition that formed in 2014 to disrupt Housing Court as a center
-              of displacement and stop the eviction crisis that has threatened
-              our families, our neighborhoods and our homes for too long. Made
-              up of tenants, organizers, advocates, legal services organizations
-              and more, we are building campaigns for an eviction-free NYC and
-              ultimately for a right to housing.
+              The{" "}
+              <OutboundLink href="https://www.righttocounselnyc.org/">
+                Right to Counsel NYC Coalition
+              </OutboundLink>{" "}
+              is a tenant-led, broad-based coalition that formed in 2014 to
+              disrupt Housing Court as a center of displacement and stop the
+              eviction crisis that has threatened our families, our
+              neighborhoods and our homes for too long. Made up of tenants,
+              organizers, advocates, legal services organizations and more, we
+              are building campaigns for an eviction-free NYC and ultimately for
+              a right to housing.
             </Trans>
           </p>
           <br />
           <p className="subtitle is-size-5">
             <Trans id="evictionfree.hj4aBlurb">
-              Housing Justice For All is a coalition of over 100 organizations,
-              from Brooklyn to Buffalo, that represent tenants and homeless New
-              Yorkers. We are united in our belief that housing is a human
-              right; that no person should live in fear of an eviction; and that
-              we can end the homelessness crisis in our State.
+              <OutboundLink href="https://twitter.com/housing4allNY">
+                Housing Justice for All
+              </OutboundLink>{" "}
+              is a coalition of over 100 organizations, from Brooklyn to
+              Buffalo, that represent tenants and homeless New Yorkers. We are
+              united in our belief that housing is a human right; that no person
+              should live in fear of an eviction; and that we can end the
+              homelessness crisis in our State.
             </Trans>
           </p>
           <br />
           <p className="subtitle is-size-5">
             <Trans id="evictionfree.justfixBlurb">
-              JustFix.nyc co-designs and builds tools for tenants, housing
-              organizers, and legal advocates fighting displacement in New York
-              City. Our mission is to galvanize a 21st century tenant movement
-              working towards housing for all — and we think the power of data
-              and technology should be accessible to those fighting this fight.
+              <LocalizedOutboundLink
+                hrefs={{
+                  en: "https://www.justfix.nyc/en/",
+                  es: "https://www.justfix.nyc/es/",
+                }}
+              >
+                JustFix.nyc
+              </LocalizedOutboundLink>{" "}
+              co-designs and builds tools for tenants, housing organizers, and
+              legal advocates fighting displacement in New York City. Our
+              mission is to galvanize a 21st century tenant movement working
+              towards housing for all — and we think the power of data and
+              technology should be accessible to those fighting this fight.
             </Trans>
           </p>
         </div>

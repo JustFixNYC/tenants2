dd37a6f3d751c5e9edc28cf97c64bef0a870a025
Atul Varma <varmaa@gmail.com>
2021-01-29 08:29:49 -0500

    Lazy-load localized preview.

--- frontend/lib/evictionfree/declaration-templates/locales.tsx
+++ frontend/lib/evictionfree/declaration-templates/locales.tsx
@@ -1,22 +1,41 @@
+import loadable from "@loadable/component";
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import { HardshipDeclarationProps } from ".";
+import { getGlobalAppServerInfo } from "../../app-context";
 import i18n, { SupportedLocaleMap } from "../../i18n";
-import English from "./en";
+import { DefaultLoadingSpinner } from "../../networking/query-loader";
 
-// TODO: Ideally we should lazy-load these.
+/**
+ * If the current URL's querystring includes this text and
+ * we are in a development environment, always show our fallback
+ * instead of the localized declaration, to make it easy
+ * to style the fallback during development.
+ */
+const SHOW_FALLBACK_QS = "?fallback";
+
+const fallback = <DefaultLoadingSpinner />;
+
 const localizations: SupportedLocaleMap<React.ComponentType<
   HardshipDeclarationProps
 >> = {
-  en: English,
+  en: loadable(() => import("./en"), {
+    fallback,
+  }),
   // TODO: Replace this with actual Spanish component.
-  es: English,
+  es: loadable(() => import("./en"), {
+    fallback,
+  }),
 };
 
 export const LocalizedHardshipDeclaration: React.FC<HardshipDeclarationProps> = (
   props
 ) => {
   const Component = localizations[i18n.locale];
+  const { search } = useLocation();
+  const showFallback =
+    getGlobalAppServerInfo().debug && search.includes(SHOW_FALLBACK_QS);
 
-  return <Component {...props} />;
+  return showFallback ? fallback : <Component {...props} />;
 };

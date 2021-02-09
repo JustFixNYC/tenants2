dddea2f4ced5cca1ea9fda916c1de1c90ac137b4
sraby <samrabiyah@gmail.com>
2021-02-09 18:19:55 -0500

    Add new languages to footer

--- frontend/lib/evictionfree/components/footer.tsx
+++ frontend/lib/evictionfree/components/footer.tsx
@@ -1,7 +1,40 @@
 import { Trans } from "@lingui/macro";
-import React from "react";
-import { FooterLanguageToggle } from "../../ui/language-toggle";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import {
+  EvictionFreeUnsupportedLocaleChoices,
+  getEvictionFreeUnsupportedLocaleChoiceLabels,
+} from "../../../../common-data/evictionfree-unsupported-locale-choices";
+import { AppContext } from "../../app-context";
+import { SwitchLanguage } from "../../ui/language-toggle";
 import { LegalDisclaimer } from "../../ui/legal-disclaimer";
+import { EvictionFreeRoutes } from "../route-info";
+
+export const EvictionFreeFooterLanguageToggle: React.FC<{}> = () => {
+  const { server } = useContext(AppContext);
+
+  if (server.enabledLocales.length === 1) return null;
+
+  return (
+    <div className="jf-language-toggle">
+      <Trans>Language:</Trans>{" "}
+      <ul>
+        {server.enabledLocales.map((locale) => (
+          <li key={locale}>
+            <SwitchLanguage locale={locale} />
+          </li>
+        ))}
+        {EvictionFreeUnsupportedLocaleChoices.map((locale) => (
+          <li key={locale}>
+            <Link to={EvictionFreeRoutes.unsupportedLocale[locale]}>
+              {getEvictionFreeUnsupportedLocaleChoiceLabels()[locale]}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export const EvictionFreeFooter: React.FC<{}> = () => (
   <footer>
@@ -9,7 +42,7 @@ export const EvictionFreeFooter: React.FC<{}> = () => (
       <div className="columns">
         <div className="column is-8">
           <div className="content is-size-7">
-            <FooterLanguageToggle />
+            <EvictionFreeFooterLanguageToggle />
             <LegalDisclaimer website="EvictionFreeNY.org" />
             <p>
               <Trans>Photo credits:</Trans> Scott Heins, Right to Counsel

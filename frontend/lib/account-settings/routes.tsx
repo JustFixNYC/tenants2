3e86e1898998f1a8e48feaf352b7e1b7f93b3424
Atul Varma <varmaa@gmail.com>
2021-02-25 07:47:17 -0500

    Add some docs.

--- frontend/lib/account-settings/routes.tsx
+++ frontend/lib/account-settings/routes.tsx
@@ -63,10 +63,45 @@ const EditLink: React.FC<{
   );
 };
 
+/**
+ * A section of information that starts out read-only, but
+ * becomes editable once the user activates an "edit" button.
+ *
+ * The "edit" button is actually just a link to a URL that
+ * toggles the editability of the content. This is done to
+ * ensure that the functionality works in non-JS contexts.
+ */
 const EditableInfo: React.FC<{
+  /**
+   * The path that clicking the "edit" button should take the
+   * user to. This should be a path in which this button will
+   * still be rendered without needing to be re-mounted.
+   */
   path: string;
+
+  /**
+   * The name of the information in this area, used for
+   * providing an accessible label for the "edit" button
+   * (there may be multiple "edit" buttons on the page, so
+   * we want to provide more context for screen reader users).
+   */
   name: string;
+
+  /** The read-only version of the content. */
   readonlyContent: string | JSX.Element;
+
+  /**
+   * The content shown when the user clicks the "edit" button.
+   * This content is responsible for focusing itself on mount
+   * to ensure that user focus isn't lost, as the "edit" button
+   * will have disappeared.
+   *
+   * This content can also contain a link back to the URL with
+   * the read-only version of the content, which is expected
+   * to be a subset of the `path` prop.  Once this is navigated to,
+   * the `children` will be unmounted and this component will ensure
+   * that the "edit" button is focused.
+   */
   children: any;
 }> = (props) => {
   const { pathname } = useLocation();
@@ -74,6 +109,12 @@ const EditableInfo: React.FC<{
   let autoFocusEditLink =
     pathname !== prevPathname &&
     prevPathname === props.path &&
+    // We additionally want to make sure that we only auto-focus
+    // the edit link in situations where we are sure nothing else
+    // wants focus, which by convention will be if our pathname
+    // starts with the current pathname (e.g. if the user just
+    // navigated from `/foo/edit-name` to `/foo`, rather than from
+    // `/foo/edit-name` to `/foo/edit-phone-number`).
     props.path.startsWith(pathname);
 
   return pathname === props.path ? (
--- frontend/lib/ui/use-auto-focus.ts
+++ frontend/lib/ui/use-auto-focus.ts
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 
 /**
- * A React Hook that automatically focuses the given ref on
- * mount, and/or when the `shouldAutoFocus` prop changes to true.
+ * A React Hook that automatically gives input focus to the given ref
+ * under the following conditions:
+ *
+ *   * At mount time, if `shouldAutoFocus` is true at mount.
+ *   * Whenever the `shouldAutoFocus` prop changes from `false` or `undefined`
+ *     to `true`.
  */
 export function useAutoFocus(
   ref: React.RefObject<HTMLElement | null>,

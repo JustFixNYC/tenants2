5f9c56b8a885b6d2cd4a449890d54e1a155710d8
Atul Varma <varmaa@gmail.com>
2021-03-24 15:52:49 -0400

    Add tests.

---
+++ frontend/tests/test_models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AnonymousUser
+
+from users.tests.factories import UserFactory
+from frontend.models import LoggedEvent
+
+
+class TestLoggedEvent:
+    def test_it_logs_events_for_anonymous_users(self, rf, db):
+        request = rf.get("/")
+        request.user = AnonymousUser()
+        le = LoggedEvent.objects.create_for_request(
+            request, kind=LoggedEvent.CHOICES.SAFE_MODE_ENABLE
+        )
+        assert le.pk
+        assert le.user is None
+        assert le.kind == "SAFE_MODE_ENABLE"
+
+    def test_it_logs_events_for_authenticated_users(self, rf, db):
+        request = rf.get("/")
+        request.user = UserFactory(username="boop")
+        le = LoggedEvent.objects.create_for_request(
+            request, kind=LoggedEvent.CHOICES.SAFE_MODE_ENABLE
+        )
+        assert le.pk
+        assert le.user.username == "boop"
+        assert le.kind == "SAFE_MODE_ENABLE"

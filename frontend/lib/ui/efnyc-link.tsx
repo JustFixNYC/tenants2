6ea6e603bb2b07fdc97bf9cb634776d8db3c1b6b
Sam Rabiyah <sraby@users.noreply.github.com>
2021-04-19 18:32:20 -0400

    Remove "en-us" locale key from efnycURL function

--- frontend/lib/ui/efnyc-link.tsx
+++ frontend/lib/ui/efnyc-link.tsx
@@ -2,6 +2,5 @@ import { getGlobalAppServerInfo } from "../app-context";
 import i18n from "../i18n";
 
 export function efnycURL(): string {
-  const efnycLocale = i18n.locale === "en" ? "en-us" : i18n.locale;
-  return `${getGlobalAppServerInfo().efnycOrigin}/${efnycLocale}`;
+  return `${getGlobalAppServerInfo().efnycOrigin}/${i18n.locale}`;
 }

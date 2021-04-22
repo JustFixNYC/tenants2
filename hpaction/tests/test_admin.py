6b7d37df901d08f528aaf055828bb9d135cd2a4d
Atul Varma <varmaa@gmail.com>
2021-04-22 10:57:19 -0400

    Remove unneeded test

--- hpaction/tests/test_admin.py
+++ hpaction/tests/test_admin.py
@@ -15,12 +15,6 @@ def test_schedule_for_deletion_works(db, django_file_storage):
     assert doc.user is None
 
 
-def test_edit_user_field_works(db):
-    user = UserFactory()
-    url = HPUserAdmin.edit_user(None, user)
-    assert str(user.id) in url
-
-
 class TestHPUserAdmin(UserProxyAdminTester):
     list_view_url = "/admin/hpaction/hpuser/"
 

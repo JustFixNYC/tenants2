4c1dfaa2472526fc583a5dfc698f18a0e51967a0
Atul Varma <varmaa@gmail.com>
2021-04-22 07:52:28 -0400

    Dynamically render tabs.

--- users/action_progress.py
+++ users/action_progress.py
@@ -1,4 +1,5 @@
-from django.db.models import Case, When, Value, Q
+from typing import NamedTuple
+from django.db.models import Case, When, Value, Q, Expression
 
 from onboarding.models import SIGNUP_INTENT_CHOICES
 from hpaction.models import HP_DOCUSIGN_STATUS_CHOICES
@@ -14,43 +15,57 @@ PROGRESS_LABELS = {
     COMPLETE: "Complete",
 }
 
-LOC_PROGRESS_EXPR = Case(
-    When(letter_request__isnull=False, then=Value(COMPLETE)),
-    When(
-        Q(onboarding_info__signup_intent=SIGNUP_INTENT_CHOICES.LOC) | Q(access_dates__isnull=False),
-        then=Value(IN_PROGRESS),
+
+class ProgressAnnotation(NamedTuple):
+    name: str
+    expression: Expression
+
+
+LOC_PROGRESS = ProgressAnnotation(
+    "loc_progress",
+    Case(
+        When(letter_request__isnull=False, then=Value(COMPLETE)),
+        When(
+            Q(onboarding_info__signup_intent=SIGNUP_INTENT_CHOICES.LOC)
+            | Q(access_dates__isnull=False),
+            then=Value(IN_PROGRESS),
+        ),
+        default=Value(NOT_STARTED),
     ),
-    default=Value(NOT_STARTED),
 )
 
-EHP_PROGRESS_EXPR = Case(
-    When(
-        hpactiondocuments__docusignenvelope__status=HP_DOCUSIGN_STATUS_CHOICES.SIGNED,
-        then=Value(COMPLETE),
+EHP_PROGRESS = ProgressAnnotation(
+    "ehp_progress",
+    Case(
+        When(
+            hpactiondocuments__docusignenvelope__status=HP_DOCUSIGN_STATUS_CHOICES.SIGNED,
+            then=Value(COMPLETE),
+        ),
+        When(
+            Q(onboarding_info__signup_intent=SIGNUP_INTENT_CHOICES.EHP)
+            | Q(hp_action_details__isnull=False),
+            then=Value(IN_PROGRESS),
+        ),
+        default=Value(NOT_STARTED),
     ),
-    When(
-        Q(onboarding_info__signup_intent=SIGNUP_INTENT_CHOICES.EHP)
-        | Q(hp_action_details__isnull=False),
-        then=Value(IN_PROGRESS),
+)
+
+NORENT_PROGRESS = ProgressAnnotation(
+    "norent_progress",
+    Case(
+        When(norent_letters__isnull=False, then=Value(COMPLETE)),
+        When(onboarding_info__agreed_to_norent_terms=True, then=Value(IN_PROGRESS)),
+        default=Value(NOT_STARTED),
     ),
-    default=Value(NOT_STARTED),
 )
 
-NORENT_PROGRESS_EXPR = Case(
-    When(norent_letters__isnull=False, then=Value(COMPLETE)),
-    When(onboarding_info__agreed_to_norent_terms=True, then=Value(IN_PROGRESS)),
-    default=Value(NOT_STARTED),
+EVICTIONFREE_PROGRESS = ProgressAnnotation(
+    "evictionfree_progress",
+    Case(
+        When(submitted_hardship_declaration__isnull=False, then=Value(COMPLETE)),
+        When(onboarding_info__agreed_to_evictionfree_terms=True, then=Value(IN_PROGRESS)),
+        default=Value(NOT_STARTED),
+    ),
 )
 
-EVICTIONFREE_PROGRESS_EXPR = Case(
-    When(submitted_hardship_declaration__isnull=False, then=Value(COMPLETE)),
-    When(onboarding_info__agreed_to_evictionfree_terms=True, then=Value(IN_PROGRESS)),
-    default=Value(NOT_STARTED),
-)
-
-PROGRESS_ANNOTATIONS = dict(
-    progress_loc=LOC_PROGRESS_EXPR,
-    progress_ehp=EHP_PROGRESS_EXPR,
-    progress_norent=NORENT_PROGRESS_EXPR,
-    progress_evictionfree=EVICTIONFREE_PROGRESS_EXPR,
-)
+PROGRESS_ANNOTATIONS = [LOC_PROGRESS, EHP_PROGRESS, NORENT_PROGRESS, EVICTIONFREE_PROGRESS]
--- users/admin_user_tabs.py
+++ users/admin_user_tabs.py
@@ -1,8 +1,81 @@
-from .action_progress import PROGRESS_ANNOTATIONS, PROGRESS_LABELS
+from typing import Any, Dict, List, NamedTuple
+from typing import Optional, Type
+from django.contrib.admin import ModelAdmin
+
+from users.models import JustfixUser
+from project.util.admin_util import get_admin_url_for_instance_or_class
+from . import action_progress
 
 
-def get_user_tab_context_info(user):
-    return {"progress_loc": PROGRESS_LABELS[user.progress_loc]}
+def get_user_tab_context_info(
+    model_admin: ModelAdmin, user: Optional[JustfixUser]
+) -> Dict[str, Any]:
+    if not user:
+        return {}
+    return {
+        "user_tabs": [
+            UserTabView(
+                tab=tab,
+                user=user,
+                model_admin=model_admin,
+            )
+            for tab in get_user_tabs()
+        ]
+    }
+
+
+def get_user_tabs() -> List["UserTab"]:
+    from users.models import JustfixUser
+    from loc.admin import LOCUser
+    from hpaction.admin import HPUser
+    from evictionfree.admin import EvictionFreeUser
+    from norent.admin import NorentUser
+
+    return [
+        UserTab(JustfixUser, "General", "General user details"),
+        UserTab(LOCUser, "LOC", "Letter of complaint details", action_progress.LOC_PROGRESS),
+        UserTab(HPUser, "HP", "HP action details", action_progress.EHP_PROGRESS),
+        UserTab(
+            EvictionFreeUser,
+            "EFNY",
+            "EvictionFreeNY.org details",
+            action_progress.EVICTIONFREE_PROGRESS,
+        ),
+        UserTab(NorentUser, "NoRent", "NoRent.org details", action_progress.NORENT_PROGRESS),
+    ]
+
+
+class UserTab(NamedTuple):
+    model_class: Type
+    label: str
+    description: str
+    progress_annotation: Optional[action_progress.ProgressAnnotation] = None
+
+
+class UserTabView(NamedTuple):
+    tab: UserTab
+    user: JustfixUser
+    model_admin: ModelAdmin
+
+    @property
+    def progress_label(self) -> Optional[str]:
+        if self.progress:
+            return action_progress.PROGRESS_LABELS[self.progress]
+        return None
+
+    @property
+    def admin_url(self) -> str:
+        return get_admin_url_for_instance_or_class(self.tab.model_class, self.user.pk)
+
+    @property
+    def progress(self) -> Optional[str]:
+        if not self.tab.progress_annotation:
+            return None
+        return getattr(self.user, self.tab.progress_annotation.name)
+
+    @property
+    def is_selected(self) -> bool:
+        return self.model_admin.model == self.tab.model_class
 
 
 class UserWithTabsMixin:
@@ -13,7 +86,7 @@ class UserWithTabsMixin:
             request,
             {
                 **context,
-                **get_user_tab_context_info(kwargs.get("obj")),
+                **get_user_tab_context_info(self, kwargs.get("obj")),
             },
             *args,
             **kwargs,
@@ -21,4 +94,6 @@ class UserWithTabsMixin:
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)  # type: ignore
-        return queryset.annotate(**PROGRESS_ANNOTATIONS)
+        return queryset.annotate(
+            **{anno.name: anno.expression for anno in action_progress.PROGRESS_ANNOTATIONS}
+        )

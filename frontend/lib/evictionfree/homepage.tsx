48a306e8e3ce28d39fff910481a67feb2d5d9a83
Sam Rabiyah <sraby@users.noreply.github.com>
2021-05-06 08:10:10 -0400

    Hot Fix: Add an asterisk and remove spaces after em dashes on EFNY (#2080)
    This PR takes care of some text-based refinements needed after merging #2062.

--- frontend/lib/evictionfree/homepage.tsx
+++ frontend/lib/evictionfree/homepage.tsx
@@ -34,13 +34,13 @@ export function getEFImageSrc(
 const SocialShareContent = {
   tweet: t(
     "evictionfree.tweetTemplateForSharingFromHomepage1"
-  )`You can use this website to send a hardship declaration form to your landlord and local courts— putting your eviction case on hold until August 31st, 2021. Check it out here: ${
+  )`You can use this website to send a hardship declaration form to your landlord and local courts—putting your eviction case on hold until August 31st, 2021. Check it out here: ${
     getGlobalAppServerInfo().originURL
   } #EvictionFreeNY via @JustFixNYC @RTCNYC @housing4allNY`,
   emailSubject: t`Protect yourself from eviction in New York State`,
   emailBody: t(
     "evictionfree.emailBodyTemplateForSharingFromHomepage1"
-  )`On December 28, 2020, New York State passed legislation that protects tenants from eviction due to lost income or COVID-19 health risks. In order to get protected, you must fill out a hardship declaration form and send it to your landlord and/or the courts. You can use this website to send a hardship declaration form to your landlord and local courts— putting your eviction case on hold until August 31st, 2021. Check it out here: ${
+  )`On December 28, 2020, New York State passed legislation that protects tenants from eviction due to lost income or COVID-19 health risks. In order to get protected, you must fill out a hardship declaration form and send it to your landlord and/or the courts. You can use this website to send a hardship declaration form to your landlord and local courts—putting your eviction case on hold until August 31st, 2021. Check it out here: ${
     getGlobalAppServerInfo().originURL
   }`,
 };

8f29a485a7c65d51cc42286730faa6e77d2b84dd
Atul Varma <varmaa@gmail.com>
2021-05-19 07:59:42 -0400

    Fix tests.

--- project/tests/test_admin_dashboard.py
+++ project/tests/test_admin_dashboard.py
@@ -1,4 +1,4 @@
-def test_dashboard_works(admin_client):
-    res = admin_client.get("/admin/dashboard/")
+def test_legacy_dashboard_works(admin_client):
+    res = admin_client.get("/admin/vega-dashboard/")
     assert res.status_code == 200
     assert b"Dashboard" in res.content

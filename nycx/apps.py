88cd851d8714b339ad34d13f9102222007e98c7e
Atul Varma <varmaa@gmail.com>
2021-06-22 16:51:59 -0400

    Run Black on apps.py.

--- nycx/apps.py
+++ nycx/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NycxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nycx'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "nycx"

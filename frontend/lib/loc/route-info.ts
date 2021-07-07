f07aa194e0ceab959b35455ef3c3e4f1badd8abb
Atul Varma <varmaa@gmail.com>
2021-07-07 08:36:42 -0400

    Attempt to make LOC onboarding more like EFNY/NoRent.

--- frontend/lib/loc/route-info.ts
+++ frontend/lib/loc/route-info.ts
@@ -2,6 +2,7 @@ import { createJustfixCrossSiteVisitorRoutes } from "../justfix-cross-site-visit
 import { createLetterStaticPageRouteInfo } from "../static-page/routes";
 import { ROUTE_PREFIX } from "../util/route-util";
 import { createIssuesRouteInfo } from "../issues/route-info";
+import { createStartAccountOrLoginRouteInfo } from "../start-account-or-login/route-info";
 
 export type LetterOfComplaintInfo = ReturnType<
   typeof createLetterOfComplaintRouteInfo
@@ -18,6 +19,14 @@ export function createLetterOfComplaintRouteInfo(prefix: string) {
     /** Letter content for the user (HTML and PDF versions). */
     letterContent: createLetterStaticPageRouteInfo(`${prefix}/letter`),
     splash: `${prefix}/splash`,
+    name: `${prefix}/name`,
+    nycAddress: `${prefix}/address/nyc`,
+    nycAddressConfirmModal: `${prefix}/address/nyc/confirm-address-modal`,
+    leaseType: `${prefix}/lease-type`,
+    email: `${prefix}/email`,
+    createAccount: `${prefix}/create-account`,
+    createAccountTermsModal: `${prefix}/create-account/terms-modal`,
+    ...createStartAccountOrLoginRouteInfo(prefix),
     welcome: `${prefix}/welcome`,
     ...createJustfixCrossSiteVisitorRoutes(prefix),
     issues: createIssuesRouteInfo(`${prefix}/issues`),

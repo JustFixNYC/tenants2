JustFixNYC/tenants2

commit 3ac0011a8e01cf12018d9af82e38dcba36fa5ff8

In backend, use onboarding scaffolding to store steps 1 and 3. (#2154)
This further moves us towards #2142 by internally storing steps 1 and 3 of app.justfix.nyc onboarding data in our database.  **It still does not change anything in the front-end.**  It just makes submitting onboarding steps change the scaffolding, and retrieving data for onboarding steps 1 and 3 from the front-end actually returns data from scaffolding in the backend.  Legacy data stored in the old location is also migrated to the scaffolding as needed.

In a future PR, we will deprecate `session.onboardingStep1` and `session.onboardingStep3` and instead only look at the scaffolding data.

We also account for the weird use of onboarding step 1 for only address information that was deprecated in #2143 and migrate the data accordingly.

onboarding/scaffolding.py
@@ -1,7 +1,8 @@
 import json
 from pathlib import Path
+from project.util.rename_dict_keys import with_keys_renamed
 from project.util.session_mutation import SessionFormMutation
-from typing import Optional, Tuple
+from typing import Any, Dict, Optional, Tuple
 from django.contrib.gis.geos import GEOSGeometry, Point
 import graphene
 from graphql import ResolveInfo
@@ -74,6 +75,10 @@ class OnboardingScaffolding(pydantic.BaseModel):
     # Whether or not we verified that the user's address was verified
     # on the server-side.
     address_verified: bool = False
+
+    lease_type: str = ""
+
+    receives_public_assistance: Optional[bool] = None
 
     # e.g. (-73.9496, 40.6501)
     lnglat: Optional[Tuple[float, float]] = None
@@ -196,9 +201,13 @@ class OnboardingScaffoldingMutation(SessionFormMutation):
         abstract = True
 
     @classmethod
+    def get_scaffolding_fields_from_form(cls, form) -> Dict[str, Any]:
+        return form.cleaned_data
+
+    @classmethod
     def perform_mutate(cls, form, info: ResolveInfo):
         request = info.context
-        update_scaffolding(request, form.cleaned_data)
+        update_scaffolding(request, cls.get_scaffolding_fields_from_form(form))
         return cls.mutation_success()
 
 
@@ -232,12 +241,69 @@ class OnboardingScaffoldingOrUserDataMutation(SessionFormMutation):
         return cls.perform_mutate_for_anonymous_user(form, info)
 
 
+def _migrate_legacy_session_data_to_scaffolding(request):
+    """
+    This function takes any data we have stored elsewhere in
+    the session by legacy endpoints and migrates it over to the
+    onboarding scaffolding if possible.
+
+    As each legacy endpoint is fully deprecated and removed, we'll
+    remove the relevant migration code from this function. Eventually
+    we'll have nothing left to migrate and we can get rid of this
+    function.
+
+    For more context around all this, see:
+
+        https://github.com/JustFixNYC/tenants2/issues/2142
+    """
+
+    d = request.session.get(SCAFFOLDING_SESSION_KEY, {})
+    updated = False
+
+    if not d.get("first_name") or not d.get("borough"):
+        from .schema import OnboardingStep1V2Info
+
+        legacy_step1 = OnboardingStep1V2Info.get_dict_from_request(request)
+        if legacy_step1:
+            if legacy_step1["first_name"] == legacy_step1["last_name"] == "ignore":
+                # This is data submitted by code deprecated in
+                # https://github.com/JustFixNYC/tenants2/pull/2143 which only set
+                # useful address info, so remove the name info.
+                del legacy_step1["first_name"]
+                del legacy_step1["last_name"]
+                if "preferred_first_name" in legacy_step1:
+                    del legacy_step1["preferred_first_name"]
+
+            d.update(with_keys_renamed(legacy_step1, {"address": "street"}))
+            updated = True
+            OnboardingStep1V2Info.clear_from_request(request)
+
+    if not d.get("lease_type"):
+        from .schema import OnboardingStep3Info
+
+        legacy_step3 = OnboardingStep3Info.get_dict_from_request(request)
+        if legacy_step3:
+            from project.forms import YesNoRadiosField
+
+            legacy_step3["receives_public_assistance"] = YesNoRadiosField.coerce(
+                legacy_step3["receives_public_assistance"]
+            )
+            d.update(legacy_step3)
+            updated = True
+            OnboardingStep3Info.clear_from_request(request)
+
+    if updated:
+        request.session[SCAFFOLDING_SESSION_KEY] = d
+
+
 def get_scaffolding(request) -> OnboardingScaffolding:
+    _migrate_legacy_session_data_to_scaffolding(request)
     scaffolding_dict = request.session.get(SCAFFOLDING_SESSION_KEY, {})
     return OnboardingScaffolding(**scaffolding_dict)
 
 
 def update_scaffolding(request, new_data):
+    _migrate_legacy_session_data_to_scaffolding(request)
     scaffolding_dict = request.session.get(SCAFFOLDING_SESSION_KEY, {})
     scaffolding_dict.update(new_data)
 

onboarding/schema.py
@@ -1,4 +1,5 @@
 import logging
+from project.forms import YesNoRadiosField
 from project.util.rename_dict_keys import with_keys_renamed
 from typing import Optional, Dict, Any, List, Type
 from django.contrib.auth import login
@@ -24,7 +25,12 @@ from partnerships import referral
 from project.util.model_form_util import OneToOneUserModelFormMutation
 from users.email_verify import send_verification_email_async
 from onboarding import forms
-from onboarding.scaffolding import update_scaffolding
+from onboarding.scaffolding import (
+    OnboardingScaffoldingMutation,
+    get_scaffolding,
+    purge_scaffolding,
+    update_scaffolding,
+)
 from onboarding.schema_util import mutation_requires_onboarding
 from onboarding.models import OnboardingInfo, BOROUGH_CHOICES, LEASE_CHOICES, SIGNUP_INTENT_CHOICES
 
@@ -88,15 +94,20 @@ class OnboardingStep1(DjangoSessionFormMutation):
 
 
 @schema_registry.register_mutation
-class OnboardingStep1V2(DjangoSessionFormMutation):
+class OnboardingStep1V2(OnboardingScaffoldingMutation):
     class Meta:
-        source = OnboardingStep1V2Info
+        form_class = forms.OnboardingStep1V2Form
+        exclude = ["no_apt_number"]
+
+    @classmethod
+    def get_scaffolding_fields_from_form(cls, form) -> Dict[str, Any]:
+        return with_keys_renamed(form.cleaned_data, {"address": "street"})
 
 
 @schema_registry.register_mutation
-class OnboardingStep3(DjangoSessionFormMutation):
+class OnboardingStep3(OnboardingScaffoldingMutation):
     class Meta:
-        source = OnboardingStep3Info
+        form_class = forms.OnboardingStep3Form
 
 
 def pick_model_fields(model, **kwargs):
@@ -157,14 +168,16 @@ class OnboardingStep4Base(SessionFormMutation):
 
     @classmethod
     def __extract_all_step_session_data(cls, request: HttpRequest) -> Optional[Dict[str, Any]]:
-        result: Dict[str, Any] = {}
-        for step in SESSION_STEPS:
-            value = step.get_dict_from_request(request)
-            if not value:
-                return None
-            else:
-                result.update(value)
-        return result
+        scf = get_scaffolding(request)
+        if not (
+            scf.first_name
+            and scf.last_name
+            and scf.street
+            and scf.lease_type
+            and scf.receives_public_assistance is not None
+        ):
+            return None
+        return with_keys_renamed(scf.dict(), {"street": "address"})
 
     @classmethod
     def perform_mutate(cls, form, info: ResolveInfo):
@@ -187,8 +200,7 @@ class OnboardingStep4Base(SessionFormMutation):
         if user.email:
             send_verification_email_async(user.pk)
 
-        for step in SESSION_STEPS:
-            step.clear_from_request(request)
+        purge_scaffolding(request)
 
         return cls.mutation_success()
 
@@ -392,8 +404,10 @@ class OnboardingSessionInfo(object):
     A mixin class defining all onboarding-related queries.
     """
 
-    onboarding_step_1 = OnboardingStep1V2Info.field()
-    onboarding_step_3 = OnboardingStep3Info.field()
+    onboarding_step_1 = graphene.Field(OnboardingStep1V2Info)
+
+    onboarding_step_3 = graphene.Field(OnboardingStep3Info)
+
     onboarding_info = graphene.Field(
         OnboardingInfoType,
         description=(
@@ -409,4 +423,21 @@ class OnboardingSessionInfo(object):
         user = info.context.user
         if hasattr(user, "onboarding_info"):
             return user.onboarding_info
+        return None
+
+    def resolve_onboarding_step_1(self, info: ResolveInfo):
+        scf = get_scaffolding(info.context)
+        if scf.first_name and scf.last_name and scf.street:
+            return with_keys_renamed(scf.dict(), {"street": "address"})
+        return None
+
+    def resolve_onboarding_step_3(self, info: ResolveInfo):
+        scf = get_scaffolding(info.context)
+        if scf.lease_type and scf.receives_public_assistance is not None:
+            return {
+                "lease_type": scf.lease_type,
+                "receives_public_assistance": YesNoRadiosField.reverse_coerce_to_str(
+                    scf.receives_public_assistance
+                ),
+            }
         return None

onboarding/tests/test_scaffolding.py
@@ -1,6 +1,7 @@
 import pytest
 
-from onboarding.scaffolding import OnboardingScaffolding
+from onboarding.scaffolding import OnboardingScaffolding, get_scaffolding, update_scaffolding
+from onboarding.schema import session_key_for_step
 
 
 @pytest.mark.parametrize(
@@ -27,3 +28,72 @@ from onboarding.scaffolding import OnboardingScaffolding
 )
 def test_is_city_in_nyc_works(scaffolding, expected):
     assert scaffolding.is_city_in_nyc() is expected
+
+
+class TestMigrateOnboardingToScaffolding:
+    def test_it_migrates_step_1(self, http_request):
+        http_request.session[session_key_for_step(1)] = {
+            "first_name": "blop",
+            "last_name": "jones",
+            "preferred_first_name": "zip",
+            "apt_number": "3",
+            "address": "123 boop street",
+            "borough": "MANHATTAN",
+            "address_verified": True,
+        }
+
+        scf = get_scaffolding(http_request)
+
+        assert scf.first_name == "blop"
+        assert scf.last_name == "jones"
+        assert scf.preferred_first_name == "zip"
+        assert scf.address_verified is True
+        assert scf.apt_number == "3"
+        assert scf.street == "123 boop street"
+        assert scf.borough == "MANHATTAN"
+
+        assert session_key_for_step(1) not in http_request
+
+    def test_it_migrates_only_step_1_address_info_when_needed(self, http_request):
+        update_scaffolding(
+            http_request,
+            {
+                "first_name": "foo",
+                "last_name": "bar",
+                "preferred_first_name": "bingy",
+            },
+        )
+        http_request.session[session_key_for_step(1)] = {
+            "first_name": "ignore",
+            "last_name": "ignore",
+            "preferred_first_name": "",
+            "apt_number": "3",
+            "address": "123 boop street",
+            "borough": "MANHATTAN",
+            "address_verified": True,
+        }
+
+        scf = get_scaffolding(http_request)
+
+        assert scf.first_name == "foo"
+        assert scf.last_name == "bar"
+        assert scf.preferred_first_name == "bingy"
+        assert scf.address_verified is True
+        assert scf.apt_number == "3"
+        assert scf.street == "123 boop street"
+        assert scf.borough == "MANHATTAN"
+
+        assert session_key_for_step(1) not in http_request
+
+    def test_it_migrates_step_3(self, http_request):
+        http_request.session[session_key_for_step(3)] = {
+            "lease_type": "RENT_STABILIZED",
+            "receives_public_assistance": "True",
+        }
+
+        scf = get_scaffolding(http_request)
+
+        assert scf.lease_type == "RENT_STABILIZED"
+        assert scf.receives_public_assistance is True
+
+        assert session_key_for_step(3) not in http_request

norent/schema.py
@@ -373,19 +373,6 @@ class BaseCreateAccount(SessionFormMutation):
     signup_intent: str = ""
 
     @classmethod
-    def fill_nyc_info_from_deprecated_endpoint(cls, request, info: Dict[str, Any]):
-        # For more details on why this is deprecated, see:
-        # https://github.com/JustFixNYC/tenants2/pull/2143
-        step1 = OnboardingStep1V2Info.get_dict_from_request(request)
-        if step1 is None:
-            return None
-        info["borough"] = step1["borough"]
-        info["address"] = step1["address"]
-        info["apt_number"] = step1["apt_number"]
-        info["address_verified"] = step1["address_verified"]
-        return info
-
-    @classmethod
     def fill_city_info(cls, request, info: Dict[str, Any], scf: OnboardingScaffolding):
         info = {
             **info,
@@ -396,12 +383,9 @@ class BaseCreateAccount(SessionFormMutation):
 
         if scf.is_city_in_nyc():
             if scf.borough:
-                # The new NYC address entry endpoint was used, so we already
-                # have the information we need.
                 info["borough"] = scf.borough
             else:
-                # The old onboarding endpoint was used, so fill things in from there.
-                return cls.fill_nyc_info_from_deprecated_endpoint(request, info)
+                return None
         else:
             if not are_all_truthy(scf.street, scf.zip_code):
                 return None

project/forms.py
@@ -27,6 +27,19 @@ def ensure_at_least_one_is_true(cleaned_data):
 
 
 class YesNoRadiosField(forms.ChoiceField):
+    """
+    This field represents a set of yes/no radios for the user to choose between.
+
+    It is distinct from a checkbox in that the user must actively answer either
+    "yes" or "no" (in contrast, a checkbox defaults to "no").
+    """
+
+    # Choice when a user selects "yes" from a yes/no radio (specific to Django).
+    TRUE = "True"
+
+    # Choice when a user selects "no" from a yes/no radio (specific to Django).
+    FALSE = "False"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, choices=[(True, "Yes"), (False, "No")])
 
@@ -34,11 +47,20 @@ class YesNoRadiosField(forms.ChoiceField):
     def coerce(cls, value: Optional[str]) -> Optional[bool]:
         if value in cls.empty_values:
             return None
-        if value == "True":
+        if value == cls.TRUE:
             return True
-        if value == "False":
+        if value == cls.FALSE:
             return False
         raise ValueError(f"Invalid YesNoRadiosField value: {value}")
+
+    @classmethod
+    def reverse_coerce_to_str(cls, value: Optional[bool]) -> str:
+        if value is None:
+            return ""
+        if value is True:
+            return cls.TRUE
+        assert value is False
+        return cls.FALSE
 
 
 class DynamicallyRequiredFieldsMixin:

commit 35d92f488437b12b0cd441ef71377792a715f64d

Make rent history use onboarding scaffolding. (#2159)
This makes the rental history flow use `OnboardingScaffolding` for its data storage, instead of using its own stash of stuff, moving us yet closer to #2142.

It also fixes a few bugs introduced in #2154.  I had forgotten that the `AddressAndBoroughFormMixin` also sets a `zipcode` field in its cleaned data (based on geocoding information), so this information is now stored into the onboarding scaffolding (under its slightly different field name, `zip_code`) for all relevant forms. I think this might technically affect some mutations outside of the onboarding scaffolding refactorings--most notably the `NycAddress` mutation--but it shouldn't make a difference in the overall behavior of the system.

onboarding/scaffolding.py
@@ -192,6 +192,23 @@ class GraphQlOnboardingScaffolding(graphene.ObjectType):
         return graphene.Field(cls, resolver=resolver)
 
 
+def get_scaffolding_fields_from_form(form) -> Dict[str, Any]:
+    """
+    Returns the form's cleaned data in a format that is suitable
+    for updating scaffolding data.
+
+    Generally this is just the form's cleaned data. However, if the
+    form has a `to_scaffolding_keys` property that maps its field
+    names to scaffolding field names, that is used to rename fields
+    as needed.
+    """
+
+    data = form.cleaned_data
+    if hasattr(form, "to_scaffolding_keys"):
+        data = with_keys_renamed(data, form.to_scaffolding_keys)
+    return data
+
+
 class OnboardingScaffoldingMutation(SessionFormMutation):
     """
     Base class for writing a form's `cleaned_data` into onboarding scaffolding.
@@ -201,13 +218,9 @@ class OnboardingScaffoldingMutation(SessionFormMutation):
         abstract = True
 
     @classmethod
-    def get_scaffolding_fields_from_form(cls, form) -> Dict[str, Any]:
-        return form.cleaned_data
-
-    @classmethod
     def perform_mutate(cls, form, info: ResolveInfo):
         request = info.context
-        update_scaffolding(request, cls.get_scaffolding_fields_from_form(form))
+        update_scaffolding(request, get_scaffolding_fields_from_form(form))
         return cls.mutation_success()
 
 
@@ -229,7 +242,7 @@ class OnboardingScaffoldingOrUserDataMutation(SessionFormMutation):
 
     @classmethod
     def perform_mutate_for_anonymous_user(cls, form, info: ResolveInfo):
-        update_scaffolding(info.context, form.cleaned_data)
+        update_scaffolding(info.context, get_scaffolding_fields_from_form(form))
         return cls.mutation_success()
 
     @classmethod
@@ -274,7 +287,11 @@ def _migrate_legacy_session_data_to_scaffolding(request):
                 if "preferred_first_name" in legacy_step1:
                     del legacy_step1["preferred_first_name"]
 
-            d.update(with_keys_renamed(legacy_step1, {"address": "street"}))
+            d.update(
+                with_keys_renamed(
+                    legacy_step1, OnboardingStep1V2Info._meta.form_class.to_scaffolding_keys
+                )
+            )
             updated = True
             OnboardingStep1V2Info.clear_from_request(request)
 
@@ -291,6 +308,15 @@ def _migrate_legacy_session_data_to_scaffolding(request):
             d.update(legacy_step3)
             updated = True
             OnboardingStep3Info.clear_from_request(request)
+
+    if not d.get("phone_number"):
+        from rh.schema import RhFormInfo
+
+        legacy_rh = RhFormInfo.get_dict_from_request(request)
+        if legacy_rh:
+            d.update(with_keys_renamed(legacy_rh, RhFormInfo._meta.form_class.to_scaffolding_keys))
+            updated = True
+            RhFormInfo.clear_from_request(request)
 
     if updated:
         request.session[SCAFFOLDING_SESSION_KEY] = d

onboarding/schema.py
@@ -99,10 +99,6 @@ class OnboardingStep1V2(OnboardingScaffoldingMutation):
         form_class = forms.OnboardingStep1V2Form
         exclude = ["no_apt_number"]
 
-    @classmethod
-    def get_scaffolding_fields_from_form(cls, form) -> Dict[str, Any]:
-        return with_keys_renamed(form.cleaned_data, {"address": "street"})
-
 
 @schema_registry.register_mutation
 class OnboardingStep3(OnboardingScaffoldingMutation):
@@ -177,7 +173,7 @@ class OnboardingStep4Base(SessionFormMutation):
             and scf.receives_public_assistance is not None
         ):
             return None
-        return with_keys_renamed(scf.dict(), {"street": "address"})
+        return with_keys_renamed(scf.dict(), forms.OnboardingStep1V2Form.from_scaffolding_keys)
 
     @classmethod
     def perform_mutate(cls, form, info: ResolveInfo):
@@ -285,7 +281,7 @@ class NycAddress(SessionFormMutation):
             cls.perform_mutate_for_logged_in_user(form, info)
         else:
             update_scaffolding(
-                info.context, with_keys_renamed(form.cleaned_data, {"address": "street"})
+                info.context, with_keys_renamed(form.cleaned_data, form.to_scaffolding_keys)
             )
 
         return cls.mutation_success()
@@ -428,7 +424,9 @@ class OnboardingSessionInfo(object):
     def resolve_onboarding_step_1(self, info: ResolveInfo):
         scf = get_scaffolding(info.context)
         if scf.first_name and scf.last_name and scf.street:
-            return with_keys_renamed(scf.dict(), {"street": "address"})
+            return with_keys_renamed(
+                scf.dict(), OnboardingStep1V2Info._meta.form_class.from_scaffolding_keys
+            )
         return None
 
     def resolve_onboarding_step_3(self, info: ResolveInfo):

project/util/address_form_fields.py
@@ -1,3 +1,4 @@
+from project.util.rename_dict_keys import flip_dict
 from typing import NamedTuple
 from django import forms
 from django.utils.translation import gettext as _
@@ -99,6 +100,16 @@ class AddressAndBoroughFormMixin(forms.Form):
     borough = forms.ChoiceField(
         choices=BOROUGH_CHOICES.choices, required=False, help_text="A New York City borough."
     )
+
+    # Our onboarding scaffolding structure uses slightly different field names for the
+    # same kind of values; this dictionary maps this form's field names to the ones the
+    # scaffolding uses.
+    to_scaffolding_keys = {
+        "address": "street",
+        "zipcode": "zip_code",
+    }
+
+    from_scaffolding_keys = flip_dict(to_scaffolding_keys)
 
     extra_graphql_output_fields = {
         "address_verified": graphene.Boolean(

project/util/rename_dict_keys.py
@@ -15,3 +15,14 @@ def with_keys_renamed(d: Dict[str, Any], renames: Dict[str, str]) -> Dict[str, A
         result[renamed] = d[original]
 
     return result
+
+
+def flip_dict(d: Dict[str, str]) -> Dict[str, str]:
+    """
+    Return the given dictionary with its keys and values swapped, e.g.:
+
+        >>> flip_dict({'foo': 'bar'})
+        {'bar': 'foo'}
+    """
+
+    return {value: key for key, value in d.items()}

rh/forms.py
@@ -1,3 +1,4 @@
+from project.util.rename_dict_keys import flip_dict
 from django import forms
 
 from project.util.phone_number import USPhoneNumberField
@@ -24,6 +25,13 @@ class RhForm(AddressAndBoroughFormMixin, forms.ModelForm):
         )
 
     phone_number = USPhoneNumberField()
+
+    to_scaffolding_keys = {
+        **AddressAndBoroughFormMixin.to_scaffolding_keys,
+        "apartment_number": "apt_number",
+    }
+
+    from_scaffolding_keys = flip_dict(to_scaffolding_keys)
 
 
 class RhSendEmail(forms.Form):

rh/schema.py
@@ -1,3 +1,10 @@
+from project.util.rename_dict_keys import with_keys_renamed
+from onboarding.scaffolding import (
+    OnboardingScaffolding,
+    OnboardingScaffoldingMutation,
+    get_scaffolding,
+    purge_scaffolding,
+)
 from . import models, forms, email_dhcr
 from typing import Dict, Any, Optional
 from django.utils import translation
@@ -7,10 +14,7 @@ from django.conf import settings
 import graphene
 from graphql import ResolveInfo
 from project import slack
-from project.util.django_graphql_session_forms import (
-    DjangoSessionFormObjectType,
-    DjangoSessionFormMutation,
-)
+from project.util.django_graphql_session_forms import DjangoSessionFormObjectType
 from project.util.session_mutation import SessionFormMutation
 from project.util.streaming_json import generate_json_rows
 from project.util.site_util import absolute_reverse, SITE_CHOICES
@@ -86,21 +90,32 @@ class RhFormInfo(DjangoSessionFormObjectType):
 
 
 @schema_registry.register_mutation
-class RhForm(DjangoSessionFormMutation):
+class RhForm(OnboardingScaffoldingMutation):
     class Meta:
-        source = RhFormInfo
+        form_class = forms.RhForm
 
     @classmethod
     def perform_mutate(cls, form, info: ResolveInfo):
         request = info.context
         result = super().perform_mutate(form, info)
-        form_data = RhFormInfo.get_dict_from_request(request)
-        assert form_data is not None
+        scf = get_scaffolding(request)
+        assert scf.street and scf.borough
 
-        full_address = form_data["address"] + ", " + form_data["borough"]
+        full_address = scf.street + ", " + scf.borough
         bbl, _, _ = lookup_bbl_and_bin_and_full_address(full_address)
         request.session[RENT_STAB_INFO_SESSION_KEY] = get_rent_stab_info_for_bbl(bbl)
         return result
+
+
+def scaffolding_has_rental_history_request_info(scf: OnboardingScaffolding) -> bool:
+    return bool(
+        scf.first_name
+        and scf.last_name
+        and scf.street
+        and scf.borough
+        and scf.phone_number
+        and scf.apt_number
+    )
 
 
 @schema_registry.register_mutation
@@ -111,33 +126,40 @@ class RhSendEmail(SessionFormMutation):
     @classmethod
     def perform_mutate(cls, form, info):
         request = info.context
-        form_data = RhFormInfo.get_dict_from_request(request)
-        if form_data is None:
+        scf = get_scaffolding(request)
+        if not scaffolding_has_rental_history_request_info(scf):
             cls.log(info, "User has not completed the rental history form, aborting mutation.")
             return cls.make_error("You haven't completed all the previous steps yet.")
 
-        rhr = models.RentalHistoryRequest(**form_data)
+        rhr = models.RentalHistoryRequest(
+            first_name=scf.first_name,
+            last_name=scf.last_name,
+            apartment_number=scf.apt_number,
+            phone_number=scf.phone_number,
+            address=scf.street,
+            address_verified=scf.address_verified,
+            borough=scf.borough,
+            zipcode=scf.zip_code,
+        )
         rhr.set_user(request.user)
         rhr.full_clean()
         rhr.save()
         slack.sendmsg_async(get_slack_notify_text(rhr), is_safe=True)
 
-        first_name: str = form_data["first_name"]
-        last_name: str = form_data["last_name"]
         email = react_render_email(
             SITE_CHOICES.JUSTFIX,
             project.locales.DEFAULT,
             "rh/email-to-dhcr.txt",
-            session={RhFormInfo._meta.session_key: form_data},
+            session=request.session,
         )
         email_dhcr.send_email_to_dhcr(email.subject, email.body)
         trigger_followup_campaign_async(
-            f"{first_name} {last_name}",
-            form_data["phone_number"],
+            f"{scf.first_name} {scf.last_name}",
+            scf.phone_number,
             "RH",
             locale=translation.get_language_from_request(request, check_path=True),
         )
-        RhFormInfo.clear_from_request(request)
+        purge_scaffolding(request)
         return cls.mutation_success()
 
 
@@ -159,7 +181,8 @@ class RhRentStabData(graphene.ObjectType):
 
 @schema_registry.register_session_info
 class RhSessionInfo(object):
-    rental_history_info = RhFormInfo.field()
+    rental_history_info = graphene.Field(RhFormInfo)
+
     rent_stab_info = graphene.Field(RhRentStabData)
 
     def resolve_rent_stab_info(self, info: ResolveInfo):
@@ -167,4 +190,11 @@ class RhSessionInfo(object):
         kwargs = request.session.get(RENT_STAB_INFO_SESSION_KEY, {})
         if kwargs:
             return RhRentStabData(**kwargs)
+        return None
+
+    def resolve_rental_history_info(self, info: ResolveInfo):
+        scf = get_scaffolding(info.context)
+        if scaffolding_has_rental_history_request_info(scf):
+            d = with_keys_renamed(scf.dict(), RhFormInfo._meta.form_class.from_scaffolding_keys)
+            return d
         return None

onboarding/tests/test_scaffolding.py
@@ -40,6 +40,7 @@ class TestMigrateOnboardingToScaffolding:
             "address": "123 boop street",
             "borough": "MANHATTAN",
             "address_verified": True,
+            "zipcode": "11201",
         }
 
         scf = get_scaffolding(http_request)
@@ -51,8 +52,9 @@ class TestMigrateOnboardingToScaffolding:
         assert scf.apt_number == "3"
         assert scf.street == "123 boop street"
         assert scf.borough == "MANHATTAN"
+        assert scf.zip_code == "11201"
 
-        assert session_key_for_step(1) not in http_request
+        assert session_key_for_step(1) not in http_request.session
 
     def test_it_migrates_only_step_1_address_info_when_needed(self, http_request):
         update_scaffolding(
@@ -71,6 +73,7 @@ class TestMigrateOnboardingToScaffolding:
             "address": "123 boop street",
             "borough": "MANHATTAN",
             "address_verified": True,
+            "zipcode": "11201",
         }
 
         scf = get_scaffolding(http_request)
@@ -83,7 +86,7 @@ class TestMigrateOnboardingToScaffolding:
         assert scf.street == "123 boop street"
         assert scf.borough == "MANHATTAN"
 
-        assert session_key_for_step(1) not in http_request
+        assert session_key_for_step(1) not in http_request.session
 
     def test_it_migrates_step_3(self, http_request):
         http_request.session[session_key_for_step(3)] = {
@@ -96,4 +99,30 @@ class TestMigrateOnboardingToScaffolding:
         assert scf.lease_type == "RENT_STABILIZED"
         assert scf.receives_public_assistance is True
 
-        assert session_key_for_step(3) not in http_request
+        assert session_key_for_step(3) not in http_request.session
+
+    def test_it_migrates_rent_history_form_info(self, http_request):
+        from rh.schema import RhFormInfo
+
+        key = RhFormInfo._meta.session_key
+
+        http_request.session[key] = {
+            "address": "123 boop st",
+            "borough": "MANHATTAN",
+            "first_name": "boop",
+            "last_name": "jonze",
+            "phone_number": "5551234567",
+            "apartment_number": "2B",
+            "zipcode": "11201",
+        }
+
+        scf = get_scaffolding(http_request)
+
+        assert scf.first_name == "boop"
+        assert scf.last_name == "jonze"
+        assert scf.street == "123 boop st"
+        assert scf.apt_number == "2B"
+        assert scf.zip_code == "11201"
+        assert scf.phone_number == "5551234567"
+
+        assert key not in http_request.session
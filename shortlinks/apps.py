cba0916d7e33fc255fc64078feeca9c5b3d554d5
Atul Varma <varmaa@gmail.com>
2021-07-21 07:07:44 -0400

    Run Black.

--- shortlinks/apps.py
+++ shortlinks/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ShortlinksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shortlinks'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shortlinks"

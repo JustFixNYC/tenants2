48c98663fd518887b73dbc9e972e4231cfb7d7eb
Samara Trilling <samara.trilling@gmail.com>
2021-08-04 13:32:30 -0400

    Capitalize voucher.

--- frontend/lib/util/housing-type.ts
+++ frontend/lib/util/housing-type.ts
@@ -1,3 +1,3 @@
 export const PUBLIC_ASSISTANCE_QUESTION_TEXT =
-  "Do you receive a housing voucher or subsidy (Section 8 voucher, FHEPS, CITYFHEPS, HASA, etc.)?";
+  "Do you receive a housing voucher or subsidy (Section 8 Voucher, FHEPS, CITYFHEPS, HASA, etc.)?";
 export const HOUSING_TYPE_FIELD_LABEL = "Housing type";

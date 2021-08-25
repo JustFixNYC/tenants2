9823a3ce5c9cb5811e25106976e47abdc58ee44f
Sam Rabiyah <sraby@users.noreply.github.com>
2021-08-25 10:46:29 -0400

    Make shortlink slug always default to lowercase

--- shortlinks/views.py
+++ shortlinks/views.py
@@ -5,5 +5,6 @@ from .models import Link
 
 
 def redirect_to_link(request, slug):
-    link = get_object_or_404(Link, slug=slug)
+    formatted_slug = slug.lower()
+    link = get_object_or_404(Link, slug=formatted_slug)
     return HttpResponseRedirect(link.url)

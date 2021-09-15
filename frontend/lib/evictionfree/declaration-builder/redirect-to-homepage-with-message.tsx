b8dbba39ebb2826363bc083efd16df9781410c22
Sam Rabiyah <sraby@users.noreply.github.com>
2021-09-15 10:57:31 -0400

    Simplify new component

--- frontend/lib/evictionfree/declaration-builder/redirect-to-homepage-with-message.tsx
+++ frontend/lib/evictionfree/declaration-builder/redirect-to-homepage-with-message.tsx
@@ -3,6 +3,6 @@ import { Redirect } from "react-router-dom";
 import { MESSAGE_QS } from "../homepage";
 import { EvictionFreeRoutes } from "../route-info";
 
-export const EvictionFreeRedirectToHomepageWithMessage: React.FC<{}> = (
-  props
-) => <Redirect to={`${EvictionFreeRoutes.locale.home}?${MESSAGE_QS}`} />;
+export const EvictionFreeRedirectToHomepageWithMessage: React.FC<{}> = () => (
+  <Redirect to={`${EvictionFreeRoutes.locale.home}?${MESSAGE_QS}`} />
+);

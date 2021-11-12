e4b16c8d1accd6cc66048301d10d5218f8245853
Samara Trilling <samara.trilling@gmail.com>
2021-11-12 10:48:25 -0500

    internationalize

--- frontend/lib/laletterbuilder/letter-builder/welcome.tsx
+++ frontend/lib/laletterbuilder/letter-builder/welcome.tsx
@@ -1,4 +1,4 @@
-import { Trans } from "@lingui/macro";
+import { t, Trans } from "@lingui/macro";
 import React from "react";
 import { Link } from "react-router-dom";
 import { SimpleClearAnonymousSessionButton } from "../../forms/clear-anonymous-session-button";
@@ -6,10 +6,15 @@ import { ProgressStepProps } from "../../progress/progress-step-route";
 import Page from "../../ui/page";
 import { LALetterBuilderRoutes } from "../route-info";
 import { assertNotNull } from "@justfixnyc/util";
+import { li18n } from "../../i18n-lingui";
 
 export const LALetterBuilderWelcome: React.FC<ProgressStepProps> = (props) => {
   return (
-    <Page title="Build your letter" className="content" withHeading="big">
+    <Page
+      title={li18n._(t`Build your letter`)}
+      className="content"
+      withHeading="big"
+    >
       <p>TODO: Add welcome content here. </p>
       <div className="buttons jf-two-buttons">
         <SimpleClearAnonymousSessionButton

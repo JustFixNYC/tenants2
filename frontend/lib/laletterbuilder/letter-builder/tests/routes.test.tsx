7e77945caccf355de7b75431dd75869464ef6b3b
Samara Trilling <samara.trilling@gmail.com>
2021-12-09 16:44:12 -0500

    Fix test

--- frontend/lib/laletterbuilder/letter-builder/tests/routes.test.tsx
+++ frontend/lib/laletterbuilder/letter-builder/tests/routes.test.tsx
@@ -43,6 +43,7 @@ tester.defineTest({
     "/en/letter/landlord/name",
     "/en/letter/landlord/email",
     "/en/letter/landlord/address",
+    "/en/letter/choose-letter",
     "/en/letter/confirmation",
   ],
 });

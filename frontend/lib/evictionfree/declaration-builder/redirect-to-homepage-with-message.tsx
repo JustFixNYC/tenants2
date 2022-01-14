3dcbec1d4710b05b8c685cc760569981fa417841
Sam Rabiyah <sraby@users.noreply.github.com>
2022-01-14 10:45:54 -0500

    Remove unneeded redirect component

--- frontend/lib/evictionfree/declaration-builder/redirect-to-homepage-with-message.tsx
+++ frontend/lib/evictionfree/declaration-builder/redirect-to-homepage-with-message.tsx
@@ -3,10 +3,6 @@ import { Redirect } from "react-router-dom";
 import { MESSAGE_QS } from "../homepage";
 import { EvictionFreeRoutes } from "../route-info";
 
-export const EvictionFreeRedirectToHomepage: React.FC<{}> = () => (
-  <Redirect to={EvictionFreeRoutes.locale.home} />
-);
-
 export const EvictionFreeRedirectToHomepageWithMessage: React.FC<{}> = () => (
   <Redirect to={`${EvictionFreeRoutes.locale.home}?${MESSAGE_QS}`} />
 );

239d6d82d1169ffa4717b03afbf091d13a6ec1de
Samara Trilling <samara.trilling@gmail.com>
2022-01-20 12:58:24 -0500

    Add landing page text

--- frontend/lib/laletterbuilder/homepage.tsx
+++ frontend/lib/laletterbuilder/homepage.tsx
@@ -8,10 +8,14 @@ import { Link } from "react-router-dom";
 import { LaLetterBuilderRouteInfo as Routes } from "./route-info";
 
 export const LaLetterBuilderHomepage: React.FC<{}> = () => (
-  <Page title="">
+  <Page title="" className="content">
     <section className="hero is-fullheight-with-navbar">
       <div className="hero-body">
         <div className="container jf-has-text-centered-tablet">
+          <h1 className="title is-spaced has-text-info">
+            <Trans>Landing page</Trans>
+          </h1>
+          <br />
           <p>This website is under construction.</p>
           <p>
             <small>
@@ -25,7 +29,7 @@ export const LaLetterBuilderHomepage: React.FC<{}> = () => (
               className="button is-light is-medium"
               to={Routes.locale.letter.latestStep}
             >
-              Build my letter
+              See which letter is right for me
             </Link>
           </p>
         </div>

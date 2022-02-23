2268aeb8c61d127029c56618008473dfd88f20cb
Samara Trilling <samara.trilling@gmail.com>
2022-02-23 10:14:41 -0500

    Remove duplicate

--- laletterbuilder/tests/factories.py
+++ laletterbuilder/tests/factories.py
@@ -15,15 +15,6 @@ class LandlordDetailsFactory(factory.django.DjangoModelFactory):
     address = "1 Cloud City"
 
 
-class LandlordDetailsFactory(LandlordDetailsFactory):
-    address = "123 Cloud City Drive\nBespin, NY 12345"
-    primary_line = "123 Cloud City Drive"
-    city = "Bespin"
-    state = "NY"
-    zip_code = "12345"
-    email = "boop@boop.com"
-
-
 class HabitabilityLetterFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = HabitabilityLetter

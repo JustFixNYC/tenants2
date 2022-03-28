c8faf4985488ce113e354d8eeea0664ab7c9a088
Samara Trilling <samara.trilling@gmail.com>
2022-03-28 14:31:50 -0400

    Wrap some more text in trans tags

--- frontend/lib/laletterbuilder/letter-builder/send-options.tsx
+++ frontend/lib/laletterbuilder/letter-builder/send-options.tsx
@@ -1,4 +1,4 @@
-import { t } from "@lingui/macro";
+import { t, Trans } from "@lingui/macro";
 import React from "react";
 import { li18n } from "../../i18n-lingui";
 import { MiddleProgressStep } from "../../progress/progress-step-route";
@@ -14,18 +14,26 @@ export const LaLetterBuilderSendOptions = MiddleProgressStep((props) => {
     >
       <hr />
       <p>
-        <b>Send for me</b>
+        <b>
+          <Trans>Send for me</Trans>
+        </b>
         <br />
-        We'll send your letter for you via certified mail in 1-2 business days,
-        at no cost to you.
+        <Trans>
+          We'll send your letter for you via certified mail in 1-2 business
+          days, at no cost to you.
+        </Trans>
       </p>
       <hr />
       <p>
-        <b>Download and send myself</b>
+        <b>
+          <Trans>Download and send myself</Trans>
+        </b>
       </p>
       <p>
-        Not sure yet? If you need more time to decide, you can always come back
-        later. We've saved your work.
+        <Trans>
+          Not sure yet? If you need more time to decide, you can always come
+          back later. We've saved your work.
+        </Trans>
       </p>
       <ProgressButtonsAsLinks back={props.prevStep} next={props.nextStep} />{" "}
     </Page>

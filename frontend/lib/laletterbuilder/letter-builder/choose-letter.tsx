57c9c7a8087787db326ebce6c11db942550320ea
Samara Trilling <samara.trilling@gmail.com>
2022-04-13 10:59:32 -0400

    I18n

--- frontend/lib/laletterbuilder/letter-builder/choose-letter.tsx
+++ frontend/lib/laletterbuilder/letter-builder/choose-letter.tsx
@@ -22,58 +22,63 @@ export const LaLetterBuilderChooseLetterStep: React.FC<ProgressStepProps> = (
       withHeading="small"
     >
       <LetterCard
-        title="Notice to Repair"
+        title={li18n._(t`Notice to Repair`)}
         time_mins={15}
-        text={`Document repairs needed in your home, and send a formal request to your landlord`}
+        text={li18n._(
+          t`Document repairs needed in your home, and send a formal request to your landlord`
+        )}
         buttonProps={{
           to: LaLetterBuilderRouteInfo.locale.habitability.latestStep,
           className: "button is-primary is-medium",
           text: li18n._(t`Start a letter`),
         }}
-        information={"notice to repair information needed"}
+        information={li18n._(t`notice to repair information needed`)}
       />
       <LetterCard
-        title="Right to Privacy"
+        title={li18n._(t`Right to Privacy`)}
         time_mins={15}
-        text={`Your landlord can't enter your unit whenever they want. Create a formal request 
-        which asks your landlord to follow proper protocol and respect your right to privacy.`}
+        text={li18n._(t`Your landlord can't enter your unit whenever they want. Create a formal request 
+        which asks your landlord to follow proper protocol and respect your right to privacy.`)}
         buttonProps={{
           to:
             "https://justfix.formstack.com/forms/saje_right_to_privacy_letter_builder_form",
           className: "button is-light is-medium",
           text: li18n._(t`Go to letter`),
         }}
-        information={"right to privacy information needed"}
+        information={li18n._(t`right to privacy information needed`)}
       />
       <LetterCard
-        title="Harassment"
+        title={li18n._(t`Harassment`)}
         time_mins={10}
-        text={`Document the harassment you and your family are experiencing and send a notice to your landlord.`}
+        text={li18n._(
+          t`Document the harassment you and your family are experiencing and send a notice to your landlord.`
+        )}
         buttonProps={{
           to:
             "https://justfix.formstack.com/forms/saje_anti_harassment_letter_builder_form",
           className: "button is-light is-medium",
           text: li18n._(t`Go to letter`),
         }}
-        information={"harassment information needed"}
+        information={li18n._(t`harassment information needed`)}
       />
       <LetterCard
-        title="Private Right of Action"
+        title={li18n._(t`Private Right of Action`)}
         time_mins={10}
-        text={`The City of LA allows residential tenants to sue for violations of COVID-19 renter protections. Take the first step by documenting violations and notifying your landlord.`}
+        text={li18n._(
+          t`The City of LA allows residential tenants to sue for violations of COVID-19 renter protections. Take the first step by documenting violations and notifying your landlord.`
+        )}
         buttonProps={{
           to:
             "https://justfix.formstack.com/forms/saje_la_city_private_right_of_action_letter_builder_form",
           className: "button is-light is-medium",
           text: li18n._(t`Go to letter`),
         }}
-        information={"private right of action information needed"}
+        information={li18n._(t`private right of action information needed`)}
       />
       <div className="buttons jf-two-buttons">
         <SimpleClearAnonymousSessionButton
           to={LaLetterBuilderRouteInfo.locale.home}
         />
-        <div></div>
       </div>
     </Page>
   );

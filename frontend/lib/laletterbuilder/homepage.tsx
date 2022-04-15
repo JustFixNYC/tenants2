6a02b825708675a6686b24ad299485cf9338c4a4
Samara Trilling <samara.trilling@gmail.com>
2022-04-15 12:32:32 -0400

    Add first beige section to homepage

--- frontend/lib/laletterbuilder/homepage.tsx
+++ frontend/lib/laletterbuilder/homepage.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Trans } from "@lingui/macro";
 
 import Page from "../ui/page";
-import { Link } from "react-router-dom";
 
 import { LaLetterBuilderRouteInfo as Routes } from "./route-info";
 import { CenteredPrimaryButtonLink } from "../ui/buttons";
+import { StaticImage } from "../ui/static-image";
 
 type LaLetterBuilderImageType = "png" | "svg";
 
@@ -33,16 +33,39 @@ export const LaLetterBuilderHomepage: React.FC<{}> = () => (
             </Trans>
           </p>
           <br />
-          <p>
-            <CenteredPrimaryButtonLink to={Routes.locale.chooseLetter}>
-              <Trans>See which letter is right for me</Trans>
-            </CenteredPrimaryButtonLink>
-          </p>
+          <CenteredPrimaryButtonLink to={Routes.locale.chooseLetter}>
+            <Trans>See which letter is right for me</Trans>
+          </CenteredPrimaryButtonLink>
         </div>
       </div>
     </section>
     <section className="jf-laletterbuilder-landing-section-secondary">
-      EXAMPLE NEXT SECTION
+      <div className="hero-body">
+        <div className="container jf-has-text-centered-tablet">
+          <h2 className="is-spaced has-text-info">
+            <Trans>Legally vetted</Trans>
+          </h2>
+          <br />
+          <p className="subtitle">
+            <Trans>
+              We built the LA Letter Builder with lawyers and non-profit tenants
+              rights organizations in Los Angeles to ensure that your letter
+              gives you the most protections.
+            </Trans>
+          </p>
+          <br />
+          <div className="is-spaced has-text-info">
+            <Trans>Created by</Trans>
+            <StaticImage
+              ratio="is-3by1"
+              src={getLaLetterBuilderImageSrc(
+                "justfix-saje-combined-logo-black"
+              )}
+              alt="JustFix SAJE"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   </Page>
 );

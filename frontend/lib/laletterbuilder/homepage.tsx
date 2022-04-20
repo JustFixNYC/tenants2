06e995cad93c2e4a4a6253bdfd7b8c4fca7a0e2a
Samara Trilling <samara.trilling@gmail.com>
2022-04-20 12:34:12 -0400

    Translate title

--- frontend/lib/laletterbuilder/homepage.tsx
+++ frontend/lib/laletterbuilder/homepage.tsx
@@ -21,7 +21,7 @@ export function getLaLetterBuilderImageSrc(
 }
 
 export const LaLetterBuilderHomepage: React.FC<{}> = () => (
-  <Page title="" className="content">
+  <Page title={li18n._(t`LA Letter Builder Homepage`)} className="content">
     <section className="hero is-fullheight-with-navbar jf-laletterbuilder-landing-section-primary">
       <div className="hero-body">
         <div className="container jf-has-text-centered-tablet">

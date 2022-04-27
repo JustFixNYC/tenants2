04da634f66971a1b533250be8043f044b5dc0947
Samara Trilling <samara.trilling@gmail.com>
2022-04-27 11:26:16 -0700

    Add a start new letter button to My Letters page (#2288)
    * Add create new letter button and add styling
    
    * Add styling to other button
    
    * lingui

--- frontend/lib/laletterbuilder/letter-builder/my-letters.tsx
+++ frontend/lib/laletterbuilder/letter-builder/my-letters.tsx
@@ -26,34 +26,51 @@ const MyLettersContent: React.FC = (props) => {
   const { session } = useContext(AppContext);
   const { hasHabitabilityLetterInProgress } = session;
   return (
-    <>
-      <p>Your Letters</p>
-      <div>
-        {hasHabitabilityLetterInProgress ? (
-          <Link
-            to={LaLetterBuilderRouteInfo.locale.habitability.issues.prefix}
-            className="button jf-is-next-button is-primary is-medium"
-          >
-            {li18n._(t`Continue my letter`)}
-          </Link>
-        ) : (
-          <SessionUpdatingFormSubmitter
-            mutation={LaLetterBuilderCreateLetterMutation}
-            initialState={{}}
-            onSuccessRedirect={
-              LaLetterBuilderRouteInfo.locale.habitability.issues.prefix
-            }
-          >
-            {(sessionCtx) => (
-              <NextButton
-                isLoading={sessionCtx.isLoading}
-                label={li18n._(t`Start a habitability letter`)}
-              />
-            )}
-          </SessionUpdatingFormSubmitter>
-        )}
-      </div>
-    </>
+    <div className="jf-my-letters">
+      <p className="subtitle">See all your finished and unfinished letters</p>
+
+      {hasHabitabilityLetterInProgress ? (
+        <div className="my-letters-box">
+          <h3>Habitability letter</h3>
+          <p>In progress</p>
+          <div className="start-letter-button">
+            <Link
+              to={LaLetterBuilderRouteInfo.locale.habitability.issues.prefix}
+              className="button jf-is-next-button is-primary is-medium"
+            >
+              {li18n._(t`Continue my letter`)}
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <SessionUpdatingFormSubmitter
+          mutation={LaLetterBuilderCreateLetterMutation}
+          initialState={{}}
+          onSuccessRedirect={
+            LaLetterBuilderRouteInfo.locale.habitability.issues.prefix
+          }
+        >
+          {(sessionCtx) => (
+            <div className="my-letters-box">
+              <p>Start your habitability letter</p>
+              <div className="start-letter-button">
+                <NextButton
+                  isLoading={sessionCtx.isLoading}
+                  label={li18n._(t`Let's go`)}
+                />
+              </div>
+            </div>
+          )}
+        </SessionUpdatingFormSubmitter>
+      )}
+
+      <Link
+        to={LaLetterBuilderRouteInfo.locale.chooseLetter}
+        className="button new-letter-button is-light is-medium "
+      >
+        {li18n._(t`Create a new letter`)}
+      </Link>
+    </div>
   );
 };
 

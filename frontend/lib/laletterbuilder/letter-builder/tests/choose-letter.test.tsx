27695bff70caf7342ae79f5a0e038dae417706b0
shakao <34112083+shakao@users.noreply.github.com>
2022-07-18 07:45:39 -0700

    Update new letter card on My Letters page, small cleanups (#2354)
    * Update new letter card on My Letters page, small cleanups
    
    * Add test for updated Choose Letters flow

--- frontend/lib/laletterbuilder/letter-builder/tests/choose-letter.test.tsx
+++ frontend/lib/laletterbuilder/letter-builder/tests/choose-letter.test.tsx
@@ -6,6 +6,10 @@ import { newSb } from "../../../tests/session-builder";
 import { Route } from "react-router-dom";
 
 import { AppTesterPal } from "../../../tests/app-tester-pal";
+import { LaLetterBuilderCreateLetterMutation } from "../../../queries/LaLetterBuilderCreateLetterMutation";
+import { BlankAllSessionInfo } from "../../../queries/AllSessionInfo";
+import { override } from "../../../tests/util";
+import { HabitabilityLetterMailChoice } from "../../../queries/globalTypes";
 
 const sb = newSb();
 
@@ -29,19 +33,52 @@ describe("choose letter page", () => {
         session: sb.value,
       }
     );
-    pal.clickButtonOrLink("Select letter");
+    pal.clickButtonOrLink("Start letter");
     await pal.rt.waitFor(() => pal.rr.getByText(/Your phone number/i));
   });
 
-  it("redirects to habitability with logged in user", async () => {
+  it("creates a new letter and redirects to issues with logged in user", async () => {
     const pal = new AppTesterPal(
       <Route component={LaLetterBuilderRouteComponent} />,
       {
         url: LaLetterBuilderRouteInfo.locale.chooseLetter,
+        updateSession: true,
         session: sb.withLoggedInJustfixUser().value,
       }
     );
-    pal.clickButtonOrLink("Select letter");
-    await pal.rt.waitFor(() => pal.rr.getByText(/My Letters/i));
+    pal.clickButtonOrLink("Start letter");
+    pal
+      .withFormMutation(LaLetterBuilderCreateLetterMutation)
+      .expect({})
+      .respondWithSuccess({
+        session: override(BlankAllSessionInfo, {
+          habitabilityLatestLetter: {
+            id: "1",
+            createdAt: "2020-03-13T19:41:09+00:00",
+            trackingNumber: "",
+            letterSentAt: "",
+            fullyProcessedAt: "",
+            mailChoice: HabitabilityLetterMailChoice.WE_WILL_MAIL,
+            emailToLandlord: false,
+          },
+          hasHabitabilityLetterInProgress: true,
+        }),
+      });
+    await pal.waitForLocation("/en/habitability/issues");
+    pal.rr.getByText(/Select the repairs/i);
+  });
+
+  it("redirects to my letters with logged in user and letter in progress", async () => {
+    const pal = new AppTesterPal(
+      <Route component={LaLetterBuilderRouteComponent} />,
+      {
+        url: LaLetterBuilderRouteInfo.locale.chooseLetter,
+        session: sb.withLoggedInJustfixUser().withHabitabilityLetterInProgress()
+          .value,
+      }
+    );
+    pal.clickButtonOrLink("Start letter");
+    await pal.waitForLocation("/en/habitability/my-letters");
+    await pal.rt.waitFor(() => pal.rr.getByText(/My letters/i));
   });
 });

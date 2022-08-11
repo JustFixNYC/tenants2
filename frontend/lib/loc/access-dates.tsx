487841acae6c203659f32b4eb3ce3bd323e74268
shakao <34112083+shakao@users.noreply.github.com>
2022-08-11 10:32:05 -0700

    Tweak copy and small font styling across LATAC (#2372)
    * Use 'Notice to Repair' instead of 'Habitability'
    
    * Remove '(recommended)' from view PDF link
    
    * Update copy and LA-specific styling for landlord access dates
    
    * Update strings, tests with new copy
    
    * Add ability to fill out the first matching form field in a test

--- frontend/lib/loc/access-dates.tsx
+++ frontend/lib/loc/access-dates.tsx
@@ -13,6 +13,7 @@ import { dateAsISO, addDays } from "../util/date-util";
 
 import validation from "../../../common-data/access-dates-validation.json";
 import { MiddleProgressStep } from "../progress/progress-step-route";
+import { Trans } from "@lingui/macro";
 
 /**
  * The minimum number of days from today that the first access date
@@ -43,15 +44,15 @@ export function getInitialState(
 const AccessDatesPage = MiddleProgressStep((props) => {
   const minDate = dateAsISO(addDays(new Date(), MIN_DAYS));
   return (
-    <Page title="Access dates">
+    <Page title="Access dates" className="jf-landlord-access-dates">
       <div>
-        <h1 className="title is-4 is-spaced">Landlord/super access dates</h1>
+        <h1 className="title is-4 is-spaced">
+          <Trans>
+            Select at least one date when you'll be available for repairs
+          </Trans>
+        </h1>
         <p className="subtitle is-6">
-          Access dates are times you know when you will be home for the landlord
-          to schedule repairs. Please provide <strong>1 - 3</strong> access
-          dates when you can be available. You can only choose access dates
-          starting {MIN_DAYS} days from today to give time for the letter to be
-          delivered.
+          <Trans>Must be at least 14 days from today.</Trans>
         </p>
         <SessionUpdatingFormSubmitter
           mutation={AccessDatesMutation}
@@ -61,20 +62,20 @@ const AccessDatesPage = MiddleProgressStep((props) => {
           {(ctx) => (
             <>
               <TextualFormField
-                label={`First access date (at least ${MIN_DAYS} days from today)`}
+                label={`Date`}
                 type="date"
                 min={minDate}
                 required
                 {...ctx.fieldPropsFor("date1")}
               />
               <TextualFormField
-                label="Second access date (optional)"
+                label="Date (optional)"
                 type="date"
                 min={minDate}
                 {...ctx.fieldPropsFor("date2")}
               />
               <TextualFormField
-                label="Third access date (optional)"
+                label="Date (optional)"
                 type="date"
                 min={minDate}
                 {...ctx.fieldPropsFor("date3")}

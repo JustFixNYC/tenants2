076b4d9cd87c2aaee54dd074937e4e6eb56302ce
kiwansim <kiwan@justfix.org>
2024-10-18 16:47:08 -0400

    Move housing type to LOC splash page (#2463)
    * add plain radio buttons to splash page for housing type
    
    * fix crashing step 3 for undefined window, replace modals with tooltips on splash page
    
    * small padding fixes
    
    * fix radio button sizes

--- frontend/lib/loc/letter-of-complaint-splash.tsx
+++ frontend/lib/loc/letter-of-complaint-splash.tsx
@@ -7,43 +7,156 @@ import { BigList } from "../ui/big-list";
 import { OutboundLink } from "../ui/outbound-link";
 import { GetStartedButton } from "../ui/get-started-button";
 import { OnboardingInfoSignupIntent } from "../queries/globalTypes";
+import { Icon } from "../ui/icon";
+import { LeaseChoice } from "../../../common-data/lease-choices";
+import classnames from "classnames";
+
+type HousingTypeFormProps = {
+  housingType: string;
+  setHousingType: (value: string) => void;
+};
+
+const HousingTypeForm: React.FC<HousingTypeFormProps> = ({
+  housingType,
+  setHousingType,
+}) => {
+  const leaseModals = createLeaseLearnMoreModals();
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHousingType(event.target.value);
+    window.sessionStorage.setItem("housingType", event.target.value);
+  };
+
+  return (
+    <form className="housing-type-container">
+      {leaseModals.map(({ title, leaseType, leaseInfo }) => (
+        <div key={leaseType} className="housing-type-selection">
+          <input
+            type="radio"
+            id={leaseType.toLowerCase()}
+            name="housing-type"
+            value={leaseType}
+            checked={housingType === leaseType}
+            onChange={handleRadioChange}
+          />
+          <label htmlFor={leaseType.toLowerCase()}>{title}</label>
+          <div
+            className={classnames(
+              "tooltip",
+              `tooltip_${leaseType.toLowerCase()}`
+            )}
+          >
+            <Icon type="info" />
+            <span className="tooltiptext">{leaseInfo}</span>
+          </div>
+        </div>
+      ))}
+    </form>
+  );
+};
+
+export type LeaseMoreInfo = {
+  title: string;
+  leaseType: LeaseChoice;
+  leaseInfo: string | JSX.Element;
+};
+
+const createLeaseLearnMoreModals = (): LeaseMoreInfo[] => [
+  {
+    title: "Rent Stabilized",
+    leaseType: "RENT_STABILIZED",
+    leaseInfo:
+      "Housing in buildings built before January 1, 1974 with six or more \
+          units, including Single Room Occupancy (“SRO”) hotels and rooming \
+          houses. All apartments in buildings that receive a tax abatement such as J-51, \
+          421a, and 421g are also stabilized.",
+  },
+  {
+    title: "Rent Controlled",
+    leaseType: "RENT_CONTROLLED",
+    leaseInfo:
+      "This is a rare kind of housing! Buildings that had three or more \
+          residential units before February 1, 1947, where the tenant or \
+          immediate family member has been continuously living in the apartment \
+          since July 1, 1971.",
+  },
+  {
+    title: "Market Rate",
+    leaseType: "MARKET_RATE",
+    leaseInfo:
+      "Market rate tenants typically live in buildings of fewer than six (6)\
+          units, newer buildings, or formerly rent stabilized apartments that a\
+          landlord deregulated before 2019.",
+  },
+  {
+    title: "NYCHA/Public Housing",
+    leaseType: "NYCHA",
+    leaseInfo:
+      "Federally-funded affordable housing developments owned by the government.",
+  },
+  {
+    title: "Affordable Housing (other than rent stabilized)",
+    leaseType: "OTHER_AFFORDABLE",
+    leaseInfo:
+      "New York City has many forms of affordable housing. Some common types\
+          include Mitchell Lama, Project-Based Section 8 buildings (also known\
+          as HUD), LIHTC, HDFC rentals, and others.",
+  },
+  {
+    title: "I'm not sure",
+    leaseType: "NOT_SURE",
+    leaseInfo: (
+      <>
+        Don’t know what type of housing you live in? Learn more by ordering your
+        rent history <a href="https://app.justfix.org/en/rh/splash">here</a> or
+        reading about{" "}
+        <a href="https://rentguidelinesboard.cityofnewyork.us/resources/faqs/rent-stabilization/">
+          rent regulation.
+        </a>
+      </>
+    ),
+  },
+];
 
 export function LocSplash(): JSX.Element {
+  const [housingType, setHousingType] = React.useState("");
+
+  React.useEffect(() => {
+    window.sessionStorage.removeItem("housingType");
+  }, []);
+
   return (
     <Page className="jf-loc-landing-page" title="Letter of Complaint">
       <section className="hero is-light">
         <div className="hero-body">
-          <div className="has-text-centered">
-            <div className="jf-loc-image">
-              <StaticImage
-                ratio="is-2by1"
-                src="frontend/img/letter-of-complaint.svg"
-                alt=""
-              />
-            </div>
-            <h1 className="title is-spaced">
-              Need Repairs in Your Apartment? Take action today!
-            </h1>
-            <p className="subtitle">
-              This is a free tool that notifies your landlord of repair issues
-              via{" "}
-              <b>
-                USPS Certified Mail<sup>&reg;</sup>
-              </b>
-              . This service is free and secure.
-            </p>
-            <GetStartedButton
-              to={JustfixRoutes.locale.locOnboarding.latestStep}
-              intent={OnboardingInfoSignupIntent.LOC}
-              pageType="splash"
-            >
-              Start my free letter
-            </GetStartedButton>
-            <p className="jf-secondary-cta">
-              Already have an account?{" "}
-              <Link to={JustfixRoutes.locale.login}>Sign in</Link>
-            </p>
-          </div>
+          <h1 className="title is-spaced">
+            Need repairs in your home?
+            <br />
+            Take action today!
+          </h1>
+          <p className="subtitle">
+            Create a letter that notifies your landlord of repair issues via
+            USPS Certified Mail<sup>&reg;</sup>. This service is free, secure,
+            and legally vetted.
+            <br />
+            <br />
+            Select your housing type to get started:
+          </p>
+          <HousingTypeForm
+            housingType={housingType}
+            setHousingType={setHousingType}
+          />
+          <GetStartedButton
+            to={JustfixRoutes.locale.locOnboarding.latestStep}
+            intent={OnboardingInfoSignupIntent.LOC}
+            pageType="splash"
+          >
+            Start my free letter
+          </GetStartedButton>
+          <p className="jf-secondary-cta has-text-centered">
+            Already have an account?{" "}
+            <Link to={JustfixRoutes.locale.login}>Sign in</Link>
+          </p>
         </div>
       </section>
 

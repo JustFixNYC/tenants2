9bcee0a62c66b070eb79d9a5fad0a7aa7eb117c5
Maxwell Austensen <m.austensen@gmail.com>
2025-11-17 16:18:56 -0500

    update letter url name, send in submission response

--- gceletter/urls.py
+++ gceletter/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from gceletter.models import GCE_LETTER_FILENAME
+
 from . import views
 
 
@@ -8,7 +10,7 @@ app_name = "gceletter"
 urlpatterns = [
     path("send-letter", views.submit_letter, name="send-letter"),
     path("verify_address", views.lob_verify_address, name="lob_verify_address"),
-    path("<str:hash>/gce-letter.pdf", views.gce_letter_pdf, name="gce_letter_pdf"),
+    path(f"<str:hash>/{GCE_LETTER_FILENAME}.pdf", views.gce_letter_pdf, name="gce_letter_pdf"),
     path("letter-link", views.get_letter_link, name="get_letter_link"),
     path("coming-soon-subscribe", views.coming_soon_subscribe, name="coming_soon_subscribe"),
 ]

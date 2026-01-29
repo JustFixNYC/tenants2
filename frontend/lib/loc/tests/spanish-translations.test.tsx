1158db2c01cb7d55b06c1a7bf504715b2c75ba5c
kiwansim <kiwan@justfix.org>
2026-01-29 10:05:37 -0500

    add spanish test

---
+++ frontend/lib/loc/tests/spanish-translations.test.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import i18n from "../../i18n";
+import ReactTestingLibraryPal from "../../tests/rtl-pal";
+import {
+  preloadLingui,
+  PreloadedLinguiI18nProvider,
+} from "../../tests/lingui-preloader";
+import { LocLinguiI18n } from "../routes";
+import { LocContent, locSampleProps } from "../letter-content";
+import { waitFor } from "@testing-library/react";
+import LetterOfComplaintRoutes from "../routes";
+import { AppTesterPal } from "../../tests/app-tester-pal";
+import JustfixRoutes from "../../justfix-route-info";
+import { newSb } from "../../tests/session-builder";
+
+beforeAll(preloadLingui(LocLinguiI18n));
+
+describe("LOC Spanish translations", () => {
+  afterEach(() => {
+    // Reset to English after each test
+    i18n.initialize("en");
+  });
+
+  describe("Welcome page", () => {
+    it("renders in English", async () => {
+      i18n.initialize("en");
+      const pal = new AppTesterPal(<LetterOfComplaintRoutes />, {
+        url: JustfixRoutes.locale.loc.welcome,
+        session: newSb().withLoggedInJustfixUser().value,
+      });
+
+      await waitFor(() => {
+        pal.rr.getByText(/Why mail a letter/i);
+        pal.rr.getByText(/Having a record of notifying your landlord/i);
+      });
+    });
+
+    it("renders in Spanish", async () => {
+      i18n.initialize("es");
+      const pal = new AppTesterPal(<LetterOfComplaintRoutes />, {
+        url: JustfixRoutes.getLocale("es").loc.welcome,
+        session: newSb().withLoggedInJustfixUser().value,
+      });
+
+      await waitFor(() => {
+        // TODO update with new translation strings once crowdin is updated
+        expect(pal.rr.container.textContent).not.toMatch(/loc\.\w+/);
+      });
+    });
+  });
+
+  describe("Letter content", () => {
+    it("letter content renders (stays in English regardless of locale)", async () => {
+      i18n.initialize("es");
+      const pal = new ReactTestingLibraryPal(
+        (
+          <PreloadedLinguiI18nProvider>
+            <LocContent {...locSampleProps} todaysDate="2020-06-10" />
+          </PreloadedLinguiI18nProvider>
+        )
+      );
+
+      await waitFor(() => {
+        pal.rr.getByText(/Dear/i);
+      });
+    });
+  });
+});
